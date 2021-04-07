d214d9aed036fe4b93e4dfd7244bda369b484309
Jordan Nelson <nejrd@amazon.com>
2021-04-07 14:29:14 -0400

    implement smoke tests

--- packages/flutter/test/widget_test.dart
+++ packages/flutter/test/widget_test.dart
@@ -1,30 +1,22 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:amplify_authenticator/authenticator/components/material/sign_in_view.dart';
+import 'package:amplify_authenticator/authenticator/components/material/sign_up_view.dart';
+import 'package:amplify_authenticator/authenticator/components/materialAuthenticator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:amplify_authenticator/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('MaterialAuthenticator smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: MaterialAuthenticator(
+          onSignInSuccess: () => print('success'),
+        ),
+      ),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that MaterialSignInView is visible by defaul & MaterialSignUpView is not
+    expect(find.byType(MaterialSignInView), findsOneWidget);
+    expect(find.byType(MaterialSignUpView), findsNothing);
   });
 }
