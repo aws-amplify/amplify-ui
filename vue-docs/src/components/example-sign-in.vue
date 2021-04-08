8d971ebc02da9f917457218eb826a0ee8e53be5d
Erik Hanchett <ehhanche@amazon.com>
2021-04-07 17:16:41 -0700

    Renamed components to fit style guide

---
+++ vue-docs/src/components/example-sign-in.vue
@@ -0,0 +1,57 @@
+<template>
+  <example-wrapper
+    :title="'Update Sign In Button Colors'"
+    :code="exampleSignIn"
+  >
+    <div class="css-example">
+      <amplify-provider defaults>
+        <authenticator>
+          <h1 class="text-6xl mb-10">
+            Welcome {{ state?.context?.user?.username }}!
+          </h1>
+          <button
+            className="px-2 bg-white rounded shadow"
+            @click="send('SIGN_OUT')"
+          >
+            Sign Out
+          </button>
+        </authenticator>
+      </amplify-provider>
+    </div>
+  </example-wrapper>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { AmplifyProvider, Authenticator, useAuth } from "@aws-amplify/ui-vue";
+import ExampleWrapper from "./example-wrapper.vue";
+import { exampleSignIn } from "../utils/code-examples";
+
+export default defineComponent({
+  components: {
+    AmplifyProvider,
+    Authenticator,
+    ExampleWrapper
+  },
+  setup() {
+    const { state, send } = useAuth();
+
+    return {
+      state,
+      exampleSignIn,
+      send
+    };
+  }
+});
+</script>
+
+<style>
+.css-example
+  [data-amplify-theme]
+  [data-amplify-authenticator]
+  [data-amplify-footer]
+  [data-amplify-button]:last-of-type {
+  background-color: orange;
+  color: purple;
+}
+</style>
