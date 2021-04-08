2eb27a8ee4485bab6b4dd29ce28325bca0fd5e94
Jordan Nelson <nejrd@amazon.com>
2021-04-08 10:33:20 -0400

    refactor generateStateMachine

--- lib/authenticator/state/state_machine_definition.dart
+++ lib/authenticator/state/state_machine_definition.dart
@@ -1,5 +1,103 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:state_machine/state_machine.dart';
+
+// returns a GeneratedStateMachine (StateMachine + the associated States and StateTransitions)
+// given a json state definition
+GeneratedStateMachine generateStateMachine(
+  Map<String, dynamic> jsonState,
+) {
+  StateMachineDefinition stateMachineDefinition =
+      StateMachineDefinition.fromJson(jsonState);
+  StateMachine machine = StateMachine(stateMachineDefinition.id);
+  Map<String, State> states = createStates(machine, stateMachineDefinition);
+  Map<String, StateTransition> stateTransitions =
+      createStateTransitions(machine, stateMachineDefinition, states);
+  Map<String, StreamSubscription<StateChange>> stateChangeSubScriptions =
+      createStateChangeSubscriptions(
+    machine,
+    stateMachineDefinition,
+    states,
+    stateTransitions,
+  );
+  machine.start(states[stateMachineDefinition.initial]);
+  debugPrint('states: ' + states.keys.toString());
+  debugPrint('stateTransitions: ' + stateTransitions.keys.toString());
+  return GeneratedStateMachine(
+    stateMachine: machine,
+    states: states,
+    stateTransitions: stateTransitions,
+  );
+}
+
+// creates a map where the key is the state name and the value is the state_machine State
+Map<String, State> createStates(
+    StateMachine machine, StateMachineDefinition stateMachineDefinition) {
+  return stateMachineDefinition.states.map((stateKey, stateValue) {
+    State newState = machine.newState(stateKey);
+    return MapEntry(stateKey, newState);
+  });
+}
+
+// creates a map where the key is the state name + the event name and the value is the state_machine StateTransition
+Map<String, StateTransition> createStateTransitions(StateMachine machine,
+    StateMachineDefinition stateMachineDefinition, Map<String, State> states) {
+  return Map<String, StateTransition>.fromEntries(stateMachineDefinition
+      .states.entries
+      .where((state) => state.value.on != null)
+      .map((state) {
+    return state.value.on.entries.map((on) {
+      String stateTransitionKey = state.key + '-' + on.key;
+      StateTransition newStateTransition = machine.newStateTransition(
+        state.key + '-' + on.key,
+        [states[state.key]],
+        states[on.value],
+      );
+      return MapEntry(stateTransitionKey, newStateTransition);
+    });
+  }).reduce((value, element) => [...value, ...element]));
+}
+
+// creates a map where the key is the state name and the value is the subscription from listenting entries to that state
+Map<String, StreamSubscription<StateChange>> createStateChangeSubscriptions(
+  StateMachine machine,
+  StateMachineDefinition stateMachineDefinition,
+  Map<String, State> states,
+  Map<String, StateTransition> stateTransitions,
+) {
+  return Map<String, StreamSubscription<StateChange>>.fromEntries(
+      stateMachineDefinition.states.entries
+          .where((state) => state.value.invoke != null)
+          .map((state) {
+    StreamSubscription<StateChange> subscription =
+        states[state.key].onEnter.listen((event) {
+      ServiceFn service =
+          stateMachineDefinition.services[state.value.invoke.src];
+      service(event).then((value) {
+        stateTransitions[state.key + "-" + state.value.invoke.onDone.target]();
+      }).catchError((error) {
+        stateTransitions[state.key + "-" + state.value.invoke.onError]();
+      });
+    });
+    return MapEntry(state.key, subscription);
+  }));
+}
+
+// GeneratedStateMachine is a StateMachine + the associated States and StateTransitions
+// accessible by the key name
+class GeneratedStateMachine {
+  StateMachine stateMachine;
+  Map<String, State> states;
+  Map<String, StateTransition> stateTransitions;
+  GeneratedStateMachine({
+    @required this.stateMachine,
+    @required this.states,
+    @required this.stateTransitions,
+  });
+}
+
+// helper classes and types for parsing the json state
 
 typedef ServiceFn = Future Function(dynamic event);
 
@@ -47,63 +145,4 @@ class StateDefinitionInvokeOnDone {
     actions = jsonState['actions'];
     target = jsonState['target'];
   }
-}
-
-class GeneratedStateMachine {
-  StateMachine stateMachine;
-  Map<String, State> states;
-  Map<String, StateTransition> stateTransitions;
-  GeneratedStateMachine({
-    @required this.stateMachine,
-    @required this.states,
-    @required this.stateTransitions,
-  });
-}
-
-GeneratedStateMachine generateStateMachine(
-  Map<String, dynamic> jsonState,
-) {
-  StateMachineDefinition stateMachineDefinition =
-      StateMachineDefinition.fromJson(jsonState);
-  StateMachine machine = StateMachine(stateMachineDefinition.id);
-  Map<String, State> states = {};
-  Map<String, StateTransition> stateTransitions = {};
-  stateMachineDefinition.states.forEach((stateKey, stateValue) {
-    State newState = machine.newState(stateKey);
-    states[stateKey] = newState;
-  });
-  stateMachineDefinition.states.forEach((stateKey, stateValue) {
-    if (stateValue.on != null) {
-      stateValue.on.forEach((toKey, toValue) {
-        String stateTransitionKey = stateKey + '-' + toKey;
-        StateTransition newStateTransition = machine.newStateTransition(
-          stateKey + '-' + toKey,
-          [states[stateKey]],
-          states[toValue],
-        );
-        stateTransitions[stateTransitionKey] = newStateTransition;
-      });
-    }
-  });
-  stateMachineDefinition.states.forEach((stateKey, stateValue) {
-    if (stateValue.invoke != null) {
-      states[stateKey].onEnter.listen((event) {
-        ServiceFn service =
-            stateMachineDefinition.services[stateValue.invoke.src];
-        service(event).then((value) {
-          stateTransitions[stateKey + "-" + stateValue.invoke.onDone.target]();
-        }).catchError((error) {
-          stateTransitions[stateKey + "-" + stateValue.invoke.onError]();
-        });
-      });
-    }
-  });
-  machine.start(states[stateMachineDefinition.initial]);
-  debugPrint('states: ' + states.keys.toString());
-  debugPrint('stateTransitions: ' + stateTransitions.keys.toString());
-  return GeneratedStateMachine(
-    stateMachine: machine,
-    states: states,
-    stateTransitions: stateTransitions,
-  );
 }
