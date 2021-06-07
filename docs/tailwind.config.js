5cc261cabf477af92cfbb3ae09e422ba961cf623
Eric Clemmons <eric@smarterspam.com>
2021-06-07 16:15:16 -0500

    Fix purge paths

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   darkMode: false, // or 'media' or 'class'
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
