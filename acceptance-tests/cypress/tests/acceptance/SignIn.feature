68a2a84c55c45a3f3fbf855b0c260661e7e2f908
Shane Laymance <shane.laymance@gmail.com>
2021-06-16 13:36:57 -0700

    Update cucumber and cypress tests

--- acceptance-tests/cypress/tests/acceptance/SignIn.feature
+++ acceptance-tests/cypress/tests/acceptance/SignIn.feature
@@ -1,8 +1,12 @@
-Feature: SignIn
+Feature: Authentication
+
+  Amplify's Authenticator is a wrapper for a developer's application.
+  It provides an application with authentication features using AWS Cognito.
+  Its features include sign in, sign up, and sign out.
 
   Scenario: Sign in with invalid credentials
     Given I'm at the sign in page
     When I type an invalid email address "fake@email.com"
     And I type an invalid password "fakepassword"
-    Then I see the error "Authentication error"
-    And I don't see the application
+    And I attempt to sign in
+    Then I see the error "User does not exist."
--- acceptance-tests/cypress/tests/acceptance/SignIn/SignIn.ts
+++ acceptance-tests/cypress/tests/acceptance/SignIn/SignIn.ts
@@ -1,21 +1,21 @@
 import { And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
 
 Given("I'm at the sign in page", () => {
-  cy.visit("/");
+  cy.visit("/auth");
 });
 
-When("I type an invalid email address {word}", (email: string) => {
-  cy.findAllByLabelText(/Username/)
-    .first()
-    .type(email);
+When("I type an invalid email address {string}", (email: string) => {
+  cy.findByRole("textbox", { name: /username/i }).type(email);
 });
 
-And("I type an invalid password {word}", (password: string) => {
-  cy.findAllByLabelText(/Password/)
-    .first()
-    .type(password);
+And("I type an invalid password {string}", (password: string) => {
+  cy.findByLabelText(/password/i).type(password);
 });
 
-Then("I see the error {string}", (errorMessage: string) => {});
+And("I attempt to sign in", () => {
+  cy.findByRole("button", { name: "Sign In" }).click();
+});
 
-And("I don't see the application", () => {});
+Then("I see the error {string}", (errorMessage: string) => {
+  cy.findByRole("alert", { name: /error/i }).should("contain", errorMessage);
+});
