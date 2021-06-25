5bafd21fbda003dd442eb20453f80b9ac59aa3c1
Eric Clemmons <eric@smarterspam.com>
2021-06-24 21:54:45 -0500

    Create separate examples/next

--- examples/next/pages/components/authenticator.tsx
+++ examples/next/pages/components/authenticator.tsx
@@ -1,9 +1,8 @@
 import { Authenticator } from "aws-amplify-react";
 import { Amplify } from "aws-amplify";
-import awsExports from "../../../../../environments/auth-with-username/src/aws-exports";
+import awsExports from "../../../../environments/auth-with-username/src/aws-exports";
 
-// Uncomment this line for styles
-// import "@aws-amplify/ui/dist/style.css";
+import "@aws-amplify/ui/dist/style.css";
 
 Amplify.configure(awsExports);
 
