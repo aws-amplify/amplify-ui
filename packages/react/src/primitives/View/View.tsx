c87bde9d75c5218115347b4eef2628fe77958316
Scott Rees <reesscot@amazon.com>
2021-06-29 12:51:18 -0700

    filter out style props before passing to element

--- packages/react/src/primitives/View/View.tsx
+++ packages/react/src/primitives/View/View.tsx
@@ -1,30 +1,14 @@
 import classNames from "classnames";
 import React from "react";
 import { ComponentClassNames } from "../shared/constants";
-import { getStyleCssVarsFromProps } from "../shared/utils";
+import { getStyleCssVarsFromProps, getNonStyleProps } from "../shared/utils";
 import { ViewProps } from "../types/index";
 
 export const View: React.FC<ViewProps> = props => {
   const {
     as: asElementTag,
-    id,
     className,
     children,
-
-    backgroundColor,
-    color,
-    shadow,
-    padding,
-    border,
-    cornerRadius,
-    height,
-    maxHeight,
-    minHeight,
-    width,
-    maxWidth,
-    minWidth,
-    opacity,
-
     role,
     ariaLabel,
     isDisabled,
@@ -39,9 +23,8 @@ export const View: React.FC<ViewProps> = props => {
       aria-label={ariaLabel}
       role={role}
       className={classNames(ComponentClassNames.View, className)}
-      id={id}
       disabled={isDisabled}
-      {...rest}
+      {...getNonStyleProps(rest)}
     >
       {children}
     </ViewTag>
--- packages/react/src/primitives/shared/utils.ts
+++ packages/react/src/primitives/shared/utils.ts
@@ -1,11 +1,28 @@
 import { CustomPropertiesMap, StyleProps } from "../types/index";
 
+const customPropertiesKeys = Object.keys(CustomPropertiesMap);
+
 export const getStyleCssVarsFromProps = (props: StyleProps) => {
   let style: React.CSSProperties = {};
-  Object.keys(CustomPropertiesMap).forEach(propKey => {
+  customPropertiesKeys.forEach(propKey => {
     if (propKey in props) {
       style[CustomPropertiesMap[propKey]] = props[propKey];
     }
   });
   return style;
 };
+
+/**
+ * Filter out known style props to prevent errors adding invalid HTML attributes
+ * @param props
+ * @returns non styled props
+ */
+export const getNonStyleProps = (props: {}) => {
+  const nonStyleProps = {};
+  Object.keys(props).forEach((propKey) => {
+    if (!(propKey in CustomPropertiesMap)) {
+      nonStyleProps[propKey] = props[propKey]
+    }
+  });
+  return nonStyleProps;
+}
