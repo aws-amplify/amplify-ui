1b6a9f438fe8a04b2f5efc3426b2718f05acfcaf
Scott Rees <reesscot@amazon.com>
2021-06-29 15:03:41 -0700

    add description to getStyleCssVarsFromProps

--- packages/react/src/primitives/shared/utils.ts
+++ packages/react/src/primitives/shared/utils.ts
@@ -1,11 +1,14 @@
 import { CustomPropertiesMap, StyleProps } from "../types/index";
 
-const customPropertiesKeys = Object.keys(CustomPropertiesMap);
-
+/**
+ * Convert style props to CSS variables for React style prop
+ * @param props StyleProps
+ * @returns CSSProperties styles
+ */
 export const getStyleCssVarsFromProps = (props: StyleProps) => {
   let style: React.CSSProperties = {};
-  customPropertiesKeys.forEach(propKey => {
-    if (propKey in props) {
+  Object.keys(CustomPropertiesMap).forEach(propKey => {
+    if (propKey in props && props[propKey].length > 0) {
       style[CustomPropertiesMap[propKey]] = props[propKey];
     }
   });
