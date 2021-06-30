3ef0e5339ae05a1970c520cb593febb11b71bf8f
Eric Clemmons <eric@smarterspam.com>
2021-06-30 16:33:26 -0500

    Use a table with dynamic URLs using BRANCH

--- docs/src/components/FeatureTests.tsx
+++ docs/src/components/FeatureTests.tsx
@@ -1,9 +1,15 @@
 import { GherkinDocument } from "@cucumber/messages";
 import { useRouter } from "next/router";
+import {
+  CodeIcon,
+  ClipboardCheckIcon,
+  ExternalLinkIcon,
+} from "@heroicons/react/solid";
 
 interface FeatureTest {
-  filepath: string;
   document: GherkinDocument;
+  filename: string;
+  name: string;
 }
 
 interface FeatureTestsProps {
@@ -17,36 +23,67 @@ export function FeatureTests({ featureTests = [] }: FeatureTestsProps) {
     return null;
   }
 
-  function getExampleHref({ filepath }) {
-    const baseUrl =
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000"
-        : "https://github.com/aws-amplify/amplify-ui/blob/main";
-
-    return `${baseUrl}${router.asPath}/${filepath.split(".").shift()}`;
-  }
-
   return (
     <>
       <h2>Features</h2>
 
-      {featureTests.map(({ filepath, document }) => (
+      {featureTests.map(({ document, filename, name }) => (
         <section key={document.feature.name}>
           <h3>{document.feature.name}</h3>
           <p>{document.feature.description}</p>
 
-          <h4>Examples</h4>
-          <ul>
-            {document.feature.children
-              .filter(({ background }) => !background)
-              .map(({ scenario }) => (
-                <li key={scenario.name}>
-                  <a href={getExampleHref({ filepath })} target="_blank">
-                    {scenario.name}
-                  </a>
-                </li>
-              ))}
-          </ul>
+          <details>
+            <summary>Examples</summary>
+
+            <table className="ml-4 table-auto">
+              <thead>
+                <tr>
+                  <th>Example</th>
+                  {process.env.NODE_ENV === "development" && <td>Demo</td>}
+                  <td>Source</td>
+                  <td>Test</td>
+                </tr>
+              </thead>
+              <tbody>
+                {document.feature.children
+                  .filter(({ background }) => !background)
+                  .map(({ scenario }) => (
+                    <tr key={scenario.name}>
+                      <td>{scenario.name}</td>
+                      {process.env.NODE_ENV === "development" && (
+                        <td>
+                          <a
+                            href={`http://localhost:3000${router.asPath}/${name}`}
+                            target="_blank"
+                          >
+                            <span className="sr-only">Demo</span>
+                            <ExternalLinkIcon className="h-4" />
+                          </a>
+                        </td>
+                      )}
+                      <td>
+                        <a
+                          href={`https://github.com/aws-amplify/amplify-ui/tree/${process.env.BRANCH}/examples/next/pages${router.asPath}/${name}`}
+                          target="_blank"
+                        >
+                          <span className="sr-only">Source</span>
+                          <CodeIcon className="h-4" />
+                        </a>
+                      </td>
+                      <td>
+                        <a
+                          href={`https://github.com/aws-amplify/amplify-ui/blob/${process.env.BRANCH}/packages/e2e/cypress/integration${router.asPath}/${filename}#L${scenario.location.line}`}
+                          target="_blank"
+                        >
+                          <span className="sr-only">Test</span>
+                          <ClipboardCheckIcon className="h-4" />
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </details>
         </section>
       ))}
     </>
