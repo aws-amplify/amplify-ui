78121ede15ebb158daa4aff97213e20e667150f2
Eric Clemmons <eric@smarterspam.com>
2021-07-01 10:52:01 -0500

    Improve AuthenticatorContext types

--- packages/react/src/components/Authenticator/AuthenticatorContext.tsx
+++ packages/react/src/components/Authenticator/AuthenticatorContext.tsx
@@ -1,7 +1,4 @@
-import { AuthContext } from "@aws-amplify/ui-core";
+import { AuthInterpreter } from "@aws-amplify/ui-core";
 import { createContext } from "react";
-import { Interpreter } from "xstate";
 
-export const AuthenticatorContext = createContext<Interpreter<AuthContext>>(
-  null
-);
+export const AuthenticatorContext = createContext<AuthInterpreter>(null);
