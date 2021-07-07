48d435327ef120e8b1cc12b482bd6a270dcf01a0
Shane Laymance <shane.laymance@gmail.com>
2021-07-07 13:10:56 -0700

    Update example to use new implementation

--- examples/next/pages/components/authenticator/sign-up/with-email.tsx
+++ examples/next/pages/components/authenticator/sign-up/with-email.tsx
@@ -1,9 +1,9 @@
-import { Authenticator } from "aws-amplify-react";
+import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import awsExports from "auth-with-email-environment/src/aws-exports";
 
 Amplify.configure(awsExports);
 
 export default function AuthenticatorWithEmail() {
-  return <Authenticator usernameAttributes="email" />;
+  return <Authenticator />;
 }
