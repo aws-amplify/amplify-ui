f610933efcd83756b0fb7afd1e097c49720705e9
Shane Laymance <shane.laymance@gmail.com>
2021-07-07 11:05:01 -0700

    Remove confirm_password config option from auth-machine

--- packages/core/src/types/authMachine.ts
+++ packages/core/src/types/authMachine.ts
@@ -10,7 +10,7 @@ export interface AuthContext {
   user?: CognitoUser;
   session?: CognitoUserSession;
   formValues?: AuthFormData;
-  config?: { login_mechanisms: string[]; confirm_password: boolean };
+  config?: { login_mechanisms: string[] };
 }
 
 export type AuthEventTypes =
