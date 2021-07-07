20f72a67637afcb4f2250baaaa3ebde6929fa309
Shane Laymance <shane.laymance@gmail.com>
2021-07-07 13:11:15 -0700

    Update tests to reflect new sign up implementation

--- packages/e2e/cypress/integration/components/authenticator/sign-up-with-email.feature
+++ packages/e2e/cypress/integration/components/authenticator/sign-up-with-email.feature
@@ -6,15 +6,14 @@ Feature: Sign Up with Email
     Given I'm using the example "components/authenticator/sign-up/with-email"
     And I click "Create account"
 
-  Scenario: Username is not a requested field
-    Then I don't see "Username" as an input field
-
-  Scenario: Sign up form lists email first followed by phone number
-    Then I see input fields in the order "Email" and "Phone Number"
+  Scenario: Email is the only requested alias
+    Then I see "Email" as an input field
+    And I don't see "Username" as an input field
+    And I don't see "Phone Number" as an input field
 
   Scenario: Sign up with valid email & password
     When I type the email "VALID_EMAIL"
     And I type the password "VALID_PASSWORD"
-    And I type the phone number "VALID_PHONE_NUMBER"
+    And I confirm the password "VALID_PASSWORD"
     And I click the "Create Account" button
     Then I see "Confirmation Code"
--- packages/e2e/cypress/integration/components/authenticator/sign-up-with-email/sign-up-with-email.steps.ts
+++ packages/e2e/cypress/integration/components/authenticator/sign-up-with-email/sign-up-with-email.steps.ts
@@ -8,34 +8,26 @@ And("I click {string}", (text: string) => {
   cy.findByText(text).click();
 });
 
+Then("I see {string} as an input field", (name: string) => {
+  cy.findByRole("textbox", { name }).should("exist");
+});
+
 Then("I don't see {string} as an input field", (name: string) => {
-  cy.findAllByRole("textbox").each($el =>
-    cy
-      .wrap($el)
-      .invoke("attr", "placeholder")
-      .should("not.contain", name)
+  cy.findByRole("textbox", { name }).should("not.exist");
+});
+
+When("I type the email {string}", (email: string) => {
+  cy.findByRole("textbox", { name: /email/i }).type(
+    Date.now() + Cypress.env(email)
   );
 });
 
-Then(
-  "I see input fields in the order {string} and {string}",
-  (...fields: string[]) => {
-    cy.findAllByRole("textbox").each(($el, i) =>
-      cy.wrap($el).should("have.attr", "placeholder", fields[i])
-    );
-  }
-);
-
-When("I type the email {string}", (email: string) => {
-  cy.findByPlaceholderText(/email/i).type(Date.now() + Cypress.env(email));
-});
-
 And("I type the password {string}", (password: string) => {
-  cy.findByPlaceholderText(/password/i).type(Cypress.env(password));
+  cy.findByLabelText(/^password$/i).type(Cypress.env(password));
 });
 
-And("I type the phone number {string}", (phoneNumber: string) => {
-  cy.findByPlaceholderText(/phone\snumber/i).type(Cypress.env(phoneNumber));
+And("I confirm the password {string}", (password: string) => {
+  cy.findByLabelText(/confirm password/i).type(Cypress.env(password));
 });
 
 And("I click the {string} button", (name: string) => {
