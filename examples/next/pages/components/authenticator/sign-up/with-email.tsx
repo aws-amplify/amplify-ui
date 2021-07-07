10a758aa7599d29a88236843529143b08e0a4f68
Shane Laymance <shane.laymance@gmail.com>
2021-07-07 15:25:58 -0700

    Commit login_mechanisms as part of the example

--- examples/next/pages/components/authenticator/sign-up/with-email.tsx
+++ examples/next/pages/components/authenticator/sign-up/with-email.tsx
@@ -2,7 +2,12 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import awsExports from "@environments/auth-with-email/src/aws-exports";
 
-Amplify.configure(awsExports);
+Amplify.configure({
+  ...awsExports,
+  auth: {
+    login_mechanisms: ["email"],
+  },
+});
 
 export default function AuthenticatorWithEmail() {
   return <Authenticator />;
