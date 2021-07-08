52193c7188587a3950054c4a5f85172aeb5f028e
Erik Hanchett <ehhanche@amazon.com>
2021-07-08 09:23:52 -0700

    Added tests for sign-up phone number

---
+++ examples/next/pages/components/authenticator/sign-up/with-phone.tsx
@@ -0,0 +1,27 @@
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import awsExports from "@environments/auth-with-phone-number/src/aws-exports";
+
+Amplify.configure({
+  ...awsExports,
+  auth: {
+    login_mechanisms: ["phone_number"],
+  },
+});
+
+export default function AuthenticatorWithEmail() {
+  return (
+    <>
+      <Authenticator>
+        {({ send }) => {
+          return (
+            <>
+              <h1>Hello</h1>
+              <button onClick={() => send("SIGN_OUT")}>Sign out</button>
+            </>
+          );
+        }}
+      </Authenticator>
+    </>
+  );
+}
