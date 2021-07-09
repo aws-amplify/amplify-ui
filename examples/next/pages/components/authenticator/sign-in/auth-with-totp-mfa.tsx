4b3efe342551f2f025ca2517529aa7a014304d73
Eddie Keller <keledwin@amazon.com>
2021-07-09 09:32:54 -0700

    feat: adding env, example, and tests for totp auth

---
+++ examples/next/pages/components/authenticator/sign-in/auth-with-totp-mfa.tsx
@@ -0,0 +1,15 @@
+import { Authenticator } from "aws-amplify-react";
+import { Amplify } from "aws-amplify";
+
+import awsExports from "@environments/auth-with-totp-mfa/src/aws-exports";
+
+Amplify.configure({
+  ...awsExports,
+  auth: {
+    login_mechanisms: ["email"],
+  },
+});
+
+export default function AuthenticatorWithTotpMfa() {
+  return <Authenticator />;
+}
---
+++ packages/e2e/cypress/integration/components/authenticator/sign-in-totp-mfa/sign-in-totp-mfa.steps.ts
@@ -0,0 +1,37 @@
+import { And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+
+Given("I'm running the example {string}", (url: string) => {
+  cy.visit(url);
+  cy.get("[data-test=sign-in-header-section]").should("be.visible");
+});
+
+When("I type a valid email {string}", (email: string) => {
+  cy.get("[data-test=username-input]").type(Cypress.env(email));
+});
+
+When("I type an invalid email {string}", (email: string) => {
+  cy.get("[data-test=username-input]").type(Cypress.env(email));
+});
+
+And("I type an invalid password {string}", (password: string) => {
+  cy.get("[data-test=sign-in-password-input]").type(Cypress.env(password));
+});
+
+And("I type a valid password {string}", (password: string) => {
+  cy.get("[data-test=sign-in-password-input]").type(Cypress.env(password));
+});
+
+And("I click the {string} button", (name: string) => {
+  cy.get("[data-test=sign-in-sign-in-button]").click();
+  // cy.findByRole("button", { name }).click();
+});
+
+Then("I will be redirected to the confirm totp mfa page", () => {
+  cy.get("body").contains("Scan then enter verification code");
+});
+
+// TODO - this test is failing in the new Authenticator until we add in the error handling in the component
+Then("I see {string}", (message: string) => {
+  const [messageString, username] = message.split(" ");
+  cy.get("body").contains([messageString, Cypress.env(username)].join(" "));
+});
