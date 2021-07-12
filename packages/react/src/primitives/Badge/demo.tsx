ecdc40c72205fae38caf4fc705d55b4c28f2304b
Buono <joebuono@88665a17582b.ant.amazon.com>
2021-07-12 14:17:29 -0400

    basic demo and styling for Badge

--- packages/react/src/primitives/Badge/demo.tsx
+++ packages/react/src/primitives/Badge/demo.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { Badge } from "@aws-amplify/ui-react";
+import React, { useState } from "react";
+import { Badge, BadgeVariant, BadgeSize } from "@aws-amplify/ui-react";
 
 export const BadgeDemo = () => {
+  const [variant, setVariant] = useState<BadgeVariant>("default");
+  const [size, setSize] = useState<BadgeSize>("medium");
+
   return (
     <div>
-      Hello Badge Demo!
-      <Badge>BADGE!</Badge>
+      <select
+        value={variant}
+        placeholder="Select button variant"
+        onChange={event => setVariant(event.target.value as BadgeVariant)}
+      >
+        <option value="default">default</option>
+        <option value="info">info</option>
+        <option value="error">error</option>
+        <option value="warning">warning</option>
+        <option value="success">success</option>
+      </select>
+      <select
+        value={size}
+        placeholder="Select button size"
+        onChange={event => setSize(event.target.value as BadgeSize)}
+      >
+        <option value="small">small</option>
+        <option value="medium">medium</option>
+        <option value="large">large</option>
+      </select>
+      <Badge variant={variant} size={size}>BADGE!</Badge>
     </div>
   );
 };
--- packages/react/src/primitives/types/badge.ts
+++ packages/react/src/primitives/types/badge.ts
@@ -1,12 +1,15 @@
 import { BaseComponentProps } from "./base";
 
+export type BadgeVariant = "default" | "info" | "error" | "warning" | "success";
+export type BadgeSize = "small" | "medium" | "large";
+
 export interface BadgeProps extends BaseComponentProps {
-  variant?: "default" | "info" | "error" | "warning" | "success";
+  variant?: BadgeVariant;
   /**
    * The size property will affect both the padding and font size of the badge.
    * Note: this component *should not* inherit its font size from its parent.
    * It should define its own font size. The reason is you could have a badge inside
    * an <h2> and the text of the badge should not have the same size.
    */
-  size?: "small" | "medium" | "large";
+  size?: BadgeSize;
 }
