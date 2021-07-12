16ebe3691e3d7c2aeef07a2be5acdddef03e14a4
Buono <joebuono@88665a17582b.ant.amazon.com>
2021-07-12 12:49:43 -0400

    created Badge demo file

--- packages/react/src/primitives/Badge/demo.tsx
+++ packages/react/src/primitives/Badge/demo.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Badge } from "@aws-amplify/ui-react";
 
 export const BadgeDemo = () => {
-	return (
-		<div>
-			Hello Badge Demo!
-			<Badge>BADGE!</Badge>
-		</div>
-	);
+  return (
+    <div>
+      Hello Badge Demo!
+      <Badge>BADGE!</Badge>
+    </div>
+  );
 };
