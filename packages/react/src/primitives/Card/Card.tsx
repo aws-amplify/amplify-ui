1292ff096ed749a63db2dea27b85f4be48a22443
Jacob Logan <lognjc@amazon.com>
2021-07-13 11:22:03 -0700

    card documentation

--- packages/react/src/primitives/Card/Card.tsx
+++ packages/react/src/primitives/Card/Card.tsx
@@ -4,18 +4,9 @@ import { ComponentClassNames } from "../shared/constants";
 import { CardProps } from "../types/Card";
 import { View } from "@aws-amplify/ui-react";
 
-export const Card: React.FC<CardProps> = ({
-  className,
-  children,
-  id,
-  ...rest
-}) => {
+export const Card: React.FC<CardProps> = ({ className, children, ...rest }) => {
   return (
-    <View
-      className={classNames(ComponentClassNames.Card, className)}
-      id={id}
-      {...rest}
-    >
+    <View className={classNames(ComponentClassNames.Card, className)} {...rest}>
       {children}
     </View>
   );
