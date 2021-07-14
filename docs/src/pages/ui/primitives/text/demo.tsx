e4865ecb1a4371ca71f789c4bf7ec02d082c72c3
jacoblogan <jacob.maiola.logan@gmail.com>
2021-07-14 12:11:29 -0600

    move text samples into sdemo file (#88)
    Co-authored-by: Jacob Logan <lognjc@amazon.com>

--- docs/src/pages/ui/primitives/text/demo.tsx
+++ docs/src/pages/ui/primitives/text/demo.tsx
@@ -122,3 +122,25 @@ export const TextDemo = ({ children }) => {
     </div>
   );
 };
+
+export const TextTruncatedSample = () => {
+  return (
+    <Text isTruncated={true}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum.
+    </Text>
+  );
+};
+
+export const TextStylingSample = () => {
+  return (
+    <Text fontWeight="bold" color="red" textDecoration="underline">
+      This is my styled text
+    </Text>
+  );
+};
