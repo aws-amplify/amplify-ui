54dca2076ae85f3df08fad3879121f651dc14c8f
Shane Laymance <shane.laymance@gmail.com>
2021-07-14 10:04:51 -0700

    Sign in with phone number: environment and tests setup (#68)
    * Set up tests for sign in with phone number
    
    * Fix url for sign-in-with-phone tests
    
    * Add scenario for force change password
    
    * Reshape example dir structure
    
    * Realign examples dir structure
    
    * Realign e2e dir structure
    
    * Rename to sign-in-with-phone
    
    * Fix sign-in-with-email visit path

---
+++ packages/e2e/cypress/integration/ui/components/authenticator/sign-in-with-phone/sign-in-with-phone.steps.ts
@@ -0,0 +1,23 @@
+import { And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
+
+Given("I'm at the sign in page", () => {
+  cy.visit("ui/components/authenticator/sign-in-with-phone");
+});
+
+When("I type the valid phone number {string}", (phoneNumber: string) => {
+  cy.findByRole("textbox", { name: /phone number/i }).type(
+    Cypress.env(phoneNumber)
+  );
+});
+
+And("I type the valid password {string}", (password: string) => {
+  cy.findByLabelText(/password/i).type(Cypress.env(password));
+});
+
+And("I click the {string} button", (name: string) => {
+  cy.findByRole("button", { name }).click();
+});
+
+Then("I see {string}", (message: string) => {
+  cy.findByRole("document").contains(new RegExp(message, "i"));
+});
