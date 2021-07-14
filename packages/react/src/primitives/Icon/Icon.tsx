868a9f92724be02a7b92e6a5347d0cc8b14fc325
Chenwei Zhang <chenwz@amazon.com>
2021-07-14 14:21:28 -0700

    Tweaking sizes for icon primitive

--- packages/react/src/primitives/Icon/Icon.tsx
+++ packages/react/src/primitives/Icon/Icon.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import classNames from "classnames";
+
 import { IconProps } from "../types";
+import { ComponentClassNames } from "../shared";
 
 const defaultViewBox = { minX: 0, minY: 0, width: 24, height: 24 };
 export const Icon: React.FC<IconProps> = props => {
   const {
+    id,
+    className,
     ariaLabel,
-    pathData,
+    pathData, // We should have a fallback icon
     fill = "currentColor",
     viewBox = defaultViewBox,
     size = "medium",
+    ...rest
   } = props;
 
   const minX = viewBox.minX ? viewBox.minX : defaultViewBox.minX;
@@ -17,9 +23,13 @@ export const Icon: React.FC<IconProps> = props => {
   const height = viewBox.height ? viewBox.height : defaultViewBox.height;
   return (
     <svg
-      viewBox={`${minX} ${minY} ${width} ${height}`}
+      id={id}
       aria-label={ariaLabel}
       data-size={size}
+      data-testid={id}
+      viewBox={`${minX} ${minY} ${width} ${height}`}
+      className={classNames(ComponentClassNames.Icon, className)}
+      {...rest}
     >
       <path d={pathData} fill={fill}></path>
     </svg>
