59b96404e5d3cd3f4eb0ba2237475a4b31b5a612
Joe Buono <joebuono724@gmail.com>
2021-07-14 12:26:13 -0400

    Update packages/react/src/primitives/Badge/Badge.tsx
    Co-authored-by: Eddie Keller <116ekg@gmail.com>

--- packages/react/src/primitives/Badge/Badge.tsx
+++ packages/react/src/primitives/Badge/Badge.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ComponentClassNames } from "../shared/constants";
 import classNames from "classnames";
 import { BadgeProps } from "../types";
-import { View, } from "@aws-amplify/ui-react";
+import { View } from "@aws-amplify/ui-react";
 
 export const Badge: React.FC<BadgeProps> = ({
   className = "",
