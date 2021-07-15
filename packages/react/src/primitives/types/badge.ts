847b36d30d6dca11e834d485016ddd874bfea4c3
Buono <joebuono@88665a17582b.ant.amazon.com>
2021-07-15 14:23:51 -0400

    comments about badge props

--- packages/react/src/primitives/types/badge.ts
+++ packages/react/src/primitives/types/badge.ts
@@ -6,12 +6,12 @@ export type BadgeVariant = 'default' | 'info' | 'error' | 'warning' | 'success';
 export type BadgeSize = 'small' | 'medium' | 'large';
 
 export interface BadgeProps extends BaseComponentProps, BaseStyleProps {
+  /**
+   * The varient property will affect the background color of the badge.
+  */
   variant?: BadgeVariant;
   /**
-   * The size property will affect both the padding and font size of the badge.
-   * Note: this component *should not* inherit its font size from its parent.
-   * It should define its own font size. The reason is you could have a badge inside
-   * an <h2> and the text of the badge should not have the same size.
+   * The size property will affect the font size of the badge.
    */
   size?: BadgeSize;
 }
