ef854070d16119c398fd520e63d1a0aa06285b43
Jacob Logan <lognjc@amazon.com>
2021-07-16 01:16:45 -0700

    updates to card primitive and docs based on PR feedback

--- docs/src/pages/ui/primitives/card/demo.tsx
+++ docs/src/pages/ui/primitives/card/demo.tsx
@@ -1,5 +1,15 @@
-import React from "react";
-import { Card, Text, View, Image } from "@aws-amplify/ui-react";
+import React from 'react';
+import {
+  Card,
+  Text,
+  View,
+  Image,
+  Badge,
+  Button,
+  Flex,
+  Divider,
+  Heading,
+} from '@aws-amplify/ui-react';
 
 export const CardSample = () => {
   return (
@@ -11,21 +21,20 @@ export const CardSample = () => {
         objectFit="cover"
       />
       <View padding="1rem">
-        <Text fontWeight="bold">New Zealand</Text>
-        <View padding="1rem">
-          <Text>
-            <b>Population: </b>4.917 million
+        <Flex>
+          <Badge backgroundColor="lightgreen">Plus</Badge>
+          <Badge backgroundColor="lightblue">Verified</Badge>
+        </Flex>
+        <Divider padding="1rem 0 0 0" />
+        <Heading level={3}>
+          <b>New Zealand White Water Outdoor Adventure</b>
+        </Heading>
+        <View padding="0 1rem">
+          <Text padding="0 0 1rem 0" isTruncated={true}>
+            Join us on this beautiful outdoor adventure through the glittering
+            rivers through the snowy peaks on New Zealand.
           </Text>
-          <Text>
-            <b>Capital: </b>Wellington
-          </Text>
-          <Text>
-            <b>Size: </b>103,483 mi<sup>2</sup>
-          </Text>
-          <Text isTruncated={true}>
-            Above are pictured the snowy peaks of southern New Zealand. This
-            view was captured on the way to Milford.
-          </Text>
+          <Button isFullWidth={true}>Book it</Button>
         </View>
       </View>
     </Card>
