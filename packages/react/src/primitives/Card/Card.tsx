9929a6ad03341c5fcb6c7bcd7387bdaafc3163ad
jacoblogan <jacob.maiola.logan@gmail.com>
2021-07-16 10:42:59 -0700

    fixing case error on import (#96)
    Co-authored-by: Jacob Logan <lognjc@amazon.com>

--- packages/react/src/primitives/Card/Card.tsx
+++ packages/react/src/primitives/Card/Card.tsx
@@ -1,8 +1,8 @@
-import classNames from "classnames";
-import React from "react";
-import { ComponentClassNames } from "../shared/constants";
-import { CardProps } from "../types/Card";
-import { View } from "@aws-amplify/ui-react";
+import classNames from 'classnames';
+import React from 'react';
+import { ComponentClassNames } from '../shared/constants';
+import { CardProps } from '../types/card';
+import { View } from '@aws-amplify/ui-react';
 
 export const Card: React.FC<CardProps> = ({ className, children, ...rest }) => {
   return (
