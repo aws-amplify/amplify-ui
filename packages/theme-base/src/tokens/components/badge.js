a50217f0aefdf2d9b3eea4c113f5fc0f3a790935
Danny Banks <djb@amazon.com>
2021-07-20 12:41:27 -0700

    Fixing default badge color

--- packages/theme-base/src/tokens/components/badge.js
+++ packages/theme-base/src/tokens/components/badge.js
@@ -4,7 +4,7 @@ module.exports = {
   "font-size": { "value": "{fontSizes.small.value}" },
   "text-align": { "value": "center" },
   "padding": { "value": ["{space.2xs.value}", "{space.small.value}"] },
-  "background-color": { "value": "{colors.background.info.value}" },
+  "background-color": { "value": "{colors.background.tertiary.value}" },
   // An arbitrarily large value to ensure that the left and right sides of the badge are perfectly rounded for any size variant
   "border-radius": { "value": "9999px" },
   
