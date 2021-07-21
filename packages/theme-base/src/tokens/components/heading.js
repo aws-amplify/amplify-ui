08f45d95b131273d5649e06f237677c6bade215a
Buono <joebuono@88665a17582b.ant.amazon.com>
2021-07-21 18:46:47 -0400

    changed 4xl to xxxxl, etc

--- packages/theme-base/src/tokens/components/heading.js
+++ packages/theme-base/src/tokens/components/heading.js
@@ -1,30 +1,30 @@
 module.exports = {
   // Color property not getting applied, issue with build order (Text built after Heading, alphabetically). Will solve later using SCSS.
-  'color': { 'value': '{colors.font.primary.value}' },
-  'lineHeight': { 'value': 1.25 },
+  color: { value: '{colors.font.primary.value}' },
+  lineHeight: { value: 1.25 },
 
-  '1': {
-    'fontSize': { 'value': '{fontSizes.4xl.value}' },
-    'fontWeight': { 'value': '{fontWeights.light.value}' },
+  1: {
+    fontSize: { value: '{fontSizes.xxxxl.value}' },
+    fontWeight: { value: '{fontWeights.light.value}' },
   },
-  '2': {
-    'fontSize': { 'value': '{fontSizes.3xl.value}' },
-    'fontWeight': { 'value': '{fontWeights.light.value}' },
+  2: {
+    fontSize: { value: '{fontSizes.xxxl.value}' },
+    fontWeight: { value: '{fontWeights.light.value}' },
   },
-  '3': {
-    'fontSize': { 'value': '{fontSizes.2xl.value}' },
-    'fontWeight': { 'value': '{fontWeights.normal.value}' },
+  3: {
+    fontSize: { value: '{fontSizes.xxl.value}' },
+    fontWeight: { value: '{fontWeights.normal.value}' },
   },
-  '4': {
-    'fontSize': { 'value': '{fontSizes.xl.value}' },
-    'fontWeight': { 'value': '{fontWeights.medium.value}' },
+  4: {
+    fontSize: { value: '{fontSizes.xl.value}' },
+    fontWeight: { value: '{fontWeights.medium.value}' },
   },
-  '5': {
-    'fontSize': { 'value': '{fontSizes.large.value}' },
-    'fontWeight': { 'value': '{fontWeights.semibold.value}' },
+  5: {
+    fontSize: { value: '{fontSizes.large.value}' },
+    fontWeight: { value: '{fontWeights.semibold.value}' },
   },
-  '6': {
-    'fontSize': { 'value': '{fontSizes.medium.value}' },
-    'fontWeight': { 'value': '{fontWeights.bold.value}' },
+  6: {
+    fontSize: { value: '{fontSizes.medium.value}' },
+    fontWeight: { value: '{fontWeights.bold.value}' },
   },
 };
