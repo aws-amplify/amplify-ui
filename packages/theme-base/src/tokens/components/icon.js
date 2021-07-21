8bcf3dfb094266fc588a26d5f45f4b25bdc01658
Scott Rees <6165315+reesscot@users.noreply.github.com>
2021-07-21 08:18:42 -0700

    Fix icon theme css variables (#107)

---
+++ packages/theme-base/src/tokens/components/icon.js
@@ -0,0 +1,12 @@
+module.exports = {
+  height: {
+    large: { value: '{fontSizes.large.value}' },
+    medium: { value: '{fontSizes.medium.value}' },
+    small: { value: '{fontSizes.small.value}' },
+  },
+  'line-height': {
+    large: { value: '{fontSizes.large.value}' },
+    medium: { value: '{fontSizes.medium.value}' },
+    small: { value: '{fontSizes.small.value}' },
+  },
+};
