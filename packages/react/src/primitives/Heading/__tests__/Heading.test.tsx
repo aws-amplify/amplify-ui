da353e6be7215aaceff4e348eee189c67bdcd5ab
Buono <joebuono@88665a17582b.ant.amazon.com>
2021-07-22 14:02:31 -0400

    modified tests

--- packages/react/src/primitives/Heading/__tests__/Heading.test.tsx
+++ packages/react/src/primitives/Heading/__tests__/Heading.test.tsx
@@ -7,33 +7,43 @@ import { ComponentPropsToStylePropsMap } from '../../types';
 import { kebabCase } from 'lodash';
 
 describe('Heading: ', () => {
-  const headingText = 'Heading primitive';
-
   it('renders an h6 tag by default', async () => {
-    render(<Heading>{headingText}</Heading>);
+    render(<Heading testId="headingId"></Heading>);
 
-    const heading = await screen.findByText(headingText);
+    const heading = await screen.findByTestId('headingId');
     expect(heading.nodeName).toBe('H6');
   });
 
   it('renders h1-h6 tags by passing level prop', async () => {
     render(
       <div>
-        <Heading level={1}>H1</Heading>
-        <Heading level={2}>H2</Heading>
-        <Heading level={3}>H3</Heading>
-        <Heading level={4}>H4</Heading>
-        <Heading level={5}>H5</Heading>
-        <Heading level={6}>H6</Heading>
+        <Heading level={1} testId="h1">
+          H1
+        </Heading>
+        <Heading level={2} testId="h2">
+          H2
+        </Heading>
+        <Heading level={3} testId="h3">
+          H3
+        </Heading>
+        <Heading level={4} testId="h4">
+          H4
+        </Heading>
+        <Heading level={5} testId="h5">
+          H5
+        </Heading>
+        <Heading level={6} testId="h6">
+          H6
+        </Heading>
       </div>
     );
 
-    const h1 = await screen.findByText('H1');
-    const h2 = await screen.findByText('H2');
-    const h3 = await screen.findByText('H3');
-    const h4 = await screen.findByText('H4');
-    const h5 = await screen.findByText('H5');
-    const h6 = await screen.findByText('H6');
+    const h1 = await screen.findByTestId('h1');
+    const h2 = await screen.findByTestId('h2');
+    const h3 = await screen.findByTestId('h3');
+    const h4 = await screen.findByTestId('h4');
+    const h5 = await screen.findByTestId('h5');
+    const h6 = await screen.findByTestId('h6');
 
     expect(h1.nodeName).toBe('H1');
     expect(h2.nodeName).toBe('H2');
@@ -44,12 +54,8 @@ describe('Heading: ', () => {
   });
 
   it('can apply styling via props', async () => {
-    render(
-      <Heading level={3} fontStyle="italic">
-        {headingText}
-      </Heading>
-    );
-    const heading = await screen.findByText(headingText);
+    render(<Heading level={3} fontStyle="italic" testId="headingId"></Heading>);
+    const heading = await screen.findByTestId('headingId');
     expect(heading.nodeName).toBe('H3');
     expect(
       heading.style.getPropertyValue(
@@ -59,18 +65,14 @@ describe('Heading: ', () => {
   });
 
   it('can apply a custom className', async () => {
-    render(<Heading className="custom-heading">{headingText}</Heading>);
-    const heading = await screen.findByText(headingText);
+    render(<Heading className="custom-heading" testId="headingId"></Heading>);
+    const heading = await screen.findByTestId('headingId');
     expect(heading.classList.contains('custom-heading')).toBe(true);
     expect(heading.classList.contains(ComponentClassNames.Heading)).toBe(true);
   });
 
   it('can render any arbitrary data-* attribute', async () => {
-    render(
-      <Heading data-demo="true" id="dataTest">
-        {headingText}
-      </Heading>
-    );
+    render(<Heading data-demo="true" testId="dataTest"></Heading>);
     const heading = await screen.findByTestId('dataTest');
     expect(heading.dataset['demo']).toBe('true');
   });
