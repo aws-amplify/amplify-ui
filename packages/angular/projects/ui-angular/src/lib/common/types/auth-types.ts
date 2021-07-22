42a77d22b3e00216cdfd84ac61613d9b2333ccc0
wlee221 <wlee221@live.unc.edu>
2021-07-21 18:09:48 -0700

    Use ui-core attribute on angular

--- packages/angular/projects/ui-angular/src/lib/common/types/auth-types.ts
+++ packages/angular/projects/ui-angular/src/lib/common/types/auth-types.ts
@@ -1,15 +1,9 @@
+import { AuthInputAttributes } from '@aws-amplify/ui-core';
+
 /**
  * TODO: This should be typed from core
  */
 export type AuthState = 'signIn' | 'signUp' | 'signedIn';
 
-/**
- * TODO: This should be replaced by the translation solution we end up with.
- */
-export interface AttributeInfo {
-  label: string;
-  placeholder: string;
-}
-
 // maps auth attribute to its repsective labels and placeholder
-export type AttributeInfoProvider = () => Record<string, AttributeInfo>;
+export type AttributeInfoProvider = () => AuthInputAttributes;
