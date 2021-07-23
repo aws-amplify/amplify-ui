51c6ecfa80ae0b74a969716a8423d9cd6e83746c
Danny Banks <djb@amazon.com>
2021-07-22 21:02:13 -0700

    fixing docs build

--- docs/src/pages/ui/theme/Colors.tsx
+++ docs/src/pages/ui/theme/Colors.tsx
@@ -1,20 +1,33 @@
-import React from "react";
-import { View, Flex } from "@aws-amplify/ui-react";
-import theme from "@aws-amplify/ui-theme-base/dist/theme.json";
+import React from 'react';
+import { View, Flex } from '@aws-amplify/ui-react';
+import theme from '@aws-amplify/ui-theme-base';
 
-const baseColors = ['red','orange','yellow','green','teal','blue','purple','pink'];
+const baseColors = [
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'teal',
+  'blue',
+  'purple',
+  'pink',
+];
 
 export const Colors = () => {
   return (
     <>
-      {baseColors.map(baseColor => (
-        <Flex direction="row" style={{marginBottom: theme.space.large.value}}>
-          {Object.values(theme.colors[baseColor]).map(({name, value}) => (
-            <View key={name} width="5rem" height="5rem" backgroundColor={value} />
+      {baseColors.map((baseColor) => (
+        <Flex direction="row" style={{ marginBottom: theme.space.large.value }}>
+          {Object.values(theme.colors[baseColor]).map(({ name, value }) => (
+            <View
+              key={name}
+              width="5rem"
+              height="5rem"
+              backgroundColor={value}
+            />
           ))}
         </Flex>
       ))}
-
     </>
-  )
-}
+  );
+};
