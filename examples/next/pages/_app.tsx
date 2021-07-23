136c8a3ec93dd169b7dab1bcc728a718702c2b65
Scott Rees <reesscot@amazon.com>
2021-07-23 15:43:32 -0700

    Add airbnb example page
    React primitives amplify environment

--- examples/next/pages/_app.tsx
+++ examples/next/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@aws-amplify/ui-react/styles.css';
+import './examples/airbnb/styles.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
