e337911662aa284109a353a9d16d805d63edde2d
Chenwei Zhang <chenwz@amazon.com>
2021-07-23 17:10:15 -0700

    docs(wip): adding documentation and demo for pagination primitive

--- docs/src/pages/ui/primitives/pagination/demo.tsx
+++ docs/src/pages/ui/primitives/pagination/demo.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+
 import { Pagination } from '@aws-amplify/ui-react';
 
 export const PaginationDemo = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const onNext = useCallback((newPage) => {
+    setCurrentPage(newPage);
+  }, []);
+
+  const onPrev = useCallback((newPage) => {
+    setCurrentPage(newPage);
+  }, []);
+
+  const onChange = useCallback((newPage, prevPage) => {
+    setCurrentPage(newPage);
+  }, []);
   return (
     <Pagination
-      currentPage={1}
+      currentPage={currentPage}
       totalPages={10}
-      onNext={() => {}}
-      onPrevious={() => {}}
-      onChange={() => {}}
+      onNext={onNext}
+      onPrevious={onPrev}
+      onChange={onChange}
     />
   );
 };
