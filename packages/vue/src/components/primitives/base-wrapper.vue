310b3c9bcc1a131831c150b481e8230c494e9142
Erik Hanchett <ehhanche@amazon.com>
2021-07-28 12:50:06 -0700

    Fixed empty props

--- packages/vue/src/components/primitives/base-wrapper.vue
+++ packages/vue/src/components/primitives/base-wrapper.vue
@@ -5,16 +5,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   provide() {
     return {};
   },
-  props: {},
   setup() {
     return {};
-  }
+  },
 });
 </script>
 
