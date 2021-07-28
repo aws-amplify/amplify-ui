3eadd7f8a3076f7e71a487bbdda84dcefb74f370
Shane Laymance <shane.laymance@gmail.com>
2021-07-28 14:08:30 -0700

    Refactor script to use proper headless pull config

--- .github/pull-environments.sh
+++ .github/pull-environments.sh
@@ -2,36 +2,46 @@
 set -e
 IFS='|'
 
-REACTCONFIG="{\
+FRONTENDCONFIG="{\
 \"SourceDir\":\"src\",\
-\"DistributionDir\":\"build\",\
+\"DistributionDir\":\"dist\",\
 \"BuildCommand\":\"npm run-script build\",\
 \"StartCommand\":\"npm run-script start\"\
 }"
-AMPLIFY="{\
-\"defaultEditor\":\"code\"\
-}"
 FRONTEND="{\
 \"frontend\":\"javascript\",\
-\"framework\":\"react\",\
-\"config\":$REACTCONFIG\
+\"framework\":\"none\",\
+\"config\":$FRONTENDCONFIG\
+}"
+AMPLIFY="{\
+\"defaultEditor\":\"code\"\
+\"envName\":\"staging\"\
 }"
 AWSCLOUDFORMATIONCONFIG="{\
+\"configLevel\":\"project\",\
+\"useProfile\":false,\
 \"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
 \"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
+\"region\":\"us-east-1\"\
 }"
 PROVIDERS="{\
 \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
 }"
 
+# Pull the backend for each environment
 for dir in environments/*/ ; do
-  cd "$dir" && \
-  amplify pull \
+  if ! [[ -f "$dir/app-id" ]]; then
+    continue
+  fi
+
+  cd "$dir"
+
+  # 'echo n' is used to answer "No" to the prompt "Do you plan on modifying this backend?"
+  # See https://github.com/aws-amplify/amplify-cli/issues/5275
+  echo n | amplify pull \
   --appId `cat app-id` \
-  --envName staging \
   --amplify $AMPLIFY \
   --frontend $FRONTEND \
   --providers $PROVIDERS \
-  --yes \
-  && cd ../..
+  && cd -
 done
