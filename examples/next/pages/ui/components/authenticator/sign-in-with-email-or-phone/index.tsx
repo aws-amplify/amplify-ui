0f5c5619dd69bee785cfbed449c750545a684e51
Shane Laymance <shane.laymance@gmail.com>
2021-08-02 11:56:33 -0700

    Add next example app

---
+++ examples/next/pages/ui/components/authenticator/sign-in-with-email-or-phone/index.tsx
@@ -0,0 +1,26 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import awsExports from '@environments/auth-with-email-and-phone/src/aws-exports';
+
+Amplify.configure({
+  ...awsExports,
+  auth: {
+    login_mechanisms: ['email', 'phone'],
+  },
+});
+
+export default function AuthenticatorWithEmailOrPhone() {
+  return (
+    <>
+      <Authenticator>
+        {({ send }) => {
+          return (
+            <>
+              <button onClick={() => send('SIGN_OUT')}>Sign out</button>
+            </>
+          );
+        }}
+      </Authenticator>
+    </>
+  );
+}
