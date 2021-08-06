6a97f6a1943f9ba687ba3b7e7cc28dd9ad430716
Jacob Logan <lognjc@amazon.com>
2021-08-06 08:11:10 -0700

    add tokens for default fonts

---
+++ packages/theme-base/src/tokens/fonts.js
@@ -0,0 +1,13 @@
+module.exports = {
+  default: {
+    variable: {
+      value: `'InterVariable', -apple-system, BlinkMacSystemFont,
+        'Helvetica Neue', 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans',
+        sans-serif`,
+    },
+    static: {
+      value: `'Inter', -apple-system, BlinkMacSystemFont, 'Helvetica Neue',
+        'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', sans-serif`,
+    },
+  },
+};
