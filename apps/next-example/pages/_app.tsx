0908510717c616e70d48439e2e04d5dc31af0c19
Eric Clemmons <eric@smarterspam.com>
2021-08-10 16:57:07 -0500

    yarn nx generate @nrwl/next:app

---
+++ apps/next-example/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
+import './styles.css';
+
+function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Welcome to next-example!</title>
+      </Head>
+      <div className="app">
+        <header className="flex">
+          <NxLogo width="75" height="50" />
+          <h1>Welcome to next-example!</h1>
+        </header>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default CustomApp;
