a712a8236a7ad5eba6af4acf517e536c0cc16629
Shane Laymance <shane.laymance@gmail.com>
2021-08-12 09:43:26 -0700

    Remove individual environment workspaces

--- examples/next/pages/ui/components/authenticator/sign-in-sms-mfa/index.tsx
+++ examples/next/pages/ui/components/authenticator/sign-in-sms-mfa/index.tsx
@@ -1,7 +1,7 @@
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 
-import awsExports from '@environments/auth-with-phone-and-sms-mfa/src/aws-exports';
+import awsExports from 'environments/auth-with-phone-and-sms-mfa/src/aws-exports';
 
 Amplify.configure({
   ...awsExports,
--- examples/vue/src/pages/ui/components/authenticator/sign-in-with-phone/index.vue
+++ examples/vue/src/pages/ui/components/authenticator/sign-in-with-phone/index.vue
@@ -13,23 +13,23 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import aws_exports from "auth-with-phone-number-environment/src/aws-exports";
+import { defineComponent } from 'vue';
+import aws_exports from 'environments/auth-with-phone-number/src/aws-exports';
 
-import Amplify from "aws-amplify";
-import "@aws-amplify/ui-vue/styles.css";
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue/styles.css';
 
-import { Authenticator, useAuth } from "@aws-amplify/ui-vue";
+import { Authenticator, useAuth } from '@aws-amplify/ui-vue';
 
 Amplify.configure({
   ...aws_exports,
   auth: {
-    login_mechanisms: ["phone_number"],
+    login_mechanisms: ['phone_number'],
   },
 });
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: { Authenticator },
   setup() {
     const { send } = useAuth();
