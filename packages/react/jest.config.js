ac1ab1b777000594a72ff37a17fcb10987e43514
Erik Hanchett <icystorm@gmail.com>
2021-08-13 09:07:03 -0700

    Angular ci (#179)
    * Added in new Angular Examples Folder
    
    * Added routes for examples to match Vue and React
    
    * Fixed tests, added in CI updates
    
    * Updated build for Angular
    
    * Added in longer wait for wait-on
    
    * Fixed workflow file
    
    * fixed tests
    
    * Fixed workspace tests again
    
    * Updated jest to ignore dist folder
    
    * Updated build for angular
    
    * Adjusted build
    
    * Adjusted build
    
    * adjusted timing
    
    * Fixing waiton
    
    * Trying conditionals
    
    * Trying conditionals
    
    * moved config
    
    * Updates from PR
    
    * Updated test.yml
    
    * deleted spec test
    
    Co-authored-by: Erik Hanchett <ehhanche@amazon.com>

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.json',
