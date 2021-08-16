f1c3671dd5c1ab8534829b4e812c8e130096c484
wlee221 <wlee221@live.unc.edu>
2021-08-16 02:45:16 -0700

    Remove unused actions

--- packages/core/src/machines/actions/auth.ts
+++ packages/core/src/machines/actions/auth.ts
@@ -1,18 +1,6 @@
-import { AuthEvent, AuthContext } from '../../types';
-import { assign, spawn, Spawnable } from 'xstate';
 import { stop } from 'xstate/lib/actions';
 
-export const setUser = assign<AuthContext, AuthEvent>({
-  user(_, event) {
-    return event.data?.user || event.data;
-  },
-});
-
-export const spawnActor = (machine: Spawnable, name: string) => {
-  return assign<AuthContext, AuthEvent>({
-    actorRef: () => spawn(machine, name),
-  });
-};
+// TODO: Add more shared actions here
 
 export const stopActor = (machineId: string) => {
   return stop(machineId);
