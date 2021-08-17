30bcaf14a74b51f8d41e03250d6344a3bab5d33f
Joe Buono <joebuono724@gmail.com>
2021-08-17 16:35:23 -0400

    Standardize primitive docs (#181)
    * added react.mdx file to each primitive demo folder
    
    * changes to badge docs
    
    * small changes to badge page
    
    * standardized badge page
    
    * sformatting badge page
    
    * added display: block to Divider CSS
    
    * standardizing button page
    
    * standardizing card page
    
    * standardizing collection page
    
    * standardizing divider page
    
    * standardizing flex page
    
    * added display: block to Heading primitive
    
    * standardizing icon page
    
    * standardizing image and pagination primitives
    
    * standardizing placeholder page
    
    * standardizing rating page
    
    * standardizing text and view pages
    
    * standardizing the badge page (second pass)
    
    * second pass on standardizing the button page
    
    * second pass on standardizing card component
    
    * second pass on standardizing collection component
    
    * second pass on standardizing divider page
    
    * second pass standardizing heading page
    
    * second pass on standardizing icon page
    
    * second pass standardizing image page
    
    * second pass standardizing pagination page
    
    * second pass on standardizing the placeholder page
    
    * second pass standardizing text page
    
    * second pass standardizing view page
    
    * removed installation section from each primitive page
    
    * small change to card component css variables example
    
    * added {...rest} props to Button.tsx
    
    Co-authored-by: Joe Buono <joebuono@amazon.com>
    Co-authored-by: Scott Rees <6165315+reesscot@users.noreply.github.com>

--- docs/src/pages/ui/primitives/badge/demo.tsx
+++ docs/src/pages/ui/primitives/badge/demo.tsx
@@ -1,60 +1,21 @@
-import React, { useState } from 'react';
-import {
-  Badge,
-  BadgeVariation,
-  BadgeSize,
-  Flex,
-  View,
-} from '@aws-amplify/ui-react';
+import React from 'react';
+import { Badge, Flex } from '@aws-amplify/ui-react';
 
-import { FieldLabeler } from '../../../../components/FieldLabeler';
+import { BadgePropControls } from '@/components/BadgePropControls';
+import { useBadgeProps } from '@/components/useBadgeProps';
+import { Example } from '@/components/Example';
 
 export const BadgeDemo = () => {
-  const [variation, setVariation] = useState<BadgeVariation>();
-  const [size, setSize] = useState<BadgeSize>();
+  const badgeProps = useBadgeProps({});
 
   return (
-    <div>
-      <Flex direction="column" gap="1rem">
-        <Flex>
-          <FieldLabeler id="variation">
-            <select
-              value={variation}
-              placeholder="Select badge variation"
-              onChange={(event) =>
-                setVariation(event.target.value as BadgeVariation)
-              }
-              id="variation"
-              name="variation"
-            >
-              <option value="">default</option>
-              <option value="info">info</option>
-              <option value="error">error</option>
-              <option value="warning">warning</option>
-              <option value="success">success</option>
-            </select>
-          </FieldLabeler>
-
-          <FieldLabeler id="size">
-            <select
-              value={size}
-              placeholder="Select badge size"
-              onChange={(event) => setSize(event.target.value as BadgeSize)}
-              id="size"
-              name="size"
-            >
-              <option value="">default</option>
-              <option value="small">small</option>
-              <option value="large">large</option>
-            </select>
-          </FieldLabeler>
-        </Flex>
-        <View>
-          <Badge variation={variation} size={size}>
-            Badge
-          </Badge>
-        </View>
-      </Flex>
-    </div>
+    <Flex direction="column" gap="0.5rem">
+      <BadgePropControls {...badgeProps} />
+      <Example>
+        <Badge size={badgeProps.size} variation={badgeProps.variation}>
+          Badge
+        </Badge>
+      </Example>
+    </Flex>
   );
 };
