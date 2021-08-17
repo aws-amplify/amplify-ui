7e5cfd5648e66e651afb993e63f54652bc19505a
Eric Clemmons <eric@smarterspam.com>
2021-08-17 14:58:27 -0500

    Inline @imports

--- packages/react/postcss.config.cjs
+++ packages/react/postcss.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('@csstools/postcss-sass'),
+    require('autoprefixer'),
+    require('postcss-easy-import'),
+    require('tailwindcss'),
+  ],
 };
