93ec5663c447ddb3600861c49ea01852d57e4e36
Scott Rees <reesscot@amazon.com>
2021-08-18 15:30:37 -0700

    Move field to components

--- packages/theme-base/src/tokens/components/field.js
+++ packages/theme-base/src/tokens/components/field.js
@@ -2,53 +2,24 @@ module.exports = {
   borderStyle: { value: 'solid' },
   borderColor: { value: '{colors.border.primary.value}' },
   borderWidth: { value: '{borderWidths.small.value}' },
+  padding: { value: ['{space.small.value}', '{space.medium.value}'] },
   borderRadius: { value: '{radii.medium.value}' },
-  boxSizing: { value: 'border-box' },
-  padding: { value: ['{space.xs.value}', '{space.medium.value}'] },
   fontSize: { value: '{fontSizes.medium.value}' },
-  lineHeight: { value: '{lineHeights.medium.value}' },
-  outlineColor: { value: '{colors.transparent.value}' },
-  outlineStyle: { value: 'solid' },
-  outlineWidth: { value: '{outlineWidths.medium.value}' },
-  outlineOffset: { value: '{outlineOffsets.medium.value}' },
 
   small: {
     fontSize: { value: '{fontSizes.small.value}' },
     padding: { value: ['{space.xs.value}', '{space.small.value}'] },
-    lineHeight: { value: '{lineHeights.small.value}' },
   },
   large: {
     fontSize: { value: '{fontSizes.large.value}' },
     padding: { value: ['{space.medium.value}', '{space.large.value}'] },
-    lineHeight: { value: '{lineHeights.large.value}' },
-  },
-
-  // for variation
-  quiet: {
-    borderStyle: { value: 'none' },
-    borderBlockEnd: {
-      value: '{borderWidths.small.value} solid {colors.border.primary.value}',
-    },
-    borderRadius: { value: '0' },
-    _focus: {
-      borderColor: { value: '{colors.focus.value}' },
-      boxShadow: { value: '{colors.focus.value} 0px 2px 0px -1px' },
-    },
   },
 
   _focus: {
-    // These focus styles have been calibrated to create
-    // highly visible focus indicator per WCAG 2.1 guidliness:
-    // See: https://www.w3.org/WAI/WCAG21/Techniques/general/G195.html
-    // * Focus indicator area is at least the 1 CSS px border around the component.
-    // * contrast of the indicator between focused and unfocused states has a ratio of 3:1 for the minimum focus indicator area.
-
-    borderColor: { value: '{colors.focus.value}' },
-    boxShadow: { value: '{colors.focus.value} 0px 0px 0px 1px' },
+    // outline: { value: '{colors.brand.primary.90.value}' },
+    // borderColor: { value: '{colors.border.primary.value}' },
   },
   _disabled: {
-    cursor: { value: 'not-allowed' },
-    borderColor: { value: '{colors.transparent.value}' },
-    backgroundColor: { value: '{colors.background.tertiary.value}' },
+    backgroundColor: { value: '{colors.neutral.20.value}' },
   },
 };
