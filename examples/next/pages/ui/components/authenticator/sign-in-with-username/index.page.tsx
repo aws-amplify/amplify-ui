607822fc75beac9678b0cbe0db8ed85397f221d8
Erik Hanchett <ehhanche@amazon.com>
2021-08-18 16:01:52 -0700

    Moved directories to legacy

--- examples/next/pages/ui/components/authenticator/sign-in-with-username/index.page.tsx
+++ examples/next/pages/ui/components/authenticator/sign-in-with-username/index.page.tsx
@@ -1,4 +1,8 @@
-import { Authenticator } from '@aws-amplify/ui-react';
+import {
+  Authenticator,
+  AmplifyAuthenticator,
+  AmplifySignOut,
+} from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 
 import awsExports from '@environments/auth-with-username-no-attributes/src/aws-exports';
@@ -12,8 +16,18 @@ Amplify.configure({
 
 export default function AuthenticatorWithUsername() {
   return (
-    <Authenticator>
-      {({ send }) => <button onClick={() => send('SIGN_OUT')}>Sign out</button>}
-    </Authenticator>
+    <>
+      <Authenticator>
+        {({ send }) => (
+          <button onClick={() => send('SIGN_OUT')}>Sign out</button>
+        )}
+      </Authenticator>
+      <AmplifyAuthenticator>
+        <div>
+          My App
+          <AmplifySignOut />
+        </div>
+      </AmplifyAuthenticator>
+    </>
   );
 }
