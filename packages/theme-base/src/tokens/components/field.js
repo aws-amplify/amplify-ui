571370de6a7667655d9461e5a4494d083ffdb24b
Chenwei Zhang <chenwz@amazon.com>
2021-08-20 11:25:48 -0700

    style: adding sharable field styles

--- packages/theme-base/src/tokens/components/field.js
+++ packages/theme-base/src/tokens/components/field.js
@@ -2,24 +2,34 @@ module.exports = {
   borderStyle: { value: 'solid' },
   borderColor: { value: '{colors.border.primary.value}' },
   borderWidth: { value: '{borderWidths.small.value}' },
-  padding: { value: ['{space.small.value}', '{space.medium.value}'] },
   borderRadius: { value: '{radii.medium.value}' },
+  boxSizing: { value: 'border-box' },
+  padding: { value: ['{space.xs.value}', '{space.medium.value}'] },
   fontSize: { value: '{fontSizes.medium.value}' },
+  lineHeight: { value: '{lineHeights.medium.value}' },
+  outlineColor: { value: '{colors.transparent.value}' },
+  outlineStyle: { value: 'solid' },
+  outlineWidth: { value: '{outlineWidths.medium.value}' },
+  outlineOffset: { value: '{outlineOffsets.medium.value}' },
 
   small: {
     fontSize: { value: '{fontSizes.small.value}' },
     padding: { value: ['{space.xs.value}', '{space.small.value}'] },
+    lineHeight: { value: '{lineHeights.small.value}' },
   },
   large: {
     fontSize: { value: '{fontSizes.large.value}' },
     padding: { value: ['{space.medium.value}', '{space.large.value}'] },
+    lineHeight: { value: '{lineHeights.large.value}' },
   },
 
   _focus: {
-    // outline: { value: '{colors.brand.primary.90.value}' },
-    // borderColor: { value: '{colors.border.primary.value}' },
+    borderColor: { value: 'hsl(220, 50%, 50%)' },
+    boxShadow: { value: '0 0 1px 2px hsl(220, 50%, 50%)' },
   },
   _disabled: {
-    backgroundColor: { value: '{colors.neutral.20.value}' },
+    cursor: { value: 'not-allowed' },
+    borderColor: { value: '{colors.transparent.value}' },
+    backgroundColor: { value: '{colors.background.tertiary.value}' },
   },
 };
---
+++ packages/theme-base/src/tokens/outlineWidths.js
@@ -0,0 +1,5 @@
+module.exports = {
+  small: { value: '1px' },
+  medium: { value: '2px' },
+  large: { value: '3px' },
+};
