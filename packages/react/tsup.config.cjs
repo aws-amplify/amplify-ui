62dade33362f94eda63e770b80145f14e492f7d2
Eric Clemmons <eric@smarterspam.com>
2021-08-23 09:26:10 -0500

    Explicitly exclude aws-amplify modules

--- packages/react/tsup.config.cjs
+++ packages/react/tsup.config.cjs
@@ -4,6 +4,8 @@
 module.exports = {
   dts: true,
   entryPoints: ['src/index.tsx'],
+  // `aws-amplify` is external, but sub-dependencies weren't automatically externalized ("require" statements were included)
+  external: ['`aws-amplify', /^@aws-amplify\//],
   format: ['cjs', 'esm'],
   sourcemap: 'both',
   splitting: false,
