918b51192e066ac83829886f095d23beb65ae8bd
Scott Rees <reesscot@amazon.com>
2021-08-25 16:13:51 -0700

    Add missing unit tests for Label

--- packages/react/src/primitives/Label/__tests__/Label.test.tsx
+++ packages/react/src/primitives/Label/__tests__/Label.test.tsx
@@ -1,5 +1,17 @@
 import { Label } from '../Label';
+import { render, screen } from '@testing-library/react';
+import { ComponentClassNames } from '../../shared';
 
 describe('Label component', () => {
-  /* Add your own component tests */
+  it('should render default and custom classname for TextField wrapper', async () => {
+    render(
+      <Label htmlFor="my-label" className="my-test-label">
+        My label
+      </Label>
+    );
+
+    const label = (await screen.findByText('My label')) as HTMLLabelElement;
+    expect(label.className).toContain('my-test-label');
+    expect(label.htmlFor).toBe('my-label');
+  });
 });
