6faad9ee2c76efacb2da473684c43e17d9f589b3
Scott Rees <reesscot@amazon.com>
2021-08-25 14:37:06 -0700

    Fix spelling of aria-labelledby

--- packages/react/src/primitives/TextField/TextField.tsx
+++ packages/react/src/primitives/TextField/TextField.tsx
@@ -70,7 +70,7 @@ export const TextField: React.FC<TextFieldProps> = (props) => {
       ) : null}
       <Flex className={ComponentClassNames.FieldWrapper}>
         <Input
-          aria-labeledby={fieldId}
+          aria-labelledby={fieldId}
           className={ComponentClassNames.TextFieldInput}
           defaultValue={defaultValue}
           hasError={hasError}
