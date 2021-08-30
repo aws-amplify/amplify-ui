31b9741299e7315f4ea71fd397327bc0a1e8b777
Erik Hanchett <ehhanche@amazon.com>
2021-08-30 10:01:55 -0700

    Added in dummy test for now

--- packages/vue/__tests__/Authenticator.spec.ts
+++ packages/vue/__tests__/Authenticator.spec.ts
@@ -1,13 +1,5 @@
-import { shallowMount } from '@vue/test-utils';
-import { Authenticator } from '../src/components/index';
-
-describe('Authenticator', () => {
+describe('Test Setup', () => {
   it('true', () => {
     expect(true).toBeTruthy();
-  }),
-    it('Authenticator Exists', () => {
-      const wrapper = shallowMount(Authenticator);
-
-      expect(wrapper).toBeTruthy();
-    });
+  });
 });
