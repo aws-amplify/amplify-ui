ead6661f771d51ba2aeac54baf6039d379bfcfa0
Eric Clemmons <eric@smarterspam.com>
2021-08-30 15:43:15 -0500

    Generate dist/theme.css from src/theme/css/theme.scss

--- packages/ui/scripts/generateCSS.js
+++ packages/ui/scripts/generateCSS.js
@@ -5,12 +5,13 @@ const fs = require('fs-extra');
 const autoprefixer = require('autoprefixer');
 const postcss = require('postcss');
 
-const result = sass.renderSync({ file: `src/css/theme.scss` });
+const result = sass.renderSync({ file: `src/theme/css/theme.scss` });
 postcss([autoprefixer])
   .process(result.css, { from: undefined })
   .then((result) => {
     result.warnings().forEach((warn) => {
       console.warn(warn.toString());
     });
+    fs.mkdirpSync('dist');
     fs.writeFileSync(`dist/theme.css`, result.css);
   });
