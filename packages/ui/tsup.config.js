f2879fbfb5cd7b1d5d04299abffc2309ef7f9196
Eric Clemmons <eric@smarterspam.com>
2021-08-31 15:29:30 -0500

    .cjs/.mjs doesn't work with Angular's webpack4 config by default!

--- packages/ui/tsup.config.js
+++ packages/ui/tsup.config.js
@@ -5,6 +5,8 @@ module.exports = {
   dts: true,
   entryPoints: ['src/index.ts'],
   format: ['cjs', 'esm'],
+  // ! .cjs/.mjs doesn't work with Angular's webpack4 config by default!
+  legacyOutput: true,
   sourcemap: 'external',
   splitting: false,
 };
