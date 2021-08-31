ff035e632af9e26c39c72b116c3fb436c10f0c35
Chenwei Zhang <chenwz@amazon.com>
2021-08-31 16:59:41 -0700

    docs: update demo

--- docs/src/components/SelectFieldPropControls.tsx
+++ docs/src/components/SelectFieldPropControls.tsx
@@ -7,6 +7,7 @@ import {
   TextField,
 } from '@aws-amplify/ui-react';
 
+import { DemoBox } from './DemoBox';
 import { FieldLabeler } from './FieldLabeler';
 
 export interface SelectFieldPropControlsProps extends SelectFieldProps {
@@ -43,103 +44,86 @@ export const SelectFieldPropControls: SelectFieldPropControlsInterface = ({
   setVariation,
 }) => {
   return (
-    <fieldset className="p-4 border-2 border-current border-solid">
-      <legend className="font-bold p-1">Select props:</legend>
-      <Flex justifyContent="space-evenly" wrap="wrap">
-        <SelectField
-          label="size"
-          name="size"
-          id="size"
-          placeholder="default"
-          onChange={(event) =>
-            setSize(event.target.value as SelectFieldProps['size'])
-          }
-        >
-          <option value="small">small</option>
-          <option value="large">large</option>
-        </SelectField>
-        <SelectField
-          label="variation"
-          name="variation"
-          id="variation"
-          placeholder="default"
-          onChange={(event) =>
-            setVariation(event.target.value as SelectFieldProps['variation'])
-          }
-        >
-          <option value="quiet">quiet</option>
-        </SelectField>
-      </Flex>
-      <Flex justifyContent="space-evenly" wrap="wrap" className="mt-5">
-        <TextField
-          label="label"
-          name="label"
-          id="label"
-          placeholder="Specify a label"
-          onChange={(event) =>
-            setLabel(event.target.value as SelectFieldProps['label'])
-          }
+    <DemoBox primitiveName="SelectField">
+      <SelectField
+        label="size"
+        name="size"
+        id="size"
+        placeholder="default"
+        onChange={(event) =>
+          setSize(event.target.value as SelectFieldProps['size'])
+        }
+      >
+        <option value="small">small</option>
+        <option value="large">large</option>
+      </SelectField>
+      <SelectField
+        label="variation"
+        name="variation"
+        id="variation"
+        placeholder="default"
+        onChange={(event) =>
+          setVariation(event.target.value as SelectFieldProps['variation'])
+        }
+      >
+        <option value="quiet">quiet</option>
+      </SelectField>
+      <TextField
+        label="label"
+        name="label"
+        id="label"
+        placeholder="Specify a label"
+        onChange={(event) =>
+          setLabel(event.target.value as SelectFieldProps['label'])
+        }
+      />
+      <TextField
+        label="errorMessage"
+        name="errorMessage"
+        id="errorMessage"
+        placeholder="Specify error message"
+        onChange={(event) =>
+          setErrorMessage(
+            event.target.value as SelectFieldProps['errorMessage']
+          )
+        }
+      />
+      <FieldLabeler id="labelHidden">
+        <input
+          type="checkbox"
+          name="labelHidden"
+          id="labelHidden"
+          onChange={(event) => {
+            setLabelHidden(
+              Boolean(event.target.checked) as SelectFieldProps['labelHidden']
+            );
+          }}
         />
-        <TextField
-          label="errorMessage"
-          name="errorMessage"
-          id="errorMessage"
-          placeholder="Specify error message"
-          onChange={(event) =>
-            setErrorMessage(
-              event.target.value as SelectFieldProps['errorMessage']
-            )
-          }
+      </FieldLabeler>
+      <FieldLabeler id="hasError">
+        <input
+          type="checkbox"
+          name="hasError"
+          id="labelHidden"
+          onChange={(event) => {
+            setHasError(
+              Boolean(event.target.checked) as SelectFieldProps['hasError']
+            );
+          }}
         />
-      </Flex>
-      <Flex justifyContent="space-evenly" wrap="wrap" className="mt-5">
-        <FieldLabeler id="labelHidden">
-          <Flex justifyContent="center">
-            <input
-              type="checkbox"
-              name="labelHidden"
-              id="labelHidden"
-              onChange={(event) => {
-                setLabelHidden(
-                  Boolean(
-                    event.target.checked
-                  ) as SelectFieldProps['labelHidden']
-                );
-              }}
-            />
-          </Flex>
-        </FieldLabeler>
-        <FieldLabeler id="hasError">
-          <Flex justifyContent="center">
-            <input
-              type="checkbox"
-              name="hasError"
-              id="labelHidden"
-              onChange={(event) => {
-                setHasError(
-                  Boolean(event.target.checked) as SelectFieldProps['hasError']
-                );
-              }}
-            />
-          </Flex>
-        </FieldLabeler>
-        <FieldLabeler id="isDisabled">
-          <Flex justifyContent="center">
-            <input
-              type="checkbox"
-              name="isDisabled"
-              id="isDisabled"
-              onChange={(event) => {
-                setIsDisabled(
-                  Boolean(
-                    event.target.checked
-                  ) as SelectFieldProps['isDisabled']
-                );
-              }}
-            />
-          </Flex>
-        </FieldLabeler>
-      </Flex>
-    </fieldset>
+      </FieldLabeler>
+      <FieldLabeler id="isDisabled">
+        <input
+          type="checkbox"
+          name="isDisabled"
+          id="isDisabled"
+          onChange={(event) => {
+            setIsDisabled(
+              Boolean(event.target.checked) as SelectFieldProps['isDisabled']
+            );
+          }}
+        />
+      </FieldLabeler>
+    </DemoBox>
   );
 };
