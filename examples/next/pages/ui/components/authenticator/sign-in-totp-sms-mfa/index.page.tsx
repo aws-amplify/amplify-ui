51b37e27a38d6a8c936993322186d7895b36e685
Eric Clemmons <eric@smarterspam.com>
2021-08-31 12:20:54 -0500

    Swap aws-amplify-react for ui-react in sign-in-totp-sms-mfa example

--- examples/next/pages/ui/components/authenticator/sign-in-totp-sms-mfa/index.page.tsx
+++ examples/next/pages/ui/components/authenticator/sign-in-totp-sms-mfa/index.page.tsx
@@ -1,7 +1,6 @@
-import { translations } from '@aws-amplify/ui-react';
+import { Authenticator, translations } from '@aws-amplify/ui-react';
 import awsExports from '@environments/auth-with-totp-and-sms-mfa/src/aws-exports';
 import { Amplify, I18n } from 'aws-amplify';
-import { Authenticator } from 'aws-amplify-react';
 
 Amplify.configure({
   ...awsExports,
