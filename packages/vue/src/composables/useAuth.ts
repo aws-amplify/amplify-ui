98920ad71223bde4d24709ea277fe2b74db1e9a9
Erik Hanchett <ehhanche@amazon.com>
2021-09-02 15:40:45 -0700

    Ignore types for useAuth for now

--- packages/vue/src/composables/useAuth.ts
+++ packages/vue/src/composables/useAuth.ts
@@ -2,6 +2,8 @@ import { useActor } from '@xstate/vue';
 import { inject } from 'vue';
 
 export const useAuth = () => {
+  //@ts-ignore
   const service = inject('service');
+  //@ts-ignore
   return useActor(service);
 };
