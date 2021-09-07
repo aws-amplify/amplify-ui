dcd3cec5e3358fb0dc0c18a09f17635cebdbcbb2
Joe Buono <joebuono724@gmail.com>
2021-09-07 12:20:43 -0400

    Tabs component (#332)
    * basic file structure for tabs component
    
    * rough draft of tabs primitive
    
    * refactored basic implementation
    
    * experimenting with type checking
    
    * working on tabs
    
    * imported radix react tabs
    
    * basic tab styling
    
    * fixed types and added styling
    
    * added grow and disabledTabs props
    
    * basic tabs demo
    
    * tabs usage examples complete
    
    * docs mostly complete
    
    * modifying docs and styling
    
    * added intellisense comments to tabs.ts interfaces
    
    * basic testing
    
    * added a test
    
    * removed textField from tokens index.js
    
    * removed textfield from styles.scss
    
    * removed export from isTabsType util function
    
    * added select to __tests__ exports
    
    Co-authored-by: Joe Buono <joebuono@amazon.com>

--- packages/ui/src/theme/tokens/components/index.js
+++ packages/ui/src/theme/tokens/components/index.js
@@ -17,5 +17,6 @@ module.exports = {
   placeholder: require('./placeholder'),
   rating: require('./rating'),
   select: require('./select'),
+  tabs: require('./tabs'),
   text: require('./text'),
 };
