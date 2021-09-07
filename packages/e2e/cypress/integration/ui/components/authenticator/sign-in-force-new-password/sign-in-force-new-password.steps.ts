f8276324fd2816e5bf66f47ec2e76a8d5894cfbf
Shane Laymance <shane.laymance@gmail.com>
2021-09-07 14:54:55 -0700

    Update steps for sign-in-force-new-password

--- packages/e2e/cypress/integration/ui/components/authenticator/sign-in-force-new-password/sign-in-force-new-password.steps.ts
+++ packages/e2e/cypress/integration/ui/components/authenticator/sign-in-force-new-password/sign-in-force-new-password.steps.ts
@@ -1,17 +1,4 @@
-import { And, Given, Then } from 'cypress-cucumber-preprocessor/steps';
-
-Given("I'm running the example {string}", (url: string) => {
-  cy.visit(url);
-  cy.get('[data-amplify-authenticator-signin]').should('be.visible');
-});
-
-And('I type in the phone number {string}', (phone: string) => {
-  cy.findByRole('textbox', { name: /phone number/i }).type(Cypress.env(phone));
-});
-
-And('I type in the password {string}', (password: string) => {
-  cy.findByLabelText(/password/i).type(Cypress.env(password));
-});
+import { Then } from 'cypress-cucumber-preprocessor/steps';
 
 Then('I should see the Force Change Password screen', () => {
   cy.findByRole('document').contains(new RegExp('Change Password', 'i'));
