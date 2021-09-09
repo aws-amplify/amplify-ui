1690e651376f3651de147b702b3b6923931b49c3
Danny Banks <djb@amazon.com>
2021-09-09 16:10:40 -0700

    Cleaning up unused files in the listing example

--- examples/next/pages/_app.page.tsx
+++ examples/next/pages/_app.page.tsx
@@ -1,4 +1,5 @@
 import '@aws-amplify/ui-react/styles.css';
+import './examples/listings/styles.scss';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
