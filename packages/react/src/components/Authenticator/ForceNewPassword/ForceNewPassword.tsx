786aa1a878bccea5d1409f115aeddad208c0bb2d
Eddie Keller <keledwin@amazon.com>
2021-09-10 11:07:13 -0700

    initial styling for ForceNewPassword

--- packages/react/src/components/Authenticator/ForceNewPassword/ForceNewPassword.tsx
+++ packages/react/src/components/Authenticator/ForceNewPassword/ForceNewPassword.tsx
@@ -1,5 +1,10 @@
 import { I18n } from 'aws-amplify';
-import { getActorState, SignInState } from '@aws-amplify/ui';
+import {
+  getActorContext,
+  getActorState,
+  SignInContext,
+  SignInState,
+} from '@aws-amplify/ui';
 
 import { useAmplify, useAuth } from '../../../hooks';
 
@@ -8,13 +13,11 @@ export const ForceNewPassword = (): JSX.Element => {
   const {
     components: {
       Button,
-      Fieldset,
-      Footer,
+      FieldGroup,
+      Flex,
       Form,
       Heading,
-      Input,
-      Label,
-      Spacer,
+      PasswordField,
       Text,
     },
   } = useAmplify(amplifyNamespace);
@@ -22,14 +25,26 @@ export const ForceNewPassword = (): JSX.Element => {
   const [_state, send] = useAuth();
   const actorState: SignInState = getActorState(_state);
   const { remoteError } = actorState.context;
+  const { validationError } = getActorContext(_state) as SignInContext;
   const isPending = actorState.matches('forceNewPassword.pending');
 
-  const headerText = 'Change Password';
+  const headerText = I18n.get('Change Password');
+  const passwordLabel = I18n.get('Password');
+  const confirmPasswordLabel = I18n.get('Confirm Password');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    send({
+      type: 'CHANGE',
+      data: { name, value },
+    });
+  };
 
   return (
     <Form
       data-amplify-authenticator-forcenewpassword=""
       method="post"
+      onChange={handleChange}
       onSubmit={(event) => {
         event.preventDefault();
 
@@ -42,38 +57,60 @@ export const ForceNewPassword = (): JSX.Element => {
         });
       }}
     >
-      <Heading level={1}>{headerText}</Heading>
+      <Flex direction="column">
+        <Heading level={3}>{headerText}</Heading>
 
-      <Fieldset disabled={isPending}>
-        <Label data-amplify-forcenewpassword-label="">
-          <Text>{I18n.get('Change Password')}</Text>
-          <Input
-            autoComplete="password"
+        <FieldGroup direction="column" disabled={isPending}>
+          <PasswordField
+            data-amplify-password
+            placeholder={passwordLabel}
+            required
             name="password"
-            placeholder="Password"
-            required={true}
-            type="password"
+            label={passwordLabel}
+            labelHidden={true}
+            hasError={!!validationError['confirm_password']}
           />
-        </Label>
-      </Fieldset>
+          <PasswordField
+            data-amplify-confirmpassword
+            placeholder={confirmPasswordLabel}
+            required
+            name="confirm_password"
+            label={confirmPasswordLabel}
+            labelHidden={true}
+            hasError={!!validationError['confirm_password']}
+          />
 
-      <Text className="forceNewPasswordErrorText" variant="error">
-        {remoteError}
-      </Text>
+          {!!validationError['confirm_password'] && (
+            <Text variation="error">{validationError['confirm_password']}</Text>
+          )}
+        </FieldGroup>
 
-      <Footer>
-        <Button onClick={() => send({ type: 'SIGN_IN' })} type="button">
+        {!!remoteError && (
+          <Text className="forceNewPasswordErrorText" variation="error">
+            {remoteError}
+          </Text>
+        )}
+
+        <Button
+          isDisabled={isPending}
+          type="submit"
+          variation="primary"
+          isLoading={isPending}
+          loadingText={I18n.get('Changing')}
+          fontWeight="normal"
+        >
+          {I18n.get('Change Password')}
+        </Button>
+        <Button
+          onClick={() => send({ type: 'SIGN_IN' })}
+          type="button"
+          fontWeight="normal"
+          variation="link"
+          size="small"
+        >
           {I18n.get('Back to Sign In')}
         </Button>
-        <Spacer />
-        <Button isDisabled={isPending} type="submit">
-          {isPending ? (
-            <>{I18n.get('Changing')}&hellip;</>
-          ) : (
-            <>{I18n.get('Change Password')}</>
-          )}
-        </Button>
-      </Footer>
+      </Flex>
     </Form>
   );
 };
