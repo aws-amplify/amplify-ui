f3085d66aae85e1ef973dda8c6fd2f15bc79b4b6
Danny Banks <djb@amazon.com>
2021-09-13 22:20:11 -0700

    Starting tabs tests

--- packages/react/src/primitives/Tabs/__tests__/Tabs.test.tsx
+++ packages/react/src/primitives/Tabs/__tests__/Tabs.test.tsx
@@ -5,6 +5,18 @@ import { ComponentPropsToStylePropsMap } from '../../types';
 import { kebabCase } from 'lodash';
 
 describe('Tabs: ', () => {
+  it('can render custom classnames', async () => {
+    render(
+      <Tabs className="custom-classname" testId="tabsId">
+        <TabItem title="Tab 1">Tab 1</TabItem>
+      </Tabs>
+    );
+
+    const tabs = await screen.findByTestId('tabsId');
+    expect(tabs.className).toContain('custom-classname');
+    expect(tabs.className).toContain(ComponentClassNames.Tabs);
+  });
+
   it('can render any arbitrary data-* attribute', async () => {
     render(
       <Tabs data-demo="true" testId="tabsTest">
@@ -14,5 +26,20 @@ describe('Tabs: ', () => {
     );
     const tabs = await screen.findByTestId('tabsTest');
     expect(tabs.dataset['demo']).toBe('true');
+  });
+
+  describe('TabItem: ', () => {
+    it('should render the disabled', async () => {
+      render(
+        <Tabs data-demo="true" testId="tabsTest">
+          <TabItem title="Tab 1" isDisabled>
+            Tab 1
+          </TabItem>
+        </Tabs>
+      );
+
+      const tab = await screen.findByRole('tab');
+      expect(tab).toHaveAttribute('aria-disabled');
+    });
   });
 });
