32b97447e04665a6a46dd950204d689f4d20954f
Shane Laymance <shane.laymance@gmail.com>
2021-09-15 17:36:04 -0700

    Clean up vue useAliases util

--- packages/vue/src/composables/useUtils.ts
+++ packages/vue/src/composables/useUtils.ts
@@ -2,14 +2,7 @@ import { LoginMechanism, userNameAliasArray } from '@aws-amplify/ui';
 
 export const useAliases = (
   login_mechanisms: LoginMechanism[]
-): LoginMechanism[] => {
-  if (login_mechanisms) {
-    if (login_mechanisms.length === 1 && login_mechanisms[0] === 'username') {
-      return [...userNameAliasArray];
-    } else {
-      return login_mechanisms;
-    }
-  } else {
-    return [...userNameAliasArray];
-  }
-};
+): LoginMechanism[] =>
+  login_mechanisms?.length > 1 && login_mechanisms[0] !== 'username'
+    ? login_mechanisms
+    : [...userNameAliasArray];
