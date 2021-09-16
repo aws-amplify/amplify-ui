e84ec1db1c81705c7c3269129b478cdd1e24628d
Shane Laymance <shane.laymance@gmail.com>
2021-09-15 17:38:26 -0700

    Add description for useAliases

--- packages/vue/src/composables/useUtils.ts
+++ packages/vue/src/composables/useUtils.ts
@@ -1,8 +1,12 @@
 import { LoginMechanism, userNameAliasArray } from '@aws-amplify/ui';
 
+/**
+ * If 'username' is the only login mechanism, then we ask for a user's
+ * email and phone number during sign up as well.
+ */
 export const useAliases = (
   login_mechanisms: LoginMechanism[]
 ): LoginMechanism[] =>
-  login_mechanisms?.length > 1 && login_mechanisms[0] !== 'username'
-    ? login_mechanisms
-    : [...userNameAliasArray];
+  login_mechanisms?.length === 1 && login_mechanisms[0] === 'username'
+    ? [...userNameAliasArray]
+    : login_mechanisms;
