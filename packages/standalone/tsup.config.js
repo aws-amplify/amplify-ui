355cbaa886eb961076b5dbb5986f6e1ecc3a192c
Eric Clemmons <eric@smarterspam.com>
2021-09-17 13:25:17 -0500

    Fix reference to aws-amplify

--- packages/standalone/tsup.config.js
+++ packages/standalone/tsup.config.js
@@ -5,7 +5,7 @@ module.exports = {
   dts: true,
   entryPoints: ['src/index.tsx', 'src/Authenticator'],
   // `aws-amplify` is external, but sub-dependencies weren't automatically externalized ("require" statements were included)
-  external: ['`aws-amplify'],
+  external: ['aws-amplify'],
   format: ['cjs', 'esm'],
   // ! .cjs/.mjs doesn't work with Angular's webpack4 config by default!
   legacyOutput: true,
