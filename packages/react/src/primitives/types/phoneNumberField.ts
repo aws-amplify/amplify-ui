d8cd079ec359679d59fa3ca1b4df8ac45fc9a120
Shane Laymance <shane.laymance@gmail.com>
2021-09-21 09:31:11 -0700

    Use default autocomplete prop

--- packages/react/src/primitives/types/phoneNumberField.ts
+++ packages/react/src/primitives/types/phoneNumberField.ts
@@ -1,7 +1,6 @@
 import { TextFieldProps } from './textField';
 
 export interface PhoneNumberFieldProps extends TextFieldProps {
-  autoComplete?: 'username';
   defaultCountryCode: string;
   type?: 'tel';
 }
