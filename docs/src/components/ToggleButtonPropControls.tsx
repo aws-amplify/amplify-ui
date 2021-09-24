e6dbe1bb21162d0e24a85e43c492009ed1ce0e7d
Chenwei Zhang <chenwz@amazon.com>
2021-09-24 12:31:35 -0700

    docs(wip): adding docs

---
+++ docs/src/components/ToggleButtonPropControls.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { ToggleButtonProps, SelectField } from '@aws-amplify/ui-react';
+import { FieldLabeler } from './FieldLabeler';
+import { DemoBox } from './DemoBox';
+
+export interface ToggleButtonPropControlsProps extends ToggleButtonProps {
+  setIsDisabled: (
+    value: React.SetStateAction<ToggleButtonProps['isDisabled']>
+  ) => void;
+  setSize: (value: React.SetStateAction<ToggleButtonProps['size']>) => void;
+  setVariation: (
+    value: React.SetStateAction<ToggleButtonProps['variation']>
+  ) => void;
+}
+
+export interface ToggleButtonPropControlsInterface {
+  (props: ToggleButtonPropControlsProps): JSX.Element;
+}
+
+export const ToggleButtonPropControls: ToggleButtonPropControlsInterface = ({
+  isDisabled,
+  setIsDisabled,
+  size,
+  setSize,
+  variation,
+  setVariation,
+}) => {
+  return (
+    <DemoBox primitiveName="ToggleButton">
+      <SelectField
+        label="variation"
+        value={variation}
+        placeholder="default"
+        onChange={(event) =>
+          setVariation(event.target.value as ToggleButtonProps['variation'])
+        }
+      >
+        <option value="primary">primary</option>
+        <option value="link">link</option>
+      </SelectField>
+
+      <SelectField
+        value={size}
+        label="size"
+        onChange={(event) =>
+          setSize(event.target.value as ToggleButtonProps['size'])
+        }
+      >
+        <option value="small">small</option>
+        <option value="medium">medium</option>
+        <option value="large">large</option>
+      </SelectField>
+      <FieldLabeler id="isDisabled">
+        <input
+          type="checkbox"
+          checked={isDisabled}
+          onChange={(event) => setIsDisabled(event.target.checked)}
+        />
+      </FieldLabeler>
+    </DemoBox>
+  );
+};
---
+++ docs/src/pages/ui/primitives/toggleButton/demo.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+import { Flex, ToggleButton } from '@aws-amplify/ui-react';
+
+import { Example } from '@/components/Example';
+import { ToggleButtonPropControls } from '@/components/ToggleButtonPropControls';
+import { useToggleButtonProps } from '@/components/useToggleButtonProps';
+export const Demo = () => {
+  const props = useToggleButtonProps({
+    isDisabled: false,
+    size: 'medium',
+    variation: 'default',
+  });
+  return (
+    <Flex direction="column">
+      <ToggleButtonPropControls {...props} />
+      <Example>
+        <ToggleButton
+          isDisabled={props.isDisabled}
+          size={props.size}
+          variation={props.variation}
+        >
+          Press me!
+        </ToggleButton>
+      </Example>
+    </Flex>
+  );
+};
+
+export const ControlledToggleButton = () => {
+  const [isSelected, setIsSelected] = useState(false);
+  return (
+    <ToggleButton
+      isSelected={isSelected}
+      onChange={() => setIsSelected(!isSelected)}
+      isDisabled
+    >
+      demo
+    </ToggleButton>
+  );
+};
