600857b7a33ed1a2f66c55fa539c76cb6a2afc70
Chenwei Zhang <chenwz@amazon.com>
2021-09-27 11:48:58 -0700

    docs(wip): adding ToggleButtonGroup demo

--- docs/src/pages/ui/primitives/toggleButton/demo.tsx
+++ docs/src/pages/ui/primitives/toggleButton/demo.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Flex, ToggleButton } from '@aws-amplify/ui-react';
+import { Flex, ToggleButton, ToggleButtonGroup } from '@aws-amplify/ui-react';
 
 import { Example } from '@/components/Example';
 import { ToggleButtonPropControls } from '@/components/ToggleButtonPropControls';
@@ -37,5 +37,39 @@ export const ControlledToggleButton = () => {
     >
       demo
     </ToggleButton>
+  );
+};
+
+export const ToggleButtonGroupDemo = () => {
+  const [multipleValue, setMultipleValue] = useState(['1', '2', '3']);
+  const [exclusiveValue, setExclusiveValue] = useState('1');
+  return (
+    <Example>
+      <p>Multiple</p>
+      <ToggleButtonGroup
+        variation="primary"
+        value={multipleValue}
+        onChange={(e, value: string[]) => setMultipleValue(value)}
+      >
+        <ToggleButton value="1">1</ToggleButton>
+        <ToggleButton value="2">2</ToggleButton>
+        <ToggleButton value="3">3</ToggleButton>
+        <ToggleButton value="4">4</ToggleButton>
+        <ToggleButton value="5">5</ToggleButton>
+      </ToggleButtonGroup>
+      <p>Exclusive</p>
+      <ToggleButtonGroup
+        variation="primary"
+        value={exclusiveValue}
+        isExclusive
+        onChange={(e, value: string) => setExclusiveValue(value)}
+      >
+        <ToggleButton value="1">1</ToggleButton>
+        <ToggleButton value="2">2</ToggleButton>
+        <ToggleButton value="3">3</ToggleButton>
+        <ToggleButton value="4">4</ToggleButton>
+        <ToggleButton value="5">5</ToggleButton>
+      </ToggleButtonGroup>
+    </Example>
   );
 };
