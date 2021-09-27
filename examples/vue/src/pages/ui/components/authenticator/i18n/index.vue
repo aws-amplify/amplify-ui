66cd2d8d8304e2704dda0162dbbc513995365168
Erik Hanchett <ehhanche@amazon.com>
2021-09-27 12:11:28 -0700

    Added new route for i18n for Vue

---
+++ examples/vue/src/pages/ui/components/authenticator/i18n/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Authenticator } from '@aws-amplify/ui-vue';
+import { Amplify, I18n } from 'aws-amplify';
+import '@aws-amplify/ui-vue/styles.css';
+import aws_exports from '@environments/auth-with-username-no-attributes/src/aws-exports';
+
+I18n.setLanguage('fr');
+
+Amplify.configure(aws_exports);
+</script>
+<template>
+  <authenticator>
+    <template v-slot="{ user, send }">
+      <h1 class="text-6xl mb-10">Hello {{ user.username }}!</h1>
+      <button
+        className="px-2 bg-white rounded shadow"
+        @click="send('SIGN_OUT')"
+      >
+        Sign Out
+      </button>
+    </template>
+  </authenticator>
+</template>
