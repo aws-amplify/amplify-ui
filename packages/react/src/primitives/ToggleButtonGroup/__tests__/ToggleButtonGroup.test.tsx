4a2f26c57675b63901dea963821ad2c5901af750
Chenwei Zhang <chenwz@amazon.com>
2021-09-27 15:23:20 -0700

    test(wip): adding unit tests for ToggleButtonGroup

---
+++ packages/react/src/primitives/ToggleButtonGroup/__tests__/ToggleButtonGroup.test.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { ComponentClassNames } from '../../shared';
+import { ToggleButtonGroup } from '../ToggleButtonGroup';
+
+describe('ToggleButtonGroup test suite', () => {});
