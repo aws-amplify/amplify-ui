7fefc338147b8f0e985e3040b35de4e30ad11f84
Eric Clemmons <eric@smarterspam.com>
2021-09-28 10:50:07 -0500

    Update Vue i18n page to use ja not fr

--- examples/vue/src/pages/ui/components/authenticator/i18n/index.vue
+++ examples/vue/src/pages/ui/components/authenticator/i18n/index.vue
@@ -4,14 +4,14 @@ import { Amplify, I18n } from 'aws-amplify';
 import '@aws-amplify/ui-vue/styles.css';
 import aws_exports from '@environments/auth-with-username-no-attributes/src/aws-exports';
 
-I18n.setLanguage('fr');
+I18n.setLanguage('ja');
 
 Amplify.configure(aws_exports);
 </script>
 <template>
   <authenticator>
     <template v-slot="{ user, send }">
-      <h1 class="text-6xl mb-10">Hello {{ user.username }}!</h1>
+      <h1 class="mb-10 text-6xl">Hello {{ user.username }}!</h1>
       <button
         className="px-2 bg-white rounded shadow"
         @click="send('SIGN_OUT')"
