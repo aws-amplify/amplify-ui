c5911b1246b30ed53e685b478ad63ebbfd071350
Chenwei Zhang <chenwz@amazon.com>
2021-09-28 15:04:11 -0700

    docs: adding docs

--- docs/src/pages/ui/primitives/toggleButton/demo.tsx
+++ docs/src/pages/ui/primitives/toggleButton/demo.tsx
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
 
-import { Flex, ToggleButton, ToggleButtonGroup } from '@aws-amplify/ui-react';
+import {
+  Flex,
+  IconFormatAlignCenter,
+  IconFormatAlignJustify,
+  IconFormatAlignLeft,
+  IconFormatAlignRight,
+  IconFormatBold,
+  IconFormatColorFill,
+  IconFormatItalic,
+  IconFormatUnderlined,
+  ToggleButton,
+  ToggleButtonGroup,
+} from '@aws-amplify/ui-react';
 
 import { Example } from '@/components/Example';
 import { ToggleButtonPropControls } from '@/components/ToggleButtonPropControls';
@@ -33,42 +45,62 @@ export const ControlledToggleButton = () => {
     <ToggleButton
       isSelected={isSelected}
       onChange={() => setIsSelected(!isSelected)}
-      isDisabled
     >
-      demo
+      Press me!
     </ToggleButton>
   );
 };
 
-export const ToggleButtonGroupDemo = () => {
-  const [multipleValue, setMultipleValue] = useState(['1', '2', '3']);
-  const [exclusiveValue, setExclusiveValue] = useState('1');
+export const MultipleSelectionDemo = () => {
+  const [multipleValue, setMultipleValue] = useState([
+    'bold',
+    'italic',
+    'underlined',
+  ]);
   return (
     <Example>
-      <p>Multiple</p>
       <ToggleButtonGroup
-        variation="primary"
         value={multipleValue}
         onChange={(e, value: string[]) => setMultipleValue(value)}
       >
-        <ToggleButton value="1">1</ToggleButton>
-        <ToggleButton value="2">2</ToggleButton>
-        <ToggleButton value="3">3</ToggleButton>
-        <ToggleButton value="4">4</ToggleButton>
-        <ToggleButton value="5">5</ToggleButton>
+        <ToggleButton value="bold">
+          <IconFormatBold />
+        </ToggleButton>
+        <ToggleButton value="italic">
+          <IconFormatItalic />
+        </ToggleButton>
+        <ToggleButton value="underlined">
+          <IconFormatUnderlined />
+        </ToggleButton>
+        <ToggleButton value="color-fill">
+          <IconFormatColorFill />
+        </ToggleButton>
       </ToggleButtonGroup>
-      <p>Exclusive</p>
+    </Example>
+  );
+};
+
+export const ExclusiveSelectionDemo = () => {
+  const [exclusiveValue, setExclusiveValue] = useState('align-left');
+  return (
+    <Example>
       <ToggleButtonGroup
-        variation="primary"
         value={exclusiveValue}
         isExclusive
         onChange={(e, value: string) => setExclusiveValue(value)}
       >
-        <ToggleButton value="1">1</ToggleButton>
-        <ToggleButton value="2">2</ToggleButton>
-        <ToggleButton value="3">3</ToggleButton>
-        <ToggleButton value="4">4</ToggleButton>
-        <ToggleButton value="5">5</ToggleButton>
+        <ToggleButton value="align-left">
+          <IconFormatAlignLeft />
+        </ToggleButton>
+        <ToggleButton value="align-center">
+          <IconFormatAlignCenter />
+        </ToggleButton>
+        <ToggleButton value="align-right">
+          <IconFormatAlignRight />
+        </ToggleButton>
+        <ToggleButton value="align-justify">
+          <IconFormatAlignJustify />
+        </ToggleButton>
       </ToggleButtonGroup>
     </Example>
   );
