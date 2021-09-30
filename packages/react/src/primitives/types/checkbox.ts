db2d98ad8e0322758c8bb652653fbf9cab7617ff
Chenwei Zhang <chenwz@amazon.com>
2021-09-30 15:58:59 -0700

    chore: updating checkbox type

--- packages/react/src/primitives/types/checkbox.ts
+++ packages/react/src/primitives/types/checkbox.ts
@@ -1,18 +1,20 @@
 import { FlexProps } from './flex';
 import { InputProps } from './input';
+import { FieldProps } from './field';
 
 export interface CheckboxProps extends FlexProps, InputProps {
   /**
    * The label text
    */
-  children: string;
+  children: FieldProps['label'];
   /**
    * The name of the input field in a checkbox (Useful for form submission).
    */
   name: string;
 
   /**
-   * The value of the checkbox.
+   * The submitted value when checked
+   * Shows on form submission as key pair `name:value`
    */
   value: string;
 }
