aa8bb31ec9f97016c96b85d9ce8b92209cda6992
Erik Hanchett <ehhanche@amazon.com>
2021-09-30 10:12:49 -0700

    Fixed vue look and feel

--- packages/vue/src/components/alias-control.vue
+++ packages/vue/src/components/alias-control.vue
@@ -1,44 +1,8 @@
 <template>
   <base-wrapper
-    class="amplify-flex amplify-field amplify-textfield"
-    style="flex-direction: column"
-    v-if="name === 'phone_number'"
-  >
-    <base-label
-      :for="'amplify-field-' + randomPhone"
-      class="amplify-label sr-only"
-      v-bind="$attrs"
-    >
-      {{ 'Country Code' }}
-    </base-label>
-    <base-wrapper class="amplify-select__wrapper">
-      <base-select
-        class="amplify-select amplify-field-group__control"
-        :id="'amplify-field-' + randomPhone"
-        aria-label="country code"
-        name="country_code"
-        :options="dialCodes"
-        :select-value="defaultDialCode"
-      >
-      </base-select>
-      <base-wrapper
-        class="amplify-flex amplify-select__icon-wrapper"
-        style="align-items: center; justify-content: center"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          data-size="large"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          class="amplify-icon"
-        >
-          <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
-        </svg>
-      </base-wrapper>
-    </base-wrapper>
-  </base-wrapper>
-  <base-wrapper
-    class="amplify-flex amplify-field amplify-textfield"
+    class="
+      amplify-flex amplify-field amplify-textfield amplify-phonenumberfield
+    "
     style="flex-direction: column"
   >
     <base-label
@@ -48,18 +12,68 @@
     >
       {{ label }}
     </base-label>
-    <base-wrapper class="amplify-flex">
-      <base-input
-        class="amplify-input amplify-field-group__control"
-        aria-invalid="false"
-        :id="'amplify-field-' + random"
-        :aria-labelledby="'amplify-field-' + random"
-        autocomplete="username"
-        :name="name"
-        required
-        :type="inputAttributes[name].type"
-        :placeholder="placeholder"
-      ></base-input>
+    <base-wrapper class="amplify-flex amplify-field-group">
+      <base-wrapper class="amplify-field-group__outer-start">
+        <!--select drop down-->
+        <base-wrapper
+          class="
+            amplify-flex
+            amplify-field
+            amplify-selectfield
+            amplify-countrycodeselect
+          "
+          style="flex-direction: column"
+          v-if="name === 'phone_number'"
+        >
+          <base-label
+            :for="'amplify-field-' + randomPhone"
+            class="amplify-label sr-only"
+            v-bind="$attrs"
+          >
+            {{ 'Country Code' }}
+          </base-label>
+          <base-wrapper class="amplify-select__wrapper">
+            <base-select
+              class="amplify-select amplify-field-group__control"
+              :id="'amplify-field-' + randomPhone"
+              autocomplete="tel-country-code"
+              aria-label="country code"
+              name="country_code"
+              :options="dialCodes"
+              :select-value="defaultDialCode"
+            >
+            </base-select>
+            <base-wrapper
+              class="amplify-flex amplify-select__icon-wrapper"
+              style="align-items: center; justify-content: center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                data-size="large"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                class="amplify-icon"
+              >
+                <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+              </svg>
+            </base-wrapper>
+          </base-wrapper>
+        </base-wrapper>
+      </base-wrapper>
+      <base-wrapper class="amplify-field-group__field-wrapper">
+        <!--Phone input-->
+        <base-input
+          class="amplify-input amplify-field-group__control"
+          aria-invalid="false"
+          :id="'amplify-field-' + random"
+          :aria-labelledby="'amplify-field-' + random"
+          autocomplete="username"
+          :name="name"
+          required
+          :type="inputAttributes[name].type"
+          :placeholder="placeholder"
+        ></base-input>
+      </base-wrapper>
     </base-wrapper>
   </base-wrapper>
 </template>
