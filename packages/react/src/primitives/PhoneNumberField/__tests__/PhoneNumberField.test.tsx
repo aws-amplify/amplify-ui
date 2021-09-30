29ff3ca3a12fb31063c0bc6f62378718fa575b1d
Shane Laymance <shane.laymance@gmail.com>
2021-09-29 17:42:25 -0700

    Improve unit tests

--- packages/react/src/primitives/PhoneNumberField/__tests__/PhoneNumberField.test.tsx
+++ packages/react/src/primitives/PhoneNumberField/__tests__/PhoneNumberField.test.tsx
@@ -1,62 +1,128 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { PhoneNumberField } from '../PhoneNumberField';
+import { PhoneNumberFieldProps } from '../../types';
+import { ComponentClassNames } from '../../shared/constants';
 
 describe('PhoneNumberField primitive', () => {
-  it('should render a country code selector', async () => {
-    render(<PhoneNumberField defaultCountryCode="+1" label="Phone Number" />);
+  const setup = async ({
+    defaultCountryCode = '+1',
+    label = 'Phone Number',
+    ...rest
+  }: Partial<PhoneNumberFieldProps>) => {
+    render(
+      <PhoneNumberField
+        defaultCountryCode={defaultCountryCode}
+        label={label}
+        {...rest}
+      />
+    );
 
-    const $countryCodeSelector = await screen.findByRole('combobox', {
-      name: /country code/i,
-    });
+    return {
+      $phoneInput: await screen.findByRole('textbox', {
+        name: /phone number/i,
+      }),
+      $countryCodeSelector: await screen.findByRole('combobox', {
+        name: /country code/i,
+      }),
+    };
+  };
 
-    expect($countryCodeSelector).toBe;
+  it('should render a country code selector with an accessible role', async () => {
+    const { $countryCodeSelector } = await setup({});
+
+    expect($countryCodeSelector).toBeDefined();
   });
 
-  it('should render a phone input field', async () => {
-    render(<PhoneNumberField defaultCountryCode="+1" label="Phone Number" />);
+  it('should render a country code selector with an accessible label', async () => {
+    await setup({});
+    const $countryCodeSelector = await screen.findByLabelText(/country code/i);
 
-    const $phoneInput = await screen.findByRole('textbox', {
-      name: /phone number/i,
-    });
+    expect($countryCodeSelector).toBeDefined();
+  });
 
-    expect($phoneInput).toBe;
+  it('should render a phone input field with an accessible role', async () => {
+    const { $phoneInput } = await setup({});
+
+    expect($phoneInput).toBeDefined();
+  });
+
+  it('should render a phone input field with an accessible role', async () => {
+    await setup({});
+    const $phoneInput = await screen.findByLabelText(/phone number/i);
+
+    expect($phoneInput).toBeDefined();
   });
 
   it('should use a specified defaultCountryCode', async () => {
     const defaultCountryCode = '+7';
-
-    render(
-      <PhoneNumberField
-        defaultCountryCode={defaultCountryCode}
-        label="Phone Number"
-      />
-    );
-
-    const $countryCodeSelector = await screen.findByRole('combobox', {
-      name: /country code/i,
-    });
+    const { $countryCodeSelector } = await setup({ defaultCountryCode });
 
     expect($countryCodeSelector).toHaveValue(defaultCountryCode);
   });
 
   it('should always use type "tel"', async () => {
-    render(<PhoneNumberField defaultCountryCode="+1" label="Phone Number" />);
+    const { $phoneInput } = await setup({});
 
-    const $phoneInput = await screen.findByRole('textbox', {
-      name: /phone number/i,
-    });
-
-    expect($phoneInput.getAttribute('type')).toBe('tel');
+    expect($phoneInput).toHaveAttribute('type', 'tel');
   });
 
-  it('should always have "username" as the autocomplete attribute', async () => {
-    render(<PhoneNumberField defaultCountryCode="+1" label="Phone Number" />);
+  it('should have "tel-national" as the default autocomplete attribute', async () => {
+    const { $phoneInput } = await setup({});
 
-    const $phoneInput = await screen.findByRole('textbox', {
-      name: /phone number/i,
+    expect($phoneInput).toHaveAttribute('autocomplete', 'tel-national');
+  });
+
+  it('should render classname for PhoneNumberField', async () => {
+    const className = 'test-class-name';
+    const testId = 'PhoneNumberFieldTestId';
+    await setup({ className, testId });
+    const $phoneInput = await screen.findByTestId(testId);
+
+    expect($phoneInput).toHaveClass(className);
+    expect($phoneInput).toHaveClass(ComponentClassNames.PhoneNumberField);
+  });
+
+  it('should be able to set a size', async () => {
+    const { $countryCodeSelector, $phoneInput } = await setup({
+      size: 'large',
     });
 
-    expect($phoneInput.getAttribute('autocomplete')).toBe('username');
+    expect($phoneInput).toHaveAttribute('data-size', 'large');
+    expect($countryCodeSelector).toHaveAttribute('data-size', 'large');
+  });
+
+  it('should be able to set a variation', async () => {
+    const { $countryCodeSelector, $phoneInput } = await setup({
+      variation: 'quiet',
+    });
+
+    expect($phoneInput).toHaveAttribute('data-variation', 'quiet');
+    expect($countryCodeSelector).toHaveAttribute('data-variation', 'quiet');
+  });
+
+  it('should fire onChange handler when phone input field is changed', async () => {
+    const onChange = jest.fn();
+    const { $phoneInput } = await setup({ onChange });
+    userEvent.type($phoneInput, '1');
+
+    expect(onChange).toHaveBeenCalled();
+  });
+
+  it('should fire onInput handler when phone input field is changed', async () => {
+    const onInput = jest.fn();
+    const { $phoneInput } = await setup({ onInput });
+    userEvent.type($phoneInput, '1');
+
+    expect(onInput).toHaveBeenCalled();
+  });
+
+  it('should fire onCountryCodeChange handler when phone input field is changed', async () => {
+    const onCountryCodeChange = jest.fn();
+    const { $countryCodeSelector } = await setup({ onCountryCodeChange });
+    userEvent.selectOptions($countryCodeSelector, '+7');
+
+    expect(onCountryCodeChange).toHaveBeenCalled();
   });
 });
