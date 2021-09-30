20a65895f057b393b14ef25f417d2b2a6c845be0
Eric Clemmons <eric@smarterspam.com>
2021-09-30 16:45:41 -0500

    Fix Vue templates to not use : binding

--- examples/vue/src/pages/ui/components/authenticator/sign-up-with-username/index.vue
+++ examples/vue/src/pages/ui/components/authenticator/sign-up-with-username/index.vue
@@ -8,7 +8,7 @@ Amplify.configure(aws_exports);
 </script>
 
 <template>
-  <authenticator :initial-state="signUp" :login-mechanisms="['username']">
+  <authenticator initial-state="signUp" :login-mechanisms="['username']">
     <template v-slot="{ user, signOut }">
       <h1>Hello {{ user.username }}!</h1>
       <button @click="signOut">Sign Out</button>
