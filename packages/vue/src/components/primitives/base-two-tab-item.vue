e008b59100abfefb73f0d34b70347f8914338ca1
Eric Clemmons <eric@smarterspam.com>
2021-10-04 17:25:48 -0500

    Update Vue Authenticator to use UI styles

--- packages/vue/src/components/primitives/base-two-tab-item.vue
+++ packages/vue/src/components/primitives/base-two-tab-item.vue
@@ -1,44 +1,21 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import useSelect from '../../composables/useSelect';
-
-const { label, firstTab, id } = withDefaults(
+const { active, id, label } = withDefaults(
   defineProps<{
     label: string;
     id: number;
-    firstTab?: boolean;
+    active?: boolean;
   }>(),
   {
-    firstTab: false,
+    active: false,
   }
 );
-
-const { active } = useSelect;
-
-const myActive = ref(firstTab);
-
-/**
- * Toggle active tab from useSelect composable
- **/
-
-watch(
-  () => active.value,
-  () => {
-    myActive.value = !myActive.value;
-  }
-);
-
-function toggleTabs() {
-  active.value = !active.value;
-}
 </script>
 
 <template>
   <base-wrapper
-    @click="toggleTabs()"
-    :tabindex="!myActive ? 0 : -1"
-    :data-state="!myActive ? 'active' : 'inactive'"
-    :aria-selected="!active ? 'true' : 'false'"
+    :tabindex="active ? 0 : -1"
+    :data-state="active ? 'active' : 'inactive'"
+    :aria-selected="active ? 'true' : 'false'"
     :aria-controls="`radix-id-${id}-8-content-0`"
     :id="`radix-id-${id}-8-trigger-0`"
     data-orientation="horizontal"
