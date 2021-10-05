a01c5559e06cc9edcaaeffefccc81b96f7421587
Eric Clemmons <eric@smarterspam.com>
2021-10-05 15:18:51 -0500

    <AmplifyProvider components> are optional

--- packages/react/src/components/AmplifyProvider/index.tsx
+++ packages/react/src/components/AmplifyProvider/index.tsx
@@ -6,7 +6,7 @@ import { defaultTheme, defaultCSSVariables, Theme } from '../../theming';
 
 interface AmplifyProviderProps {
   children: ReactNode;
-  components: Record<string, ReactNode>;
+  components?: Record<string, ReactNode>;
   theming?: { theme: Theme; CSSVariables: {} };
 }
 export function AmplifyProvider({
@@ -15,12 +15,7 @@ export function AmplifyProvider({
   theming = { theme: defaultTheme, CSSVariables: defaultCSSVariables },
 }: AmplifyProviderProps) {
   return (
-    <AmplifyContext.Provider
-      value={{
-        components,
-        theming,
-      }}
-    >
+    <AmplifyContext.Provider value={{ components, theming }}>
       <IdProvider>
         <div data-amplify-theme="">{children}</div>
       </IdProvider>
