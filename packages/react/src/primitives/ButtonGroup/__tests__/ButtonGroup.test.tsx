a540ae916b2bddf43123b86dfeb2c97e7bc3780e
Chenwei Zhang <chenwz@amazon.com>
2021-10-05 15:43:32 -0700

    test: adding unit tests

--- packages/react/src/primitives/ButtonGroup/__tests__/ButtonGroup.test.tsx
+++ packages/react/src/primitives/ButtonGroup/__tests__/ButtonGroup.test.tsx
@@ -1,3 +1,55 @@
 import { render, screen } from '@testing-library/react';
 
-describe('ButtonGroup: ', () => {});
+import { Button } from '../../Button';
+import { ButtonGroup } from '../ButtonGroup';
+import { ButtonGroupProps } from '../../types';
+import { ComponentClassNames } from '../../shared/constants';
+import {
+  testFlexProps,
+  expectFlexStyleProps,
+} from '../../Flex/__tests__/Flex.test';
+
+describe('ButtonGroup: ', () => {
+  const getButtonGroup = (props: Partial<ButtonGroupProps>) => {
+    return (
+      <ButtonGroup {...props}>
+        <Button>Button 1</Button>
+        <Button>Button 2</Button>
+        <Button>Button 3</Button>
+      </ButtonGroup>
+    );
+  };
+  it('should render classname and custom classname', async () => {
+    const className = 'class-test';
+    render(getButtonGroup({ className }));
+
+    const buttonGroup = await screen.findByRole('group');
+    expect(buttonGroup).toHaveClass(ComponentClassNames.ButtonGroup, className);
+  });
+
+  it('should render all flex style props', async () => {
+    render(getButtonGroup(testFlexProps));
+    const buttonGroup = await screen.findByRole('group');
+    expectFlexStyleProps(buttonGroup);
+  });
+
+  it('should set size for each child button correctly', async () => {
+    const size = 'large';
+    render(getButtonGroup({ size }));
+
+    const buttons = await screen.findAllByRole('button');
+    expect(buttons[0]).toHaveAttribute('data-size', size);
+    expect(buttons[1]).toHaveAttribute('data-size', size);
+    expect(buttons[2]).toHaveAttribute('data-size', size);
+  });
+
+  it('should set variation for each child button correctly', async () => {
+    const variation = 'primary';
+    render(getButtonGroup({ variation }));
+
+    const buttons = await screen.findAllByRole('button');
+    expect(buttons[0]).toHaveAttribute('data-variation', variation);
+    expect(buttons[1]).toHaveAttribute('data-variation', variation);
+    expect(buttons[2]).toHaveAttribute('data-variation', variation);
+  });
+});
--- packages/react/src/primitives/types/buttonGroup.ts
+++ packages/react/src/primitives/types/buttonGroup.ts
@@ -2,8 +2,11 @@ import { AriaProps } from './base';
 import { BaseStyleProps } from './style';
 import { ButtonProps } from './button';
 import { FlexProps } from './flex';
+import React from 'react';
 export interface ButtonGroupProps
   extends AriaProps,
     BaseStyleProps,
     FlexProps,
-    Pick<ButtonProps, 'size' | 'variation'> {}
+    Pick<ButtonProps, 'size' | 'variation'> {
+  children: React.ReactNode;
+}
