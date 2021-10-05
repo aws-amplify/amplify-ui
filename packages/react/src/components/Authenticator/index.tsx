b6e953a9651cd5777bf6a0720a78dd0d63fc885e
Eric Clemmons <eric@smarterspam.com>
2021-10-05 17:01:20 -0500

    Authenticator Screen (#466)
    Co-authored-by: wlee221 <wlee221@live.unc.edu>

--- packages/react/src/components/Authenticator/index.tsx
+++ packages/react/src/components/Authenticator/index.tsx
@@ -8,7 +8,8 @@ import {
 import { useMachine } from '@xstate/react';
 import { I18n } from 'aws-amplify';
 import * as React from 'react';
-import { useAmplify } from '../../hooks';
+import { View } from '../../primitives';
+import { useTheming } from '../../theming';
 import { AuthenticatorContext } from './AuthenticatorContext';
 import { ConfirmSignIn } from './ConfirmSignIn';
 import { ConfirmSignUp } from './ConfirmSignUp';
@@ -48,52 +49,44 @@ export function Authenticator({
     return children(facade);
   }
 
-  const {
-    components: {
-      // @ts-ignore How to tell the context that this may exist for this scope?
-      ConfirmSignUp = Authenticator.ConfirmSignUp,
-      // @ts-ignore How to tell the context that this may exist for this scope?
-      SignIn = Authenticator.SignIn,
-      // @ts-ignore How to tell the context that this may exist for this scope?
-      SignUp = Authenticator.SignUp,
-      Wrapper,
-    },
-  } = useAmplify('Authenticator');
-
   const actorState = getActorState(state);
 
   return (
     <AuthenticatorContext.Provider value={service}>
-      <Wrapper className={className} data-amplify-authenticator="">
-        {(() => {
-          switch (true) {
-            case state.matches('authenticate'):
-              return null;
-            case actorState?.matches('confirmSignUp'):
-              return <ConfirmSignUp />;
-            case actorState?.matches('confirmSignIn'):
-              return <ConfirmSignIn />;
-            case actorState?.matches('setupTOTP'):
-              return <SetupTOTP />;
-            case actorState?.matches('signIn'):
-            case actorState?.matches('signUp'):
-              return <SignInSignUpTabs />;
-            case actorState?.matches('forceNewPassword'):
-              return <ForceNewPassword />;
-            case actorState?.matches('resetPassword'):
-              return <ResetPassword />;
-            case actorState?.matches('confirmResetPassword'):
-              return <ConfirmResetPassword />;
-            case actorState?.matches('verifyUser'):
-              return <VerifyUser />;
-            case actorState?.matches('confirmVerifyUser'):
-              return <ConfirmVerifyUser />;
-            default:
-              console.warn('Unhandled Auth state', state.value);
-              return null;
-          }
-        })()}
-      </Wrapper>
+      <View className={className} data-amplify-authenticator="">
+        <View data-amplify-modal="">
+          <View data-amplify-container="">
+            {(() => {
+              switch (true) {
+                case state.matches('authenticate'):
+                  return null;
+                case actorState?.matches('confirmSignUp'):
+                  return <ConfirmSignUp />;
+                case actorState?.matches('confirmSignIn'):
+                  return <ConfirmSignIn />;
+                case actorState?.matches('setupTOTP'):
+                  return <SetupTOTP />;
+                case actorState?.matches('signIn'):
+                case actorState?.matches('signUp'):
+                  return <SignInSignUpTabs />;
+                case actorState?.matches('forceNewPassword'):
+                  return <ForceNewPassword />;
+                case actorState?.matches('resetPassword'):
+                  return <ResetPassword />;
+                case actorState?.matches('confirmResetPassword'):
+                  return <ConfirmResetPassword />;
+                case actorState?.matches('verifyUser'):
+                  return <VerifyUser />;
+                case actorState?.matches('confirmVerifyUser'):
+                  return <ConfirmVerifyUser />;
+                default:
+                  console.warn('Unhandled Auth state', state.value);
+                  return null;
+              }
+            })()}
+          </View>
+        </View>
+      </View>
     </AuthenticatorContext.Provider>
   );
 }
