b9e236194c786c6a83395498193877fcf16eb523
Eric Clemmons <eric@smarterspam.com>
2021-10-05 15:19:05 -0500

    Remove console.log

--- examples/next/pages/_app.page.tsx
+++ examples/next/pages/_app.page.tsx
@@ -1,7 +1,6 @@
 import { AmplifyProvider } from '@aws-amplify/ui-react';
 
 export default function ExampleApp({ Component, pageProps }) {
-  console.log('Render');
   return (
     <AmplifyProvider>
       <Component {...pageProps} />
