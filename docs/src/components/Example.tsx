91180a14a7006c91bfe7ce62be86ed1c1761c65a
Eric Clemmons <eric@smarterspam.com>
2021-10-12 13:20:35 -0500

    Always include .example class on <Example />

--- docs/src/components/Example.tsx
+++ docs/src/components/Example.tsx
@@ -7,13 +7,13 @@ interface ExampleProps {
   className?: string;
 }
 
-export function Example({ children, className = 'example' }: ExampleProps) {
+export function Example({ children, className = '' }: ExampleProps) {
   return (
     <View
       backgroundColor="rgba(249, 250, 251, 1)"
       borderRadius="0.25rem"
       boxShadow="inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)"
-      className={className}
+      className={`example ${className}`}
       style={{ marginBottom: '1.5rem' }}
       padding="1.5rem"
     >
