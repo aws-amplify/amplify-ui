4b25649a236bbf22d4023771322c4cebac0e46a2
Eric Clemmons <eric@smarterspam.com>
2021-10-13 09:58:27 -0500

    Add reset-password Angular example

---
+++ examples/angular/src/pages/ui/components/authenticator/reset-password/reset-password.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Amplify } from 'aws-amplify';
+
+import awsExports from '@environments/auth-with-username-no-attributes/src/aws-exports';
+
+@Component({
+  selector: 'reset-password',
+  templateUrl: 'reset-password.component.html',
+})
+export class ResetPasswordComponent {
+  constructor() {
+    Amplify.configure(awsExports);
+  }
+}
