1f5528d40268cf8a5734607b8c8564c6385d3dde
Shane Laymance <shane.laymance@gmail.com>
2021-10-14 11:34:41 -0700

    Clean up some styling with phone primitive docs

--- docs/src/pages/ui/primitives/phonenumberfield/examples.tsx
+++ docs/src/pages/ui/primitives/phonenumberfield/examples.tsx
@@ -36,6 +36,18 @@ export const AccessibilityExample = () => (
   </Flex>
 );
 
+export const CountryCodePropsExample = () => (
+  <Flex direction="column">
+    <PhoneNumberField
+      label="Phone Number"
+      defaultCountryCode="+7"
+      countryCodeName="country_code"
+      countryCodeLabel="Country Code"
+      onCountryCodeChange={(e) => console.log(e.target.value)}
+    />
+  </Flex>
+);
+
 export const SizeExample = () => (
   <Flex direction="column" gap="1rem">
     <Flex>
