e6aeba399ae550ae4ac2de80673528a46fc6b00b
wlee221 <wlee221@live.unc.edu>
2021-10-18 15:37:46 -0700

    Use secondary entrypoint

--- packages/angular/projects/ui-angular/legacy/src/legacy-ui-angular.module.ts
+++ packages/angular/projects/ui-angular/legacy/src/legacy-ui-angular.module.ts
@@ -76,5 +76,4 @@ customElements.define('amplify-icon', AmplifyIcon);
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-// TODO: rename this to LegacyAmplifyUiAngularModule
-export class LegacyUiAngularModule {}
+export class LegacyAmplifyUiModule {}
--- packages/angular/projects/ui-angular/src/index.ts
+++ packages/angular/projects/ui-angular/src/index.ts
@@ -3,7 +3,6 @@
  */
 
 export { UiAngularModule } from './lib/ui-angular.module';
-export { LegacyUiAngularModule } from './legacy/legacy-ui-angular.module';
 
 export * from './lib/common';
 export * from './lib/components';
@@ -11,18 +10,3 @@ export * from './lib/primitives';
 export * from './lib/services/authenticator-context.service';
 export * from './lib/services/state-machine.service';
 export * from './lib/directives/amplify-slot.directive';
-
-export {
-  AmplifyButton,
-  AmplifyChatbot,
-  AmplifyIcon,
-  AmplifyInput,
-  AmplifyPhotoPicker,
-  AmplifyPicker,
-  AmplifyS3Album,
-  AmplifyS3Image,
-  AmplifyS3ImagePicker,
-  AmplifyS3Text,
-  AmplifyS3TextPicker,
-  AmplifyToast,
-} from './legacy/proxies';
