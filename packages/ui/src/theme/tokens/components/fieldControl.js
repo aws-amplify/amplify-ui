9cc9895597a107d81f4abbda8f35afcf33704d04
Scott Rees <6165315+reesscot@users.noreply.github.com>
2021-10-18 21:07:19 -0700

    Docs and size styling

--- packages/ui/src/theme/tokens/components/fieldControl.js
+++ packages/ui/src/theme/tokens/components/fieldControl.js
@@ -4,10 +4,10 @@ module.exports = {
   borderWidth: { value: '{borderWidths.small.value}' },
   borderRadius: { value: '{radii.medium.value}' },
   paddingBlockStart: {
-    value: '{space.small.value}',
+    value: '{space.xs.value}',
   },
   paddingBlockEnd: {
-    value: '{space.small.value}',
+    value: '{space.xs.value}',
   },
   paddingInlineStart: {
     value: '{space.medium.value}',
@@ -43,18 +43,18 @@ module.exports = {
   large: {
     fontSize: { value: '{components.field.large.fontSize.value}' },
     paddingBlockStart: {
-      value: '{space.medium.value}',
+      value: '{space.xs.value}',
     },
     paddingBlockEnd: {
-      value: '{space.medium.value}',
+      value: '{space.xs.value}',
     },
     paddingInlineStart: {
-      value: '{space.large.value}',
+      value: '{space.medium.value}',
     },
     paddingInlineEnd: {
-      value: '{space.large.value}',
+      value: '{space.medium.value}',
     },
-    lineHeight: { value: '{lineHeights.large.value}' },
+    lineHeight: { value: '{lineHeights.small.value}' },
   },
 
   quiet: {
