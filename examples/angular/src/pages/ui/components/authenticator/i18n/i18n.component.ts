98e9650eb659e6d79fd625193f06ec83b3ad8682
wlee221 <wlee221@live.unc.edu>
2021-10-20 18:38:13 -0700

    Remove custom translations

--- examples/angular/src/pages/ui/components/authenticator/i18n/i18n.component.ts
+++ examples/angular/src/pages/ui/components/authenticator/i18n/i18n.component.ts
@@ -13,22 +13,6 @@ export class I18nComponent implements OnInit {
 
   ngOnInit() {
     I18n.setLanguage('ja');
-
-    // Provide missing translations
-    I18n.putVocabulariesForLanguage('ja', {
-      [DefaultTexts.CONFIRM_PASSWORD_LABEL]: 'パスワードの確認',
-      [DefaultTexts.CONFIRM_SMS_LABEL]: 'SMSコードを確認する',
-      [DefaultTexts.CONFIRM_TOTP_LABEL]: 'TOTPコードを確認する',
-    });
-
-    // Or customers can use helper to get autocompletition
-    // import { putTranslationsForLang } from '@aws-amplify/ui'
-
-    // putTranslationsForLang('ja', {
-    //   'Confirm Password': 'パスワードの確認',
-    //   'Confirm SMS Code': 'SMSコードを確認する',
-    //   'Confirm TOTP Code': 'TOTPコードを確認する',
-    // });
   }
 
   ngOnDestroy() {
