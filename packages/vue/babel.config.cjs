29aecaebf6de7c2a01ab7fa41ca29bae5f33eafa
Erik Hanchett <ehhanche@amazon.com>
2021-10-21 10:03:14 -0700

    Updated to use esModules

--- packages/vue/babel.config.cjs
+++ packages/vue/babel.config.cjs
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+  ],
 };
