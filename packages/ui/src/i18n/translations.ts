64524b36c11cce683c4fb98fea38f8c26209b3c1
wlee221 <wlee221@live.unc.edu>
2021-10-21 11:12:12 -0700

    Move i18n file back out of types/

--- packages/ui/src/i18n/translations.ts
+++ packages/ui/src/i18n/translations.ts
@@ -11,8 +11,74 @@
  * and limitations under the License.
  */
 import { I18n } from 'aws-amplify';
-import { Dict, NoInfer, Phrase } from '../types';
+import { NoInfer } from '../types';
 import { deDict, esDict, frDict, itDict, jaDict, zhDict } from './dictionaries';
+
+/**
+ * Contains translatable strings that authenticator provides by default. Customers
+ * can use this to add custom vocabularies:
+ *
+ * ```
+ * I18n.putVocabulariesForLanguage("en", {
+ *  [DefaultTexts.SIGN_IN_TEXT]: "Custom Sign In Header Text",
+ *  [DefaultTexts.SIGN_IN_BUTTON_TEXT]: "Custom Click Here to Sign In"
+ * });
+ * ```
+ *
+ * TODO: this needs clean up. What's needed and what's not needed?
+ */
+export const DefaultTexts = {
+  BACK_SIGN_IN: 'Back to Sign In',
+  CHANGE_PASSWORD: 'Change Password',
+  CHANGING_PASSWORD: 'Changing',
+  CODE: 'Code',
+  CONFIRM_PASSWORD: 'Confirm Password',
+  CONFIRM_RESET_PASSWORD_HEADING: 'Reset your Password',
+  SUBMIT: 'Submit',
+  CONFIRM_SIGNUP_HEADING: 'Confirm Sign Up',
+  CONFIRM_SMS: 'Confirm SMS Code',
+  CONFIRM: 'Confirm',
+  CONFIRM_TOTP: 'Confirm TOTP Code',
+  CONFIRMATION_CODE: 'Confirmation Code',
+  CONFIRMING: 'Confirming',
+  CREATE_ACCOUNT: 'Create Account',
+  CREATING_ACCOUNT: 'Creating Account',
+  EMAIL_ADDRESS: 'Email',
+  ENTER_CODE: 'Enter your code',
+  ENTER_USERNAME: 'Enter your username',
+  FORGOT_YOUR_PASSWORD: 'Forgot your password? ',
+  HIDE_PASSWORD: 'Hide password',
+  LOADING: 'Loading',
+  LOGIN_NAME: 'Username',
+  PASSWORD: 'Password',
+  PHONE_NUMBER: 'Phone Number',
+  RESEND_CODE: 'Resend Code',
+  RESET_PASSWORD_HEADING: 'Reset your password',
+  RESET_PASSWORD: 'Send Code',
+  SEND_CODE: 'Send code',
+  SENDING: 'Sending',
+  SETUP_TOTP: 'Setup TOTP',
+  SHOW_PASSWORD: 'Show password',
+  SIGN_IN_BUTTON: 'Sign in',
+  SIGN_IN_TAB: 'Sign In',
+  SIGN_IN: 'Sign in to your account',
+  SIGN_IN_WITH_AMAZON: 'Sign In with Amazon',
+  SIGN_IN_WITH_APPLE: 'Sign In with Apple',
+  SIGN_IN_WITH_FACEBOOK: 'Sign In with Facebook',
+  SIGN_IN_WITH_GOOGLE: 'Sign In with Google',
+  SIGN_UP_BUTTON: 'Create a new account',
+  SIGNING_IN_BUTTON: 'Signing in',
+  SKIP: 'Skip',
+  SUBMITTING: 'Submitting',
+  VERIFY_CONTACT: 'Verify Contact',
+  VERIFY_HEADING: 'Account recovery requires verified contact information',
+  VERIFY: 'Verify',
+} as const;
+
+// type Phrase = "Back to Sign In" | "Change Password" | ...
+export type Phrase = typeof DefaultTexts[keyof typeof DefaultTexts];
+
+export type Dict = Partial<Record<Phrase, string>>;
 
 /**
  * This helper type checks that given phrase is one of the texts @aws-amplify/ui
