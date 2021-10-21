f1ebd7fcd96f62650da9db90cc8c67f12642f1f0
Erik Hanchett <ehhanche@amazon.com>
2021-10-21 12:27:58 -0700

    Fixed Vue phone sign in

--- packages/vue/src/components/user-name-alias.vue
+++ packages/vue/src/components/user-name-alias.vue
@@ -105,7 +105,7 @@ const { userNameAlias, userName, disabled } = withDefaults(
   }
 );
 
-const { state } = useAuth();
+const { state, send } = useAuth();
 
 const {
   value: { context },
@@ -142,4 +142,10 @@ if (userNameAlias) {
   name = 'username';
 }
 label = I18n.get(label);
+if (type === 'tel') {
+  send({
+    type: 'CHANGE',
+    data: { name: 'country_code', value: defaultDialCode },
+  });
+}
 </script>
