195528d25a8b6d845074f20066fd49fbd399d9c0
Chenwei Zhang <chenwz@amazon.com>
2021-10-21 17:10:08 -0700

    test: adding unit tests

--- packages/react/src/primitives/Loader/__tests__/Loader.test.tsx
+++ packages/react/src/primitives/Loader/__tests__/Loader.test.tsx
@@ -1,3 +1,32 @@
 import { render, screen } from '@testing-library/react';
 
+import { Loader } from '../Loader';
 import { ComponentClassNames } from '../../shared';
+
+describe('Loader: ', () => {
+  it('should render default and custom classname', async () => {
+    const className = 'class-test';
+    render(<Loader className={className} />);
+    const loader = await screen.findByRole('img');
+    expect(loader).toHaveClass(ComponentClassNames.Loader, className);
+  });
+
+  it('should set size attribute', async () => {
+    render(<Loader size="large" />);
+    const loader = await screen.findByRole('img');
+    expect(loader).toHaveAttribute('data-size', 'large');
+  });
+
+  it('should set variation attribute', async () => {
+    render(<Loader variation="linear" />);
+    const loader = await screen.findByRole('img');
+    expect(loader).toHaveAttribute('data-variation', 'linear');
+  });
+
+  it('should set ariaLabel', async () => {
+    const ariaLabel = 'This is a label.';
+    render(<Loader ariaLabel={ariaLabel} />);
+    const loader = await screen.findByRole('img');
+    expect(loader).toHaveAttribute('aria-label', ariaLabel);
+  });
+});
