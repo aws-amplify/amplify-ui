2d89536249db4ed28bc8d941d69778b796d71412
Eric Clemmons <eric@smarterspam.com>
2021-10-22 09:53:05 -0500

    Add /ui/components/authenticator/sign-up-fields example

---
+++ examples/next/pages/ui/components/authenticator/sign-up-fields/index.page.tsx
@@ -0,0 +1,43 @@
+import { Amplify } from 'aws-amplify';
+
+import {
+  Authenticator,
+  CheckboxField,
+  Link,
+  withAuthenticator,
+} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from '@environments/auth-with-username-no-attributes/src/aws-exports';
+Amplify.configure(awsExports);
+
+function App({ signOut }) {
+  return <button onClick={signOut}>Sign out</button>;
+}
+
+export default withAuthenticator(App, {
+  initialState: 'signUp',
+  components: {
+    SignUp: {
+      FormFields() {
+        return (
+          <>
+            <Authenticator.SignUp.FormFields />
+            <CheckboxField name="acknowledgement" value="yes">
+              I agree with the <Link href="#">Terms & Conditions</Link>
+            </CheckboxField>
+          </>
+        );
+      },
+    },
+  },
+  services: {
+    validateSignUp(formData) {
+      if (!formData.acknowledgement) {
+        return {
+          acknowledgement: 'You must agree to the Terms & Conditions',
+        };
+      }
+    },
+  },
+});
