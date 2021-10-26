756700ae1a615b04923adc3b1f77a32dc1ca1d88
Eric Clemmons <eric@smarterspam.com>
2021-10-26 11:39:45 -0500

    Fix typing of defaultComponents

--- packages/react/src/components/Authenticator/Provider/defaultComponents.tsx
+++ packages/react/src/components/Authenticator/Provider/defaultComponents.tsx
@@ -2,6 +2,8 @@ import { SignUp } from '../SignUp';
 
 export const defaultComponents = {
   SignUp: {
-    FormFields: SignUp.FormFields,
+    // Note – Because `typeof defaultComponents` is used for typing,
+    // casting to `React.ElementType` is required to correctly generate types
+    FormFields: SignUp.FormFields as React.ElementType,
   },
 };
