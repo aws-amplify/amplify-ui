63c8a84c1f97ec00226db6c9d6ca9531c39569b8
Eric Clemmons <eric@smarterspam.com>
2021-10-28 11:30:22 -0700

    Document Customization > Labels & Text (#587)

---
+++ docs/src/pages/ui/components/authenticator/LabelsAndTextDemo.tsx
@@ -0,0 +1,85 @@
+import { translations } from '@aws-amplify/ui';
+import {
+  Authenticator,
+  Loader,
+  useAuthenticator,
+  View,
+} from '@aws-amplify/ui-react';
+import { I18n } from 'aws-amplify';
+import * as React from 'react';
+
+type ScreenProps = {
+  Component:
+    | Authenticator.SignIn
+    | Authenticator.SignUp
+    | Authenticator.SetupTOTP;
+};
+
+function Screen({ Component }: ScreenProps) {
+  // Used to render the component when side-effects happen
+  const [version, setVersion] = React.useState(0);
+  const { route } = useAuthenticator();
+
+  React.useEffect(() => {
+    I18n.putVocabulariesForLanguage('en', {
+      // Sign In screen
+      'Sign In': 'Login', // Tab header
+      'Sign in': 'Log in', // Button label
+      'Sign in to your account': 'Welcome Back!', // Header text
+      Username: 'Enter your username', // Username label
+      Password: 'Enter your password', // Password label
+      'Forgot your password? ': 'Reset Password',
+
+      // Sign Up screen
+      'Create Account': 'Register', // Tab header
+      'Create a new account': 'New User', // Header text
+      'Confirm Password': 'Confirm your password', // Confirm Password label
+      Email: 'Enter your email',
+      'Phone Number': 'Enter your phone number',
+
+      // Setup TOTP screen
+      Loading: 'QR code would show here',
+      Code: '2FA Code',
+      Confirm: 'Confirm 2FA',
+      'Back to Sign In': 'Back to Login',
+
+      // Reset Password screen
+      'Reset your password': 'Forgot your password?',
+      'Enter your username': 'Username or Email',
+      'Send code': 'Reset my password',
+      // 'Back to Sign In': 'Back to Login', # Already set
+    });
+
+    setVersion(version + 1);
+
+    return () => {
+      I18n.putVocabulariesForLanguage('en', translations['en']);
+    };
+  }, []);
+
+  if (route === 'idle') {
+    return <Loader variation="linear" />;
+  }
+
+  if (!Component) {
+    throw new Error(
+      'Component is not defined. Please check the component name'
+    );
+  }
+
+  return <Component key={version} />;
+}
+
+export function LabelsAndTextDemo({ Component }: ScreenProps) {
+  return (
+    <Authenticator.Provider>
+      <View data-amplify-authenticator="">
+        <View data-authenticator-variation="modal" />
+
+        <View data-amplify-container="">
+          <Screen Component={Component} />
+        </View>
+      </View>
+    </Authenticator.Provider>
+  );
+}
