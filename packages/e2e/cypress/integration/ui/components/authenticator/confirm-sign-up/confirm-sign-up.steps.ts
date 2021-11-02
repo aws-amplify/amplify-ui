12616af5ba3975a3b504b3ae5d508ebb602488ea
Eric Clemmons <eric@smarterspam.com>
2021-11-02 13:22:46 -0500

    Add "I type a valid confirmation code" behavior

--- packages/e2e/cypress/integration/ui/components/authenticator/confirm-sign-up/confirm-sign-up.steps.ts
+++ packages/e2e/cypress/integration/ui/components/authenticator/confirm-sign-up/confirm-sign-up.steps.ts
@@ -1,5 +1,10 @@
 import { Then, When } from 'cypress-cucumber-preprocessor/steps';
 
+When('I type a valid confirmation code', () => {
+  // This should be intercepted & mocked
+  cy.findByLabelText('Confirmation Code').type('validcode');
+});
+
 When('I type an invalid confirmation code', () => {
   cy.findInputField('confirmation_code').type('invalidcode');
 });
