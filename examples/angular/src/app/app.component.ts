2127e1527bf77bd6c7c9fa76a390c9195a2cacb8
Eric Clemmons <eric@smarterspam.com>
2021-11-02 13:55:12 -0500

    Expose window.Amplify.* for Angular

--- examples/angular/src/app/app.component.ts
+++ examples/angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Amplify } from 'aws-amplify';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+
+  constructor() {
+    if (typeof window !== 'undefined') {
+      window['Amplify'] = Amplify;
+    }
+  }
 }
