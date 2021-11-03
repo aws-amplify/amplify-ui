66210f9428ae95af2771a24d0f67ee6036e79006
Erik Hanchett <ehhanche@amazon.com>
2021-11-03 13:06:00 -0700

    Added 3 more examples

---
+++ examples/next/pages/ui/components/authenticator/sign-in-with-email-lambda/index.page.tsx
@@ -0,0 +1,15 @@
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from '@environments/auth-with-email-lambda-signup-trigger/src/aws-exports';
+Amplify.configure(awsExports);
+
+function App({ signOut }) {
+  return <button onClick={signOut}>Sign out</button>;
+}
+
+export default withAuthenticator(App, {
+  loginMechanisms: ['email'],
+});
