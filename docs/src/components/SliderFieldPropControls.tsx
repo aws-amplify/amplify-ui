904966ace5fee864c445e914c795a705d873df94
Chenwei Zhang <40295569+zchenwei@users.noreply.github.com>
2021-11-09 15:09:57 -0800

    SliderField Primitive (#593)
    * feat(wip): adding SliderField primitive
    
    * style: adding styles
    
    * test: adding tests
    
    * test: updating tests
    
    * docs: adding docs
    
    * test: updating theme snapshot
    
    * chore: addressing comments
    
    * test: updating theme snapshot
    
    * chore: resolving conflicts
    
    * chore: updating README
    
    * chore: updating yarn.lock
    
    * chore: updating yarn.lock

---
+++ docs/src/components/SliderFieldPropControls.tsx
@@ -0,0 +1,173 @@
+import * as React from 'react';
+
+import {
+  CheckboxField,
+  SliderFieldProps,
+  SelectField,
+  TextField,
+} from '@aws-amplify/ui-react';
+
+import { DemoBox } from './DemoBox';
+
+export interface SliderFieldPropControlsProps extends SliderFieldProps {
+  setIsDisabled: (
+    value: React.SetStateAction<SliderFieldProps['isDisabled']>
+  ) => void;
+  setIsValueHidden: (
+    value: React.SetStateAction<SliderFieldProps['isValueHidden']>
+  ) => void;
+  setLabel: (value: React.SetStateAction<SliderFieldProps['label']>) => void;
+  setLabelHidden: (
+    value: React.SetStateAction<SliderFieldProps['labelHidden']>
+  ) => void;
+  setMax: (value: React.SetStateAction<SliderFieldProps['max']>) => void;
+  setMin: (value: React.SetStateAction<SliderFieldProps['min']>) => void;
+  setOrientation: (
+    value: React.SetStateAction<SliderFieldProps['orientation']>
+  ) => void;
+  setStep: (value: React.SetStateAction<SliderFieldProps['step']>) => void;
+  setTrackSize: (
+    value: React.SetStateAction<SliderFieldProps['trackSize']>
+  ) => void;
+  setEmptyTrackColor: (
+    value: React.SetStateAction<SliderFieldProps['emptyTrackColor']>
+  ) => void;
+  setFilledTrackColor: (
+    value: React.SetStateAction<SliderFieldProps['filledTrackColor']>
+  ) => void;
+  setThumbColor: (
+    value: React.SetStateAction<SliderFieldProps['thumbColor']>
+  ) => void;
+}
+
+export const SliderFieldPropControls: React.FC<SliderFieldPropControlsProps> =
+  ({
+    isDisabled,
+    isValueHidden,
+    label,
+    labelHidden,
+    max,
+    min,
+    orientation,
+    step,
+    trackSize,
+    emptyTrackColor,
+    filledTrackColor,
+    thumbColor,
+    setEmptyTrackColor,
+    setFilledTrackColor,
+    setIsDisabled,
+    setIsValueHidden,
+    setLabel,
+    setLabelHidden,
+    setMax,
+    setMin,
+    setOrientation,
+    setStep,
+    setThumbColor,
+    setTrackSize,
+  }) => {
+    return (
+      <DemoBox primitiveName="SliderField">
+        <TextField
+          label="label"
+          value={label as string}
+          onChange={(event) =>
+            setLabel(event.target.value as SliderFieldProps['label'])
+          }
+        />
+        <TextField
+          type="number"
+          label="min"
+          value={min}
+          onChange={(event) =>
+            setMin(Number(event.target.value) as SliderFieldProps['min'])
+          }
+        />
+        <TextField
+          type="number"
+          label="max"
+          value={max}
+          onChange={(event) =>
+            setMax(Number(event.target.value) as SliderFieldProps['max'])
+          }
+        />
+        <TextField
+          type="number"
+          label="step"
+          value={step}
+          onChange={(event) =>
+            setStep(Number(event.target.value) as SliderFieldProps['step'])
+          }
+        />
+        <TextField
+          label="trackSize"
+          value={trackSize}
+          onChange={(event) =>
+            setTrackSize(event.target.value as SliderFieldProps['trackSize'])
+          }
+        />
+        <TextField
+          label="emptyTrackColor"
+          value={emptyTrackColor}
+          onChange={(event) =>
+            setEmptyTrackColor(
+              event.target.value as SliderFieldProps['emptyTrackColor']
+            )
+          }
+        />
+        <TextField
+          label="thumbColor"
+          value={thumbColor}
+          onChange={(event) =>
+            setThumbColor(event.target.value as SliderFieldProps['thumbColor'])
+          }
+        />
+        <TextField
+          label="filledTrackColor"
+          value={filledTrackColor}
+          onChange={(event) =>
+            setFilledTrackColor(
+              event.target.value as SliderFieldProps['filledTrackColor']
+            )
+          }
+        />
+        <SelectField
+          label="orientation"
+          value={orientation}
+          onChange={(event) =>
+            setOrientation(
+              event.target.value as SliderFieldProps['orientation']
+            )
+          }
+        >
+          <option value="horizontal">horizontal</option>
+          <option value="vertical">vertical</option>
+        </SelectField>
+        <CheckboxField
+          name="disabled"
+          value="yes"
+          checked={isDisabled}
+          onChange={(event) => setIsDisabled(event.target.checked)}
+        >
+          isDisabled
+        </CheckboxField>
+        <CheckboxField
+          name="value-hidden"
+          value="yes"
+          checked={isValueHidden}
+          onChange={(event) => setIsValueHidden(event.target.checked)}
+        >
+          isValueHidden
+        </CheckboxField>
+        <CheckboxField
+          name="label-hidden"
+          value="yes"
+          checked={labelHidden}
+          onChange={(event) => setLabelHidden(event.target.checked)}
+        >
+          labelHidden
+        </CheckboxField>
+      </DemoBox>
+    );
+  };
---
+++ docs/src/components/useSliderFieldProps.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+
+import { SliderFieldProps } from '@aws-amplify/ui-react';
+
+import { SliderFieldPropControlsProps } from './SliderFieldPropControls';
+
+export interface UseSliderFieldProps {
+  (initialValues: SliderFieldProps): SliderFieldPropControlsProps;
+}
+
+export const useSliderFieldProps: UseSliderFieldProps = (initialValues) => {
+  const [isDisabled, setIsDisabled] = React.useState(initialValues.isDisabled);
+  const [isValueHidden, setIsValueHidden] = React.useState(
+    initialValues.isValueHidden
+  );
+  const [label, setLabel] = React.useState(initialValues.label);
+  const [labelHidden, setLabelHidden] = React.useState(
+    initialValues.labelHidden
+  );
+  const [max, setMax] = React.useState(initialValues.max);
+  const [min, setMin] = React.useState(initialValues.min);
+  const [step, setStep] = React.useState(initialValues.step);
+  const [orientation, setOrientation] = React.useState(
+    initialValues.orientation
+  );
+  const [trackSize, setTrackSize] = React.useState(initialValues.trackSize);
+  const [emptyTrackColor, setEmptyTrackColor] = React.useState(
+    initialValues.emptyTrackColor
+  );
+  const [filledTrackColor, setFilledTrackColor] = React.useState(
+    initialValues.filledTrackColor
+  );
+  const [thumbColor, setThumbColor] = React.useState(initialValues.thumbColor);
+  return {
+    defaultValue: initialValues.defaultValue,
+    isDisabled,
+    isValueHidden,
+    label,
+    labelHidden,
+    max,
+    min,
+    step,
+    orientation,
+    trackSize,
+    emptyTrackColor,
+    filledTrackColor,
+    thumbColor,
+    setEmptyTrackColor,
+    setFilledTrackColor,
+    setIsDisabled,
+    setIsValueHidden,
+    setLabel,
+    setLabelHidden,
+    setMax,
+    setMin,
+    setOrientation,
+    setStep,
+    setThumbColor,
+    setTrackSize,
+  };
+};
