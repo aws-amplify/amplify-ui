47e4296ecc5587fb2887b9b344f0fa3fe906b877
Eric Clemmons <eric@smarterspam.com>
2021-11-10 16:19:52 -0600

    Render required attributes by default in Angular (#668)
    * Remove @todo-angular placeholders
    
    * Update inputAttributes map
    
    * Add sign-up-with-attributes examples
    
    * Update sign up to show all configured attributes
    
    * Don't hide labels for sign up attributes
    
    * Fix variable name
    
    * Require LoginMechanism in `AuthInputAttributes`
    
    * Revert "Require LoginMechanism in `AuthInputAttributes`"
    
    This reverts commit 5d9efb625354cc4300fb570f330b838046329061.
    
    * Clean up signUpFields tpyes
    
    * Correct signUpFields
    
    * Add signUpAttributes prop
    
    * Infer autocomplete
    
    Co-authored-by: wlee221 <wlee221@live.unc.edu>

--- packages/angular/projects/ui-angular/src/lib/primitives/amplify-form-field/amplify-form-field.component.ts
+++ packages/angular/projects/ui-angular/src/lib/primitives/amplify-form-field/amplify-form-field.component.ts
@@ -22,7 +22,6 @@ import { AuthenticatorService } from '../../services/authenticator.service';
 })
 export class AmplifyFormFieldComponent implements OnInit {
   @Input() name: string;
-  // TODO: Separate entry for id
   @Input() type: string;
   @Input() required = true;
   @Input() placeholder = '';
@@ -30,6 +29,8 @@ export class AmplifyFormFieldComponent implements OnInit {
   @Input() initialValue = '';
   @Input() disabled = false;
   @Input() autocomplete = '';
+  @Input() labelHidden = true;
+
   public defaultCountryCode: string;
   public countryDialCodes = countryDialCodes;
   public textFieldId: string;
@@ -80,6 +81,10 @@ export class AmplifyFormFieldComponent implements OnInit {
   // infers what the `type` of underlying input element should be.
   inferType(): string {
     return this.type ?? this.attributeMap[this.name]?.type ?? 'text';
+  }
+
+  inferAutocomplete(): string {
+    return this.autocomplete || this.attributeMap[this.name]?.placeholder;
   }
 
   // TODO(enhancement): use enum to differentiate special field types
