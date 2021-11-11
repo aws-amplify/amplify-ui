9f02d51b5e8c678f5b87fb60331bfc5148e91528
Eric Clemmons <eric@smarterspam.com>
2021-11-11 14:42:36 -0600

    Add Authenticator Header & Footer slots

--- packages/react/src/components/Authenticator/Provider/defaultComponents.tsx
+++ packages/react/src/components/Authenticator/Provider/defaultComponents.tsx
@@ -1,7 +1,9 @@
 import { SignUp } from '../SignUp';
 
 export const defaultComponents = {
+  Header: (): JSX.Element => null,
   SignUp: {
     FormFields: SignUp.FormFields,
   },
+  Footer: (): JSX.Element => null,
 };
