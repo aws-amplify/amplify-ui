5482edcbb8fa1d5d99988d3eada2a15316e7ec69
Scott Rees <6165315+reesscot@users.noreply.github.com>
2021-11-12 13:41:29 -0700

    Add forwardRef support to Button & View (#698)

--- packages/react/src/primitives/types/view.ts
+++ packages/react/src/primitives/types/view.ts
@@ -7,6 +7,20 @@ type MergeProps<A, B> = A & Omit<B, keyof A>;
 
 export type ElementType = React.FC<any> | keyof JSX.IntrinsicElements;
 
+/**
+ * Convert string element type to DOMElement Type
+ * e.g. 'button' => HTMLButtonElement
+ */
+export type HTMLElementType<Element extends ElementType> =
+  Element extends keyof JSX.IntrinsicElements
+    ? JSX.IntrinsicElements[Element] extends React.DetailedHTMLProps<
+        unknown,
+        infer HTMLType
+      >
+      ? HTMLType
+      : never
+    : HTMLDivElement;
+
 export type ElementProps<Element extends ElementType> =
   Element extends keyof JSX.IntrinsicElements
     ? JSX.IntrinsicElements[Element]
@@ -18,14 +32,31 @@ export type PrimitiveProps<
   Props extends ViewProps,
   Element extends ElementType
 > = MergeProps<
-  Omit<Props, 'as'> & { as?: Element | Props['as'] },
-  ElementProps<Element>
+  Omit<Props, 'as'> & {
+    as?: Element | Props['as'];
+  },
+  Omit<ElementProps<Element>, 'ref'> // exclude `ref?: LegacyRef` included in DetailedHTMLProps
 >;
+
+export type PrimitivePropsWithRef<
+  Props extends ViewProps,
+  Element extends ElementType
+> = PrimitiveProps<Props, Element> & {
+  ref?: React.Ref<HTMLElementType<Element>>;
+};
 
 export type Primitive<
   Props extends ViewProps,
   Element extends ElementType
 > = React.FC<PrimitiveProps<Props, Element>>;
+
+export type PrimitiveWithForwardRef<
+  Props extends ViewProps,
+  Element extends ElementType
+> = React.ForwardRefRenderFunction<
+  HTMLElementType<Element>,
+  PrimitivePropsWithRef<Props, Element>
+>;
 
 export interface ViewProps
   extends BaseComponentProps,
