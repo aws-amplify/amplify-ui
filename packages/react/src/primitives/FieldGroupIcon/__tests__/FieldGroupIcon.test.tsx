0c593398a856a5f724673e4b3ec40eccbbc14319
Scott Rees <6165315+reesscot@users.noreply.github.com>
2021-11-15 15:47:41 -0700

    Forwardref simple input components

---
+++ packages/react/src/primitives/FieldGroupIcon/__tests__/FieldGroupIcon.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+
+import * as React from 'react';
+
+import { FieldGroupIcon } from '../FieldGroupIcon';
+import { Text } from '../../Text';
+import { Button } from '../../Button';
+
+import { ComponentClassNames } from '../../shared';
+
+describe('FieldGroupIcon component', () => {
+  const testId = 'fieldGroupTestId';
+  it('should render default and custom classname for FieldGroupIcon', async () => {
+    render(<FieldGroupIcon className="custom-class" testId={testId} />);
+
+    const fieldGroup = await screen.findByTestId(testId);
+
+    expect(fieldGroup).toHaveClass('custom-class');
+    expect(fieldGroup).toHaveClass(ComponentClassNames.FieldGroupIcon);
+  });
+
+  it('should forward ref to DOM element', async () => {
+    const ref = React.createRef<HTMLDivElement>();
+    render(
+      <FieldGroupIcon ref={ref} className="custom-class" testId={testId} />
+    );
+    await screen.findByTestId(testId);
+    expect(ref.current.nodeName).toBe('DIV');
+  });
+});
--- packages/react/src/primitives/Label/Label.tsx
+++ packages/react/src/primitives/Label/Label.tsx
@@ -1,26 +1,28 @@
+import * as React from 'react';
 import classNames from 'classnames';
 
 import { ComponentClassNames } from '../shared/constants';
-import { LabelProps, Primitive } from '../types';
+import { LabelProps, PrimitiveWithForwardRef } from '../types';
 import { View } from '../View';
 
-export const Label: Primitive<LabelProps, 'label'> = ({
-  children,
-  className,
-  visuallyHidden,
-  ...rest
-}) => {
+const LabelInner: PrimitiveWithForwardRef<LabelProps, 'label'> = (
+  { children, className, visuallyHidden, ...rest },
+  ref
+) => {
   return (
     <View
       as="label"
       className={classNames(ComponentClassNames.Label, className, {
         'sr-only': visuallyHidden,
       })}
+      ref={ref}
       {...rest}
     >
       {children}
     </View>
   );
 };
+
+export const Label = React.forwardRef(LabelInner);
 
 Label.displayName = 'Label';
