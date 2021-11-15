2f4673565eead6801071c3722de3eae1517a401a
Scott Rees <6165315+reesscot@users.noreply.github.com>
2021-11-15 14:57:42 -0700

    Remove AmplifyProvider custom components feature

--- packages/react/src/components/AmplifyProvider/__tests__/AmplifyProvider.test.tsx
+++ packages/react/src/components/AmplifyProvider/__tests__/AmplifyProvider.test.tsx
@@ -1,13 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import { useAmplify } from '../../../hooks';
 
-import { AmplifyProvider } from '..';
+import { AmplifyProvider } from '../index';
+import { Heading } from '../../../primitives';
 
 const App = () => {
-  const {
-    components: { Heading },
-  } = useAmplify();
-
   return <Heading>Howdy</Heading>;
 };
 
@@ -33,22 +30,5 @@ describe('AmplifyProvider', () => {
     const wrapper = container.querySelector('[data-amplify-theme]');
     expect(wrapper).toBeInTheDocument();
     expect(wrapper).toHaveAttribute('data-amplify-theme', 'default-theme');
-  });
-
-  it('accepts custom primitives as components', async () => {
-    render(
-      <AmplifyProvider
-        components={{
-          Heading({ children }) {
-            return <h1>Custom {children}</h1>;
-          },
-        }}
-      >
-        <App />
-      </AmplifyProvider>
-    );
-
-    const heading = await screen.getByText('Custom Howdy');
-    expect(heading.nodeName).toBe('H1');
   });
 });
