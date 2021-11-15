e155ef0be2db65fc126021b8969fd7fd151f26c7
Scott Rees <6165315+reesscot@users.noreply.github.com>
2021-11-15 16:41:56 -0700

    Remove AmplifyProvider custom components feature (#716)

--- packages/react/src/components/AmplifyProvider/AmplifyContext.tsx
+++ packages/react/src/components/AmplifyProvider/AmplifyContext.tsx
@@ -2,14 +2,10 @@ import * as React from 'react';
 
 import { defaultTheme, WebTheme } from '@aws-amplify/ui';
 
-import * as primitives from '../../primitives/components';
-
 interface AmplifyContextType {
-  components: Partial<typeof primitives>;
   theme: WebTheme;
 }
 
 export const AmplifyContext = React.createContext<AmplifyContextType>({
-  components: primitives,
   theme: defaultTheme,
 });
