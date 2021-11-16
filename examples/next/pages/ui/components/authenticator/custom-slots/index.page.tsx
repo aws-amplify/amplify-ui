c554aa9ef98266c5f4550dd920c6fe10b687f019
Eric Clemmons <eric@smarterspam.com>
2021-11-16 11:54:22 -0600

    Prefer `Authenticator`

--- examples/next/pages/ui/components/authenticator/custom-slots/index.page.tsx
+++ examples/next/pages/ui/components/authenticator/custom-slots/index.page.tsx
@@ -1,6 +1,7 @@
 import { Amplify } from 'aws-amplify';
 
 import {
+  Authenticator,
   // Amplify UI Primitives to simplify the custom fields
   Button,
   Heading,
@@ -11,100 +12,96 @@ import {
   useAuthenticator,
   useTheme,
 } from '@aws-amplify/ui-react';
-import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from '@environments/auth-with-federated/src/aws-exports';
 Amplify.configure(awsExports);
 
-function App({ signOut }) {
-  return <button onClick={signOut}>Sign out</button>;
-}
+const Header = () => {
+  const { tokens } = useTheme();
 
-export default withAuthenticator(App, {
-  components: {
-    Header() {
-      const { tokens } = useTheme();
+  return (
+    <View textAlign="center" padding={`${tokens.space.large}`}>
+      <Image
+        alt="Amplify logo"
+        src="https://docs.amplify.aws/assets/logo-dark.svg"
+      />
+    </View>
+  );
+};
 
-      return (
-        <View textAlign="center" padding={`${tokens.space.large}`}>
-          <Image
-            alt="Amplify logo"
-            src="https://docs.amplify.aws/assets/logo-dark.svg"
-          />
-        </View>
-      );
-    },
-    SignIn: {
-      Header() {
-        const { tokens } = useTheme();
+const Footer = () => {
+  const { tokens } = useTheme();
 
-        return (
-          <Heading
-            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-            level={3}
-          >
-            Sign in to your account
-          </Heading>
-        );
-      },
-      Footer() {
-        const { toResetPassword } = useAuthenticator();
+  return (
+    <View textAlign="center" padding={`${tokens.space.large}`}>
+      <Text color={`${tokens.colors.neutral['80']}`}>
+        &copy; All Rights Reserved
+      </Text>
+    </View>
+  );
+};
 
-        return (
-          <View textAlign="center">
-            <Button
-              fontWeight="normal"
-              onClick={toResetPassword}
-              size="small"
-              variation="link"
-            >
-              Reset Password
-            </Button>
-          </View>
-        );
-      },
-    },
-    SignUp: {
-      Header() {
-        const { tokens } = useTheme();
+const SignIn = {
+  Header() {
+    const { tokens } = useTheme();
 
-        return (
-          <Heading
-            padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-            level={3}
-          >
-            Create a new account
-          </Heading>
-        );
-      },
-      Footer() {
-        const { toSignIn } = useAuthenticator();
-
-        return (
-          <View textAlign="center">
-            <Button
-              fontWeight="normal"
-              onClick={toSignIn}
-              size="small"
-              variation="link"
-            >
-              Back to Sign In
-            </Button>
-          </View>
-        );
-      },
-    },
-    Footer() {
-      const { tokens } = useTheme();
-
-      return (
-        <View textAlign="center" padding={`${tokens.space.large}`}>
-          <Text color={`${tokens.colors.neutral['80']}`}>
-            &copy; All Rights Reserved
-          </Text>
-        </View>
-      );
-    },
+    return (
+      <Heading padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`} level={3}>
+        Sign in to your account
+      </Heading>
+    );
   },
-});
+  Footer() {
+    const { toResetPassword } = useAuthenticator();
+
+    return (
+      <View textAlign="center">
+        <Button
+          fontWeight="normal"
+          onClick={toResetPassword}
+          size="small"
+          variation="link"
+        >
+          Reset Password
+        </Button>
+      </View>
+    );
+  },
+};
+
+const SignUp = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <Heading padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`} level={3}>
+        Create a new account
+      </Heading>
+    );
+  },
+  Footer() {
+    const { toSignIn } = useAuthenticator();
+
+    return (
+      <View textAlign="center">
+        <Button
+          fontWeight="normal"
+          onClick={toSignIn}
+          size="small"
+          variation="link"
+        >
+          Back to Sign In
+        </Button>
+      </View>
+    );
+  },
+};
+
+export default function App() {
+  return (
+    <Authenticator components={{ Header, SignIn, SignUp, Footer }}>
+      {({ signOut }) => <button onClick={signOut}>Sign out</button>}
+    </Authenticator>
+  );
+}
