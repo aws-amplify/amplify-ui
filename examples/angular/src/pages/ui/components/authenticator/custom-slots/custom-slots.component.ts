72b07a9c2b0969dcb0897132e848d977b033ff03
Erik Hanchett <ehhanche@amazon.com>
2021-11-16 09:27:28 -0800

    fixed angular authenticator service buttons

--- examples/angular/src/pages/ui/components/authenticator/custom-slots/custom-slots.component.ts
+++ examples/angular/src/pages/ui/components/authenticator/custom-slots/custom-slots.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import Amplify from 'aws-amplify';
 import awsExports from '@environments/auth-with-federated/src/aws-exports';
+import { AuthenticatorService } from '@aws-amplify/ui-angular';
 
 @Component({
   selector: 'custom-slots',
   templateUrl: 'custom-slots.component.html',
 })
 export class CustomSlotsComponent {
-  constructor() {
+  constructor(private authenticator: AuthenticatorService) {
     Amplify.configure(awsExports);
+  }
+
+  get auth() {
+    const { toResetPassword, toSignIn } = this.authenticator;
+    return { toResetPassword, toSignIn };
   }
 }
