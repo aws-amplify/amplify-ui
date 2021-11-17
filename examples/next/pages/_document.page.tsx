db3be74955bdc2892b2a8b1a978c2ee41bf112c1
Eric Clemmons <eric@smarterspam.com>
2021-11-17 13:20:06 -0600

    Add title for React Example App

--- examples/next/pages/_document.page.tsx
+++ examples/next/pages/_document.page.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>React Example App</title>
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
