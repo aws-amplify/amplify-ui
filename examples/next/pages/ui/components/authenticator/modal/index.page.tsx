734edc5d864851a4c45fd14d3bc166497795bdd7
Eric Clemmons <eric@smarterspam.com>
2021-11-17 13:54:44 -0600

    Document `variation`

--- examples/next/pages/ui/components/authenticator/modal/index.page.tsx
+++ examples/next/pages/ui/components/authenticator/modal/index.page.tsx
@@ -1,18 +1,15 @@
 import { Amplify } from 'aws-amplify';
 
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from '@environments/auth-with-username-no-attributes/src/aws-exports';
 Amplify.configure(awsExports);
 
-function App({ signOut, user }) {
+export default function App({ signOut }) {
   return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
+    <Authenticator variation="modal">
+      {({ signOut }) => <button onClick={signOut}>Sign out</button>}
+    </Authenticator>
   );
 }
-
-export default withAuthenticator(App);
--- examples/next/pages/ui/components/authenticator/sign-in-with-username/index.page.tsx
+++ examples/next/pages/ui/components/authenticator/sign-in-with-username/index.page.tsx
@@ -1,13 +1,15 @@
 import { Amplify } from 'aws-amplify';
 
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from '@environments/auth-with-username-no-attributes/src/aws-exports';
 Amplify.configure(awsExports);
 
-function App({ signOut }) {
-  return <button onClick={signOut}>Sign out</button>;
+export default function App({ signOut }) {
+  return (
+    <Authenticator>
+      {({ signOut }) => <button onClick={signOut}>Sign out</button>}
+    </Authenticator>
+  );
 }
-
-export default withAuthenticator(App);
