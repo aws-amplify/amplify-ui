8f0301f0b4677b1886f4a450207cfc39b5245f9d
Scott Rees <6165315+reesscot@users.noreply.github.com>
2021-11-17 09:16:28 -0700

    ForwardRef support - CountryCode, SelectField, and SliderField (#737)

--- packages/react/src/primitives/PhoneNumberField/CountryCodeSelect.tsx
+++ packages/react/src/primitives/PhoneNumberField/CountryCodeSelect.tsx
@@ -1,16 +1,16 @@
-import { useMemo } from 'react';
+import * as React from 'react';
 import classNames from 'classnames';
 import { countryDialCodes } from '@aws-amplify/ui';
 
 import { SelectField } from '../SelectField';
 import { ComponentClassNames } from '../shared/constants';
-import { CountryCodeSelectProps, Primitive } from '../types';
+import { CountryCodeSelectProps, PrimitiveWithForwardRef } from '../types';
 
-export const CountryCodeSelect: Primitive<CountryCodeSelectProps, 'select'> = ({
-  className,
-  ...props
-}) => {
-  const countryCodeOptions = useMemo(
+const CountryCodeSelectPrimitive: PrimitiveWithForwardRef<
+  CountryCodeSelectProps,
+  'select'
+> = ({ className, ...props }, ref) => {
+  const countryCodeOptions = React.useMemo(
     () =>
       countryDialCodes.map((dialCode) => (
         <option key={dialCode} value={dialCode}>
@@ -25,11 +25,14 @@ export const CountryCodeSelect: Primitive<CountryCodeSelectProps, 'select'> = ({
       autoComplete="tel-country-code"
       className={classNames(ComponentClassNames.CountryCodeSelect, className)}
       labelHidden={true}
+      ref={ref}
       {...props}
     >
       {countryCodeOptions}
     </SelectField>
   );
 };
+
+export const CountryCodeSelect = React.forwardRef(CountryCodeSelectPrimitive);
 
 CountryCodeSelect.displayName = 'CountryCodeSelect';
