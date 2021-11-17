3129b671e696f7c01f5732d9a99748d7bfb5e9b8
Scott Rees <6165315+reesscot@users.noreply.github.com>
2021-11-17 16:28:11 -0700

    Address feedback

--- packages/react/src/helpers/utils.ts
+++ packages/react/src/helpers/utils.ts
@@ -3,12 +3,15 @@ export const isDevelopment = () => process.env.NODE_ENV !== 'production';
 export const isInputOrSelectElement = (
   target: unknown
 ): target is HTMLInputElement | HTMLSelectElement => {
-  return (
-    (target as HTMLInputElement)?.nodeName === 'INPUT' ||
-    (target as HTMLSelectElement)?.nodeName === 'SELECT'
-  );
+  return isInputElement(target) || isSelectElement(target);
 };
 
 export const isInputElement = (target: unknown): target is HTMLInputElement => {
-  return (target as HTMLInputElement)?.nodeName === 'INPUT';
+  return (target as HTMLElement)?.nodeName === 'INPUT';
+};
+
+export const isSelectElement = (
+  target: unknown
+): target is HTMLInputElement => {
+  return (target as HTMLElement)?.nodeName === 'SELECT';
 };
