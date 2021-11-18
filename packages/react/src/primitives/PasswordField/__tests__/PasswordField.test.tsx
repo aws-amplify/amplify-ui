5b3f7137837ae5cfd647037dc316ff240578fbd8
Eric Clemmons <eric@smarterspam.com>
2021-11-18 14:03:07 -0600

    Revert "Remove `usePagination` and `SharedText` from exports (#774)"
    This reverts commit e0837c2c49b9cb471da1486b083d6260e5bc4b0c.

--- packages/react/src/primitives/PasswordField/__tests__/PasswordField.test.tsx
+++ packages/react/src/primitives/PasswordField/__tests__/PasswordField.test.tsx
@@ -2,8 +2,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { PasswordField } from '../PasswordField';
-import { ComponentClassNames } from '../../shared';
-import { SharedText } from '../../shared/i18n';
+import { ComponentClassNames, SharedText } from '../../shared';
 
 const ariaLabelText = SharedText.ShowPasswordButton.ariaLabel;
 
