d0b8ed6b26162a6fe49e92dce819b418f9a0e818
pramod prajapati <prampraj@amazon.com>
2021-11-22 17:58:57 +0530

    Refactoring i18n package to accomodate new UI modules in cleaner and  modular way

---
+++ packages/ui/src/i18n/dictionaries/authenticator/index.ts
@@ -0,0 +1,11 @@
+// translation dictionaries
+export { deDict } from './de';
+export { enDict } from './en';
+export { esDict } from './es';
+export { frDict } from './fr';
+export { itDict } from './it';
+export { jaDict } from './ja';
+export { zhDict } from './zh';
+
+// default text phrases
+export { defaultTexts } from './defaultTexts';
