85579c1b7ae8e0e07244cfe24c6d0dca4bbaed96
Eric Clemmons <eric@smarterspam.com>
2021-11-24 09:33:14 -0600

    Attempt to log `@aws-amplify/ui-react-native`

--- examples/expo/App.tsx
+++ examples/expo/App.tsx
@@ -2,14 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import * as Authenticator from '@aws-amplify/ui-react-native';
+
+console.log({ Authenticator });
+
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>Howdy World</Text>
+      {/* <Authenticator>{() => <Text>Logged In</Text>}</Authenticator> */}
       <StatusBar style="auto" />
     </View>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
