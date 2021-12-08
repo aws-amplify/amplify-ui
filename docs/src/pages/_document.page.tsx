a2ea7949c74778e08836295161ededa626d3acbd
Scott Rees <6165315+reesscot@users.noreply.github.com>
2021-12-08 11:22:04 -0800

    Add dev mode headers, move csp to document

--- docs/src/pages/_document.page.tsx
+++ docs/src/pages/_document.page.tsx
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 const favicon =
@@ -5,11 +6,33 @@ const favicon =
     ? '/svg/favicon-dev.svg'
     : '/svg/favicon.svg';
 
+const cspHashOf = (text) => {
+  const hash = crypto.createHash('sha256');
+  hash.update(text);
+  return `'sha256-${hash.digest('base64')}'`;
+};
 class MyDocument extends Document {
   render() {
+    // See: https://github.com/vercel/next.js/blob/master/examples/with-strict-csp/pages/_document.js
+    let csp = `default-src 'self';
+              style-src 'self' fonts.googleapis.com;
+              font-src 'self' fonts.gstatic.com;
+              script-src 'self' ${cspHashOf(
+                NextScript.getInlineScriptSource(this.props)
+              )}`;
+    if (process.env.NODE_ENV !== 'production') {
+      csp = `default-src 'self';
+            style-src 'self' fonts.googleapis.com  'unsafe-inline';
+            font-src 'self' fonts.gstatic.com data:;
+            script-src 'unsafe-eval' 'self' ${cspHashOf(
+              NextScript.getInlineScriptSource(this.props)
+            )}`;
+    }
+
     return (
       <Html>
         <Head>
+          <meta httpEquiv="Content-Security-Policy" content={csp} />
           <link rel="icon" type="image/svg+xml" href={favicon} />
           {/* Adding custom variable fonts from google */}
           {/* Including multiple to show theming capabilities */}
