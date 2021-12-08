4abcdb1898d7a894f3bfe8dd1fbb7bbadddf6431
Scott Rees <6165315+reesscot@users.noreply.github.com>
2021-12-08 14:16:56 -0800

    Remove Google fonts

--- docs/src/pages/_document.page.tsx
+++ docs/src/pages/_document.page.tsx
@@ -15,15 +15,15 @@ class MyDocument extends Document {
   render() {
     // See: https://github.com/vercel/next.js/blob/master/examples/with-strict-csp/pages/_document.js
     let csp = `default-src 'self';
-              style-src 'self' fonts.googleapis.com 'unsafe-inline';
-              font-src 'self' fonts.gstatic.com;
+              style-src 'self' 'unsafe-inline';
+              font-src 'self';
               script-src 'self' ${cspHashOf(
                 NextScript.getInlineScriptSource(this.props)
               )}`;
     if (process.env.NODE_ENV !== 'production') {
       csp = `default-src 'self';
-            style-src 'self' fonts.googleapis.com  'unsafe-inline';
-            font-src 'self' fonts.gstatic.com data:;
+            style-src 'self' 'unsafe-inline';
+            font-src 'self' data:;
             script-src 'unsafe-eval' 'self' ${cspHashOf(
               NextScript.getInlineScriptSource(this.props)
             )}`;
@@ -34,18 +34,6 @@ class MyDocument extends Document {
         <Head>
           <meta httpEquiv="Content-Security-Policy" content={csp} />
           <link rel="icon" type="image/svg+xml" href={favicon} />
-          {/* Adding custom variable fonts from google */}
-          {/* Including multiple to show theming capabilities */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300..800&family=Work+Sans:wght@100..900&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
