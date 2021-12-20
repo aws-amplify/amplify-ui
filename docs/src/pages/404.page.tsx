4eebe7da0e9197c27ea8c099ff450d4dc8088134
Danny Banks <djb@amazon.com>
2021-12-20 11:48:02 -0800

    Fixing the build and adding cats

--- docs/src/pages/404.page.tsx
+++ docs/src/pages/404.page.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
-import { Heading, Text, View, Link, useTheme } from '@aws-amplify/ui-react';
+import {
+  Heading,
+  Text,
+  View,
+  Link,
+  Image,
+  useTheme,
+} from '@aws-amplify/ui-react';
+
+import { cats } from '@/data/cats';
 
 const Custom404 = () => {
   const { tokens } = useTheme();
   let [href, setHref] = React.useState('https://ui.docs.amplify.aws');
+  const cat = cats[Math.round(Math.random() * cats.length)];
 
   React.useEffect(() => {
     setHref(window.location.href);
@@ -18,8 +28,8 @@ const Custom404 = () => {
       >
         <Heading level={1}>404</Heading>
         <Text fontSize={tokens.fontSizes.large}>
-          Apologies––we can't seem to find this page. <br />
-          {`If this is a mistake, please `}
+          {`Apologies––we can't seem to find this page.
+If this is a mistake, please `}
           <Link
             isExternal={true}
             href={`https://github.com/aws-amplify/amplify-ui/issues/new?title=[missing-page]&labels=Documentation&body=${encodeURI(`**Page**: [\`${href}\`](${href})
@@ -29,6 +39,8 @@ const Custom404 = () => {
           </Link>
           {` and we will get it fixed ASAP.`}
         </Text>
+
+        <Image src={cat.url} alt={cat.alt} maxHeight="50vh" />
       </View>
     </>
   );
