716e06dd22a67b1e45a6987922fbe5dc7f2999b0
Danny Banks <djb@amazon.com>
2021-12-20 11:49:18 -0800

    Addressing comments

--- docs/src/pages/404.page.tsx
+++ docs/src/pages/404.page.tsx
@@ -20,29 +20,27 @@ const Custom404 = () => {
   }, []);
 
   return (
-    <>
-      <View
-        as="section"
-        className="docs-home-section container"
-        textAlign="center"
-      >
-        <Heading level={1}>404</Heading>
-        <Text fontSize={tokens.fontSizes.large}>
-          {`Apologies––we can't seem to find this page.
+    <View
+      as="section"
+      className="docs-home-section container"
+      textAlign="center"
+    >
+      <Heading level={1}>404</Heading>
+      <Text fontSize={tokens.fontSizes.large}>
+        {`Apologies––we can't seem to find this page.
 If this is a mistake, please `}
-          <Link
-            isExternal={true}
-            href={`https://github.com/aws-amplify/amplify-ui/issues/new?title=[missing-page]&labels=Documentation&body=${encodeURI(`**Page**: [\`${href}\`](${href})
+        <Link
+          isExternal={true}
+          href={`https://github.com/aws-amplify/amplify-ui/issues/new?title=[missing-page]&labels=Documentation&body=${encodeURI(`**Page**: [\`${href}\`](${href})
 **Feedback**: <!-- your feedback here -->`)}`}
-          >
-            file an issue
-          </Link>
-          {` and we will get it fixed ASAP.`}
-        </Text>
+        >
+          file an issue
+        </Link>
+        {` and we will get it fixed ASAP.`}
+      </Text>
 
-        <Image src={cat.url} alt={cat.alt} maxHeight="50vh" />
-      </View>
-    </>
+      <Image src={cat.url} alt={cat.alt} maxHeight="50vh" />
+    </View>
   );
 };
 
