950071bf1756bd521246b571a079cd00f9365802
Danny Banks <djb@amazon.com>
2021-12-20 15:46:56 -0800

    Fix cats on 404 (#1025)

--- docs/src/pages/404.page.tsx
+++ docs/src/pages/404.page.tsx
@@ -5,6 +5,7 @@ import {
   View,
   Link,
   Image,
+  Flex,
   useTheme,
 } from '@aws-amplify/ui-react';
 
@@ -13,10 +14,11 @@ import { cats } from '@/data/cats';
 const Custom404 = () => {
   const { tokens } = useTheme();
   let [href, setHref] = React.useState('https://ui.docs.amplify.aws');
-  const cat = cats[Math.round(Math.random() * cats.length)];
+  let [cat, setCat] = React.useState(undefined);
 
   React.useEffect(() => {
     setHref(window.location.href);
+    setCat(cats[Math.floor(Math.random() * cats.length)]);
   }, []);
 
   return (
@@ -25,21 +27,23 @@ const Custom404 = () => {
       className="docs-home-section container"
       textAlign="center"
     >
-      <Heading level={1}>404</Heading>
-      <Text fontSize={tokens.fontSizes.large}>
-        {`Apologies––we can't seem to find this page.
-If this is a mistake, please `}
-        <Link
-          isExternal={true}
-          href={`https://github.com/aws-amplify/amplify-ui/issues/new?title=[missing-page]&labels=Documentation&body=${encodeURI(`**Page**: [\`${href}\`](${href})
+      <Flex direction="column" gap={tokens.space.large} alignItems="center">
+        <Heading level={1}>404</Heading>
+        <Text fontSize={tokens.fontSizes.large}>
+          {`Apologies—we can't seem to find this page.`} <br />
+          {`If this is a mistake, please `}
+          <Link
+            isExternal={true}
+            href={`https://github.com/aws-amplify/amplify-ui/issues/new?title=[missing-page]&labels=Documentation&body=${encodeURI(`**Page**: [\`${href}\`](${href})
 **Feedback**: <!-- your feedback here -->`)}`}
-        >
-          file an issue
-        </Link>
-        {` and we will get it fixed ASAP.`}
-      </Text>
+          >
+            file an issue
+          </Link>
+          {` and we will get it fixed ASAP.`}
+        </Text>
 
-      <Image src={cat.url} alt={cat.alt} maxHeight="50vh" />
+        {cat ? <Image src={cat.url} alt={cat.alt} maxHeight="50vh" /> : null}
+      </Flex>
     </View>
   );
 };
