87af01316f5e467de14b8ffbc73ec1851321d9e3
Danny Banks <djb@amazon.com>
2021-12-20 13:40:35 -0800

    Adding a 404 page (#1022)
    Fixes #985

---
+++ docs/src/data/cats.ts
@@ -0,0 +1,46 @@
+export const cats = [
+  {
+    url: '/cats/1.jpg',
+    alt: 'Donna Meagle laying on a cozy white blanket',
+  },
+  {
+    url: '/cats/2.jpg',
+    alt: 'Burt Macklin, FBI peaking out of a wicker basket',
+  },
+  {
+    url: '/cats/3.jpg',
+    alt: 'Cream tabby kitten sleeping on a towel',
+  },
+  {
+    url: '/cats/4.jpg',
+    alt: 'Fluffy tabby kitten sleeping on a towel',
+  },
+  {
+    url: '/cats/5.jpg',
+    alt: 'Orange tabby and brown tabby kittens cuddling',
+  },
+  {
+    url: '/cats/6.jpg',
+    alt: 'Tuxedo cat and grey tortoise shell cat laying on a bed',
+  },
+  {
+    url: '/cats/7.jpg',
+    alt: '2 tabby kittens sitting next to each other',
+  },
+  {
+    url: '/cats/8.jpg',
+    alt: 'Tuxedo cat laying on a small chair',
+  },
+  {
+    url: '/cats/9.jpg',
+    alt: 'Dark tabby kitten sitting on cream tabby kitten',
+  },
+  {
+    url: '/cats/10.jpg',
+    alt: 'Tuxedo cat sleeping on a comforter',
+  },
+  {
+    url: '/cats/11.jpg',
+    alt: 'Grey tortoise shell cat wearing a yogurt container as a hat',
+  },
+];
---
+++ docs/src/pages/404.page.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import {
+  Heading,
+  Text,
+  View,
+  Link,
+  Image,
+  useTheme,
+} from '@aws-amplify/ui-react';
+
+import { cats } from '@/data/cats';
+
+const Custom404 = () => {
+  const { tokens } = useTheme();
+  let [href, setHref] = React.useState('https://ui.docs.amplify.aws');
+  const cat = cats[Math.round(Math.random() * cats.length)];
+
+  React.useEffect(() => {
+    setHref(window.location.href);
+  }, []);
+
+  return (
+    <View
+      as="section"
+      className="docs-home-section container"
+      textAlign="center"
+    >
+      <Heading level={1}>404</Heading>
+      <Text fontSize={tokens.fontSizes.large}>
+        {`Apologies––we can't seem to find this page.
+If this is a mistake, please `}
+        <Link
+          isExternal={true}
+          href={`https://github.com/aws-amplify/amplify-ui/issues/new?title=[missing-page]&labels=Documentation&body=${encodeURI(`**Page**: [\`${href}\`](${href})
+**Feedback**: <!-- your feedback here -->`)}`}
+        >
+          file an issue
+        </Link>
+        {` and we will get it fixed ASAP.`}
+      </Text>
+
+      <Image src={cat.url} alt={cat.alt} maxHeight="50vh" />
+    </View>
+  );
+};
+
+export default Custom404;
