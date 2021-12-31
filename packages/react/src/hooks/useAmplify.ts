33089dddc2cacd1200cf9c63cce091323b16a72d
Chenwei Zhang <chenwz@amazon.com>
2021-12-30 17:16:49 -0800

    chore: updating reference in other function

--- packages/react/src/hooks/useAmplify.ts
+++ packages/react/src/hooks/useAmplify.ts
@@ -3,15 +3,13 @@ import * as React from 'react';
 import { Theme } from '@aws-amplify/ui';
 
 import { AmplifyContext } from '../components/AmplifyProvider/AmplifyContext';
-import { getThemeFromContext } from './useTheme';
 
 interface UseAmplifyOutput {
   theme: Theme;
 }
 
 export function useAmplify(): UseAmplifyOutput {
-  const context = React.useContext(AmplifyContext);
-  const theme = getThemeFromContext(context);
+  const { theme } = React.useContext(AmplifyContext);
 
   return { theme };
 }
