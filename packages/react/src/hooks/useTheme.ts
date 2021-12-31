1d83a800f5627926d350add3d49c84d23dc596e6
Chenwei Zhang <chenwz@amazon.com>
2021-12-30 16:56:38 -0800

    chore: removing unnecessary conditional check in useTheme

--- packages/react/src/hooks/useTheme.ts
+++ packages/react/src/hooks/useTheme.ts
@@ -1,24 +1,9 @@
 import * as React from 'react';
 
-import { defaultTheme, WebTheme } from '@aws-amplify/ui';
-import {
-  AmplifyContext,
-  AmplifyContextType,
-} from '../components/AmplifyProvider/AmplifyContext';
+import { WebTheme } from '@aws-amplify/ui';
+import { AmplifyContext } from '../components/AmplifyProvider/AmplifyContext';
 
 export const useTheme = (): WebTheme => {
-  const context = React.useContext(AmplifyContext);
-  return getThemeFromContext(context);
-};
-
-/**
- * Get current Theme object value from Amplify context.
- * Returns a default theme if context is not available
- */
-export const getThemeFromContext = (context: AmplifyContextType) => {
-  if (typeof context === 'undefined' || typeof context.theme === 'undefined') {
-    return defaultTheme;
-  }
-
-  return context.theme;
+  const { theme } = React.useContext(AmplifyContext);
+  return theme;
 };
