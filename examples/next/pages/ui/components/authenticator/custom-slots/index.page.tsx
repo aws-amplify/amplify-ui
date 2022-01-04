ce60a5282928559ab6c8578bb141379eb257e46d
wlee221 <willeea@amazon.com>
2022-01-03 16:22:08 -0800

    Remove full-headless related content

--- examples/next/pages/ui/components/authenticator/custom-slots/index.page.tsx
+++ examples/next/pages/ui/components/authenticator/custom-slots/index.page.tsx
@@ -18,31 +18,6 @@ import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 const components = {
-  Header() {
-    const { tokens } = useTheme();
-
-    return (
-      <View textAlign="center" padding={tokens.space.large}>
-        <Image
-          alt="Amplify logo"
-          src="https://docs.amplify.aws/assets/logo-dark.svg"
-        />
-      </View>
-    );
-  },
-
-  Footer() {
-    const { tokens } = useTheme();
-
-    return (
-      <View textAlign="center" padding={tokens.space.large}>
-        <Text color={`${tokens.colors.neutral['80']}`}>
-          &copy; All Rights Reserved
-        </Text>
-      </View>
-    );
-  },
-
   SignIn: {
     Header() {
       const { tokens } = useTheme();
@@ -54,53 +29,6 @@ const components = {
         >
           Sign in to your account
         </Heading>
-      );
-    },
-    Footer() {
-      const { toResetPassword } = useAuthenticator();
-
-      return (
-        <View textAlign="center">
-          <Button
-            fontWeight="normal"
-            onClick={toResetPassword}
-            size="small"
-            variation="link"
-          >
-            Reset Password
-          </Button>
-        </View>
-      );
-    },
-  },
-
-  SignUp: {
-    Header() {
-      const { tokens } = useTheme();
-
-      return (
-        <Heading
-          padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-          level={3}
-        >
-          Create a new account
-        </Heading>
-      );
-    },
-    Footer() {
-      const { toSignIn } = useAuthenticator();
-
-      return (
-        <View textAlign="center">
-          <Button
-            fontWeight="normal"
-            onClick={toSignIn}
-            size="small"
-            variation="link"
-          >
-            Back to Sign In
-          </Button>
-        </View>
       );
     },
   },
