ef8496dded54b3f38fbc8ee395e23485a103ecbf
wlee221 <willeea@amazon.com>
2022-01-26 11:25:27 -0800

    Remove unused export

--- examples/angular/src/app/app.module.ts
+++ examples/angular/src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import {
-  AmplifyAuthenticatorModule,
-  AuthenticatorService,
-} from '@aws-amplify/ui-angular';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
