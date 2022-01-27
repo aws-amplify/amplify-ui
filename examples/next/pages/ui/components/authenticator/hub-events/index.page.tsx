a77fff99b96c07af313f5e4ed35631a4ba0f7175
wlee221 <willeea@amazon.com>
2022-01-26 20:04:47 -0800

    Add react example + .feature

--- examples/next/pages/ui/components/authenticator/hub-events/index.page.tsx
+++ examples/next/pages/ui/components/authenticator/hub-events/index.page.tsx
@@ -1,4 +1,4 @@
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -6,8 +6,8 @@ import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-function App({ signOut }) {
-  return <button onClick={signOut}>Sign out</button>;
+function App({}) {
+  return <button onClick={() => Auth.signOut()}>Sign out</button>;
 }
 
 export default withAuthenticator(App);
