18a813661e38b2e1cc7aab25c54534e3635012e2
Danny Banks <djb@amazon.com>
2022-02-01 14:57:33 -0800

    Updating icon examples

--- docs/src/pages/components/icon/examples/CustomIconWithLibExample.tsx
+++ docs/src/pages/components/icon/examples/CustomIconWithLibExample.tsx
@@ -2,5 +2,5 @@ import { Icon } from '@aws-amplify/ui-react';
 import { DiJsBadge } from 'react-icons/di';
 
 export const CustomIconWithLibExample = () => {
-  return <Icon ariaLabel="Javascript" color="rebeccapurple" as={DiJsBadge} />;
+  return <Icon ariaLabel="Javascript" as={DiJsBadge} />;
 };
