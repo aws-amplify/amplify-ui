bdc6b96cd09bc69291a25116e902305e241e4be7
Erik Hanchett <ehhanche@amazon.com>
2022-02-03 11:51:32 -0800

    Updated vite config so it wouldn't fail

--- canary/vue/vite/vite.config.js
+++ canary/vue/vite/vite.config.js
@@ -7,6 +7,11 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [{}],
+    alias: [
+      {
+        find: './runtimeConfig',
+        replacement: './runtimeConfig.browser',
+      },
+    ],
   },
 });
