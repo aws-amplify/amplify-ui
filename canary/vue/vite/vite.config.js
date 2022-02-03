de7df97d8a9efa4bbacbc94771e2a4e5074959b8
Erik Hanchett <ehhanche@amazon.com>
2022-02-03 12:11:27 -0800

    triggering failure build

--- canary/vue/vite/vite.config.js
+++ canary/vue/vite/vite.config.js
@@ -7,11 +7,6 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: [
-      {
-        find: './runtimeConfig',
-        replacement: './runtimeConfig.browser',
-      },
-    ],
+    alias: [{}],
   },
 });
