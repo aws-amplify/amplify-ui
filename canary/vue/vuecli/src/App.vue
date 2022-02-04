92272fdfd920af0fcacce93209c648f41413ec38
Erik Hanchett <icystorm@gmail.com>
2022-02-03 17:18:27 -0800

    Canary builds (#1241)
    * Added first few canary builds
    
    * Added more changes to package.json
    
    * Skeleton workflow for running builds
    
    * Run install and build
    
    * Debug pwd
    
    * Confrim taht each step starts in root working directory
    
    * Run from canary folder
    
    * Add trigger-alarm job
    
    * Add needs
    
    * Set fail fast to false
    
    * Update working-dir
    
    * Setup node
    
    * Ignore postcss.config.js at monorepo
    
    * Run every 2 hours
    
    * Run every 2 hours
    
    * Remove locks
    
    * Added cloud warning and added test for build to fail
    
    * Updated build
    
    * Updated secrets
    
    * Updated environment
    
    * Configure credentials in trigger-alarm job
    
    * Updated values for region
    
    * Updated build success/failure
    
    * Updated vite config so it wouldn't fail
    
    * Updated yml
    
    * triggering failure build
    
    * Fixed issue
    
    * Update .github/workflows/run-builds.yml
    
    Co-authored-by: Erik Hanchett <ehhanche@amazon.com>
    Co-authored-by: wlee221 <willeea@amazon.com>
    Co-authored-by: William Lee <43682783+wlee221@users.noreply.github.com>

---
+++ canary/vue/vuecli/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Authenticator } from '@aws-amplify/ui-vue';
+import '@aws-amplify/ui-vue/styles.css';
+
+import Amplify from 'aws-amplify';
+
+Amplify.configure({});
+</script>
+
+<template>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
+</template>
