78266ea20a7f28f57cdf4f24bdd0bb1d76e9ab9c
Shane Laymance <shane.laymance@gmail.com>
2022-02-08 15:26:53 -0800

    Remove sample test

--- packages/e2e/cypress/integration/ui/components/map/first/first.steps.ts
+++
@@ -1,7 +0,0 @@
-import { Then, When } from 'cypress-cucumber-preprocessor/steps';
-
-When('I drag the map', () => {});
-
-When('I zoom in on the map', () => {});
-
-Then('I see the viewport change', () => {});
