5dd88899c4134e9f4f05229ef27e300d68f60e2a
Jacob Logan <lognjc@amazon.com>
2022-02-16 09:15:29 -0700

    remove old DemoBox component and replace it with the Demo component

--- docs/src/pages/components/table/demo.tsx
+++ docs/src/pages/components/table/demo.tsx
@@ -1,26 +1,67 @@
 import * as React from 'react';
 
-import { Flex } from '@aws-amplify/ui-react';
+import { Demo } from '@/components/Demo';
 
-import { Example } from '@/components/Example';
 import { TablePropControls } from './TablePropControls';
 import { useTableProps } from './useTableProps';
 import { BasicExample } from './examples';
 
+const propsToCode = (props) => {
+  const { caption, highlightOnHover, size, variation } = props;
+  return `
+<Table
+  caption={${caption}}
+  highlightOnHover={${highlightOnHover}}
+  size={${size}}
+  variation={${variation}}
+  >
+  <TableHead>
+    <TableRow>
+      <TableCell as="th">Citrus</TableCell>
+      <TableCell as="th">Stone Fruit</TableCell>
+      <TableCell as="th">Berry</TableCell>
+    </TableRow>
+  </TableHead>
+  <TableBody>
+    <TableRow>
+      <TableCell>Orange</TableCell>
+      <TableCell>Nectarine</TableCell>
+      <TableCell>Raspberry</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell>Grapefruit</TableCell>
+      <TableCell>Apricot</TableCell>
+      <TableCell>Blueberry</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableCell>Lime</TableCell>
+      <TableCell>Peach</TableCell>
+      <TableCell>Strawberry</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
+  `;
+};
+
+const initialValues = {
+  caption: '',
+  highlightOnHover: false,
+};
+
 export const TableDemo = () => {
-  const tableProps = useTableProps({});
+  const tableProps = useTableProps(initialValues);
 
   return (
-    <Flex direction="column" gap="0.5rem">
-      <TablePropControls {...tableProps} />
-      <Example>
-        <BasicExample
-          caption={tableProps.caption}
-          highlightOnHover={tableProps.highlightOnHover}
-          size={tableProps.size}
-          variation={tableProps.variation}
-        />
-      </Example>
-    </Flex>
+    <Demo
+      code={propsToCode(tableProps)}
+      propControls={<TablePropControls {...tableProps} />}
+    >
+      <BasicExample
+        caption={tableProps.caption}
+        highlightOnHover={tableProps.highlightOnHover}
+        size={tableProps.size}
+        variation={tableProps.variation}
+      />
+    </Demo>
   );
 };
