5301cb77a04012c9ae8b019611d51353a36c9858
wlee221 <willeea@amazon.com>
2022-02-24 10:44:25 -0800

    Add AttributeField component

---
+++ packages/react/src/components/Authenticator/shared/AttributeField.tsx
@@ -0,0 +1,194 @@
+import {
+  ActorContextWithForms,
+  getActorContext,
+  translate,
+} from '@aws-amplify/ui';
+import { useAuthenticator } from '..';
+import { PhoneNumberField, TextField } from '../../..';
+
+export function AttributeField({ name, ...attrs }) {
+  const { _state } = useAuthenticator();
+  const { country_code, formValues } = getActorContext(
+    _state
+  ) as ActorContextWithForms;
+
+  // See: https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-settings-attributes.html#user-pool-settings-custom-attributes
+  switch (name) {
+    case 'birthdate':
+      return (
+        <TextField
+          autoComplete="bday"
+          key={name}
+          isRequired
+          label={translate('Birthdate')}
+          name={name}
+          placeholder={translate('Birthdate')}
+          type="date"
+          {...attrs}
+        />
+      );
+
+    case 'email':
+      return (
+        <TextField
+          autoComplete="email"
+          defaultValue={formValues.email ?? null}
+          key={name}
+          isRequired
+          label={translate('Email')}
+          labelHidden
+          name={name}
+          placeholder={translate('Email')}
+          type="email"
+          {...attrs}
+        />
+      );
+
+    case 'family_name':
+      return (
+        <TextField
+          autoComplete="family-name"
+          key={name}
+          isRequired
+          label={translate('Family Name')}
+          name={name}
+          placeholder={translate('Family Name')}
+          {...attrs}
+        />
+      );
+
+    case 'given_name':
+      return (
+        <TextField
+          autoComplete="given-name"
+          key={name}
+          isRequired
+          label={translate('Given Name')}
+          name={name}
+          placeholder={translate('Given Name')}
+          {...attrs}
+        />
+      );
+
+    case 'middle_name':
+      return (
+        <TextField
+          autoComplete="additional-name"
+          key={name}
+          isRequired
+          label={translate('Middle Name')}
+          name={name}
+          placeholder={translate('Middle Name')}
+          {...attrs}
+        />
+      );
+
+    case 'name':
+      return (
+        <TextField
+          autoComplete="name"
+          key={name}
+          isRequired
+          label={translate('Name')}
+          name={name}
+          placeholder={translate('Name')}
+          {...attrs}
+        />
+      );
+
+    case 'nickname':
+      return (
+        <TextField
+          key={name}
+          isRequired
+          label={translate('Nickname')}
+          name={name}
+          placeholder={translate('Nickname')}
+          {...attrs}
+        />
+      );
+
+    case 'phone_number':
+      return (
+        <PhoneNumberField
+          autoComplete="tel"
+          countryCodeName="country_code"
+          defaultCountryCode={country_code}
+          // errorMessage={error}
+          isRequired
+          key="phone"
+          label={translate('Phone Number')}
+          labelHidden
+          name="phone"
+          placeholder={translate('Phone Number')}
+          {...attrs}
+        />
+      );
+
+    case 'preferred_username':
+      return (
+        <TextField
+          isRequired
+          key={name}
+          label={translate('Preferred Username')}
+          name={name}
+          placeholder={translate('Preferred Username')}
+          required
+          {...attrs}
+        />
+      );
+
+    case 'profile':
+      return (
+        <TextField
+          autoComplete="url"
+          isRequired
+          key={name}
+          label={translate('Profile')}
+          name={name}
+          placeholder={translate('Profile')}
+          type="url"
+          {...attrs}
+        />
+      );
+
+    case 'username':
+      return (
+        <TextField
+          autoComplete="username"
+          isRequired
+          key={name}
+          label={translate('Username')}
+          placeholder={translate('Username')}
+          {...attrs}
+        />
+      );
+
+    case 'website':
+      return (
+        <TextField
+          autoComplete="url"
+          isRequired
+          key={name}
+          label={translate('Website')}
+          name="website"
+          placeholder={translate('Website')}
+          type="url"
+          {...attrs}
+        />
+      );
+
+    case 'address':
+    case 'gender':
+    case 'locale':
+    case 'picture':
+    case 'updated_at':
+    case 'zoneinfo':
+    default:
+      // There's a `custom:*` attribute or one we don't already have an implementation for
+      console.debug(
+        `Authenticator does not have a default implementation for ${name}. Customize Authenticator.SignUp.FormFields to add your own.`
+      );
+      return null;
+  }
+}
