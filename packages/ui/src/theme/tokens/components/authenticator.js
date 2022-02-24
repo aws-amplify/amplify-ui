e56e98cf75e12b5f6c80ae708fafd76513bca210
MJ☔️ <o.6180339+github@gmail.com>
2022-02-24 17:49:52 +0000

    refactor(authenticator): remove redundant CSS var

--- packages/ui/src/theme/tokens/components/authenticator.js
+++ packages/ui/src/theme/tokens/components/authenticator.js
@@ -3,12 +3,10 @@ module.exports = {
     width: { value: '100%' },
     height: { value: '100%' },
     backgroundColor: { value: '{colors.background.secondary.value}' },
-    position: { value: 'fixed' },
     top: { value: '{space.zero}' },
     left: { value: '{space.zero}' },
   },
   container: {
-    placeSelf: { value: 'center' },
     widthDesktop: { value: '70%' },
     widthHandheld: { value: '{space.relative.full}' },
     widthMax: { value: '30rem' },
