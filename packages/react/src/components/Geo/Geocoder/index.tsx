33f1e3650eb1f893a76287db5c8ea79805284b07
Shane Laymance <shane.laymance@gmail.com>
2022-03-02 15:34:28 -0800

    Refactor geocoder with useControl

--- packages/react/src/components/Geo/Geocoder/index.tsx
+++ packages/react/src/components/Geo/Geocoder/index.tsx
@@ -1,52 +1,47 @@
 import maplibregl from 'maplibre-gl';
 import { createAmplifyGeocoder } from 'maplibre-gl-js-amplify';
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
+import { useControl } from 'react-map-gl';
 
 const GEOCODER_OPTIONS = {
   maplibregl,
+  marker: { color: '#4668F2' },
   popup: true,
+  showResultMarkers: { color: '#4668F2' },
   showResultsWhileTyping: true,
 };
 
 const GEOCODER_CONTAINER = 'geocoder-container';
 
 export const Geocoder = ({ position = 'top-right', style = {}, ...props }) => {
-  const geocoder: any = useRef();
+  if (props.mapRef) {
+    useControl(
+      () =>
+        createAmplifyGeocoder({
+          ...GEOCODER_OPTIONS,
+          ...props,
+        }) as any
+    );
+  } else {
+    useEffect(() => {
+      (
+        createAmplifyGeocoder({
+          ...GEOCODER_OPTIONS,
+          ...props,
+        }) as any
+      ).addTo(`#${GEOCODER_CONTAINER}`);
+    }, []);
+  }
 
   useEffect(() => {
-    const initializeGeocoder = () => {
-      const map = props.mapRef?.current.getMap();
-      geocoder.current = createAmplifyGeocoder({
-        ...GEOCODER_OPTIONS,
-        ...props,
-      });
-
-      map
-        ? map?.addControl(geocoder.current, position)
-        : geocoder.current.addTo(`#${GEOCODER_CONTAINER}`);
-
-      if (typeof window !== 'undefined') {
-        document
-          .querySelectorAll('.maplibregl-ctrl-geocoder')
-          .forEach((geocoderEl: HTMLElement) => {
-            geocoderEl.setAttribute('data-amplify-geocoder', '');
-            Object.assign(geocoderEl.style, style);
-          });
-      }
-    };
-
-    const removeGeocoder = () => {
-      if (props.mapRef?.current) {
-        const map = props.mapRef.current.getMap();
-        map?.removeControl(geocoder.current);
-      }
-    };
-
-    initializeGeocoder();
-
-    return () => {
-      removeGeocoder();
-    };
+    if (typeof window !== 'undefined') {
+      document
+        .querySelectorAll('.maplibregl-ctrl-geocoder')
+        .forEach((geocoderEl: HTMLElement) => {
+          geocoderEl.setAttribute('data-amplify-geocoder', '');
+          Object.assign(geocoderEl.style, style);
+        });
+    }
   }, [props.mapRef?.current]);
 
   return !props.mapRef ? <div id={GEOCODER_CONTAINER} /> : null;
