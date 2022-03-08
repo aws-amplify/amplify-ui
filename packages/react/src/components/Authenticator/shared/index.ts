4cf7eb39f84ba0563355128e0812e224326572da
wlee221 <willeea@amazon.com>
2022-03-07 18:08:44 -0800

    Remove UserNameAlias from react

--- packages/react/src/components/Authenticator/shared/index.ts
+++ packages/react/src/components/Authenticator/shared/index.ts
@@ -1,6 +1,5 @@
 export * from './ConfirmationCodeInput';
 export * from './ConfirmSignInFooter';
-export * from './UserNameAlias';
 export * from './TwoButtonSubmitFooter';
 export * from './RemoteErrorMessage';
 export * from './SignInSignUpTabs';
