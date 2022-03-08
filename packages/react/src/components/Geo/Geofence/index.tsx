12c2997b7c7457881c4b7bb18cdc0b2f4c3ec30b
Shane Laymance <shane.laymance@gmail.com>
2022-03-08 10:16:33 -0800

    Remove geofence component from ui-geo/main

--- packages/react/src/components/Geo/Geofence/index.tsx
+++
@@ -1,8 +0,0 @@
-import { AmplifyGeofenceControl } from 'maplibre-gl-js-amplify';
-import { useControl } from 'react-map-gl';
-
-export const Geofence = () => {
-  useControl(() => new AmplifyGeofenceControl() as any);
-
-  return null;
-};
