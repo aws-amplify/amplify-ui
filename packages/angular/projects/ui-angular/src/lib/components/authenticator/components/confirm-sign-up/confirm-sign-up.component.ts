82e2645e569a2195f8e4cbec81c3222ca4fba855
wlee221 <willeea@amazon.com>
2022-03-09 07:22:23 -0800

    Backwards compatibility in angular confirmSignUp

--- packages/angular/projects/ui-angular/src/lib/components/authenticator/components/confirm-sign-up/confirm-sign-up.component.ts
+++ packages/angular/projects/ui-angular/src/lib/components/authenticator/components/confirm-sign-up/confirm-sign-up.component.ts
@@ -3,8 +3,12 @@ import { AuthenticatorService } from '../../../../services/authenticator.service
 import {
   SortedFormFields,
   getFormDataFromEvent,
-  getSortedFormFields,
   translate,
+  getDefaultFormFields,
+  hasTranslation,
+  applyDefaults,
+  sortFormFields,
+  getActorContext,
 } from '@aws-amplify/ui';
 @Component({
   selector: 'amplify-confirm-sign-up',
@@ -38,8 +42,28 @@ export class ConfirmSignUpComponent implements OnInit {
   }
 
   public setFormFields() {
-    const _state = this.authenticator.authState;
-    this.formFields = getSortedFormFields('confirmSignUp', _state);
+    /**
+     * @todo(breaking): Angular has different placholder here from other frameworks.
+     *
+     * Translating here in a backwards-compatible manner, but should be resolved in next major version.
+     */
+    const state = this.authenticator.authState;
+
+    // backwards compatible placeholder text
+    const placeholder = !hasTranslation('Confirmation Code')
+      ? translate('Enter your code') // prioritize new placeholder
+      : translate('Confirmation Code'); // legacy placeholder
+    let defaultFormFields = getDefaultFormFields('confirmSignUp', state);
+
+    if (defaultFormFields.confirmation_code.placeholder) {
+      defaultFormFields.confirmation_code.placeholder = placeholder;
+    }
+
+    const customFormFields =
+      getActorContext(state).formFields?.confirmSignUp || {};
+
+    const formFields = applyDefaults(defaultFormFields, customFormFields);
+    this.formFields = sortFormFields(formFields);
   }
 
   public get context() {
