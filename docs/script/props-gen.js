e11d071468c817077a7ab55a6a419df21a86c1e3
MJ☔️ <o.6180339+github@gmail.com>
2022-03-16 23:36:06 +0000

    PoC: output props json

--- docs/script/props-gen.js
+++ docs/script/props-gen.js
@@ -2,19 +2,15 @@ const docgen = require('react-docgen-typescript');
 const path = require('path');
 const { readFileSync } = require('fs');
 
-// const filePath = '../../packages/react/src/primitives/types/test-component.tsx'; // works!! ✅
-// const filePath = '../../packages/react/src/primitives/types/test-component-index.tsx'; // works!! ✅
 const filePath = '../../packages/react/src/primitives/Button/index.ts';
-// const filePath = '../test-component.tsx';
-// const {
-//   ButtonProps,
-// } = require('@aws-amplify/ui-react/src/primitives/types/button');
 
 const pathToFile = path.join(__dirname, filePath);
-const file = readFileSync(pathToFile);
+
+const options = {
+  skipChildrenPropWithoutDoc: true,
+};
 
 // Parse a file for docgen info
 const res = docgen.parse(pathToFile);
 
-console.log('1111111111111', res);
-console.log('2222222222222', file);
+console.log('Output: ', JSON.stringify(res[0], null, 2));
