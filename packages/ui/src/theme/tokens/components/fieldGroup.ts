0d38977ddf23b8531c808e02349e966e62a14038
Scott Rees <6165315+reesscot@users.noreply.github.com>
2022-03-15 17:28:52 -0700

    chore: convert tokens to ts

---
+++ packages/ui/src/theme/tokens/components/fieldGroup.ts
@@ -0,0 +1,9 @@
+export const fieldgroup = {
+  gap: { value: 0 },
+  vertical: {
+    alignItems: { value: 'center' },
+  },
+  outer: {
+    alignItems: { value: 'center' },
+  },
+};
---
+++ packages/ui/src/theme/tokens/components/toggleButtonGroup.ts
@@ -0,0 +1,5 @@
+export const togglebuttongroup = {
+  alignItems: { value: 'center' },
+  alignContent: { value: 'center' },
+  justifyContent: { value: 'flex-start' },
+};
