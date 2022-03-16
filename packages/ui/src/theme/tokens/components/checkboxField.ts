a51224a2130da5875a22b885ba7618eecce7a4c5
Scott Rees <6165315+reesscot@users.noreply.github.com>
2022-03-16 09:20:25 -0700

    chore: convert component tokens to typescript (#1525)
    * chore: convert tokens to ts
    
    * docs: update theme links

---
+++ packages/ui/src/theme/tokens/components/checkboxField.ts
@@ -0,0 +1,6 @@
+export const checkboxfield = {
+  alignItems: { value: 'flex-start' },
+  alignContent: { value: 'center' },
+  flexDirection: { value: 'column' },
+  justifyContent: { value: 'center' },
+};
