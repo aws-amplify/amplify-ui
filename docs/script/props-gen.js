5684f8170065c8319f8a44f16bcd5edfd7bd22f5
MJ☔️ <o.6180339+github@gmail.com>
2022-03-17 17:24:45 +0000

    categorize props

--- docs/script/props-gen.js
+++ docs/script/props-gen.js
@@ -13,4 +13,16 @@ const options = {
 // Parse a file for docgen info
 const res = docgen.parse(pathToFile);
 
-console.log('Output: ', JSON.stringify(res[0], null, 2));
+const categorized = Object.entries(res[0].props).reduce((acc, curr) => {
+  const [key, val] = curr;
+  debugger;
+  return {
+    ...acc,
+    [val.declarations[0].name]: {
+      ...acc[val.declarations[0].name],
+      [key]: val,
+    },
+  };
+}, {});
+
+console.log(categorized);
