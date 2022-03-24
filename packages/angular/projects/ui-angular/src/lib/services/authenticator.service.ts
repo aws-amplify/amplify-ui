fc71b28c2c57d8c0a2d37b925eb6cc8136a829dd
wlee221 <willeea@amazon.com>
2022-03-24 05:29:40 -0700

    angular: send INIT event on 'setup' state

--- packages/angular/projects/ui-angular/src/lib/services/authenticator.service.ts
+++ packages/angular/projects/ui-angular/src/lib/services/authenticator.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable, OnDestroy, OnInit } from '@angular/core';
 import { Logger } from '@aws-amplify/core';
 import {
   AuthContext,
@@ -31,29 +31,10 @@ export class AuthenticatorService implements OnDestroy {
   private _hubSubscription: ReturnType<typeof listenToAuthHub>;
   private _facade: ReturnType<typeof getServiceContextFacade>;
 
-  public startMachine({
-    initialState,
-    loginMechanisms,
-    services,
-    signUpAttributes,
-    socialProviders,
-    formFields,
-  }: AuthenticatorMachineOptions) {
+  constructor() {
     const machine = createAuthenticatorMachine();
 
     const authService = interpret(machine).start();
-
-    authService.send({
-      type: 'INIT',
-      data: {
-        initialState,
-        loginMechanisms,
-        socialProviders,
-        signUpAttributes,
-        services,
-        formFields,
-      },
-    });
 
     this._machineSubscription = authService.subscribe((state) => {
       this._authState = state;
