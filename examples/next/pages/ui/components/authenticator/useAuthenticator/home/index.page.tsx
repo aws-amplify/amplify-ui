5a090b8a4aff40d75d81775ded14bc38dc159afa
wlee221 <willeea@amazon.com>
2022-03-24 04:38:57 -0700

    Configure Amplify on `/home`

--- examples/next/pages/ui/components/authenticator/useAuthenticator/home/index.page.tsx
+++ examples/next/pages/ui/components/authenticator/useAuthenticator/home/index.page.tsx
@@ -1,4 +1,8 @@
+import { Amplify } from 'aws-amplify';
 import { useAuthenticator } from '@aws-amplify/ui-react';
+import awsExports from '../aws-exports';
+
+Amplify.configure(awsExports);
 
 export default function App() {
   const { user } = useAuthenticator();
