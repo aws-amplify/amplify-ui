8ef71c17569424df7cfc430091894252fa983a3f
Chenwei Zhang <chenwz@amazon.com>
2022-03-28 16:23:07 -0700

    feat: adding determinate loader support

--- packages/react/src/primitives/Loader/Loader.tsx
+++ packages/react/src/primitives/Loader/Loader.tsx
@@ -11,10 +11,28 @@ export const LINEAR_FILLED = 'linear-filled';
 export const CIRCULAR_EMPTY = 'circular-empty';
 export const CIRCULAR_FILLED = 'circular-filled';
 
+// circumference = 2 * r * PI  (r = 42)
+export const CIRCUMFERENCE = 2 * 42 * Math.PI;
+
 const LoaderPrimitive: Primitive<LoaderProps, 'svg'> = (
-  { className, filledColor, emptyColor, size, variation, ...rest },
+  {
+    className,
+    filledColor,
+    emptyColor,
+    size,
+    variation,
+    isDeterminate = false,
+    isPercentageTextHidden = false,
+    percentage = 0,
+    ...rest
+  },
   ref
 ) => {
+  percentage = Math.min(percentage, 100);
+  percentage = Math.max(percentage, 0);
+
+  const percent = `${percentage}%`;
+
   const linearLoader = (
     <g>
       <line
@@ -27,12 +45,29 @@ const LoaderPrimitive: Primitive<LoaderProps, 'svg'> = (
       />
       <line
         x1="0"
-        x2="100%"
+        x2={isDeterminate ? percent : '100%'}
         y1="50%"
         y2="50%"
-        style={{ stroke: filledColor }}
+        style={{
+          // To get rid of the visible stroke linecap when percentage is 0
+          stroke: isDeterminate && percentage === 0 ? 'none' : filledColor,
+        }}
         data-testid={LINEAR_FILLED}
       />
+      {isDeterminate ? (
+        <text
+          aria-live="polite"
+          className={classNames(
+            ComponentClassNames.LoaderPercentageText,
+            isPercentageTextHidden ? ComponentClassNames.VisuallyHidden : null
+          )}
+          // -1% offset makes the text position look nicest
+          x={`${-1 + percentage}%`}
+          y="200%"
+        >
+          {percent}
+        </text>
+      ) : null}
     </g>
   );
 
@@ -52,16 +87,42 @@ const LoaderPrimitive: Primitive<LoaderProps, 'svg'> = (
         cy="50%"
         r="42%"
         strokeWidth="8%"
-        style={{ stroke: filledColor }}
+        style={{
+          stroke: filledColor,
+          strokeDasharray: isDeterminate
+            ? `${CIRCUMFERENCE}% ${CIRCUMFERENCE}%`
+            : undefined,
+          strokeDashoffset: isDeterminate
+            ? `${CIRCUMFERENCE - (CIRCUMFERENCE * percentage) / 100}%`
+            : undefined,
+        }}
         data-testid={CIRCULAR_FILLED}
       />
+      {isDeterminate ? (
+        <text
+          aria-live="polite"
+          className={classNames(
+            ComponentClassNames.LoaderPercentageText,
+            isPercentageTextHidden ? ComponentClassNames.VisuallyHidden : null
+          )}
+          // this x and y make text position look nicest
+          x="130%"
+          y="80%"
+        >
+          {percent}
+        </text>
+      ) : null}
     </g>
   );
 
   return (
     <View
       as="svg"
-      className={classNames(ComponentClassNames.Loader, className)}
+      className={classNames(
+        ComponentClassNames.Loader,
+        isDeterminate ? ComponentClassNames.LoaderDeterminate : null,
+        className
+      )}
       data-size={size}
       data-variation={variation}
       ref={ref}
