a0140b93ff46991f1a2b1e71c0378371d6dd6657
Chenwei Zhang <chenwz@amazon.com>
2022-03-28 16:24:19 -0700

    docs: updating docs

---
+++ docs/src/pages/components/loader/examples/LoaderVariationExample.tsx
@@ -0,0 +1,10 @@
+import { Loader } from '@aws-amplify/ui-react';
+
+export const LoaderVariationExample = () => {
+  return (
+    <>
+      <Loader />
+      <Loader variation="linear" />
+    </>
+  );
+};
