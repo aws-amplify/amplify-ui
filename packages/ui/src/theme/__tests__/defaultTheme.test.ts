0a7e67235dedffd186662c7db42f9a351664431d
Danny Banks <djb@amazon.com>
2022-04-06 13:06:01 -0700

    cleaning up tests

--- packages/ui/src/theme/__tests__/defaultTheme.test.ts
+++ packages/ui/src/theme/__tests__/defaultTheme.test.ts
@@ -1,7 +1,23 @@
-import { createTheme } from '../';
+import StyleDictionary from 'style-dictionary';
+import { defaultTheme, createTheme } from '../';
 
 describe('@aws-amplify/ui', () => {
   describe('defaultTheme', () => {
+    it('all references should resolve', () => {
+      expect(() => {
+        StyleDictionary.extend({
+          // @ts-ignore next-line
+          tokens: { ...defaultTheme.tokens },
+          platforms: {
+            // Need a platform for Style Dictionary to build so it can try to
+            // resolve references
+            test: {},
+          },
+        }).buildAllPlatforms();
+        // This will throw an error if there are any references that can't be resolved
+      }).not.toThrow();
+    });
+
     it('should match snapshot', () => {
       expect(createTheme().cssText).toMatchInlineSnapshot(`
         "[data-amplify-theme=\\"default-theme\\"] {
