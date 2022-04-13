0e18575f3eb44930d1a19814042360a5bb0cb1c7
Heather Buchel <buchel@amazon.com>
2022-04-13 12:53:36 -0400

    make checked follow _state pattern, organize file and update tests

--- packages/ui/src/theme/tokens/components/switchField.ts
+++ packages/ui/src/theme/tokens/components/switchField.ts
@@ -1,4 +1,30 @@
 export const switchfield = {
+  // Child elements
+  label: {
+    padding: { value: '{space.xs.value}' },
+  },
+
+  thumb: {
+    backgroundColor: { value: '{colors.background.primary.value}' },
+    borderColor: { value: '{colors.border.tertiary.value}' },
+    borderRadius: { value: '{radii.xxxl.value}' },
+    transition: {
+      duration: { value: '{time.medium.value}' },
+    },
+    width: { value: '{space.relative.medium.value}' },
+  },
+
+  track: {
+    backgroundColor: { value: '{colors.background.tertiary.value}' },
+    borderRadius: { value: '{radii.xxxl.value}' },
+    height: { value: '{space.relative.medium.value}' },
+    padding: { value: '{outlineWidths.medium.value}' },
+    transition: {
+      duration: { value: '{time.short.value}' },
+    },
+    width: { value: '{space.relative.xl.value}' },
+  },
+
   // States
   _disabled: {
     opacity: { value: '{opacities.60.value}' },
@@ -16,6 +42,14 @@ export const switchfield = {
       },
     },
   },
+  _checked: {
+    thumb: {
+      transform: { value: '{transforms.slideX.medium.value}' },
+    },
+    track: {
+      backgroundColor: { value: '{colors.brand.primary.60.value}' },
+    },
+  },
 
   // Sizes
   large: {
@@ -23,37 +57,5 @@ export const switchfield = {
   },
   small: {
     fontSize: { value: '{fontSizes.small.value}' },
-  },
-
-  // Child elements
-  label: {
-    padding: { value: '{space.xs.value}' },
-  },
-
-  thumb: {
-    backgroundColor: { value: '{colors.background.primary.value}' },
-    borderColor: { value: '{colors.border.tertiary.value}' },
-    borderRadius: { value: '{radii.xxxl.value}' },
-    checked: {
-      transform: { value: '{transforms.slideX.medium.value}' },
-    },
-    transition: {
-      duration: { value: '{time.medium.value}' },
-    },
-    width: { value: '{space.relative.medium.value}' },
-  },
-
-  track: {
-    backgroundColor: { value: '{colors.background.tertiary.value}' },
-    borderRadius: { value: '{radii.xxxl.value}' },
-    checked: {
-      backgroundColor: { value: '{colors.brand.primary.60.value}' },
-    },
-    height: { value: '{space.relative.medium.value}' },
-    padding: { value: '{outlineWidths.medium.value}' },
-    transition: {
-      duration: { value: '{time.short.value}' },
-    },
-    width: { value: '{space.relative.xl.value}' },
   },
 };
