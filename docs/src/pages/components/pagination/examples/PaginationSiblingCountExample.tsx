5005cb4dfb18a8b4f2c8eb73e799254c6548fd3c
Joe Buono <joebuono@amazon.com>
2022-04-15 17:14:27 -0500

    demonstrate usePagination hook, and include Theme example for styling

--- docs/src/pages/components/pagination/examples/PaginationSiblingCountExample.tsx
+++ docs/src/pages/components/pagination/examples/PaginationSiblingCountExample.tsx
@@ -1,33 +1,11 @@
-import * as React from 'react';
-import { Pagination } from '@aws-amplify/ui-react';
+import { Pagination, usePagination } from '@aws-amplify/ui-react';
 
 export const PaginationSiblingCountExample = () => {
-  const [currentPageIndex, setCurrentPageIndex] = React.useState(5);
-  const totalPages = 10;
-  const numberOfSiblingsOnEachSideOfCurrentPage = 2;
+  const paginationProps = usePagination({
+    totalPages: 11,
+    currentPage: 5,
+    siblingCount: 2,
+  });
 
-  const handleNextPage = () => {
-    if (currentPageIndex < totalPages) {
-      setCurrentPageIndex(currentPageIndex + 1);
-    }
-  };
-
-  const handlePreviousPage = () => {
-    if (currentPageIndex > 1) {
-      setCurrentPageIndex(currentPageIndex - 1);
-    }
-  };
-
-  const handleOnChange = (pageIndex) => setCurrentPageIndex(pageIndex);
-
-  return (
-    <Pagination
-      currentPage={currentPageIndex}
-      totalPages={totalPages}
-      siblingCount={numberOfSiblingsOnEachSideOfCurrentPage}
-      onNext={handleNextPage}
-      onPrevious={handlePreviousPage}
-      onChange={handleOnChange}
-    />
-  );
+  return <Pagination {...paginationProps} />;
 };
