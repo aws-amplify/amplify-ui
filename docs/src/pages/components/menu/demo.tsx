a253fc412008aeb449a6fb0a6d3beac54ef33200
Scott Rees <6165315+reesscot@users.noreply.github.com>
2022-04-18 09:14:21 -0700

    address feedback

--- docs/src/pages/components/menu/demo.tsx
+++ docs/src/pages/components/menu/demo.tsx
@@ -6,27 +6,27 @@ import { MenuPropControls, MenuPropControlsProps } from './MenuPropControls';
 import { useMenuProps } from './useMenuProps';
 
 const propsToCode = (props: MenuPropControlsProps) => {
-  const size = props.size ? `\n  size=${props.size}` : '';
+  const size = props.size ? `\n  size="${props.size}"` : '';
   return `<Menu ${size}
-  menuAlign={${props.menuAlign}}
+  menuAlign="${props.menuAlign}"
   >
-      <MenuItem onClick={() => alert('Download')}>
-        Download
-      </MenuItem>
-      <MenuItem onClick={() => alert('Create a Copy')}>
-        Create a Copy
-      </MenuItem>
-      <MenuItem onClick={() => alert('Mark as Draft')}>
-        Mark as Draft
-      </MenuItem>
-      <Divider />
-      <MenuItem isDisabled onClick={() => alert('Delete')}>
-        Delete
-      </MenuItem>
-      <MenuItem onClick={() => alert('Attend a workshop')}>
-        Attend a workshop
-      </MenuItem>
-  </Menu>`;
+  <MenuItem onClick={() => alert('Download')}>
+    Download
+  </MenuItem>
+  <MenuItem onClick={() => alert('Create a Copy')}>
+    Create a Copy
+  </MenuItem>
+  <MenuItem onClick={() => alert('Mark as Draft')}>
+    Mark as Draft
+  </MenuItem>
+  <Divider />
+  <MenuItem isDisabled onClick={() => alert('Delete')}>
+    Delete
+  </MenuItem>
+  <MenuItem onClick={() => alert('Attend a workshop')}>
+    Attend a workshop
+  </MenuItem>
+</Menu>`;
 };
 
 export const MenuDemo = () => {
