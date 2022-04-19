fb7b9005734cf00dfed178490e256394db065d2f
Caleb Pollman <cpollman@amazon.com>
2022-04-19 15:23:17 -0700

    chore(ui-react): type withAuthenticator

--- packages/react/src/components/Authenticator/withAuthenticator.tsx
+++ packages/react/src/components/Authenticator/withAuthenticator.tsx
@@ -2,13 +2,13 @@ import { Authenticator, AuthenticatorProps } from './Authenticator';
 
 export type WithAuthenticatorOptions = Omit<AuthenticatorProps, 'children'>;
 
-export function withAuthenticator(
-  Component,
+export function withAuthenticator<T>(
+  Component: (props?: T) => JSX.Element,
   options: WithAuthenticatorOptions = {}
 ) {
   const { variation = 'modal' } = options;
 
-  return function WrappedWithAuthenticator(props) {
+  return function WrappedWithAuthenticator(props: T) {
     return (
       <Authenticator variation={variation} {...options}>
         {(authenticator) => <Component {...props} {...authenticator} />}
