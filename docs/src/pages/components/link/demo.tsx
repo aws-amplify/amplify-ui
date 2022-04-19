ca90b513ffc8493fc96af38f1edea2760703feaf
Chenwei Zhang <40295569+zchenwei@users.noreply.github.com>
2022-04-19 15:16:25 -0700

    docs: updating Link docs (#1709)
    * docs: updating Link docs
    
    * chore: addressing comments

--- docs/src/pages/components/link/demo.tsx
+++ docs/src/pages/components/link/demo.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
-import { Link, Text, View } from '@aws-amplify/ui-react';
+import { Link, LinkProps, Text, useTheme } from '@aws-amplify/ui-react';
 
-import { LinkPropControls } from '@/components/LinkPropControls';
-import { useLinkProps } from '@/components/useLinkProps';
-import { Example } from '@/components/Example';
+import { Demo } from '@/components/Demo';
+import { LinkPropControls } from './LinkPropControls';
+import { useLinkProps } from './useLinkProps';
+
+const propsToCode = (props: LinkProps) => {
+  return (
+    '<Link' +
+    (props.color ? `\n color="${props.color}"` : '') +
+    (props.textDecoration
+      ? `\n textDecoration="${props.textDecoration}"`
+      : '') +
+    (props.isExternal ? `\n isExternal={${props.isExternal}}` : '') +
+    '\n>' +
+    `\n ${props.children}\n</Link>`
+  );
+};
 
 export const LinkDemo = () => {
   const linkProps = useLinkProps({
@@ -13,22 +26,29 @@ export const LinkDemo = () => {
     children: 'My Demo Link',
   });
 
+  const { tokens } = useTheme();
+
   return (
-    <View>
-      <LinkPropControls {...linkProps} />
-      <Example>
+    <Demo
+      code={propsToCode(linkProps)}
+      propControls={<LinkPropControls {...linkProps} />}
+    >
+      <>
         <Link
           color={linkProps.color}
           isExternal={linkProps.isExternal}
           textDecoration={linkProps.textDecoration}
         >
-          {linkProps.children}
+          My Demo Link
         </Link>
-        <Text fontSize={'.75em'}>
-          <sup>*</sup>Rel Attribute=
-          {linkProps.isExternal ? 'noopener noreferrer' : ''}
+        <Text fontSize={tokens.space.relative.small}>
+          {linkProps.isExternal ? (
+            <>
+              <sup>*</sup>Rel Attribute=noopener noreferrer
+            </>
+          ) : null}
         </Text>
-      </Example>
-    </View>
+      </>
+    </Demo>
   );
 };
