afdb73ad65b95423e90ba45eb7ea48599b28c4c0
Chenwei Zhang <chenwz@amazon.com>
2022-04-19 15:53:06 -0700

    chore: addressing comments

--- docs/src/pages/components/placeholder/examples/PlaceholderIsLoadedExample.tsx
+++ docs/src/pages/components/placeholder/examples/PlaceholderIsLoadedExample.tsx
@@ -1,5 +1,5 @@
 import { Placeholder } from '@aws-amplify/ui-react';
 
 export const PlaceholderIsLoadedExample = () => {
-  return <Placeholder isLoaded />;
+  return <Placeholder isLoaded={true} />;
 };
