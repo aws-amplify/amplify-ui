4607486e78c5dc05672a76e2e9e513525afdd17e
Joe Buono <joebuono724@gmail.com>
2022-04-19 14:38:40 -0400

    docs: update Pagination (#1695)
    * refactoring main pagination demo
    
    * more pagination examples
    
    * pagination styling examples
    
    * adjust styles
    
    * demonstrate usePagination hook, and include Theme example for styling
    
    * remove extra backticks
    
    * remove empty code section
    
    Co-authored-by: Joe Buono <joebuono@amazon.com>

--- docs/src/pages/components/pagination/demo.tsx
+++ docs/src/pages/components/pagination/demo.tsx
@@ -1,122 +1,41 @@
-import React, { useState, useCallback } from 'react';
+import { Pagination } from '@aws-amplify/ui-react';
+import { Demo } from '@/components/Demo';
+import { PaginationPropControls } from './PaginationPropControls';
+import { usePaginationProps } from './usePaginationProps';
 
-import { Flex, Pagination } from '@aws-amplify/ui-react';
-import { Example } from '@/components/Example';
-
-interface PaginationDemoProps {
-  isDemo?: boolean;
-  defaultCurrentPage?: number;
-  defaultTotalPages?: number;
-  defaultSiblingCount?: number;
-  defaultHasMorePages?: boolean;
-}
-export const PaginationDemo: React.FC<PaginationDemoProps> = (props) => {
-  const {
-    isDemo = true,
-    defaultCurrentPage = 1,
-    defaultTotalPages = 10,
-    defaultSiblingCount = 1,
-    defaultHasMorePages = false,
-    ...rest
-  } = props;
-
-  const [currentPage, setCurrentPage] = useState(defaultCurrentPage);
-  const [totalPages, setTotalPages] = useState(defaultTotalPages);
-  const [siblingCount, setSiblingCount] = useState(defaultSiblingCount);
-  const [hasMorePages, setHasMorePages] = useState(defaultHasMorePages);
-
-  const onNext = useCallback(() => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  }, [currentPage, totalPages]);
-
-  const onPrev = useCallback(() => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  }, [currentPage]);
-
-  const onChange = useCallback((newPage, prevPage) => {
-    setCurrentPage(newPage);
-  }, []);
+const propsToCode = (paginationProps) => {
   return (
-    <div className="amplify-pagination-demo">
-      {isDemo ? (
-        <Flex justifyContent="center" direction="column">
-          <Flex justifyContent="center">
-            <Example>
-              <Pagination
-                currentPage={currentPage}
-                totalPages={totalPages}
-                siblingCount={siblingCount}
-                hasMorePages={hasMorePages}
-                onNext={onNext}
-                onPrevious={onPrev}
-                onChange={onChange}
-                {...rest}
-              />
-            </Example>
-          </Flex>
-          <Flex justifyContent="center" alignItems="center">
-            <label htmlFor="current-page">currentPage</label>
-            <input
-              type="text"
-              id="current-page"
-              value={currentPage}
-              placeholder="Enter current page"
-              onChange={(e) => {
-                const newCurrentPage = isNaN(Number(e.target.value))
-                  ? defaultCurrentPage
-                  : Number(e.target.value);
-                setCurrentPage(newCurrentPage);
-              }}
-            />
-          </Flex>
-          <Flex justifyContent="center" alignItems="center">
-            <label htmlFor="total-pages">totalPages</label>
-            <input
-              type="text"
-              id="total-pages"
-              value={totalPages}
-              placeholder="Enter total pages"
-              onChange={(e) => {
-                const newTotalPages = isNaN(Number(e.target.value))
-                  ? defaultTotalPages
-                  : Number(e.target.value);
-                setTotalPages(newTotalPages);
-              }}
-            />
-          </Flex>
-          <Flex justifyContent="center" alignItems="center">
-            <label htmlFor="sibling-count">siblingCount</label>
-            <input
-              type="text"
-              id="sibling-count"
-              value={siblingCount}
-              placeholder="Enter sibling count"
-              onChange={(e) => {
-                const newSiblingCount = isNaN(Number(e.target.value))
-                  ? defaultSiblingCount
-                  : Number(e.target.value);
-                setSiblingCount(newSiblingCount);
-              }}
-            />
-          </Flex>
-          <Flex justifyContent="center" alignItems="center">
-            <label htmlFor="hasMorePages">hasMorePages</label>
-            <input
-              type="checkbox"
-              id="hasMorePages"
-              checked={hasMorePages}
-              placeholder="Enter hasMorePages"
-              onChange={(e) => {
-                setHasMorePages(!hasMorePages);
-              }}
-            />
-          </Flex>
-        </Flex>
-      ) : null}
-    </div>
+    `<Pagination
+  currentPage={${paginationProps.currentPage}}
+  totalPages={${paginationProps.totalPages}}
+  siblingCount={${paginationProps.siblingCount}}` +
+    (paginationProps.hasMorePages ? `\n  hasMorePages` : '') +
+    `\n/>`
+  );
+};
+
+export const PaginationDemo = () => {
+  const paginationProps = usePaginationProps({
+    currentPage: 1,
+    totalPages: 10,
+    siblingCount: 1,
+    hasMorePages: false,
+  });
+
+  return (
+    <Demo
+      code={propsToCode(paginationProps)}
+      propControls={<PaginationPropControls {...paginationProps} />}
+    >
+      <Pagination
+        currentPage={paginationProps.currentPage}
+        totalPages={paginationProps.totalPages}
+        siblingCount={paginationProps.siblingCount}
+        hasMorePages={paginationProps.hasMorePages}
+        onNext={paginationProps.onNext}
+        onPrevious={paginationProps.onPrevious}
+        onChange={paginationProps.onChange}
+      />
+    </Demo>
   );
 };
