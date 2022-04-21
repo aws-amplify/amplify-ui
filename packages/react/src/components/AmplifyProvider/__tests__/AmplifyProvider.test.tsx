7fb111b16e4be7320caea314b7465e2f2eede4d5
Joe Buono <joebuono@amazon.com>
2022-04-21 14:58:32 -0500

    alternate solution to using dangerouslySetInnerHTML

--- packages/react/src/components/AmplifyProvider/__tests__/AmplifyProvider.test.tsx
+++ packages/react/src/components/AmplifyProvider/__tests__/AmplifyProvider.test.tsx
@@ -38,49 +38,4 @@ describe('AmplifyProvider', () => {
       ''
     );
   });
-
-  it('filters out XSS attacks which attempt to escape the CSS context', async () => {
-    const name = 'maliciousTheme';
-    const xss = `</style><script>alert('XSS')</script>`;
-    const maliciousTheme: Theme = {
-      name,
-      tokens: {
-        colors: {
-          font: { primary: { value: 'pink' }, secondary: { value: xss } },
-        },
-      },
-    };
-
-    const { container } = render(
-      <AmplifyProvider theme={maliciousTheme}>
-        <App />
-      </AmplifyProvider>
-    );
-
-    const styleTag = container.querySelector(`#amplify-theme-${name}`);
-    expect(styleTag).toBe(null);
-  });
-
-  it('filters out XSS attacks which attempt to escape the CSS context using whitespace characters', async () => {
-    const name = 'maliciousTheme';
-    const xss = `</style
-    ><script>alert('XSS')</script>`;
-    const maliciousTheme: Theme = {
-      name,
-      tokens: {
-        colors: {
-          font: { primary: { value: 'pink' }, secondary: { value: xss } },
-        },
-      },
-    };
-
-    const { container } = render(
-      <AmplifyProvider theme={maliciousTheme}>
-        <App />
-      </AmplifyProvider>
-    );
-
-    const styleTag = container.querySelector(`#amplify-theme-${name}`);
-    expect(styleTag).toBe(null);
-  });
 });
