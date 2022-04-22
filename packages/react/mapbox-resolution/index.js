6bb2292b82be86ea226d65e6a060309dae50418e
Shane Laymance <shane.laymance@gmail.com>
2022-04-22 15:46:32 -0700

    Replace mapbox resolution with a local file

---
+++ packages/react/mapbox-resolution/index.js
@@ -0,0 +1,6 @@
+/**
+ * This empty file is used in our `package.json` to resolve the `mapbox-gl` module. It provides a resolution for
+ * bundlers like Webpack which treats a dynamically imported module like `mapbox-gl` as a separate bundle even if it
+ * isn't used.
+ */
+module.exports = {};
