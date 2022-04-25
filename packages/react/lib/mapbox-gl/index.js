e3048d4d2923f43130341de6db20a936ba6361ad
Shane Laymance <shane.laymance@gmail.com>
2022-04-25 11:49:48 -0700

    Move mapbox resolution to lib dir

--- packages/react/lib/mapbox-gl/index.js
+++ packages/react/lib/mapbox-gl/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /**
  * This empty file is used in our `package.json` to resolve the `mapbox-gl` module. It provides a resolution for
  * bundlers like Webpack which treats a dynamically imported module like `mapbox-gl` as a separate bundle even if it
