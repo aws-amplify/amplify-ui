34b22d43f9df8c0e779267ddfd37e54c887fbdcf
Danny Banks <djb@amazon.com>
2022-04-27 11:15:32 -0700

    Updating snippets

--- docs/playroom/snippets.ts
+++ docs/playroom/snippets.ts
@@ -1,30 +1,3 @@
 import { alertSnippets } from '../src/pages/components/alert/snippets';
 
 export default [...alertSnippets];
-
-// import {
-//   baseComponents,
-//   connectedComponents,
-//   inputComponents,
-//   navigationComponents,
-//   layoutComponents
-// } from '../src/data/links';
-
-// export default [
-//   ...baseComponents.map(navItem => {
-//     return {
-//       group: 'Base',
-//       name: navItem.label,
-//       code: `
-//         <${navItem.label}>Hello world</${navItem.label}>
-//       `
-//     }
-//   }),
-//   ...inputComponents.map(navItem => {
-//     return {
-//       group: 'Input',
-//       name: navItem.label,
-//       code:
-//     }
-//   })
-// ]
