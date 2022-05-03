d2d1c43b81538375ea8008473e7995fea7b11874
Joe Buono <joebuono@amazon.com>
2022-05-03 13:36:50 -0500

    added explanation for using Map

--- docs/src/utils/demoState.ts
+++ docs/src/utils/demoState.ts
@@ -1,5 +1,13 @@
-// Add a comment explaining your reasoning
-// We have to use 'any' as well (try it out on Alert to see why)
-// The key is the component's display name
-// The value is the component demo's props
+/*
+
+We are using a Map as a lightweight solution to persist component demo state across NextJS re-renders without the complexity of state management (e.g., Redux). 
+
+We are not using React Context for two reasons (based on the React Context docs: https://reactjs.org/docs/context.html):
+- Since the props for each component's demo do not need to be shared across the entire docs site, 
+  these props aren't "global" state in the same way that a current authenticated user, theme, or preferred language would be
+- React Context is primarily used when some data needs to be accessible by many components at different nesting levels, which does not match our use case
+
+The Map's key-value pairs are the component's name and its demo's props. 
+*/
+
 export const demoState = new Map<string, any>();
