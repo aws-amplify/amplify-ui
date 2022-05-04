4af75cff2f60e88aa61f01853a1128607a8462ec
wlee221 <willeea@amazon.com>
2022-05-04 00:40:50 -0700

    Remove unused imports

--- packages/angular/projects/ui-angular/src/lib/components/authenticator/components/authenticator/authenticator.component.ts
+++ packages/angular/projects/ui-angular/src/lib/components/authenticator/components/authenticator/authenticator.component.ts
@@ -9,14 +9,11 @@ import {
   TemplateRef,
   ViewEncapsulation,
 } from '@angular/core';
-
-import { Hub } from 'aws-amplify';
 import {
   AuthenticatorMachineOptions,
   SocialProvider,
   translate,
 } from '@aws-amplify/ui';
-
 import { AmplifySlotDirective } from '../../../../utilities/amplify-slot/amplify-slot.directive';
 import { CustomComponentsService } from '../../../../services/custom-components.service';
 import { AuthenticatorService } from '../../../../services/authenticator.service';
