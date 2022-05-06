43682d05ecf5f7ab83817f701787a3a3bc1cdc0c
Scott Rees <6165315+reesscot@users.noreply.github.com>
2022-05-05 21:18:10 -0700

    fix: update docs, make orientation deprecated

--- docs/src/pages/components/scrollview/demo.tsx
+++ docs/src/pages/components/scrollview/demo.tsx
@@ -1,35 +1,28 @@
-import { Image, ScrollView, ScrollViewProps } from '@aws-amplify/ui-react';
+import { Image, ScrollView, useTheme } from '@aws-amplify/ui-react';
 
 import { Demo } from '@/components/Demo';
-import { useScrollViewProps } from './useScrollViewProps';
-import { ScrollViewPropControls } from './ScrollViewPropControls';
 
-const propsToCode = (props: ScrollViewProps) => {
-  return `<ScrollView
-  orientation="${props.orientation}"
-  height="300px"
-  width="400px"
->
+export const ScrollViewDemo = () => {
+  const {
+    tokens: { colors },
+  } = useTheme();
+  return (
+    <Demo
+      code={`
+<ScrollView width="100%" height="300px" maxWidth="580px">
   <Image
     width="800px"
     maxWidth="800px"
     src="/amplify-logo.svg"
     alt="Amplify-logo"
   />
-</ScrollView>`;
-};
-
-export const ScrollViewDemo = () => {
-  const scrollViewProps = useScrollViewProps({ orientation: 'horizontal' });
-  return (
-    <Demo
-      code={propsToCode(scrollViewProps)}
-      propControls={<ScrollViewPropControls {...scrollViewProps} />}
+</ScrollView>`}
     >
       <ScrollView
-        orientation={scrollViewProps.orientation}
+        width="100%"
         height="300px"
-        width="400px"
+        maxWidth="580px"
+        backgroundColor={colors.neutral[10]}
       >
         <Image
           width="800px"
