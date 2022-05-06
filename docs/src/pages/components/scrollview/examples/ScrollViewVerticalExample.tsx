286e7df790a103d164cd8424161a9cd6dc4483e3
Scott Rees <6165315+reesscot@users.noreply.github.com>
2022-05-06 11:47:20 -0700

    fix: scrollview scrollbars always showing (#1835)

--- docs/src/pages/components/scrollview/examples/ScrollViewVerticalExample.tsx
+++ docs/src/pages/components/scrollview/examples/ScrollViewVerticalExample.tsx
@@ -2,7 +2,7 @@ import { ScrollView } from '@aws-amplify/ui-react';
 
 export const ScrollViewVerticalExample = () => {
   return (
-    <ScrollView height="100px" width="200px" orientation="vertical">
+    <ScrollView height="100px" width="200px">
       {`Michaelmas term lately over, and the Lord Chancellor sitting in Lincoln's
       Inn Hall. Implacable November weather. As much mud in the streets as if
       the waters had but newly retired from the face of the earth.`}
