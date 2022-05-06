be965466dd4eca1d5a73b90acc4709fa0a2d95de
MJ☔ <zhamujun@amazon.com>
2022-05-05 21:06:18 -0700

    fix meta-info

--- docs/src/pages/_app.page.tsx
+++ docs/src/pages/_app.page.tsx
@@ -7,7 +7,7 @@ import { configure, trackPageVisit } from '../utils/track';
 
 import Head from 'next/head';
 import { Header } from '@/components/Layout/Header';
-import { META_INFO } from '@/data/meta';
+import { META_INFO } from '@/data/meta'; // TODO: use data generated from pages.preval.ts instead
 import Script from 'next/script';
 import { theme } from '../theme';
 import { useRouter } from 'next/router';
@@ -18,11 +18,17 @@ import { useRouter } from 'next/router';
 if (typeof window === 'undefined') React.useLayoutEffect = React.useEffect;
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const { platform = 'react' } = router.query;
+  const {
+    pathname,
+    query: { platform = 'react' },
+  } = useRouter();
+  const filepath = `/${pathname
+    .split('/')
+    .filter((n) => n && n !== '[platform]')
+    .join('/')}`;
   const [colorMode, setColorMode] = React.useState<ColorMode>('system');
   const [themeOverride, setThemeOverride] = React.useState('');
-
+  console.log(pathname, filepath);
   React.useEffect(() => {
     document.documentElement.setAttribute(
       'data-amplify-theme-override',
@@ -33,23 +39,19 @@ function MyApp({ Component, pageProps }) {
   configure();
   trackPageVisit();
 
-  // if (
-  //   !META_INFO[router.pathname]?.description ||
-  //   !META_INFO[router.pathname]?.title
-  // ) {
-  //   throw new Error(`Meta Info missing on ${router.pathname}`);
-  // }
+  if (!META_INFO[filepath]?.description || !META_INFO[filepath]?.title) {
+    throw new Error(`Meta Info missing on ${filepath}`);
+  }
 
   return (
     <>
       <Head>
-        <title>{META_INFO[router.pathname]?.title} | Amplify UI</title>
+        <title>
+          {META_INFO[filepath]?.title} | {platform} | Amplify UI
+        </title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {META_INFO[router.pathname] && (
-          <meta
-            name="description"
-            content={META_INFO[router.pathname].description}
-          />
+        {META_INFO[filepath] && (
+          <meta name="description" content={META_INFO[filepath].description} />
         )}
       </Head>
       <AmplifyProvider theme={theme} colorMode={colorMode}>
