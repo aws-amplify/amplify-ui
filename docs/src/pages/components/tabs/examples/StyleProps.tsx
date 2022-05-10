1928bedb77339c6cc5e2d0025c65871829586c79
Heather Buchel <buchel@amazon.com>
2022-05-10 15:44:22 -0400

    update style props example to use tokens with useTheme()

--- docs/src/pages/components/tabs/examples/StyleProps.tsx
+++ docs/src/pages/components/tabs/examples/StyleProps.tsx
@@ -1,20 +1,23 @@
-import { Tabs, TabItem } from '@aws-amplify/ui-react';
+import { Tabs, TabItem, useTheme } from '@aws-amplify/ui-react';
 
-export const StyleProps = () => (
-  <Tabs backgroundColor="var(--amplify-colors-background-primary)">
-    <TabItem
-      title="Tab 1"
-      color="var(--amplify-colors-font-primary)"
-      backgroundColor="transparent"
-    >
-      Content of Tab 1
-    </TabItem>
-    <TabItem
-      title="Tab 2"
-      color="var(--amplify-colors-font-primary)"
-      backgroundColor="transparent"
-    >
-      Content of Tab 2
-    </TabItem>
-  </Tabs>
-);
+export const StyleProps = () => {
+  const { tokens } = useTheme();
+  return (
+    <Tabs backgroundColor={tokens.colors.neutral[10]}>
+      <TabItem
+        title="Tab 1"
+        color={tokens.colors.font.secondary}
+        backgroundColor="transparent"
+      >
+        Content of Tab 1
+      </TabItem>
+      <TabItem
+        title="Tab 2"
+        color={tokens.colors.brand.secondary[60]}
+        backgroundColor="transparent"
+      >
+        Content of Tab 2
+      </TabItem>
+    </Tabs>
+  );
+};
