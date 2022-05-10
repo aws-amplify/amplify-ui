46ff53920c8514c015a566ab1600d8d7e0926085
Joe Buono <joebuono@amazon.com>
2022-05-10 16:50:26 -0500

    dial codes

--- docs/src/pages/components/phonenumberfield/examples/CountryCodeSelectExample.tsx
+++ docs/src/pages/components/phonenumberfield/examples/CountryCodeSelectExample.tsx
@@ -4,6 +4,7 @@ export const CountryCodeSelectExample = () => (
   <PhoneNumberField
     label="Phone Number"
     defaultCountryCode="+7"
+    dialCodeList={['+1', '+44', '+52', '+86', '+91']}
     countryCodeName="country_code"
     countryCodeLabel="Country Code"
     onCountryCodeChange={(e) =>
