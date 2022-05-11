d9ba5d81b85b488746dd8300092a347f5391408d
Danny Banks <djb@amazon.com>
2022-05-11 14:43:41 -0700

    Undoing some in-progress stuff not related

--- docs/src/pages/theming/default-theme/themeStructure.tsx
+++ docs/src/pages/theming/default-theme/themeStructure.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useTheme, Heading, View } from '@aws-amplify/ui-react';
+import { useTheme } from '@aws-amplify/ui-react';
 
 function flattenTheme(obj, arr = []) {
   if (obj.hasOwnProperty('value')) {
@@ -15,77 +15,26 @@ function flattenTheme(obj, arr = []) {
   return arr;
 }
 
-const Token = ({ name, value }) => {
-  return (
-    <div>
-      {name} | {value}
-    </div>
-  );
-};
-
-const Section = ({ heading, tokens }) => {
-  return (
-    <View as="section">
-      <Heading level={3}>{heading}</Heading>
-      {tokens.map(({ name, value }) => (
-        <Token key={name} name={name} value={value} />
-      ))}
-    </View>
-  );
-};
-
 export const ThemeStructure = () => {
-  // const allTokens = flattenTheme(useTheme().tokens);
-  const { tokens } = useTheme();
-  const componentSections = Object.keys(tokens.components).map((key) => {
-    return {
-      heading: key,
-      tokens: flattenTheme(tokens.components[key]),
-    };
-  });
-
+  const allTokens = flattenTheme(useTheme().tokens);
   return (
-    <>
-      <Heading level={2}>Components</Heading>
-      <table>
-        <tbody>
-          {componentSections.map(({ heading, tokens }) => (
-            <>
-              <tr>
-                <td colSpan={2}>{heading}</td>
-              </tr>
-              {tokens.map(({ name, value }) => (
-                <tr>
-                  <td>{name}</td>
-                  <td>{value}</td>
-                </tr>
-              ))}
-            </>
-          ))}
-        </tbody>
-      </table>
-      {/* {componentSections.map(({heading,tokens}) => <Section heading={heading} tokens={tokens} />)} */}
-    </>
+    <table>
+      <thead>
+        <tr>
+          <td>Name</td>
+          <td>Value</td>
+        </tr>
+      </thead>
+      {allTokens.map(({ name, value }) => (
+        <tr key={name}>
+          <td>
+            <code>{name}</code>
+          </td>
+          <td>
+            <code>{value}</code>
+          </td>
+        </tr>
+      ))}
+    </table>
   );
-
-  // return (
-  //   <table>
-  //     <thead>
-  //       <tr>
-  //         <td>Name</td>
-  //         <td>Value</td>
-  //       </tr>
-  //     </thead>
-  //     {allTokens.map(({ name, value }) => (
-  //       <tr key={name}>
-  //         <td>
-  //           <code>{name}</code>
-  //         </td>
-  //         <td>
-  //           <code>{value}</code>
-  //         </td>
-  //       </tr>
-  //     ))}
-  //   </table>
-  // );
 };
