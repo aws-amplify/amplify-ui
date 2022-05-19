d6743bc6d089cb42820078ab0ab8a5973619ff1d
Shane Laymance <shane.laymance@gmail.com>
2022-05-19 13:03:15 -0700

    Refactor examples to use LocationSearch component

--- examples/next/pages/ui/components/geo/map-with-location-search/index.page.tsx
+++ examples/next/pages/ui/components/geo/map-with-location-search/index.page.tsx
@@ -1,4 +1,4 @@
-import { MapView, Geocoder } from '@aws-amplify/ui-react';
+import { MapView, LocationSearch } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 
 import '@aws-amplify/ui-react/styles.css';
@@ -7,10 +7,10 @@ import awsExports from './aws-exports';
 
 Amplify.configure(awsExports);
 
-export default function MapWithGeocoder() {
+export default function MapWithLocationSearch() {
   return (
     <MapView>
-      <Geocoder />
+      <LocationSearch />
     </MapView>
   );
 }
