411f5e957e8a03c7d633b1b337ae21822007922e
Shane Laymance <shane.laymance@gmail.com>
2022-05-23 23:14:28 -0700

    Type the mapRef in useRef hook

--- examples/next/pages/ui/components/geo/map-with-forward-ref/index.page.tsx
+++ examples/next/pages/ui/components/geo/map-with-forward-ref/index.page.tsx
@@ -1,6 +1,7 @@
 import { Button, MapView } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import { useCallback, useRef } from 'react';
+import type { MapRef } from 'react-map-gl';
 
 import '@aws-amplify/ui-react/styles.css';
 
@@ -9,7 +10,7 @@ import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 export default function MapWithRef() {
-  const mapRef = useRef();
+  const mapRef = useRef<MapRef>();
 
   const flyToMordor = useCallback(() => {
     mapRef.current.flyTo({ center: [172.78, -42.28], zoom: 5 });
