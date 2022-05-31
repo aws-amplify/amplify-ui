358918f5ca500ea951d231cc41919930b64f6dfc
Joe Buono <joebuono@amazon.com>
2022-05-31 15:03:15 -0500

    adjusting description

--- docs/src/pages/[platform]/getting-started/usage/examples/ReactExampleCode.tsx
+++ docs/src/pages/[platform]/getting-started/usage/examples/ReactExampleCode.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Button } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css'; // default styles
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return <Button variation="primary">Hello world</Button>;
