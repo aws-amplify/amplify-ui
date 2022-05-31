5a34d6824fb4d7e99395174569f9ae4f71d0ce8b
MJ☔️ <o.6180339+github@gmail.com>
2022-05-31 17:38:09 +0000

    fix(docs): getPageManifest

--- docs/src/data/pages.preval.ts
+++ docs/src/data/pages.preval.ts
@@ -1,4 +1,9 @@
+import { getContentPaths } from '@/utils/getContentPaths';
 import { getPagesManifest } from '@/utils/getPagesManifest';
+import { getPageFromSlug } from '@/utils/getPageFromSlug';
 import preval from 'next-plugin-preval';
+import { META_INFO } from './meta';
 
-export default preval(getPagesManifest());
+export default preval(
+  getPagesManifest(getContentPaths, getPageFromSlug, META_INFO)
+);
--- docs/src/utils/getPagesManifest.ts
+++ docs/src/utils/getPagesManifest.ts
@@ -1,22 +1,21 @@
-import { getContentPaths } from '@/utils/getContentPaths';
-import { getPageFromSlug } from '@/utils/getPageFromSlug';
-import { META_INFO } from '@/data/meta';
-
 function pluckMeta({ frontmatter, href, slug }) {
   return { frontmatter, href, slug };
 }
-
-export async function getPagesManifest() {
+export async function getPagesManifest(
+  getContentPaths,
+  getPageFromSlug,
+  default_meta_info
+) {
   const paths = await getContentPaths();
-  const pages = await Promise.all(
+  const pages = (await Promise.all(
     paths.map(getPageFromSlug).map((page) => page.then(pluckMeta))
-  );
+  )) as any[];
 
   return {
     ...pages.reduce(
       (acc, { href, ...rest }) => ({ ...acc, [href]: { ...rest } }),
       {}
     ),
-    ...META_INFO,
+    ...default_meta_info,
   };
 }
