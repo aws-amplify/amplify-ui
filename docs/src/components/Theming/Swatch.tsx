68cd18b9f05ca66b0a05cd002788827d4b313181
Heather Buchel <buchel@amazon.com>
2022-06-01 19:08:15 -0400

    start testing swatch styles

--- docs/src/components/Theming/Swatch.tsx
+++ docs/src/components/Theming/Swatch.tsx
@@ -1,51 +1,95 @@
-import { Text, Grid, Flex, View, useTheme } from '@aws-amplify/ui-react';
+import { Text, Flex, View, useTheme } from '@aws-amplify/ui-react';
+import { tokens } from '@aws-amplify/ui/dist/types/theme/tokens';
 
-export function Swatch({ colorGroup, color, value }) {
-  const { tokens } = useTheme();
+export function SwatchToken({ path }) {
   return (
-    <Flex gap={tokens.space.xxxs} direction="column">
-      <View height="40px" backgroundColor={value}></View>
-      <div>
-        <Text fontWeight="bold">
-          {colorGroup}[{color}]
-        </Text>
-        <Text
-          color={tokens.colors.neutral['80']}
-          fontSize={tokens.fontSizes.small}
-        >
-          {value}
-        </Text>
-      </div>
-    </Flex>
+    <div className="docs-swatch-token">
+      {path.map((pathFragment, index) => {
+        return (
+          <>
+            {index !== 0 && isNaN(pathFragment) && '.'}
+            {!isNaN(pathFragment) ? `[${pathFragment}]` : pathFragment}
+          </>
+        );
+      })}
+    </div>
   );
 }
 
-export function SwatchGroup({ colorGroup }) {
+export function SwatchMeta({ children }) {
+  return <div className="docs-swatch-meta">{children}</div>;
+}
+
+export function Swatch({ value, name, path, namespace }) {
+  path.shift();
+  console.log(path);
+  return (
+    <div className={`docs-swatch docs-swatch--${namespace}`}>
+      {namespace === 'colors' && (
+        <>
+          <View className="docs-swatch-color" backgroundColor={value}></View>
+          <SwatchToken path={path} />
+          <SwatchMeta>{name}</SwatchMeta>
+          <SwatchMeta>{value}</SwatchMeta>
+        </>
+      )}
+      {namespace === 'space' && (
+        <>
+          <SwatchToken path={path} />
+          <div className="docs-swatch-space">
+            <View className="docs-swatch-spaceblock" width={value}></View>
+          </div>
+
+          <SwatchMeta>{value}</SwatchMeta>
+
+          <SwatchMeta>{name}</SwatchMeta>
+        </>
+      )}
+    </div>
+  );
+}
+
+export function SwatchGroup({ namespace, childKey }) {
   const { tokens } = useTheme();
   console.log('tokens', tokens);
-  const tokenGroup = tokens.colors[colorGroup];
+  const tokenGroup = childKey ? tokens[namespace][childKey] : tokens[namespace];
+
   let swatches = [];
-  for (const [key, value] of Object.entries(tokenGroup)) {
-    swatches.push({
-      color: key,
-      ...value,
-    });
+
+  function iterateGroup(group) {
+    for (const [key, value] of Object.entries(group)) {
+      console.log(value);
+      if (typeof value === 'object' && value.hasOwnProperty('name')) {
+        swatches.push({
+          color: key,
+          ...value,
+        });
+      } else {
+        iterateGroup(value);
+      }
+    }
   }
+
+  if (tokenGroup.hasOwnProperty('name')) {
+    swatches.push(tokenGroup);
+  } else {
+    iterateGroup(tokenGroup);
+  }
+
   return (
-    <Grid
-      rowGap={tokens.space.medium}
-      templateColumns="repeat(auto-fill, minmax(14rem, 1fr))"
-    >
+    <ul className="docs-swatch-group">
       {swatches.map((swatch) => {
         return (
-          <Swatch
-            key={swatch.name}
-            colorGroup={colorGroup}
-            color={swatch.color}
-            value={swatch.value}
-          />
+          <li key={swatch.name}>
+            <Swatch
+              path={swatch.path}
+              value={swatch.value}
+              name={swatch.name}
+              namespace={namespace}
+            />
+          </li>
         );
       })}
-    </Grid>
+    </ul>
   );
 }
