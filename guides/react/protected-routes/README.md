dd1fba8228f49a4de8d26125f0de10438ec4c4e5
Erik Hanchett <icystorm@gmail.com>
2022-06-01 08:51:37 -0700

    fix: Move sample repo to Amplify UI branch (#1987)
    * started work on moving guides
    
    * Update link
    
    * Updated readme
    
    * Updated package.json
    
    * E2e sample repo (#1994)
    
    * Test pr
    
    * Updated workflows
    
    * Fixed workflow
    
    * fixed workflow file again
    
    * Updated test
    
    * Added new test to deploy main
    
    * Updated workflowf ile
    
    Co-authored-by: Erik Hanchett <ehhanche@amazon.com>
    
    Co-authored-by: Erik Hanchett <ehhanche@amazon.com>

---
+++ guides/react/protected-routes/README.md
@@ -0,0 +1,3 @@
+# Procted Routes Sample App
+
+This sample app is featured in the [Protected Routes Guide](https://dev.ui.docs.amplify.aws/react/guides/auth-protected). Feel free to follow along there.
---
+++ guides/react/protected-routes/src/components/Home.js
@@ -0,0 +1,8 @@
+import { Heading } from '@aws-amplify/ui-react';
+export function Home() {
+  return (
+    <Heading level={3}>
+      Please use the buttons at the top to test out protected routes!
+    </Heading>
+  );
+}
