a3a6554279eb5768407b5018607c0d1c5e8fb365
Joe Buono <joebuono@amazon.com>
2022-06-01 15:09:38 -0500

    add csp rule for prod env

--- docs/src/pages/_document.page.tsx
+++ docs/src/pages/_document.page.tsx
@@ -36,7 +36,7 @@ const getCSPContent = (context: Readonly<HtmlProps>) => {
     style-src 'self' 'unsafe-inline';
     font-src 'self';
     frame-src 'self' *.codesandbox.io aws.demdex.net;
-    img-src 'self' cm.everesttech.net amazonwebservices.d2.sc.omtrdc.net dpm.demdex.net;
+    img-src 'self' cm.everesttech.net amazonwebservices.d2.sc.omtrdc.net dpm.demdex.net images.unsplash.com;
     connect-src 'self' *.shortbread.aws.dev amazonwebservices.d2.sc.omtrdc.net dpm.demdex.net https://*.algolia.net https://*.algolianet.com;
     script-src 'self' '${cspInlineScriptHash}' a0.awsstatic.com;
   `;
