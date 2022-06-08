ba3f20c4ee72f3853ff9e832a75c73b35c9619d9
Heather Buchel <buchel@amazon.com>
2022-06-07 21:54:39 -0400

    Add size examples

---
+++ docs/src/pages/[platform]/theming/default-theme/sizes/examples/BorderWidthExample.tsx
@@ -0,0 +1,20 @@
+import { Button, useTheme, ThemeProvider } from '@aws-amplify/ui-react';
+
+export const BorderWidthExample = () => {
+  const { tokens } = useTheme();
+  const theme = {
+    name: 'custom-theme',
+    tokens: {
+      components: {
+        button: {
+          borderWidth: tokens.borderWidths.large,
+        },
+      },
+    },
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <Button>Custom border button</Button>
+    </ThemeProvider>
+  );
+};
