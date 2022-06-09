988c703132f748f762ce397b354a0a4fea672560
Heather Buchel <buchel@amazon.com>
2022-06-09 14:07:45 -0400

    Add example theme for high contrast borders

--- docs/src/pages/[platform]/getting-started/accessibility/examples/index.ts
+++ docs/src/pages/[platform]/getting-started/accessibility/examples/index.ts
@@ -2,3 +2,4 @@ export { LabelInputExample } from './LabelInputExample';
 export { CheckboxExample } from './CheckboxExample';
 export { NavRoleExample } from './NavRoleExample';
 export { AriaLiveExample } from './AriaLiveExample';
+export { ThemeExample } from './ThemeExample';
