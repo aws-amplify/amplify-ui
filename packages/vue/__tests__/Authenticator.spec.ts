99dfa86dd3676e2bc8e0fd1945efc64d5b537c8d
wlee221 <willeea@amazon.com>
2022-06-09 17:09:01 -0700

    Add crypto polyfill to jest

--- packages/vue/__tests__/Authenticator.spec.ts
+++ packages/vue/__tests__/Authenticator.spec.ts
@@ -1,6 +1,15 @@
 import { mount } from '@vue/test-utils';
 import { Authenticator } from '../dist';
 
+const crypto = require('crypto');
+
+// This is needed for jest to get crypto module needed by nanoid
+Object.defineProperty(global.self, 'crypto', {
+  value: {
+    getRandomValues: (arr: Array<any>) => crypto.randomBytes(arr.length),
+  },
+});
+
 describe('Authenticator', () => {
   it('Authenticator Exists', () => {
     const wrapper = mount(Authenticator);
