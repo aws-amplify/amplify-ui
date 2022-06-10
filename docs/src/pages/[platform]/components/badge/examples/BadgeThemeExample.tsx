e27c24dc760660b62b04b0d24c9b71bad4ad9b35
Joe Buono <joebuono@amazon.com>
2022-06-10 13:23:26 -0500

    badge theme example

---
+++ docs/src/pages/[platform]/components/badge/examples/BadgeThemeExample.tsx
@@ -0,0 +1,38 @@
+import { Badge, ThemeProvider, Theme } from '@aws-amplify/ui-react';
+
+const theme: Theme = {
+  name: 'badge-theme',
+  tokens: {
+    components: {
+      badge: {
+        // Default styles
+        color: { value: '{colors.white.value}' },
+        fontWeight: { value: '{fontWeights.normal.value}' },
+        fontSize: { value: '{fontSizes.large.value}' },
+        backgroundColor: { value: '{colors.purple.80.value}' },
+        borderRadius: { value: '{radii.small.value}' },
+
+        // Variations
+        success: {
+          color: { value: '{colors.black.value}' },
+          backgroundColor: { value: '{colors.yellow.60.value}' },
+        },
+
+        // Sizes
+        large: {
+          fontSize: { value: '{fontSizes.xxl.value}' },
+        },
+      },
+    },
+  },
+};
+
+export const BadgeThemeExample = () => (
+  <ThemeProvider theme={theme}>
+    <>
+      <Badge>Default</Badge>
+      <Badge variation="success">Success</Badge>
+      <Badge size="large">Large</Badge>
+    </>
+  </ThemeProvider>
+);
