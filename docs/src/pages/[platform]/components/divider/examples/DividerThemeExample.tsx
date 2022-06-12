d91e5aa9ebb757788c8003233a860dba30bedfee
Joe Buono <joebuono@amazon.com>
2022-06-12 12:54:47 -0500

    button, divider, togglebutton

--- docs/src/pages/[platform]/components/divider/examples/DividerThemeExample.tsx
+++ docs/src/pages/[platform]/components/divider/examples/DividerThemeExample.tsx
@@ -1,23 +1,22 @@
-import {
-  Flex,
-  Text,
-  Divider,
-  ThemeProvider,
-  Theme,
-} from '@aws-amplify/ui-react';
+import { Divider, Flex, ThemeProvider, Theme } from '@aws-amplify/ui-react';
 
 const theme: Theme = {
   name: 'divider-theme',
   tokens: {
-    borderWidths: {
-      // This will affect the large divider and other components that reference this
-      large: { value: '20px' },
-    },
     components: {
       divider: {
         borderStyle: { value: 'dotted' },
-        // You can reference other theme tokens:
         borderColor: { value: '{colors.brand.secondary.80.value}' },
+        borderWidth: { value: '{borderWidths.small.value}' },
+
+        label: {
+          color: { value: '{colors.white.value}' },
+          backgroundColor: { value: '{colors.brand.secondary.80.value}' },
+        },
+
+        large: {
+          borderWidth: { value: '{borderWidths.large.value}' },
+        },
       },
     },
   },
@@ -25,38 +24,9 @@ const theme: Theme = {
 
 export const DividerThemeExample = () => (
   <ThemeProvider theme={theme}>
-    <Flex direction="column">
-      <Text>Before</Text>
-      <Divider />
-      <Text>After</Text>
-      <Divider size="large" />
+    <Flex direction="column" gap="3rem">
+      <Divider label="Default" />
+      <Divider size="large" label="Large" />
     </Flex>
   </ThemeProvider>
 );
-
-/*
-
-  borderStyle: { value: 'solid' },
-  borderColor: { value: '{colors.border.primary.value}' },
-  borderWidth: { value: '{borderWidths.medium.value}' },
-
-  label: {
-    color: { value: '{colors.font.tertiary.value}' },
-    paddingInline: { value: '{space.medium.value}' },
-    fontSize: { value: '{fontSizes.small.value}' },
-    backgroundColor: { value: '{colors.background.primary.value}' },
-  },
-
-  small: {
-    borderWidth: { value: '{borderWidths.small.value}' },
-  },
-
-  large: {
-    borderWidth: { value: '{borderWidths.large.value}' },
-  },
-
-  opacity: {
-    value: '{opacities.60.value}',
-  },
-
-*/
--- docs/src/pages/[platform]/components/togglebutton/examples/ToggleButtonThemeExample.tsx
+++ docs/src/pages/[platform]/components/togglebutton/examples/ToggleButtonThemeExample.tsx
@@ -1,13 +1,6 @@
-import * as React from 'react';
-import {
-  MdFormatAlignCenter,
-  MdFormatAlignJustify,
-  MdFormatAlignLeft,
-  MdFormatAlignRight,
-} from 'react-icons/md';
 import {
   ToggleButton,
-  ToggleButtonGroup,
+  Flex,
   ThemeProvider,
   Theme,
 } from '@aws-amplify/ui-react';
@@ -17,134 +10,39 @@ const theme: Theme = {
   tokens: {
     components: {
       togglebutton: {
-        // TODO: customize here
-      },
-      togglebuttongroup: {
-        // TODO: customize here
+        borderColor: { value: '{colors.brand.secondary.80.value}' },
+        color: { value: '{colors.brand.secondary.80.value}' },
+        _hover: {
+          backgroundColor: { value: 'red' },
+        },
+        _focus: {
+          color: { value: 'orange' },
+        },
+        _active: {
+          backgroundColor: { value: 'yellow' },
+        },
+        _pressed: {
+          backgroundColor: { value: 'green' },
+          color: { value: '{colors.white.value}' },
+          _hover: {
+            backgroundColor: { value: 'blue' },
+          },
+        },
+        primary: {
+          backgroundColor: { value: '{colors.pink.20.value}' },
+        },
       },
     },
   },
 };
 
 export const ToggleButtonThemeExample = () => {
-  const [exclusiveValue, setExclusiveValue] = React.useState('align-left');
   return (
     <ThemeProvider theme={theme}>
-      <ToggleButtonGroup
-        value={exclusiveValue}
-        isExclusive
-        onChange={(value) => setExclusiveValue(value as string)}
-      >
-        <ToggleButton value="align-left">
-          <MdFormatAlignLeft />
-        </ToggleButton>
-        <ToggleButton value="align-center">
-          <MdFormatAlignCenter />
-        </ToggleButton>
-        <ToggleButton value="align-right">
-          <MdFormatAlignRight />
-        </ToggleButton>
-        <ToggleButton value="align-justify">
-          <MdFormatAlignJustify />
-        </ToggleButton>
-      </ToggleButtonGroup>
+      <Flex>
+        <ToggleButton>Default</ToggleButton>
+        <ToggleButton variation="primary">Primary</ToggleButton>
+      </Flex>
     </ThemeProvider>
   );
 };
-
-/*
-
-  borderColor: { value: '{colors.border.primary.value}' },
-  color: { value: '{colors.font.primary.value}' },
-  _hover: {
-    backgroundColor: { value: '{colors.overlay.10.value}' },
-  },
-  _focus: {
-    borderColor: { value: '{colors.border.focus.value}' },
-    color: { value: '{colors.font.primary.value}' },
-  },
-  _active: {
-    backgroundColor: { value: '{colors.transparent.value}' },
-  },
-  _disabled: {
-    backgroundColor: { value: '{colors.transparent.value}' },
-    borderColor: { value: '{colors.border.disabled.value}' },
-    color: { value: '{colors.font.disabled.value}' },
-  },
-  _pressed: {
-    borderColor: { value: '{colors.border.pressed.value}' },
-    color: { value: '{colors.font.primary.value}' },
-    backgroundColor: { value: '{colors.overlay.20.value}' },
-    _hover: {
-      backgroundColor: { value: '{colors.overlay.30.value}' },
-    },
-  },
-  primary: {
-    backgroundColor: { value: '{colors.transparent.value}' },
-    borderWidth: { value: '{borderWidths.small.value}' },
-    _focus: {
-      borderColor: { value: '{colors.border.focus.value}' },
-      backgroundColor: { value: '{colors.transparent.value}' },
-      boxShadow: { value: '{components.fieldcontrol._focus.boxShadow.value}' },
-      color: { value: '{colors.font.primary.value}' },
-    },
-    _hover: {
-      backgroundColor: { value: '{colors.overlay.10.value}' },
-      color: { value: '{colors.font.primary.value}' },
-    },
-    _disabled: {
-      borderColor: { value: '{colors.border.disabled.value}' },
-      backgroundColor: { value: '{colors.background.disabled.value}' },
-      color: { value: '{colors.font.disabled.value}' },
-    },
-    _pressed: {
-      backgroundColor: { value: '{colors.brand.primary.80.value}' },
-      borderColor: { value: '{colors.brand.primary.80.value}' },
-      color: { value: '{colors.white.value}' },
-      _focus: {
-        backgroundColor: {
-          value: '{colors.border.focus.value}',
-        },
-        borderColor: { value: '{colors.border.focus.value}' },
-        color: { value: '{colors.white.value}' },
-      },
-      _hover: {
-        borderColor: { value: '{colors.brand.primary.60.value}' },
-        backgroundColor: {
-          value: '{colors.brand.primary.60.value}',
-        },
-        boxShadow: { value: '{colors.brand.primary.60.value}' },
-        color: { value: '{colors.white.value}' },
-      },
-    },
-  },
-  link: {
-    backgroundColor: { value: '{colors.transparent.value}' },
-    color: { value: '{colors.overlay.50.value}' },
-    _hover: {
-      backgroundColor: { value: '{colors.transparent.value}' },
-      color: { value: '{colors.overlay.50.value}' },
-    },
-    _focus: {
-      backgroundColor: { value: '{colors.transparent.value}' },
-      color: { value: '{colors.overlay.50.value}' },
-    },
-    _disabled: {
-      backgroundColor: { value: '{colors.transparent.value}' },
-      color: { value: '{colors.font.disabled.value}' },
-    },
-    _pressed: {
-      backgroundColor: { value: '{colors.transparent.value}' },
-      color: { value: '{colors.overlay.90.value}' },
-      _focus: {
-        backgroundColor: { value: '{colors.transparent.value}' },
-        color: { value: '{colors.overlay.90.value}' },
-      },
-      _hover: {
-        color: { value: '{colors.overlay.90.value}' },
-        backgroundColor: { value: '{colors.transparent.value}' },
-      },
-    },
-  },
-
-*/
