89d67e64222aa30ce31563ec1a86fba9a29b5eb3
Joe Buono <joebuono@amazon.com>
2022-06-13 14:34:00 -0500

    expander

--- docs/src/pages/[platform]/components/expander/examples/ExpanderThemeExample.tsx
+++ docs/src/pages/[platform]/components/expander/examples/ExpanderThemeExample.tsx
@@ -6,11 +6,29 @@ import {
 } from '@aws-amplify/ui-react';
 
 const theme: Theme = {
-  name: 'Expander-theme',
+  name: 'expander-theme',
   tokens: {
     components: {
       expander: {
-        // TODO: customize here
+        backgroundColor: { value: '{colors.brand.primary.60.value}' },
+        borderRadius: { value: '{radii.small.value}' },
+        trigger: {
+          minHeight: { value: '5rem' },
+          justifyContent: { value: 'center' },
+          _hover: {
+            backgroundColor: { value: '{colors.overlay.20.value}' },
+          },
+        },
+        content: {
+          paddingInlineStart: { value: '{space.xxl.value}' },
+          text: {
+            color: { value: '{colors.white.value}' },
+            paddingBlockEnd: { value: '{space.large.value}' },
+          },
+          _open: {
+            animationDuration: { value: '{time.short.value}' },
+          },
+        },
       },
     },
   },
@@ -19,11 +37,11 @@ const theme: Theme = {
 export const ExpanderThemeExample = () => (
   <ThemeProvider theme={theme}>
     <Expander>
-      <ExpanderItem title="What is an ExpanderItem?" value="expander-item">
-        An ExpanderItem contains all the parts of a collapsible section.
+      <ExpanderItem title="Check out this themed Expander" value="item1">
+        Pretty cool!
       </ExpanderItem>
-      <ExpanderItem title="This is the item's title" value="unique-value">
-        The `children` of the ExpanderItem are displayed here.
+      <ExpanderItem title="It's very customizable" value="item2">
+        I love it.
       </ExpanderItem>
     </Expander>
   </ThemeProvider>
