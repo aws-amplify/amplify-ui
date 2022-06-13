c461da2ca721fcde8e1fd4fab26a656bce24e036
Joe Buono <joebuono@amazon.com>
2022-06-13 15:13:51 -0500

    tabs

--- docs/src/pages/[platform]/components/tabs/examples/TabsThemeExample.tsx
+++ docs/src/pages/[platform]/components/tabs/examples/TabsThemeExample.tsx
@@ -1,19 +1,30 @@
-import { ThemeProvider, Tabs, TabItem } from '@aws-amplify/ui-react';
+import { ThemeProvider, Tabs, TabItem, Theme } from '@aws-amplify/ui-react';
 
-const theme = {
+const theme: Theme = {
   name: 'tabs-theme',
   tokens: {
-    colors: {
-      border: {
-        secondary: { value: '#eccedd' },
-      },
-    },
     components: {
       tabs: {
+        borderColor: { value: '{colors.neutral.20.value}' },
         item: {
+          color: { value: 'green' },
+          fontSize: { value: '{fontSizes.xl.value}' },
+          fontWeight: { value: '{fontWeights.normal.value}' },
+
+          _hover: {
+            color: { value: 'yellow' },
+          },
+          _focus: {
+            color: { value: 'orange' },
+          },
           _active: {
-            color: { value: '#9551a0' },
-            borderColor: { value: '#9551a0' },
+            color: { value: 'purple' },
+            borderColor: { value: 'red' },
+            backgroundColor: { value: '{colors.blue.20.value}' },
+          },
+          _disabled: {
+            color: { value: 'gray' },
+            backgroundColor: { value: 'transparent' },
           },
         },
       },
@@ -27,6 +38,9 @@ export const TabsThemeExample = () => {
       <Tabs>
         <TabItem title="Tab 1">Tab 1 Content</TabItem>
         <TabItem title="Tab 2">Tab 2 Content</TabItem>
+        <TabItem title="Tab 3" isDisabled>
+          Tab 3 Content
+        </TabItem>
       </Tabs>
     </ThemeProvider>
   );
