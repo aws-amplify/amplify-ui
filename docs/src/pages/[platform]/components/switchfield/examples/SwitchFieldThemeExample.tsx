895ab975517a9148504cedc5a165b24c0c4c7ad1
Joe Buono <joebuono@amazon.com>
2022-06-13 11:02:02 -0500

    switchfield

--- docs/src/pages/[platform]/components/switchfield/examples/SwitchFieldThemeExample.tsx
+++ docs/src/pages/[platform]/components/switchfield/examples/SwitchFieldThemeExample.tsx
@@ -5,63 +5,24 @@ const theme: Theme = {
   tokens: {
     components: {
       switchfield: {
-        // States
-        _disabled: {
-          opacity: { value: '{opacities.60.value}' },
-        },
-        _focused: {
-          shadow: {
-            value: {
-              offsetX: '0px',
-              offsetY: '0px',
-              blurRadius: '0px',
-              spreadRadius: '2px',
-              color: '{colors.border.focus.value}',
-            },
-          },
-        },
-
-        // Sizes
-        large: {
-          fontSize: { value: '{fontSizes.large.value}' },
-        },
-        small: {
-          fontSize: { value: '{fontSizes.small.value}' },
-        },
-
-        // Child elements
-        label: {
-          padding: { value: '{space.xs.value}' },
-        },
-
         thumb: {
-          backgroundColor: { value: '{colors.background.primary.value}' },
+          backgroundColor: { value: '{colors.green.40.value}' },
           borderColor: { value: '{colors.border.tertiary.value}' },
-          borderRadius: { value: '{radii.xxxl.value}' },
-          checked: {
-            transform: { value: '{transforms.slideX.medium.value}' },
-          },
           transition: {
-            duration: { value: '{time.medium.value}' },
+            duration: { value: '{time.long.value}' },
           },
-          width: { value: '{space.relative.medium.value}' },
         },
 
         track: {
-          backgroundColor: { value: '{colors.background.quaternary.value}' },
-          borderRadius: { value: '{radii.xxxl.value}' },
+          backgroundColor: { value: '{colors.neutral.60.value}' },
           checked: {
-            backgroundColor: { value: '{colors.brand.primary.80.value}' },
+            backgroundColor: { value: '{colors.brand.secondary.60.value}' },
           },
-          height: { value: '{space.relative.medium.value}' },
-          padding: { value: '{outlineWidths.medium.value}' },
           transition: {
-            duration: { value: '{time.short.value}' },
+            duration: { value: '{time.long.value}' },
           },
-          width: { value: '{space.relative.xl.value}' },
         },
       },
-      // is switch a subcomponent?
     },
   },
 };
