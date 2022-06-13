dbed601051af40ae26588f4de0841cc00c30a271
Heather Buchel <buchel@amazon.com>
2022-06-13 15:24:31 -0400

    fix(docs): add margin to ThemeAlert

--- docs/src/components/ThemeAlert.tsx
+++ docs/src/components/ThemeAlert.tsx
@@ -1,8 +1,9 @@
-import { Alert } from '@aws-amplify/ui-react';
+import { Alert, useTheme } from '@aws-amplify/ui-react';
 
 export const ThemeAlert = () => {
+  const { tokens } = useTheme();
   return (
-    <Alert variation="info">
+    <Alert variation="info" marginBottom={tokens.space.medium}>
       AmplifyProvider has been renamed to ThemeProvider. The ThemeProvider
       export is available since version 2.18.3, previous versions must still use
       AmplifyProvider.
