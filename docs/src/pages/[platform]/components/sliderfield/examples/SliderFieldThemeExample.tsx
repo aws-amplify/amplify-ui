67b2e3fc039c29df896ea25d89a00658f55ec2ec
Joe Buono <joebuono@amazon.com>
2022-06-13 15:30:50 -0500

    sliderfield

--- docs/src/pages/[platform]/components/sliderfield/examples/SliderFieldThemeExample.tsx
+++ docs/src/pages/[platform]/components/sliderfield/examples/SliderFieldThemeExample.tsx
@@ -6,16 +6,32 @@ const theme: Theme = {
     components: {
       sliderfield: {
         thumb: {
-          backgroundColor: { value: '{colors.green.40.value}' },
-          borderRadius: { value: '0' },
+          width: { value: '{space.xl.value}' },
+          height: { value: '{space.xl.value}' },
+          backgroundColor: { value: '{colors.blue.40.value}' },
+          borderRadius: { value: '{radii.medium.value}' },
+          _hover: {
+            backgroundColor: { value: '{colors.green.40.value}' },
+            borderColor: { value: 'orange' },
+          },
+          _focus: {
+            borderColor: { value: 'red' },
+            boxShadow: {
+              value: {
+                spreadRadius: '3px',
+                color: 'yellow',
+              },
+            },
+          },
         },
         track: {
           backgroundColor: {
             value: '{colors.neutral.80.value}',
           },
+          height: { value: '{fontSizes.medium.value}' },
         },
         range: {
-          backgroundColor: { value: 'blue' },
+          backgroundColor: { value: '{colors.brand.secondary.80.value}' },
         },
       },
     },
@@ -25,7 +41,7 @@ const theme: Theme = {
 export const SliderFieldThemeExample = () => {
   return (
     <ThemeProvider theme={theme}>
-      <SliderField label="Themed Slider" defaultValue={50} />
+      <SliderField label="Themed Slider" defaultValue={50} labelHidden />
     </ThemeProvider>
   );
 };
