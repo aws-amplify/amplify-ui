81e75b1ed75db99b42c8ebcd3a3776ff34790b58
Joe Buono <joebuono@amazon.com>
2022-06-13 16:01:04 -0500

    searchfield in progress

--- docs/src/pages/[platform]/components/searchfield/examples/SearchFieldThemeExample.tsx
+++ docs/src/pages/[platform]/components/searchfield/examples/SearchFieldThemeExample.tsx
@@ -1,30 +1,51 @@
-import {
-  SearchField,
-  ThemeProvider,
-  Theme,
-  useTheme,
-} from '@aws-amplify/ui-react';
+import { SearchField, ThemeProvider, Theme } from '@aws-amplify/ui-react';
 
-export const SearchFieldThemeExample = () => {
-  const { tokens } = useTheme();
-
-  const theme: Theme = {
-    name: 'searchfield-theme',
-    tokens: {
-      components: {
-        searchfield: {
+const theme: Theme = {
+  name: 'searchfield-theme',
+  tokens: {
+    components: {
+      searchfield: {
+        // color seemingly has no effect
+        // color: { value: 'red' },
+        button: {
           color: { value: 'red' },
-          button: {
-            color: { value: 'red' },
-            _hover: {
-              color: { value: tokens.colors.blue[80].value },
+          _active: {
+            backgroundColor: {
+              value: 'green',
             },
+            borderColor: {
+              value: 'yellow',
+            },
+            color: { value: 'white' },
+          },
+          _focus: {
+            backgroundColor: {
+              value: 'purple',
+            },
+            borderColor: {
+              value: 'red',
+            },
+            color: { value: 'yellow}' },
+          },
+          _hover: {
+            backgroundColor: {
+              value: 'orange',
+            },
+            borderColor: {
+              value: 'blue',
+            },
+            color: { value: 'black' },
           },
         },
       },
+      // fieldcontrol: {
+      //   color: { value: 'red' },
+      // },
     },
-  };
+  },
+};
 
+export const SearchFieldThemeExample = () => {
   return (
     <ThemeProvider theme={theme}>
       <SearchField label="search" />
