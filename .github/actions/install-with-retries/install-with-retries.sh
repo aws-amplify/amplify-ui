357fe388ad8fac505e4724ce0e6de192ec16ac30
wlee221 <willeea@amazon.com>
2022-06-15 20:19:37 -0700

    Separate out shell command

---
+++ .github/actions/install-with-retries/install-with-retries.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "$SKIP_CYPRESS_BINARY" = "true" ]; then
+  # Skip cypress binary installation if specified
+  # https://docs.cypress.io/guides/references/advanced-installation#Environment-variables
+  echo "[LOG]: Setting flag to skip cypress binary installation"
+  export CYPRESS_INSTALL_BINARY=0;
+fi
+
+for i in {1..3}; do
+  echo "===================="
+  echo "Attempt $i out of 3:"
+  echo "===================="
+
+  yarn install
+
+  # Check return value and exit early if successful
+  return_value=$?
+  [ $return_value -eq 0 ] && break
+  echo "[ERROR]: yarn install failed with exit code $return_value, waiting to retry..."
+
+  # Sleep 5 seconds before retrying
+  sleep 5
+done
