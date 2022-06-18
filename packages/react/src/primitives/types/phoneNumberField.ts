3ace39ce07454a0ef53bc94491dac6183d89da43
Joe Buono <joebuono724@gmail.com>
2022-06-18 13:50:48 -0400

    Add missing prop descriptions (#2132)
    * add in missing descriptions for props
    
    * Create unlucky-crabs-brake.md
    
    * Update unlucky-crabs-brake.md
    
    * add @description tag
    
    * add descriptions for as, ref, ariaLabel and role
    
    * change 'country or region code' to 'dial code'
    
    Co-authored-by: Joe Buono <joebuono@amazon.com>
    Co-authored-by: MJ <0618@users.noreply.github.com>

--- packages/react/src/primitives/types/phoneNumberField.ts
+++ packages/react/src/primitives/types/phoneNumberField.ts
@@ -4,13 +4,44 @@ import { SelectFieldProps } from './selectField';
 import { TextInputFieldProps } from './textField';
 
 export interface PhoneNumberFieldProps extends TextInputFieldProps {
-  countryCodeLabel?: string;
-  countryCodeName?: string;
-  defaultCountryCode: string;
-  dialCodeList?: Array<string>;
-  onCountryCodeChange?: React.ChangeEventHandler<HTMLSelectElement>;
-  type?: 'tel';
   /**
+   * @description
+   * Sets a hidden and accessible label for the dial code selector
+   */
+  countryCodeLabel?: string;
+
+  /**
+   * @description
+   * Sets the name used when handling form submission for the dial code selector
+   */
+  countryCodeName?: string;
+
+  /**
+   * @description
+   * Sets the default dial code that will be selected on initial render
+   */
+  defaultCountryCode: string;
+
+  /**
+   * @description
+   * Accepts an array of dial codes (strings) used as options in the dial code selector
+   */
+  dialCodeList?: Array<string>;
+
+  /**
+   * @description
+   * Handles change events for the dial code selector
+   */
+  onCountryCodeChange?: React.ChangeEventHandler<HTMLSelectElement>;
+
+  /**
+   * @description
+   * <input> elements of type 'tel' are used to let the user enter and edit a telephone number
+   */
+  type?: 'tel';
+
+  /**
+   * @description
    * Forwarded ref for access to Country Code select DOM element
    */
   countryCodeRef?: React.Ref<HTMLSelectElement>;
