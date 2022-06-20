37c99792925d9b37d7afacaed2559306275e36fd
Joe Buono <joebuono@amazon.com>
2022-06-20 15:55:41 -0500

    add mdn descriptions to style props

--- packages/react/src/primitives/types/style.ts
+++ packages/react/src/primitives/types/style.ts
@@ -52,115 +52,429 @@ export type ResponsiveStyle<PropertyType> =
   | ResponsiveObject<StyleProp<PropertyType>>;
 
 export interface BaseStyleProps extends FlexItemStyleProps, GridItemStyleProps {
+  /**
+   * @description
+   * Overrides a grid or flex item's align-items value.
+   * In Grid, it aligns the item inside the grid area. In Flexbox, it aligns the item on the cross axis.
+   */
   alignSelf?: ResponsiveStyle<StyleToken<Property.AlignSelf>>;
+
+  /**
+   * @description
+   * Sets the background color of an element.
+   */
   backgroundColor?: ResponsiveStyle<
     ColorKeys<StyleToken<Property.BackgroundColor>>
   >;
+
+  /**
+   * @description
+   * Sets one or more background images on an element.
+   */
   backgroundImage?: ResponsiveStyle<StyleToken<Property.BackgroundImage>>;
+
+  /**
+   * @description
+   * Shorthand CSS property that sets an element's border-width, border-style, and border-color.
+   */
   border?: ResponsiveStyle<StyleToken<Property.Border>>;
+
+  /**
+   * @description
+   * Rounds the corners of an element's outer border edge.
+   * You can set a single radius to make circular corners, or two radii to make elliptical corners.
+   */
   borderRadius?: ResponsiveStyle<RadiiKeys<StyleToken<Property.BorderRadius>>>;
+
+  /**
+   * @description
+   * Participates in setting the vertical position of a positioned element.
+   * It has no effect on non-positioned elements.
+   */
   bottom?: ResponsiveStyle<SpaceKeys<StyleToken<Property.Bottom>>>;
+
+  /**
+   * @description
+   * Adds shadow effects around an element's frame. You can set multiple effects separated by commas.
+   * A box shadow is described by X and Y offsets relative to the element, blur and spread radius, and color.
+   */
   boxShadow?: ResponsiveStyle<BoxShadowKeys<StyleToken<Property.BoxShadow>>>;
+
+  /**
+   * @description
+   * Sets the foreground color value of an element's text and text decorations, and sets the `currentcolor` value.
+   * `currentcolor` may be used as an indirect value on other properties and is the default for other color properties, such as border-color.
+   */
   color?: ResponsiveStyle<ColorKeys<StyleToken<Property.Color>>>;
+
+  /**
+   * @description
+   * Sets whether an element is treated as a block or inline element and the layout used for its children, such as flow layout, grid or flex.
+   */
   display?: ResponsiveStyle<StyleToken<Property.Display>>;
+
+  /**
+   * @description
+   * Specifies a prioritized list of one or more font family names and/or generic family names for the selected element.
+   */
   fontFamily?: ResponsiveStyle<FontFamilyKeys<StyleToken<Property.FontFamily>>>;
+
+  /**
+   * @description
+   * Sets the size of the font. Changing the font size also updates the sizes of the font size-relative <length> units, such as em, ex, and so forth.
+   */
   fontSize?: ResponsiveStyle<FontSizeKeys<StyleToken<Property.FontSize>>>;
+
+  /**
+   * @description
+   * Sets whether a font should be styled with a normal, italic, or oblique face from its font-family.
+   */
   fontStyle?: ResponsiveStyle<StyleToken<Property.FontStyle>>;
+
+  /**
+   * @description
+   * Sets the weight (or boldness) of the font. The weights available depend on the font-family that is currently set.
+   */
   fontWeight?: ResponsiveStyle<FontWeightKeys<StyleToken<Property.FontWeight>>>;
+
+  /**
+   * @description
+   * specifies the height of an element. By default, the property defines the height of the content area.
+   * If box-sizing is set to border-box, however, it instead determines the height of the border area.
+   */
   height?: ResponsiveStyle<SpaceKeys<StyleToken<Property.Height>>>;
+
+  /**
+   * @description
+   * Participates in specifying the horizontal position of a positioned element. It has no effect on non-positioned elements.
+   */
   left?: ResponsiveStyle<SpaceKeys<StyleToken<Property.Left>>>;
+
+  /**
+   * @description
+   * Sets the horizontal spacing behavior between text characters.
+   * This value is added to the natural spacing between characters while rendering the text.
+   */
   letterSpacing?: ResponsiveStyle<
     SpaceKeys<StyleToken<Property.LetterSpacing>>
   >;
+
+  /**
+   * @description
+   * Sets the height of a line box. It's commonly used to set the distance between lines of text.
+   */
   lineHeight?: ResponsiveStyle<LineHeightKeys<StyleToken<Property.LineHeight>>>;
+
+  /**
+   * @description
+   * Shorthand CSS property that sets the margin area on all four sides of an element.
+   */
   margin?: ResponsiveStyle<SpaceKeys<StyleToken<Property.Margin>>>;
+
+  /**
+   * @description
+   * Shorthand CSS property that defines the logical block start and end margins of an element,
+   * which maps to physical margins depending on the element's writing mode, directionality, and text orientation.
+   */
   marginBlock?: ResponsiveStyle<SpaceKeys<StyleToken<Property.MarginBlock>>>;
+
+  /**
+   * @description
+   * Defines the logical block end margin of an element,
+   * which maps to a physical margin depending on the element's writing mode, directionality, and text orientation.
+   */
   marginBlockEnd?: ResponsiveStyle<
     SpaceKeys<StyleToken<Property.MarginBlockEnd>>
   >;
+
+  /**
+   * @description
+   * Defines the logical block start margin of an element,
+   * which maps to a physical margin depending on the element's writing mode, directionality, and text orientation.
+   */
   marginBlockStart?: ResponsiveStyle<
     SpaceKeys<StyleToken<Property.MarginBlockStart>>
   >;
+
+  /**
+   * @description
+   * Sets the margin area on the bottom of an element.
+   * A positive value places it farther from its neighbors, while a negative value places it closer.
+   */
   marginBottom?: ResponsiveStyle<SpaceKeys<StyleToken<Property.MarginBottom>>>;
-  marginInline?: ResponsiveStyle<
-    SpaceKeys<StyleToken<Property.MarginInline>>
-  >;
+
+  /**
+   * @description
+   * Shorthand CSS property that defines both the logical inline start and end margins of an element,
+   * which maps to physical margins depending on the element's writing mode, directionality, and text orientation.
+   */
+  marginInline?: ResponsiveStyle<SpaceKeys<StyleToken<Property.MarginInline>>>;
+
+  /**
+   * @description
+   * Defines the logical inline end margin of an element,
+   * which maps to a physical margin depending on the element's writing mode, directionality, and text orientation.
+   */
   marginInlineEnd?: ResponsiveStyle<
     SpaceKeys<StyleToken<Property.MarginInlineEnd>>
   >;
+
+  /**
+   * @description
+   * Defines the logical inline start margin of an element,
+   * which maps to a physical margin depending on the element's writing mode, directionality, and text orientation.
+   */
   marginInlineStart?: ResponsiveStyle<
     SpaceKeys<StyleToken<Property.MarginInlineStart>>
   >;
+
+  /**
+   * @description
+   * Sets the margin area on the left side of an element.
+   * A positive value places it farther from its neighbors, while a negative value places it closer.
+   */
   marginLeft?: ResponsiveStyle<SpaceKeys<StyleToken<Property.MarginLeft>>>;
+
+  /**
+   * @description
+   * Sets the margin area on the right side of an element.
+   * A positive value places it farther from its neighbors, while a negative value places it closer.
+   */
   marginRight?: ResponsiveStyle<SpaceKeys<StyleToken<Property.MarginRight>>>;
+
+  /**
+   * @description
+   * Sets the margin area on the top of an element.
+   * A positive value places it farther from its neighbors, while a negative value places it closer.
+   */
   marginTop?: ResponsiveStyle<SpaceKeys<StyleToken<Property.MarginTop>>>;
+
+  /**
+   * @description
+   * Sets the maximum height of an element.
+   * It prevents the used value of the height property from becoming larger than the value specified for max-height.
+   */
   maxHeight?: ResponsiveStyle<SpaceKeys<StyleToken<Property.MaxHeight>>>;
+
+  /**
+   * @description
+   * Sets the maximum width of an element.
+   * It prevents the used value of the width property from becoming larger than the value specified by max-width.
+   */
   maxWidth?: ResponsiveStyle<SpaceKeys<StyleToken<Property.MaxWidth>>>;
+
+  /**
+   * @description
+   * Sets the minimum height of an element.
+   * It prevents the used value of the height property from becoming smaller than the value specified for min-height.
+   */
   minHeight?: ResponsiveStyle<SpaceKeys<StyleToken<Property.MinHeight>>>;
+
+  /**
+   * @description
+   * Sets the minimum width of an element.
+   * It prevents the used value of the width property from becoming smaller than the value specified for min-width.
+   */
   minWidth?: ResponsiveStyle<SpaceKeys<StyleToken<Property.MinWidth>>>;
+
+  /**
+   * @description
+   * Sets the opacity of an element.
+   * Opacity is the degree to which content behind an element is hidden, and is the opposite of transparency.
+   */
   opacity?: ResponsiveStyle<OpacityKeys<StyleToken<Property.Opacity>>>;
+
+  /**
+   * @description
+   * Shorthand CSS property that sets the desired behavior for an element's overflow
+   * — i.e. when an element's content is too big to fit in its block formatting context — in both directions.
+   */
   overflow?: ResponsiveStyle<StyleToken<Property.Overflow>>;
+
+  /**
+   * @description
+   * Shorthand CSS property that sets the padding area on all four sides of an element at once.
+   */
   padding?: ResponsiveStyle<SpaceKeys<StyleToken<Property.Padding>>>;
+
+  /**
+   * @description
+   * Shorthand CSS property that defines the logical block start and end padding of an element,
+   * which maps to physical padding properties depending on the element's writing mode, directionality, and text orientation.
+   */
   paddingBlock?: ResponsiveStyle<SpaceKeys<StyleToken<Property.PaddingBlock>>>;
+
+  /**
+   * @description
+   * Defines the logical block end padding of an element,
+   * which maps to a physical padding depending on the element's writing mode, directionality, and text orientation.
+   */
   paddingBlockEnd?: ResponsiveStyle<
     SpaceKeys<StyleToken<Property.PaddingBlockEnd>>
   >;
+
+  /**
+   * @description
+   * Defines the logical block start padding of an element,
+   * which maps to a physical padding depending on the element's writing mode, directionality, and text orientation.
+   */
   paddingBlockStart?: ResponsiveStyle<
     SpaceKeys<StyleToken<Property.PaddingBlockStart>>
   >;
+
+  /**
+   * @description
+   * Sets the height of the padding area on the bottom of an element.
+   */
   paddingBottom?: ResponsiveStyle<
     SpaceKeys<StyleToken<Property.PaddingBottom>>
   >;
-  paddingInline?: ResponsiveStyle<SpaceKeys<StyleToken<Property.PaddingInline>>>;
+
+  /**
+   * @description
+   * Shorthand CSS property that defines the logical inline start and end padding of an element,
+   * which maps to physical padding properties depending on the element's writing mode, directionality, and text orientation.
+   */
+  paddingInline?: ResponsiveStyle<
+    SpaceKeys<StyleToken<Property.PaddingInline>>
+  >;
+
+  /**
+   * @description
+   * Defines the logical inline end padding of an element,
+   * which maps to a physical padding depending on the element's writing mode, directionality, and text orientation.
+   */
   paddingInlineEnd?: ResponsiveStyle<
     SpaceKeys<StyleToken<Property.PaddingInlineEnd>>
   >;
+
+  /**
+   * @description
+   * Defines the logical inline start padding of an element,
+   * which maps to a physical padding depending on the element's writing mode, directionality, and text orientation.
+   */
   paddingInlineStart?: ResponsiveStyle<
     SpaceKeys<StyleToken<Property.PaddingInlineStart>>
   >;
+
+  /**
+   * @description
+   * Sets the width of the padding area to the left of an element.
+   */
   paddingLeft?: ResponsiveStyle<SpaceKeys<StyleToken<Property.PaddingLeft>>>;
+
+  /**
+   * @description
+   * Sets the width of the padding area on the right of an element.
+   */
   paddingRight?: ResponsiveStyle<SpaceKeys<StyleToken<Property.PaddingRight>>>;
+
+  /**
+   * @description
+   * Sets the height of the padding area on the top of an element.
+   */
   paddingTop?: ResponsiveStyle<SpaceKeys<StyleToken<Property.PaddingTop>>>;
+
+  /**
+   * @description
+   * Sets how an element is positioned in a document.
+   * The top, right, bottom, and left properties determine the final location of positioned elements.
+   */
   position?: ResponsiveStyle<StyleToken<Property.Position>>;
+
+  /**
+   * @description
+   * Participates in specifying the horizontal position of a positioned element. It has no effect on non-positioned elements.
+   */
   right?: ResponsiveStyle<SpaceKeys<StyleToken<Property.Right>>>;
+
+  /**
+   * @description
+   * Sets the horizontal alignment of the content inside a block element or table-cell box.
+   * This means it works like vertical-align but in the horizontal direction.
+   */
   textAlign?: ResponsiveStyle<StyleToken<Property.TextAlign>>;
+
+  /**
+   * @description
+   * Shorthand CSS property that sets the appearance of decorative lines on text.
+   * It is a shorthand for text-decoration-line, text-decoration-color, text-decoration-style, and the newer text-decoration-thickness property.
+   */
   textDecoration?: ResponsiveStyle<StyleToken<Property.TextDecoration>>;
+
+  /**
+   * @description
+   * Specifies how to capitalize an element's text. It can be used to make text
+   * appear in all-uppercase or all-lowercase, or with each word capitalized.
+   */
   textTransform?: ResponsiveStyle<StyleToken<Property.TextTransform>>;
+
+  /**
+   * @description
+   * Participates in specifying the vertical position of a positioned element. It has no effect on non-positioned elements.
+   */
   top?: ResponsiveStyle<SpaceKeys<StyleToken<Property.Top>>>;
+
+  /**
+   * @description
+   * Lets you rotate, scale, skew, or translate an element. It modifies the coordinate space of the CSS visual formatting model.
+   */
   transform?: ResponsiveStyle<TransformKeys<StyleToken<Property.Transform>>>;
+
+  /**
+   * @description
+   * Sets the origin for an element's transformations.
+   */
   transformOrigin?: ResponsiveStyle<StyleToken<Property.TransformOrigin>>;
+
+  /**
+   * @description
+   * Sets an element's width. By default, it sets the width of the content area,
+   * but if box-sizing is set to border-box, it sets the width of the border area.
+   */
   width?: ResponsiveStyle<SpaceKeys<StyleToken<Property.Width>>>;
+
+  /**
+   * @description
+   * Sets how white space inside an element is handled.
+   */
   whiteSpace?: ResponsiveStyle<StyleToken<Property.WhiteSpace>>;
 }
 
 export interface CSSLayoutStyleProps {
   /**
+   * @description
    * Controls where the Flex/Grid items sit on the cross axis.
    */
   alignItems?: ResponsiveStyle<Property.AlignItems>;
 
   /**
+   * @description
    * Sets the distribution of space between and around content items
    */
   alignContent?: ResponsiveStyle<Property.AlignContent>;
 
   /**
+   * @description
    * Controls where the Flex/Grid items sit on the main axis.
    */
   justifyContent?: ResponsiveStyle<Property.JustifyContent>;
 
   /**
-   * Spacing between child components. Shorthand for rowGap and columnGap.
+   * @description
+   * Controls the spacing between child components. Shorthand for rowGap and columnGap.
    */
   gap?: ResponsiveStyle<SpaceKeys<StyleToken<Property.Gap>>>;
 
   /**
-   * Spacing between Flex/Grid child columns
+   * @description
+   * Controls the spacing between Flex/Grid child columns
    */
   columnGap?: ResponsiveStyle<SpaceKeys<StyleToken<Property.GridColumnGap>>>;
 
   /**
-   * Spacing between Flex/Grid child rows
+   * @description
+   * Controls the spacing between Flex/Grid child rows
    */
   rowGap?: ResponsiveStyle<SpaceKeys<StyleToken<Property.RowGap>>>;
 }
