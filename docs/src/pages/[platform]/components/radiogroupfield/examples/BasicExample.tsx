b11bd4ffc0d9de3ef63e6d0ef8d348b60d899918
Joe Buono <joebuono@amazon.com>
2022-06-22 12:43:23 -0400

    update controlled example

--- docs/src/pages/[platform]/components/radiogroupfield/examples/BasicExample.tsx
+++ docs/src/pages/[platform]/components/radiogroupfield/examples/BasicExample.tsx
@@ -1,13 +1,9 @@
 import { Radio, RadioGroupField } from '@aws-amplify/ui-react';
 
-const options = ['html', 'css', 'javascript'];
-
 export const BasicExample = () => (
-  <RadioGroupField label="Language" name="language2" defaultValue="html">
-    {options.map((option) => (
-      <Radio key={option} value={option}>
-        {option}
-      </Radio>
-    ))}
+  <RadioGroupField label="Language" name="language" defaultValue="HTML">
+    <Radio value="HTML">HTML</Radio>
+    <Radio value="CSS">CSS</Radio>
+    <Radio value="JavaScript">JavaScript</Radio>
   </RadioGroupField>
 );
