897e55de962672d76cccdb5e13f8e4f180316a9b
jacoblogan <jacob.maiola.logan@gmail.com>
2022-06-22 10:34:28 -0400

    add design tokens and theme section to stepperfield primitive (#2123)
    * add design tokens and theme section to stepperfield primitive
    
    * Create breezy-cups-carry.md
    
    * update transparent token, increase contrast
    
    * update export tests
    
    * hide label on theme example
    
    Co-authored-by: Jacob Logan <lognjc@amazon.com>

--- packages/ui/src/theme/tokens/components/stepperField.ts
+++ packages/ui/src/theme/tokens/components/stepperField.ts
@@ -1,21 +1,65 @@
 import {
+  ColorValue,
   DesignToken,
+  FontSizeValue,
   FlexDirectionValue,
   TextAlignValue,
+  BorderColorValue,
 } from '../types/designToken';
 
 interface StepperFieldInputTokens {
   textAlign: DesignToken<TextAlignValue>;
+  color: DesignToken<ColorValue>;
+  fontSize: DesignToken<FontSizeValue>;
+}
+
+interface ButtonStateColorTokens {
+  color: DesignToken<ColorValue>;
+  backgroundColor: DesignToken<ColorValue>;
 }
 
 export interface StepperFieldTokens {
+  borderColor: DesignToken<BorderColorValue>;
   flexDirection: DesignToken<FlexDirectionValue>;
   input: StepperFieldInputTokens;
+  button: {
+    color: DesignToken<ColorValue>;
+    backgroundColor: DesignToken<ColorValue>;
+    _active: ButtonStateColorTokens;
+    _focus: ButtonStateColorTokens;
+    _disabled: ButtonStateColorTokens;
+    _hover: ButtonStateColorTokens;
+  };
 }
 
 export const stepperfield: StepperFieldTokens = {
+  borderColor: { value: '{components.fieldcontrol.borderColor}' },
   flexDirection: { value: 'column' },
   input: {
+    color: { value: '{components.fieldcontrol.color}' },
+    fontSize: { value: '{components.fieldcontrol.fontSize}' },
     textAlign: { value: 'center' },
+  },
+  button: {
+    color: { value: '{components.button.color}' },
+    backgroundColor: { value: '{colors.transparent}' },
+    _active: {
+      color: { value: '{components.button._active.color}' },
+      backgroundColor: { value: '{components.button._active.backgroundColor}' },
+    },
+    _focus: {
+      color: { value: '{components.button._focus.color}' },
+      backgroundColor: { value: '{components.button._focus.backgroundColor}' },
+    },
+    _disabled: {
+      color: { value: '{components.button._disabled.color}' },
+      backgroundColor: {
+        value: '{components.button._disabled.backgroundColor}',
+      },
+    },
+    _hover: {
+      color: { value: '{components.button._hover.color}' },
+      backgroundColor: { value: '{components.button._hover.backgroundColor}' },
+    },
   },
 };
