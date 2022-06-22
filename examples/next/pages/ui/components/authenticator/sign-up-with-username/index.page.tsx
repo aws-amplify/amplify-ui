4c62227e565384a109b3c1e77fc88dc1c1e05317
wlee221 <willeea@amazon.com>
2022-06-22 18:14:36 -0400

    Revert "Add isListening logger"
    This reverts commit 6b8a77f587856e832e6cde6392218b353d8a8d25.

--- examples/next/pages/ui/components/authenticator/sign-up-with-username/index.page.tsx
+++ examples/next/pages/ui/components/authenticator/sign-up-with-username/index.page.tsx
@@ -1,4 +1,4 @@
-import { Amplify, Auth } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -10,21 +10,10 @@ export default function App() {
   return (
     <Authenticator initialState="signUp">
       {({ signOut, user }) => (
-        <>
-          {user.attributes.email}
-          <button
-            onClick={() => Auth.currentAuthenticatedUser().then(console.log)}
-          >
-            Get Authenticated User
-          </button>
-          <button onClick={() => user.getUserData(console.error)}>
-            Get user data
-          </button>
-          <button onClick={() => console.log(user.attributes)}>
-            Get user attribute
-          </button>
-          <button onClick={() => signOut()}>Sign Out</button>
-        </>
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
       )}
     </Authenticator>
   );
