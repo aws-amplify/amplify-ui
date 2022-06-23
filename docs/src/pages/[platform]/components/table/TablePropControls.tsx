ab1b0144ec15a68812795b69a7435e6e39322a67
Heather Buchel <buchel@amazon.com>
2022-06-23 15:19:21 -0400

    fix size prop id which had same issue

--- docs/src/pages/[platform]/components/table/TablePropControls.tsx
+++ docs/src/pages/[platform]/components/table/TablePropControls.tsx
@@ -48,7 +48,7 @@ export const TablePropControls: TablePropControlsInterface = ({
       onChange={(e) => setHighlightOnHover(e.target.checked)}
     />
     <SelectField
-      id="size"
+      id="sizeProp"
       name="size"
       label="size"
       placeholder="(default)"
