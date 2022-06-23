ae8888f032bfa3fe2b5174689a80acbe4153ff1d
Joe Buono <joebuono724@gmail.com>
2022-06-23 15:27:34 -0400

    Update docs/src/pages/[platform]/components/radiogroupfield/examples/BasicExample.tsx
    Co-authored-by: Heather Buchel <hbuchel@gmail.com>

--- docs/src/pages/[platform]/components/radiogroupfield/examples/BasicExample.tsx
+++ docs/src/pages/[platform]/components/radiogroupfield/examples/BasicExample.tsx
@@ -1,9 +1,9 @@
 import { Radio, RadioGroupField } from '@aws-amplify/ui-react';
 
 export const BasicExample = () => (
-  <RadioGroupField label="Language" name="language" defaultValue="HTML">
-    <Radio value="HTML">HTML</Radio>
-    <Radio value="CSS">CSS</Radio>
-    <Radio value="JavaScript">JavaScript</Radio>
+  <RadioGroupField label="Animal" name="animal" defaultValue="HTML">
+    <Radio value="Dog">Dog</Radio>
+    <Radio value="Cat">Cat</Radio>
+    <Radio value="Bird">Bird</Radio>
   </RadioGroupField>
 );
