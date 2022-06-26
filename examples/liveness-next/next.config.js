32a960333d3b35b096e872ea39c4776c2fc02077
Thadd Trinh <thadtrin@amazon.com>
2022-06-25 23:05:09 -0700

    fix: add next config to pull react/react-dom from main node_modules

--- examples/liveness-next/next.config.js
+++ examples/liveness-next/next.config.js
@@ -1,7 +1,21 @@
-const { withGlobalCss } = require('next-global-css');
+const path = require('path');
+const { patchWebpackConfig } = require('next-global-css');
 
-module.exports = withGlobalCss()({
+module.exports = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts'],
   basePath: '/liveness-next-example',
-});
+  webpack: (config, options) => {
+    // allows importing of css files inside modules
+    patchWebpackConfig(config, options);
+
+    // resolve react and react-dom from project node_modules
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+    };
+
+    return config;
+  },
+};
