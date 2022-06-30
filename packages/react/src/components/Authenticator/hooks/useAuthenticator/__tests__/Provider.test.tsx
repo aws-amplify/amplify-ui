48b1d6d03c58dd8dc0c107fb9ecfbb38b0c31d99
wlee221 <willeea@amazon.com>
2022-06-30 10:47:43 -0700

    Add test for listenToAuthHub

--- packages/react/src/components/Authenticator/hooks/useAuthenticator/__tests__/Provider.test.tsx
+++ packages/react/src/components/Authenticator/hooks/useAuthenticator/__tests__/Provider.test.tsx
@@ -1,17 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import * as React from 'react';
 import { Hub } from 'aws-amplify';
 
 import { Provider } from '../';
-import * as UI from '@aws-amplify/ui';
-
-jest.mock('@aws-amplify/ui', () => ({
-  ...(jest.requireActual('@aws-amplify/ui') as {}),
-  defaultAuthHubHandler: jest.fn(),
-}));
 
 const hubListenSpy = jest.spyOn(Hub, 'listen');
-const hubHandlerSpy = jest.spyOn(UI, 'defaultAuthHubHandler');
 
 describe('Authenticator.Provider', () => {
   it('listens to Auth Hub events on init', () => {
@@ -22,19 +15,10 @@ describe('Authenticator.Provider', () => {
     );
 
     expect(hubListenSpy).toBeCalledTimes(1);
-  });
-
-  it('responds to tokenRefresh event', () => {
-    render(
-      <Provider>
-        <></>
-      </Provider>
+    expect(hubListenSpy).toHaveBeenCalledWith(
+      'auth',
+      expect.any(Function),
+      'authenticator-hub-handler'
     );
-
-    Hub.dispatch('auth', {
-      event: 'tokenRefresh',
-    });
-
-    expect(hubHandlerSpy).toHaveBeenCalledTimes(1);
   });
 });
