c6358206b9088cd35e67808a68236bc5e5593f24
Danny Banks <djb@amazon.com>
2022-07-01 12:20:46 -0700

    chore(docs): making intro authenticator docs read better (#2238)

--- docs/src/pages/[platform]/connected-components/authenticator/NextSteps.tsx
+++ docs/src/pages/[platform]/connected-components/authenticator/NextSteps.tsx
@@ -1,5 +1,5 @@
 import { CardLink, CardLinkGroup } from '@/components/CardLink';
-import { MdLayers, MdHandyman, MdLightbulb } from 'react-icons/md';
+import { MdOutlineAutoAwesome, MdTune, MdFlipToFront } from 'react-icons/md';
 import { useCustomRouter } from '@/components/useCustomRouter';
 
 export function NextSteps() {
@@ -9,18 +9,18 @@ export function NextSteps() {
     <CardLinkGroup title="Next steps" id="next-steps">
       <CardLink
         title="Configuration"
-        icon={<MdHandyman />}
+        icon={<MdTune />}
         href={`/${platform}/connected-components/authenticator/configuration`}
         desc="How to setup and configure your Authenticator component"
       />
       <CardLink
         title="Customization"
-        icon={<MdLightbulb />}
+        icon={<MdOutlineAutoAwesome />}
         href={`/${platform}/connected-components/authenticator/customization`}
         desc="Override and customize your Authenticator"
       />
       <CardLink
-        icon={<MdLayers />}
+        icon={<MdFlipToFront />}
         title="Headless usage"
         href={`/${platform}/connected-components/authenticator/headless`}
         desc="How to run the Authenticator in a headless mode"
