ffc556d749f18c458a7b1d8f2a9e7a76dce8db15
Heather Buchel <buchel@amazon.com>
2022-07-05 01:47:43 -0400

    use style props

--- docs/src/components/ComponentStyleDisplay.tsx
+++ docs/src/components/ComponentStyleDisplay.tsx
@@ -1,20 +1,18 @@
 import * as React from 'react';
 import { ComponentClassTable } from '@/components/ComponentClassTable';
 import { ComponentVariableTable } from '@/components/ComponentVariableTable';
-import { Tabs, TabItem, View, useTheme } from '@aws-amplify/ui-react';
+import { Tabs, TabItem, View } from '@aws-amplify/ui-react';
 
 export const ComponentStyleDisplay = ({ componentName }) => {
-  const { tokens } = useTheme();
-
   return (
     <Tabs>
       <TabItem title="Target Classes">
-        <View padding={`${tokens.space.medium} 0`}>
+        <View padding="medium 0">
           <ComponentClassTable componentName={componentName} />
         </View>
       </TabItem>
       <TabItem title="CSS Variables">
-        <View padding={`${tokens.space.medium} 0`}>
+        <View padding="medium 0">
           <ComponentVariableTable componentName={componentName.toLowerCase()} />
         </View>
       </TabItem>
