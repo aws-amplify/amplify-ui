a8b77d4849fe6df85b61c8af23a264d40f799de6
Erik Hanchett <icystorm@gmail.com>
2022-07-06 11:48:08 -0700

    chore: Cleaned up Vue code (#2184)
    * Moved to latest version of Jest
    
    * Added changeset
    
    * Un pinned resolutions
    
    * Added comment
    
    Co-authored-by: Erik Hanchett <ehhanche@amazon.com>

---
+++ packages/vue/global-spec.ts
@@ -0,0 +1,21 @@
+import BaseWrapper from './src/components/primitives/base-wrapper.vue';
+import BaseFooter from './src/components/primitives/base-footer.vue';
+import BaseForm from './src/components/primitives/base-form.vue';
+import BaseAlert from './src/components/primitives/base-alert.vue';
+import BaseTwoTabItem from './src/components/primitives/base-two-tab-item.vue';
+import BaseTwoTabs from './src/components/primitives/base-two-tabs.vue';
+import BaseFieldSet from './src/components/primitives/base-field-set.vue';
+import BaseHeading from './src/components/primitives/base-heading.vue';
+import AmplifyButton from './src/components/primitives/amplify-button.vue';
+
+export const components = {
+  BaseWrapper,
+  BaseFooter,
+  BaseForm,
+  AmplifyButton,
+  BaseTwoTabItem,
+  BaseTwoTabs,
+  BaseAlert,
+  BaseFieldSet,
+  BaseHeading,
+};
