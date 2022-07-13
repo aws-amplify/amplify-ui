4d64dedd4b95e6c6b54ceb40138d71fe31f41b67
Joe Buono <joebuono@amazon.com>
2022-07-13 16:46:46 -0500

    change MDN to mdnUrl for consistency with frontmatter naming

--- docs/scripts/util/getFrontmatter.ts
+++ docs/scripts/util/getFrontmatter.ts
@@ -1,13 +1,13 @@
 export function getFrontmatter(fileData: string) {
   // get MDN URL
-  const mdn = fileData.indexOf('MDN:');
+  const mdn = fileData.indexOf('mdnUrl');
   const endOfLine1 = fileData.indexOf('\n', mdn);
-  const MDN = fileData.slice(mdn, endOfLine1).split(' ')[1];
+  const mdnUrl = fileData.slice(mdn, endOfLine1).split(' ')[1];
 
   // get htmlElement
   const el = fileData.indexOf('htmlElement');
   const endOfLine2 = fileData.indexOf('\n', el);
   const htmlElement = fileData.slice(el, endOfLine2).split(' ')[1];
 
-  return { MDN, htmlElement };
+  return { mdnUrl, htmlElement };
 }
