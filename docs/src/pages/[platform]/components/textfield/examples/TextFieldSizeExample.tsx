bb030d0cc492c7a2c2c3000e8a8ebeff3f1f60de
Joe Buono <joebuono@amazon.com>
2022-07-14 16:10:25 -0500

    fix overflow bug

--- docs/src/pages/[platform]/components/textfield/examples/TextFieldSizeExample.tsx
+++ docs/src/pages/[platform]/components/textfield/examples/TextFieldSizeExample.tsx
@@ -3,9 +3,9 @@ import { Flex, TextField } from '@aws-amplify/ui-react';
 export const TextFieldSizeExample = () => {
   return (
     <Flex direction="column">
-      <TextField label="Small" size="small" width="10rem" />
-      <TextField label="Default" width="20rem" />
-      <TextField label="Large" size="large" width="30rem" />
+      <TextField label="Small" size="small" width="50%" />
+      <TextField label="Default" width="75%" />
+      <TextField label="Large" size="large" width="100%" />
     </Flex>
   );
 };
