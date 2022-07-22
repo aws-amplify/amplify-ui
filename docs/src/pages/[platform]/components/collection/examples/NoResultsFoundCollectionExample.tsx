b037e67fb114a4c9d3716246f5d41ea66e3e084b
Joe Buono <joebuono@amazon.com>
2022-07-22 14:24:36 -0500

    update docs

---
+++ docs/src/pages/[platform]/components/collection/examples/NoResultsFoundCollectionExample.tsx
@@ -0,0 +1,36 @@
+import { Collection, Button, Text, Flex } from '@aws-amplify/ui-react';
+import { countries } from 'countries-list';
+
+export const NoResultsFoundCollectionExample = () => {
+  return (
+    <Collection
+      type="grid"
+      templateColumns="1fr 1fr 1fr"
+      gap="15px"
+      items={Object.values(countries).map(({ name, emoji }) => ({
+        name,
+        emoji,
+      }))}
+      isSearchable
+      isPaginated
+      itemsPerPage={9}
+      noResultsFound={
+        <Flex justifyContent="center">
+          <Text color="purple.60" fontSize="2rem">
+            Nothing found, please try again
+          </Text>
+        </Flex>
+      }
+      searchPlaceholder="Type to search..."
+      searchFilter={(regions, keyword) =>
+        (regions as any).name.toLowerCase().startsWith(keyword.toLowerCase())
+      }
+    >
+      {(regions, index) => (
+        <Button grow="1" key={index}>
+          {regions.emoji} {regions.name}
+        </Button>
+      )}
+    </Collection>
+  );
+};
