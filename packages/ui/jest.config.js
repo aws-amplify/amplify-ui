454a34731b9da86a57443cef9e493cdfc1ed1e88
Santosh Latpate <santoshlatpate12@gmail.com>
2022-07-25 01:10:05 -0700

    undo jest file changes.

--- packages/ui/jest.config.js
+++ packages/ui/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  // modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.json',
