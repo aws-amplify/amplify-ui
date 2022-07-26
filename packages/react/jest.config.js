8ecd1702ddef3b920567aec96989c4ad7dfccca7
Caleb Pollman <cpollman@amazon.com>
2022-07-26 14:38:54 -0700

    chore(ui-react): add jest coverage and thresholds

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./jest.setup.ts'],
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.(ts|tsx)'],
+  // ignore coverage for top level "export", PrimitiveCatalog, and style files
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/(index|internal|legacy|PrimitiveCatalog|styles).(ts|tsx)',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 71,
+      functions: 60,
+      lines: 79,
+      statements: 79,
     },
   },
+  globals: { 'ts-jest': { tsconfig: 'tsconfig.json' } },
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  testEnvironment: 'jsdom',
 };
