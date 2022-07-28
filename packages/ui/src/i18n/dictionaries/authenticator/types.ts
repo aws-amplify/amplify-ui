53146bbd6f57d0febdd84407608bc79b23089497
Joe Buono <joebuono@amazon.com>
2022-07-28 17:56:02 -0500

    update comment

--- packages/ui/src/i18n/dictionaries/authenticator/types.ts
+++ packages/ui/src/i18n/dictionaries/authenticator/types.ts
@@ -48,6 +48,6 @@ type AuthenticatorText =
   | 'Verify Contact'
   | 'Verify';
 
-// Including Record<string, string> to handle additional translations that customers have added
+// Including Record<string, string> to handle additional translations provided by customers
 export type AuthenticatorDictionary = Record<AuthenticatorText, string> &
   Record<string, string>;
