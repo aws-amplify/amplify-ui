a34aeb7130cd947a8ad7fa009ad04737f6ee9c68
Caleb Pollman <cpollman@amazon.com>
2022-07-28 11:20:12 -0700

    prevent React Native key/value from being exposed on FRAMEWORK_DISPLAY_NAMES when isReactNativeEnabled is false

--- docs/src/data/frameworks.ts
+++ docs/src/data/frameworks.ts
@@ -6,10 +6,16 @@ export const FRAMEWORKS: Frameworks = isReactNativeEnabled
   ? ['angular', 'flutter', 'react', 'react native', 'vue']
   : ['angular', 'flutter', 'react', 'vue'];
 
-export const FRAMEWORK_DISPLAY_NAMES = {
+const BASE_FRAMEWORK_DISPLAY_NAMES = {
   angular: 'Angular',
   flutter: 'Flutter',
   react: 'React',
-  'react native': 'React Native',
   vue: 'Vue',
 };
+
+export const FRAMEWORK_DISPLAY_NAMES = isReactNativeEnabled
+  ? {
+      ...BASE_FRAMEWORK_DISPLAY_NAMES,
+      'react native': 'React Native',
+    }
+  : BASE_FRAMEWORK_DISPLAY_NAMES;
