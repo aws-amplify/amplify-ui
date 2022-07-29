da882288ce16952356833e08594a94d6c78d7388
Santosh Latpate <santoshlatpate12@gmail.com>
2022-07-29 13:20:08 -0700

    remove karma.config.js and revert changes

--- examples/angular/karma.conf.js
+++ examples/angular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ui-angular'),
+      dir: require('path').join(__dirname, './coverage/angular'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
