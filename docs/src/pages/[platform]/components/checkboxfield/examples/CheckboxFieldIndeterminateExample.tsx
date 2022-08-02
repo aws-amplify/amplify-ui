db60524edfa0ebb1d35303777f98ae329c74a0a4
Chenwei Zhang <chenwz@amazon.com>
2022-08-02 15:21:37 -0700

    chore: update example to show tri-state functionality

--- docs/src/pages/[platform]/components/checkboxfield/examples/CheckboxFieldIndeterminateExample.tsx
+++ docs/src/pages/[platform]/components/checkboxfield/examples/CheckboxFieldIndeterminateExample.tsx
@@ -4,33 +4,65 @@ import { CheckboxField, Flex, View } from '@aws-amplify/ui-react';
 
 export const CheckboxFieldIndeterminateExample = () => {
   const [checkedItems, setCheckedItems] = React.useState([false, false]);
+  const checkedItemsRef = React.useRef(null);
   const allChecked = checkedItems.every(Boolean);
   const isIndeterminate = checkedItems.some(Boolean) && !allChecked;
+
+  if (isIndeterminate) {
+    checkedItemsRef.current = [...checkedItems];
+  }
+  const handleAllPetsChange = () => {
+    if (isIndeterminate) {
+      setCheckedItems([true, true]);
+    } else if (allChecked) {
+      setCheckedItems([false, false]);
+    } else if (checkedItemsRef.current) {
+      setCheckedItems(checkedItemsRef.current);
+    } else {
+      setCheckedItems([true, true]);
+    }
+  };
+
+  const handleCatChange = (e) => {
+    const newCheckedItems = [e.target.checked, checkedItems[1]];
+    if (!newCheckedItems.some(Boolean) || newCheckedItems.every(Boolean)) {
+      checkedItemsRef.current = null;
+    }
+    setCheckedItems(newCheckedItems);
+  };
+
+  const handleDogChange = (e) => {
+    const newCheckedItems = [checkedItems[0], e.target.checked];
+    if (!newCheckedItems.some(Boolean) || newCheckedItems.every(Boolean)) {
+      checkedItemsRef.current = null;
+    }
+    setCheckedItems(newCheckedItems);
+  };
 
   return (
     <Flex direction="column" gap="0">
       <CheckboxField
-        name="parent-checkbox"
-        label="Parent Checkbox"
-        value="parent"
+        name="all-pets"
+        label="All Pets"
+        value="allPets"
         checked={allChecked}
         isIndeterminate={isIndeterminate}
-        onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
+        onChange={handleAllPetsChange}
       />
       <View paddingLeft="25px">
         <CheckboxField
-          name="child-checkbox-1"
-          label=" Child Checkbox 1"
-          value="child2"
+          name="cat"
+          label="Cat"
+          value="cat"
           checked={checkedItems[0]}
-          onChange={(e) => setCheckedItems([e.target.checked, checkedItems[1]])}
+          onChange={handleCatChange}
         />
         <CheckboxField
-          name="child-checkbox-2"
-          label=" Child Checkbox 2"
-          value="child2"
+          name="dog"
+          label="Dog"
+          value="dog"
           checked={checkedItems[1]}
-          onChange={(e) => setCheckedItems([checkedItems[0], e.target.checked])}
+          onChange={handleDogChange}
         />
       </View>
     </Flex>
