20b9d6602c277191bb7e1d8d9c5b1337cfea2a36
Jordan Nelson <nejrd@amazon.com>
2022-08-03 17:45:12 -0400

    feat: consume new example options in docs

--- docs/src/components/FlutterAuthenticatorExample.tsx
+++ docs/src/components/FlutterAuthenticatorExample.tsx
@@ -1,4 +1,4 @@
-import { Alert, Loader, View } from '@aws-amplify/ui-react';
+import { Alert, Loader, TabItem, Tabs, View } from '@aws-amplify/ui-react';
 import React, { useCallback } from 'react';
 
 export function FlutterAuthenticatorExample({
@@ -8,6 +8,8 @@ export function FlutterAuthenticatorExample({
   includeSocialProviders = false,
   useCustomUI = false,
   useCustomTheme = false,
+  showTablet = true,
+  showWeb = true,
   // id is passed to the flutter authenticator.
   // it is used by the authenticator to signal when the authenticator has finished loading.
   id = generateId(),
@@ -19,18 +21,56 @@ export function FlutterAuthenticatorExample({
     ? colorElements[0].getAttribute(colorAttr)
     : 'light';
 
-  const baseUrl = '/flutter/authenticator/component.html';
-  const queryParams: Record<string, any> = {
-    id,
-    themeMode,
-    initialStep,
-    usernameAttribute,
-    includeSocialProviders,
-    useCustomUI,
-    useCustomTheme,
-    ...(signUpAttributes.length && { signUpAttributes }),
+  const getDeviceView = (device: string) => {
+    const baseUrl = '/flutter/authenticator/component.html';
+    const queryParams: Record<string, any> = {
+      id,
+      themeMode,
+      initialStep,
+      usernameAttribute,
+      includeSocialProviders,
+      useCustomUI,
+      useCustomTheme,
+      device,
+      ...(signUpAttributes.length && { signUpAttributes }),
+    };
+    var src = `${baseUrl}?${new URLSearchParams(queryParams).toString()}`;
+
+    const size = {
+      ios: {
+        height: { base: '600px', medium: '800px' },
+        width: { base: '300px', medium: '400px' },
+      },
+      android: {
+        height: { base: '600px', medium: '800px' },
+        width: { base: '315px', medium: '420px' },
+      },
+      ipad: {
+        height: { base: '1000px' },
+        width: { base: '700px' },
+      },
+      web: {
+        height: { base: '600px' },
+        width: { base: '100%' },
+      },
+    };
+    return (
+      <>
+        <FlutterAuthenticatorLoader id={id} />
+        <View width="100%" textAlign="center">
+          <View
+            as="iframe"
+            key={id}
+            height={size[device].height}
+            width={size[device].width}
+            src={src}
+            loading="lazy"
+            frameBorder="0"
+          />
+        </View>
+      </>
+    );
   };
-  var src = `${baseUrl}?${new URLSearchParams(queryParams).toString()}`;
 
   return (
     <>
@@ -39,18 +79,18 @@ export function FlutterAuthenticatorExample({
           'The Authenticator demo below uses a mock backend. Any users you create are stored in memory. You can verify accounts that you create with the code "123456".'
         }
       </Alert>
-      <FlutterAuthenticatorLoader id={id} />
-      <View width="100%" textAlign="center">
-        <View
-          as="iframe"
-          key={id}
-          height={{ base: '600px', medium: '800px' }}
-          width={{ base: '300px', medium: '400px' }}
-          src={src}
-          loading="lazy"
-          frameBorder="0"
-        />
-      </View>
+      <Tabs justifyContent="flex-start">
+        <TabItem title="iPhone">{getDeviceView('ios')}</TabItem>
+        <TabItem title="Android">{getDeviceView('android')}</TabItem>
+        {showTablet ? (
+          <TabItem title="Tablet">{getDeviceView('ipad')}</TabItem>
+        ) : null}
+        {showWeb ? (
+          <TabItem title="Web & Desktop (developer preview)">
+            {getDeviceView('web')}
+          </TabItem>
+        ) : null}
+      </Tabs>
     </>
   );
 }
