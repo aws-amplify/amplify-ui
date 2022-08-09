8d7e6770429f741624ae0a2eae97141268d011ab
Ioana Brooks <tryioana@amazon.com>
2022-08-09 10:41:42 -0700

    chore(ui-react-native): Add env support to example app

--- examples/react-native/App.tsx
+++ examples/react-native/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { Greeting } from '@env';
 
 const App = () => {
   return (
     <View style={style.container}>
-      <Text>Hello World!</Text>
+      <Text>{Greeting}</Text>
     </View>
   );
 };
