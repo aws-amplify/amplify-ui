8a47573c74ed8a957ce6b4436dfb24903ba35048
Ioana Brooks <tryioana@amazon.com>
2022-08-12 14:32:34 -0700

    chore(ui-react-native): Add storybook

--- examples/react-native/index.js
+++ examples/react-native/index.js
@@ -1,5 +1,11 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import StorybookUIRoot from './storybook';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+//TODO: replace with env var
+const isStorybook = true;
+
+AppRegistry.registerComponent(appName, () =>
+  isStorybook ? StorybookUIRoot : App
+);
