78e7e2835e166ce961f7ec0db193fd4f18eb4922
Caleb Pollman <cpollman@amazon.com>
2022-08-17 14:28:42 -0700

    remove extraneous comment

--- examples/react-native/storybook/storyLoader.js
+++ examples/react-native/storybook/storyLoader.js
@@ -1,8 +1,3 @@
-// Auto-generated file created by react-native-storybook-loader
-// Do not edit.
-//
-// https://github.com/elderfo/react-native-storybook-loader.git
-
 function loadStories() {
   require('./stories/Button.stories');
 }
