72a0ad9441b898a65422f63d7e7252bee9a0b651
Caleb Pollman <cpollman@amazon.com>
2022-08-17 15:32:07 -0700

    fix(rn-example): update storybook behavior (#2472)

--- examples/react-native/storybook/index.tsx
+++ examples/react-native/storybook/index.tsx
@@ -1,21 +1,29 @@
-import { getStorybookUI, configure } from '@storybook/react-native';
 import { Platform } from 'react-native';
-
+import { getStorybookUI, configure } from '@storybook/react-native';
+import noop from 'lodash/noop';
 import { loadStories } from './storyLoader';
 
-// import stories
-configure(() => {
-  loadStories();
-}, module);
+// only initialize storybook resources when initStorybook is true
+export function setupStorybook(initStorybook: boolean) {
+  if (initStorybook) {
+    configure(() => {
+      loadStories();
+    }, module);
+  }
 
-// Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
-// To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({
-  /*
-   * Get React Native server IP if hostname is `localhost`
-   * On Android emulator, the IP of host is `10.0.2.2`
-   */
-  host: Platform.OS === 'android' ? '10.0.2.2' : '0.0.0.0',
-});
+  // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
+  // To find allowed options for getStorybookUI
+  const StorybookRoot = initStorybook
+    ? getStorybookUI({
+        asyncStorage: require('@react-native-async-storage/async-storage')
+          .default,
+        /*
+         * Get React Native server IP if hostname is `localhost`
+         * On Android emulator, the IP of host is `10.0.2.2`
+         */
+        host: Platform.OS === 'android' ? '10.0.2.2' : '0.0.0.0',
+      })
+    : noop;
 
-export default StorybookUIRoot;
+  return { StorybookRoot };
+}
---
+++ examples/react-native/storybook/storyLoader.js
@@ -0,0 +1,10 @@
+function loadStories() {
+  require('./stories/Button.stories');
+}
+
+const stories = ['./stories/Button.stories'];
+
+module.exports = {
+  loadStories,
+  stories,
+};
