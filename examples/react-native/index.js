20de6fd0db8b89980c94971093d3613bcd40fe8f
Caleb Pollman <cpollman@amazon.com>
2022-08-16 17:43:06 -0700

    fix(rn-example): update storybook behavior

--- examples/react-native/index.js
+++ examples/react-native/index.js
@@ -1,11 +1,29 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
+import { getStorybookUI, configure } from '@storybook/react-native';
+
 import App from './App';
-import StorybookUIRoot from './storybook';
 import { name as appName } from './app.json';
+import { loadStories } from './storybook/storyLoader';
 
 //TODO: replace with env var
 const isStorybook = false;
 
-AppRegistry.registerComponent(appName, () =>
-  isStorybook ? StorybookUIRoot : App
-);
+if (isStorybook) {
+  configure(() => {
+    loadStories();
+  }, module);
+}
+
+const Root = isStorybook
+  ? getStorybookUI({
+      asyncStorage: require('@react-native-async-storage/async-storage')
+        .default,
+      /*
+       * Get React Native server IP if hostname is `localhost`
+       * On Android emulator, the IP of host is `10.0.2.2`
+       */
+      host: Platform.OS === 'android' ? '10.0.2.2' : '0.0.0.0',
+    })
+  : App;
+
+AppRegistry.registerComponent(appName, () => Root);
