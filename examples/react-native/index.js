883f13331825520b4df48d5efd3a54eb2951a621
Caleb Pollman <cpollman@amazon.com>
2022-08-17 12:41:24 -0700

    remove storybookLoader reverts on precommit

--- examples/react-native/index.js
+++ examples/react-native/index.js
@@ -1,29 +1,14 @@
-import { AppRegistry, Platform } from 'react-native';
-import { getStorybookUI, configure } from '@storybook/react-native';
+import { AppRegistry } from 'react-native';
 
 import App from './App';
 import { name as appName } from './app.json';
-import { loadStories } from './storybook/storyLoader';
+import { setupStorybook } from './storybook';
 
 //TODO: replace with env var
-const isStorybook = false;
+const initStorybook = true;
 
-if (isStorybook) {
-  configure(() => {
-    loadStories();
-  }, module);
-}
+const { StorybookRoot } = setupStorybook(initStorybook);
 
-const Root = isStorybook
-  ? getStorybookUI({
-      asyncStorage: require('@react-native-async-storage/async-storage')
-        .default,
-      /*
-       * Get React Native server IP if hostname is `localhost`
-       * On Android emulator, the IP of host is `10.0.2.2`
-       */
-      host: Platform.OS === 'android' ? '10.0.2.2' : '0.0.0.0',
-    })
-  : App;
-
-AppRegistry.registerComponent(appName, () => Root);
+AppRegistry.registerComponent(appName, () =>
+  initStorybook ? StorybookRoot : App
+);
---
+++ examples/react-native/storybook/index.tsx
@@ -0,0 +1,29 @@
+import { Platform } from 'react-native';
+import { getStorybookUI, configure } from '@storybook/react-native';
+import noop from 'lodash/noop';
+import { loadStories } from './storyLoader';
+
+// only initialize storbook resources when initStorybook is true
+export function setupStorybook(initStorybook: boolean) {
+  if (initStorybook) {
+    configure(() => {
+      loadStories();
+    }, module);
+  }
+
+  // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
+  // To find allowed options for getStorybookUI
+  const StorybookRoot = initStorybook
+    ? getStorybookUI({
+        asyncStorage: require('@react-native-async-storage/async-storage')
+          .default,
+        /*
+         * Get React Native server IP if hostname is `localhost`
+         * On Android emulator, the IP of host is `10.0.2.2`
+         */
+        host: Platform.OS === 'android' ? '10.0.2.2' : '0.0.0.0',
+      })
+    : noop;
+
+  return { StorybookRoot };
+}
