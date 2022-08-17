58a779c8716fa741ca06c4688762b0ad45ccdb9b
Joe Buono <joebuono@amazon.com>
2022-08-17 18:03:07 -0500

    refactor Heading stories to add decorator

--- examples/react-native/storybook/stories/Heading.stories.tsx
+++ examples/react-native/storybook/stories/Heading.stories.tsx
@@ -4,20 +4,17 @@ import { Heading } from '@aws-amplify/ui-react-native/dist/primitives';
 import { Screen } from '../ui';
 
 storiesOf('Heading', module)
-  .add('Heading Test', () => (
+  .addDecorator((Story: any) => (
     <Screen>
-      <Heading>This is a heading!</Heading>
+      <Story />
     </Screen>
   ))
+  .add('Heading Test', () => <Heading>This is a heading!</Heading>)
   .add('level prop', () => (
-    <Screen>
-      <Heading level={1}>This should be a large level 1 heading</Heading>
-    </Screen>
+    <Heading level={1}>This should be a large level 1 heading</Heading>
   ))
-  .add('isTruncated', () => (
-    <Screen>
-      <Heading level={1}>
-        Really long heading that should be truncated with an ellipsis
-      </Heading>
-    </Screen>
+  .add('truncated', () => (
+    <Heading level={1}>
+      Really long heading that should be truncated with an ellipsis
+    </Heading>
   ));
