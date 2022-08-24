9ba55b514998605c8c938500451025bf8eab609b
wlee221 <willeea@amazon.com>
2022-08-24 15:06:27 -0700

    Add more comments

--- environments/pull-environments.sh
+++ environments/pull-environments.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# using bin/bash for macOS and Linux compatibility
 set -e
 IFS='|'
 
@@ -27,8 +28,9 @@ fi
 # (2) dirname gets rid of filename and returns the relative path to the 
 #     directory this shell is in (e.g. ..)
 #
-# source: https://stackoverflow.com/a/24112741
+# adapted from: https://stackoverflow.com/a/24112741
 shell_path="$(dirname "${BASH_SOURCE[0]}")" # under normal use, this points to `../`
 
+# Use printf instead of echo because echo does not print newline by default. 
 printf $dirs | xargs -P $numParallelTasks -I {} sh -c ""$shell_path"/pull-environment.sh {}";
 
