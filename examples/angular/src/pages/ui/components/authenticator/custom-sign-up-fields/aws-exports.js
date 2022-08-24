36fd34e016918cfa654dd1dbd0b4793f574ca2a9
William Lee <43682783+wlee221@users.noreply.github.com>
2022-08-24 13:53:08 -0700

    chore(environments): move environments to their own folder (#2501)
    * Split environments into respective folders
    
    * chore(environments): Update scripts for environments split (#2502)
    
    * Initial approach
    
    * Simpler solution
    
    * Add auth/geo/datastore scripts
    
    * Add general pull command
    
    * Explicitly get path to the shell file
    
    * fix typo
    
    * chore(environments): update `awsExports` to point to new paths (#2504)
    
    * Update react aws-exports
    
    * Vue aws-exports update
    
    * Angular aws-exports updates

--- examples/angular/src/pages/ui/components/authenticator/custom-sign-up-fields/aws-exports.js
+++ examples/angular/src/pages/ui/components/authenticator/custom-sign-up-fields/aws-exports.js
@@ -1,2 +1,2 @@
-import awsExports from '@environments/auth-with-email-and-custom-attributes/src/aws-exports';
+import awsExports from '@environments/auth/auth-with-email-and-custom-attributes/src/aws-exports';
 export default awsExports;
