6b676712fc4dd93be2a07906a543d13ace5adfcc
William Lee <43682783+wlee221@users.noreply.github.com>
2022-08-24 09:56:54 -0700

    chore(environments): update `awsExports` to point to new paths (#2504)
    * Update react aws-exports
    
    * Vue aws-exports update
    
    * Angular aws-exports updates

--- examples/vue/src/pages/ui/components/authenticator/sign-up-with-email-lambda/aws-exports.js
+++ examples/vue/src/pages/ui/components/authenticator/sign-up-with-email-lambda/aws-exports.js
@@ -1,2 +1,2 @@
-import awsExports from '@environments/auth-with-email-lambda-signup-trigger/src/aws-exports';
+import awsExports from '@environments/auth/auth-with-email-lambda-signup-trigger/src/aws-exports';
 export default awsExports;
