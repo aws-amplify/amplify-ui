88d1c3b5aa72abb3be54689e9771505dff742f95
0618 <o.6180339@gmail.com>
2022-08-25 17:12:48 -0700

    refactor(docs): use preval for getCustomStaticPath

--- docs/src/utils/getCustomStaticPath.ts
+++ docs/src/utils/getCustomStaticPath.ts
@@ -1,6 +1,11 @@
-export const getCustomStaticPath = (platforms) => {
+import { FRAMEWORKS } from '@/data/frameworks';
+
+export const getCustomStaticPath = (platforms: string) => {
+  const platformsArr = platforms.includes('all')
+    ? FRAMEWORKS
+    : platforms.split('|');
   return {
-    paths: platforms.map((platform) => ({ params: { platform } })),
+    paths: platformsArr.map((platform) => ({ params: { platform } })),
     fallback: false,
   };
 };
