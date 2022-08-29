6ec85b3d41efdad2a30e5e7d4252dafadfb46c79
0618 <o.6180339@gmail.com>
2022-08-29 11:42:58 -0700

    fix(docs): getCustomStaticPath platformsArr

--- docs/src/utils/getCustomStaticPath.ts
+++ docs/src/utils/getCustomStaticPath.ts
@@ -1,9 +1,12 @@
 import { FRAMEWORKS } from '@/data/frameworks';
 
-export const getCustomStaticPath = (platforms: string) => {
-  const platformsArr = platforms.includes('all')
+export const getCustomStaticPath = (platforms: string[] | string) => {
+  const platformsArr = Array.isArray(platforms)
+    ? platforms
+    : platforms.includes('all')
     ? FRAMEWORKS
     : platforms.split('|');
+
   return {
     paths: platformsArr.map((platform) => ({ params: { platform } })),
     fallback: false,
