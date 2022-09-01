aefc0a277b934541d702b75865035cda76a457fb
Singh <avirajdh@bcd07416d101.ant.amazon.com>
2022-09-01 15:19:20 -0700

    formatted multiline comments for _app page tsx file

--- docs/src/pages/_app.page.tsx
+++ docs/src/pages/_app.page.tsx
@@ -40,9 +40,11 @@ if (typeof window === 'undefined') {
   window['theme'] = defaultTheme;
 }
 
-//The following awsExports object contains fake user pool id and web client id values
-//which do not translate to any actual aws user information. The purpose of this awsExports
-//object is to mock Amplify.configure() and remove the Auth config erros from the console.
+/**
+ * The following awsExports object contains fake user pool id and web client id values
+ * which do not translate to any actual aws user information. The purpose of this awsExports
+ * object is to mock Amplify.configure() and remove the Auth config erros from the console.
+ */
 const awsExports = {
   aws_user_pools_id: 'xx-xxxx-x_xxxxx', // (optional) -  Amazon Cognito User Pool ID
   aws_user_pools_web_client_id: 'xxxxxxxxxxxxxx', // (optional) - Amazon Cognito App Client ID (App client secret needs to be disabled)
