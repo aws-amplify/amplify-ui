24ce40ff8d7dda20a289df767ed0fd02aa76ce29
wlee221 <willeea@amazon.com>
2022-09-12 10:52:51 -0700

    Bump examples/angular

--- examples/angular/src/polyfills.ts
+++ examples/angular/src/polyfills.ts
@@ -57,7 +57,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
+import 'zone.js'; // Included with Angular CLI.
 (window as any).Zone['__zone_symbol__ignoreConsoleErrorUncaughtError'] = true;
 
 (window as any).global = window;
