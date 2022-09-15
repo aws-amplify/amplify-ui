7a095538e093f3f295696688abd65ef6c04414b3
Singh <avirajdh@bcd07416d101.ant.amazon.com>
2022-09-15 12:26:44 -0700

    Added and updated a checkPhone function in checkFixedCredentials

--- docs/src/data/amplifyMock/services.ts
+++ docs/src/data/amplifyMock/services.ts
@@ -10,26 +10,13 @@ const verifiedUsers = new Map();
 let signUpPassword = '';
 
 const checkFixedCredentials = (username, password) => {
-  let user = '';
-  if (username.length === 14) {
-    if (username.substring(4) === FIXED_PHONE) {
-      user = username.substring(4);
-    }
-  } else if (username.length === 13) {
-    if (username.substring(3) === FIXED_PHONE) {
-      user = username.substring(3);
-    }
-  } else if (username.length === 12) {
-    if (username.substring(2) === FIXED_PHONE) {
-      user = username.substring(2);
-    }
-  } else if (username.length === 10) {
-    user = username;
-  }
+  const checkPhone = (username) => {
+    return username.substring(username.length - 10);
+  };
   return (
     (username === FIXED_USERNAME ||
       username === FIXED_EMAIL ||
-      user === FIXED_PHONE) &&
+      checkPhone(username) === FIXED_PHONE) &&
     password === FIXED_PASSWORD
   );
 };
