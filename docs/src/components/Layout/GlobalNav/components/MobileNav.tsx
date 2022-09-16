e28bb840a0321454aba9fce67864c09ec6c5a86c
Jacob Logan <lognjc@amazon.com>
2022-09-16 11:09:58 -0700

    pass in the show global nav function

--- docs/src/components/Layout/GlobalNav/components/MobileNav.tsx
+++ docs/src/components/Layout/GlobalNav/components/MobileNav.tsx
@@ -50,6 +50,7 @@ export function MobileNav({
             currentMenuItem={currentSite}
             hasSecondaryNav={hasSecondaryNav}
             isMobile={true}
+            setShowGlobalNav={setShowGlobalNav}
           />
         </View>
       ));
