1121a816fabbc7b3a9f9ba41c8463474b0378afc
Tim Nguyen <timngyn@gmail.com>
2022-09-16 17:55:35 -0700

    Remove unused visually hidden

--- docs/src/components/Layout/GlobalNav/components/secondary-nav-components/MobileSecondaryNav.tsx
+++ docs/src/components/Layout/GlobalNav/components/secondary-nav-components/MobileSecondaryNav.tsx
@@ -1,10 +1,4 @@
-import {
-  Flex,
-  Button,
-  VisuallyHidden,
-  Icon,
-  Text,
-} from '@aws-amplify/ui-react';
+import { Flex, Button, Icon, Text } from '@aws-amplify/ui-react';
 import styles from '../../GlobalNav.module.scss';
 
 interface MobileSecondaryNavProps {
@@ -36,7 +30,6 @@ export function MobileSecondaryNav({
         className={`${styles['secondary-nav-button']}`}
         ariaLabel={`Back to all Amplify sites`}
       >
-        <VisuallyHidden>Learn</VisuallyHidden>
         <Icon
           viewBox={{ minX: 4, minY: 0, width: 16, height: 18 }}
           pathData="M13.4102 5.41L8.83016 10L13.4102 14.59L12.0002 16L6.00016 10L12.0002 4L13.4102 5.41Z"
