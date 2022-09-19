6edfe48d44d3bb0b3caef783fec89fcb9426d0a4
wlee221 <willeea@amazon.com>
2022-09-19 10:53:05 -0700

    Add arguments to pull-environmnet scripts

--- environments/pull-environments.sh
+++ environments/pull-environments.sh
@@ -1,11 +1,53 @@
 #!/bin/bash
 # using bin/bash for macOS and Linux compatibility
-set -e
-IFS='|'
 
 dirs=""
-# Pull the backend for each environment
-for dir in ./*/ ; do
+
+# Get Options from CLI
+
+# Options:
+#  -r (region): Specifies aws region to pull environments from. Defaults to us-east-2.
+#  -i (include): Regex pattern that matches environment folders to pull from.
+#  -e (exclude): Regex pattern that matches environment folders to ignore.
+# If both -i and -e are specified, it'll match all dirs that match -i but not -e.
+
+# Examples:
+# - Include only sms/phone environments:
+#   ./pull-environments.sh -r us-east-2 -i "\./auth/auth-with-((phone-and-sms-mfa)|(phone-number)|(totp-and-sms-mfa))"
+
+while getopts ':r:i:e:' OPTION; do
+
+  case "$OPTION" in
+  r)
+    region="$OPTARG"
+    ;;
+  i)
+    include=$OPTARG
+    ;;
+
+  e)
+    exclude="$OPTARG"
+    ;;
+
+  ?)
+    echo "Usage: $(basename $0) [-r region] [-i include] [-e exclude]"
+    exit 1
+    ;;
+  esac
+
+done
+
+[ -z "$region" ] && region="us-east-2"        # default to us-east-2
+[ -z "$include" ] && include="\./[a-zA-Z\-]*" # default to all folders in cwd
+
+regexMatch=""
+if ! [ -z "$regexMatch" ]; then
+  regexMatch=$(find -E . -type d -regex "$include" -not -regex "$exclude")
+else
+  regexMatch=$(find -E . -type d -regex "$include")
+fi
+
+for dir in $regexMatch; do
   if ! [ -f "$dir/package.json" ]; then
     echo "If $dir is an environment, ensure the a package.json file exists with a \"pull\" command that pulls the environment (see the README)."
     continue
@@ -16,22 +58,22 @@ for dir in ./*/ ; do
 done
 
 # max number of parallel tasks at a time
-numParallelTasks=8; # Future improvement: could set this to # of logical cores in localdevice
+numParallelTasks=8 # Future improvement: could set this to # of logical cores in localdevice
 
 if [ "$NODE_ENV" = "test" ]; then
-  numParallelTasks=1; # GitHub actions has trouble handling parallel executions
+  numParallelTasks=1 # GitHub actions has trouble handling parallel executions
 fi
 
 # Get the path to this shell file relative to cwd
 # (1) bash_source[0] contains the filename of this shell relative to cwd
 #     (e.g. `../pull-environments.sh`)
-# (2) dirname gets rid of filename and returns the relative path to the 
+# (2) dirname gets rid of filename and returns the relative path to the
 #     directory is in (e.g. ..)
 #
 # source: https://stackoverflow.com/a/24112741
 shell_path="$(dirname "${BASH_SOURCE[0]}")" # under normal use, this points to `../`
 
+printf $dirs
 # Pull environments in parallel
 # Note that printf is used because echo dosn't handle `\n` by default in bash.
-printf $dirs | xargs -P $numParallelTasks -I {} sh -c ""$shell_path"/pull-environment.sh {}";
-
+printf $dirs | xargs -P $numParallelTasks -I {} sh -c ""$shell_path"/pull-environment.sh {} "$region""
