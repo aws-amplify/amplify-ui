012f0eb28e15089217e1c9051023f2ae6923b56b
Joe Buono <joebuono@amazon.com>
2022-09-20 12:57:25 -0500

    experimenting with styles

--- packages/react-native/src/primitives/Radio/styles.ts
+++ packages/react-native/src/primitives/Radio/styles.ts
@@ -4,7 +4,7 @@ import { RadioStyles } from './types';
 
 export const styles: RadioStyles = StyleSheet.create({
   inner: {
-    backgroundColor: '#7dd6e8',
+    backgroundColor: '#317d95',
     height: 120,
     width: 120,
     borderRadius: 60,
@@ -12,10 +12,17 @@ export const styles: RadioStyles = StyleSheet.create({
   outer: {
     height: 200,
     width: 200,
+    padding: 50,
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 100,
     borderWidth: 20,
     borderColor: '#838c95',
   },
+  // focused: {
+  //   borderWidth: 30,
+  //   borderColor: '#15404d',
+  //   outlineColor: 'red',
+  //   outlineWidth: 4,
+  // },
 });
