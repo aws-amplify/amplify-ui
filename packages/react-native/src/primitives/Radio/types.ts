6cc96d38f1f86c230e47682b7a7086b7b7aebc41
Joe Buono <joebuono@amazon.com>
2022-09-21 12:55:59 -0500

    remove unnecessary iconStyle prop

--- packages/react-native/src/primitives/Radio/types.ts
+++ packages/react-native/src/primitives/Radio/types.ts
@@ -1,17 +1,10 @@
-import {
-  ImageStyle,
-  StyleProp,
-  TextStyle,
-  ViewProps,
-  ViewStyle,
-} from 'react-native';
+import { StyleProp, TextStyle, ViewProps, ViewStyle } from 'react-native';
 
 export interface RadioProps<T> extends ViewProps {
   buttonStyle?: StyleProp<TextStyle>;
   // direction can be a prop on RadioGroupField
   // direction?: 'horizontal' | 'vertical';
   disabled?: boolean;
-  iconStyle?: StyleProp<ImageStyle>;
   label?: string;
   labelPosition?: 'start' | 'end' | 'top' | 'bottom';
   labelStyle?: StyleProp<TextStyle>;
