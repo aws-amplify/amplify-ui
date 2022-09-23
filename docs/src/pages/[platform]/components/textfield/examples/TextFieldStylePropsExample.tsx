a0d4519402bc9ca358c33a9260a9f22bd81e915a
Danny Banks <djb@amazon.com>
2022-09-23 09:44:34 -0700

    feat(primitives): add inputStyles prop

--- docs/src/pages/[platform]/components/textfield/examples/TextFieldStylePropsExample.tsx
+++ docs/src/pages/[platform]/components/textfield/examples/TextFieldStylePropsExample.tsx
@@ -3,20 +3,34 @@ import { Text, TextField, useTheme } from '@aws-amplify/ui-react';
 export const TextFieldStylePropsExample = () => {
   const { tokens } = useTheme();
   return (
-    <TextField
-      direction="row"
-      alignItems="baseline"
-      fontSize={tokens.fontSizes.xl}
-      label={
-        <Text
-          fontWeight={tokens.fontWeights.bold}
-          fontSize={tokens.fontSizes.xl}
-        >
-          Name:
-        </Text>
-      }
-      backgroundColor={tokens.colors.background.primary}
-      color={tokens.colors.black}
-    />
+    <>
+      <TextField
+        direction="row"
+        alignItems="bottom"
+        fontSize={tokens.fontSizes.xl}
+        label={
+          <Text
+            fontWeight={tokens.fontWeights.bold}
+            fontSize={tokens.fontSizes.xl}
+          >
+            Name:
+          </Text>
+        }
+        backgroundColor={tokens.colors.background.primary}
+        color={tokens.colors.black}
+        width="400px"
+      />
+      <TextField
+        label="Special Field"
+        inputStyles={{
+          backgroundColor: tokens.colors.brand.primary[10],
+          border: `${tokens.borderWidths.medium} solid ${tokens.colors.brand.primary[80]}`,
+        }}
+        fontSize={tokens.fontSizes.xl}
+        backgroundColor={tokens.colors.background.primary}
+        color={tokens.colors.black}
+        width="400px"
+      />
+    </>
   );
 };
