820d692f93bfa685f49a3d9dfc935dc361bd3887
Joe Buono <joebuono@amazon.com>
2022-09-23 14:33:49 -0500

    preparing for merge with Checkbox PR

--- examples/react-native/src/ui/index.ts
+++ examples/react-native/src/ui/index.ts
@@ -2,21 +2,12 @@
  * Re-export internal React Native UI primitives and types for use in example apps and storybook.
  * This file should be removed when primitves are exposed for external usage
  */
-// CHANGE TO export * when done testing Radio implementation
-export {
-  Button,
-  Carousel,
-  Heading,
-  IconButton,
-  Label,
-  Radio,
-} from '@aws-amplify/ui-react-native/dist/primitives';
+export * from '@aws-amplify/ui-react-native/dist/primitives';
 
 /**
  * The below components should be migrated to ui-react-native/src/primitives once completed,
  * e.g. have full type documentation, controlled vs uncontrolled handling, stories, touch feedback,
  * accessibility props, unit tests, etc
  */
-export * from './Checkbox';
-// export * from './Radio';
+export * from './Radio';
 export * from './RadioGroup';
