7e8760f4e56d82a2af8a313b3f656dddf294e9da
Ioana Brooks <68251134+ioanabrooks@users.noreply.github.com>
2022-09-23 10:33:54 -0700

    Update packages/react-native/src/primitives/Label/utils.ts
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

--- packages/react-native/src/primitives/Label/utils.ts
+++ packages/react-native/src/primitives/Label/utils.ts
@@ -7,7 +7,20 @@ const FLEX_DIRECTIONS: Record<LabelPosition, ViewStyle['flexDirection']> = {
   top: 'column-reverse',
   bottom: 'column',
 };
-
+ /**
+ * The `flexDirection` value returned from `getFlexDirectionFromLabelPosition`
+ * assumes a `Label` component follows the `Primary` component it labels inside
+ * the `Container` that the `flexDirection` is applied to.
+ *
+ * Example Usage:
+ * ```jsx
+ *   const flexDirection = getFlexDirectionFromLabelPosition(labelPosition);
+ *   <Container style={{ flexDirection }}>
+ *     <Primary />
+ *     <Label />
+ *   </Container>
+ * ```
+ */
 export const getFlexDirectionFromLabelPosition = (
   position: LabelPosition
 ): ViewStyle['flexDirection'] => FLEX_DIRECTIONS[position];
