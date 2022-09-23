d7248cb58138cd6336fa7ce1518d8ce805a04a80
Heather Buchel <buchel@amazon.com>
2022-09-23 14:30:23 -0400

    update files from latest in liveness-main

--- packages/react/src/components/Liveness/shared/RecordingIcon.tsx
+++ packages/react/src/components/Liveness/shared/RecordingIcon.tsx
@@ -1,35 +1,23 @@
 import React from 'react';
 
-import { useTheme } from '../../../hooks';
-import { Text, Flex } from '../../../primitives';
+import { Text, Flex, Icon } from '../../../primitives';
 
 export const RecordingIcon: React.FC = () => {
-  const { tokens } = useTheme();
-
   return (
     <Flex
       direction="column"
       alignItems="center"
-      backgroundColor={`${tokens.colors.background.primary}`}
-      padding={`${tokens.space.xxs}`}
-      gap={`${tokens.space.xxs}`}
-      borderRadius={`${tokens.borderWidths.large}`}
+      backgroundColor="background.primary"
+      padding="xxs"
+      gap="xxs"
+      borderRadius="small"
     >
       <Flex data-testid="rec-icon" justifyContent="center">
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="red"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="10" cy="10" r="8" />
-        </svg>
+        <Icon viewBox={{ width: 20, height: 20 }} width="20" height="20">
+          <circle cx="10" cy="10" r="8" fill="red" />
+        </Icon>
       </Flex>
-      <Text
-        color={`${tokens.colors.font.primary}`}
-        fontWeight={`${tokens.fontWeights.bold}`}
-      >
+      <Text as="span" fontWeight="bold">
         Rec
       </Text>
     </Flex>
