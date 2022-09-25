64d7b01fb80a8df15c151f830948046bb317b016
Caleb Pollman <cpollman@amazon.com>
2022-09-24 18:54:03 -0700

    configure jest.config.ts

--- packages/react-core/jest.config.js
+++ packages/react-core/jest.config.js
@@ -1,22 +1,23 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    'src/(.*)': '<rootDir>/src/$1',
-  },
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.(ts|tsx)',
+
+    // do not collect coverage from constants files
+    '!<rootDir>/src/**/*(c|C)onstants.ts',
+    // do not collect coverage from primary exports file
+    '!<rootDir>/src/index.ts',
+  ],
   coverageThreshold: {
+    // temp setting these to zero
     global: {
-      // temp setting these to zero
       branches: 0, // 90,
       functions: 0, // 90,
       lines: 0, // 90,
       statements: 0, // 90,
     },
   },
+  globals: { 'ts-jest': { tsconfig: 'tsconfig.json' } },
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
 };
