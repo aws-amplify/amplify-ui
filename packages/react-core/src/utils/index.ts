214b31924cff80471100275f53afa7ec5f579d85
Caleb Pollman <cpollman@amazon.com>
2022-09-26 09:49:28 -0700

    update some utils

--- packages/react-core/src/utils/index.ts
+++ packages/react-core/src/utils/index.ts
@@ -5,14 +5,14 @@ function isEmptyArray<T>(value: T): boolean {
   return Array.isArray(value) && isEmpty(value);
 }
 
-export function areEmptyArrays<T>(...args: T[]): boolean {
-  return args.every(isEmptyArray);
+export function areEmptyArrays<T>(...values: T[]): boolean {
+  return values.every(isEmptyArray);
 }
 
 function isEmptyObject<T>(value: T): boolean {
   return isObject(value) && !Array.isArray(value) && isEmpty(value);
 }
 
-export function areEmptyObjects<T>(...args: T[]): boolean {
-  return args.every(isEmptyObject);
+export function areEmptyObjects<T>(...values: T[]): boolean {
+  return values.every(isEmptyObject);
 }
