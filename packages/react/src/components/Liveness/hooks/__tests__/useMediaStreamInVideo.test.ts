f719f832bf1df933892f196cf63906e995a15a27
Thadd Trinh <thadtrin@amazon.com>
2022-10-04 21:01:50 -0700

    fix: separate into two unit tests

--- packages/react/src/components/Liveness/hooks/__tests__/useMediaStreamInVideo.test.ts
+++ packages/react/src/components/Liveness/hooks/__tests__/useMediaStreamInVideo.test.ts
@@ -26,7 +26,6 @@ describe('useMediaStreamInVideo', () => {
     };
 
     const { result, rerender, unmount } = renderHook(() => {
-      console.log(stream);
       return useMediaStreamInVideo(stream, videoConstraints);
     });
 
@@ -38,10 +37,33 @@ describe('useMediaStreamInVideo', () => {
     unmount();
     expect(stream.removeTrack).toHaveBeenCalledWith(track);
     expect(track.stop).toHaveBeenCalled();
+  });
 
-    // FIXME: What is the purpose of this test?
-    // window.innerHeight = 150;
-    // rerender({ stream, videoConstraints });
-    // expect(result.current.streamOffset).toBe(0);
+  it('should return streamOffset of 0 if inner height is less than heigh', () => {
+    jest.spyOn(React, 'useRef').mockReturnValue({
+      current: document.createElement('video'),
+    });
+    window.innerHeight = 150;
+
+    const track = {
+      getSettings: () => ({ height: 200, width: 200 }),
+      stop: jest.fn(),
+    } as unknown as MediaStreamTrack;
+
+    const stream = {
+      getTracks: () => [track],
+      removeTrack: jest.fn(),
+    } as unknown as MediaStream;
+
+    const videoConstraints = {
+      height: { ideal: 100 },
+      width: { ideal: 100 },
+    };
+
+    const { result } = renderHook(() => {
+      return useMediaStreamInVideo(stream, videoConstraints);
+    });
+
+    expect(result.current.streamOffset).toBe(0);
   });
 });
