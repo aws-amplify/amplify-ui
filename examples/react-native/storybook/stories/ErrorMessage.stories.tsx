6e3c3ee6b2f164802b6d1ed2709d975d5e6d8bca
Joe Buono <joebuono@amazon.com>
2022-10-05 15:08:52 -0500

    add stories for props

--- examples/react-native/storybook/stories/ErrorMessage.stories.tsx
+++ examples/react-native/storybook/stories/ErrorMessage.stories.tsx
@@ -1,15 +1,43 @@
-import React from 'react';
-// import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import { ErrorMessage } from '@aws-amplify/ui-react-native/dist/primitives';
 
-storiesOf('ErrorMessage', module).add('default', () => (
-  <ErrorMessage>Default ErrorMessage</ErrorMessage>
-));
-// .add('style', () => <ErrorMessage style={styles.redText}>This should be red</ErrorMessage>);
+const OnDismissDemo = () => {
+  const [dismissed, setDismissed] = useState<boolean>(false);
 
-// const styles = StyleSheet.create({
-//   redText: {
-//     color: 'red',
-//   },
-// });
+  return (
+    <>
+      <ErrorMessage onDismiss={() => setDismissed(!dismissed)}>
+        Press the close icon (x) to dismiss
+      </ErrorMessage>
+      {dismissed ? <Text>ErrorMessage dismissed</Text> : null}
+    </>
+  );
+};
+
+storiesOf('ErrorMessage', module)
+  .add('default', () => <ErrorMessage>Default ErrorMessage</ErrorMessage>)
+  .add('onDismiss', () => <OnDismissDemo />)
+  .add('styles', () => (
+    <ErrorMessage style={styles.alert} textStyle={styles.whiteText}>
+      White text, orange background
+    </ErrorMessage>
+  ))
+  .add('long text', () => (
+    <ErrorMessage>
+      This is a long string of text in order to test how the ErrorMessage
+      container responds
+    </ErrorMessage>
+  ));
+
+const styles = StyleSheet.create({
+  alert: {
+    backgroundColor: '#ff7f50',
+  },
+  whiteText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
