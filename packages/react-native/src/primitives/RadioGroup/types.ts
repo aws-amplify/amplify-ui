479fcd070f92c557f28f0036216c1ce75eb08612
Joe Buono <joebuono@amazon.com>
2022-10-06 12:04:32 -0500

    extend from ViewProps as well

--- packages/react-native/src/primitives/RadioGroup/types.ts
+++ packages/react-native/src/primitives/RadioGroup/types.ts
@@ -1,20 +1,21 @@
-import { TextStyle, ViewStyle } from 'react-native';
+import { TextStyle, ViewStyle, ViewProps } from 'react-native';
 import { RadioProps } from '../Radio';
 
 export type Direction = 'vertical' | 'horizontal';
 
 export interface RadioGroupProps<T>
   extends Pick<
-    RadioProps<T>,
-    | 'accessibilityRole'
-    | 'disabled'
-    | 'label'
-    | 'labelPosition'
-    | 'labelStyle'
-    | 'onChange'
-    | 'size'
-    | 'value'
-  > {
+      RadioProps<T>,
+      | 'accessibilityRole'
+      | 'disabled'
+      | 'label'
+      | 'labelPosition'
+      | 'labelStyle'
+      | 'onChange'
+      | 'size'
+      | 'value'
+    >,
+    ViewProps {
   children: React.ReactElement<RadioProps<T>>[];
   defaultValue?: T;
   direction?: Direction;
