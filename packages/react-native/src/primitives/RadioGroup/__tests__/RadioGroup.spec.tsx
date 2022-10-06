13bc3892f3bad3dd5cc99f50b741864738648571
Joe Buono <joebuono@amazon.com>
2022-10-06 14:44:59 -0500

    update tests

--- packages/react-native/src/primitives/RadioGroup/__tests__/RadioGroup.spec.tsx
+++ packages/react-native/src/primitives/RadioGroup/__tests__/RadioGroup.spec.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 
 import { Radio } from '../../Radio';
 import { Size } from '../../Radio/types';
@@ -10,14 +10,33 @@ import { Direction } from '../types';
 const directions: Direction[] = ['vertical', 'horizontal'];
 const sizes: Size[] = ['small', 'medium', 'large'];
 
-const ControlledRadioGroup = ({ ...props }: any) => {
+const onChangeMock = jest.fn();
+
+const ControlledRadioGroup = ({
+  onChange,
+  ...rest
+}: {
+  onChange?: jest.Mock<any, any>;
+  direction?: Direction;
+  size?: Size;
+  disabled?: boolean;
+}) => {
   const [value, setValue] = useState('option-1');
-  const onChange = (nextValue: string) => {
+  const handleOnChange = (nextValue: string) => {
     setValue(nextValue);
+    onChange?.(nextValue);
   };
 
   return (
-    <RadioGroup {...props} value={value} onChange={onChange}>
+    <RadioGroup
+      {...rest}
+      value={value}
+      onChange={(nextValue) => {
+        if (typeof nextValue === 'string') {
+          handleOnChange(nextValue);
+        }
+      }}
+    >
       <Radio value="option-1" label="Option 1" />
       <Radio value="option-2" label="Option 2" />
       <Radio value="option-3" label="Option 3" />
@@ -25,16 +44,44 @@ const ControlledRadioGroup = ({ ...props }: any) => {
   );
 };
 
-const onChange = jest.fn();
-
 describe('RadioGroup', () => {
   beforeEach(() => {
-    onChange.mockClear();
+    onChangeMock.mockClear();
   });
 
   it('renders default RadioGroup as expected', () => {
-    const { toJSON } = render(<ControlledRadioGroup />);
+    const { queryAllByRole, toJSON } = render(<ControlledRadioGroup />);
+
+    const radios = queryAllByRole('radio');
+    expect(radios.length).toBe(3);
+
     expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('allows users to select radios', () => {
+    const { getByText } = render(
+      <ControlledRadioGroup onChange={onChangeMock} />
+    );
+
+    const option2 = getByText('Option 2');
+    const option3 = getByText('Option 3');
+
+    fireEvent.press(option2);
+    expect(onChangeMock).toBeCalledWith('option-2');
+
+    fireEvent.press(option3);
+    expect(onChangeMock).toBeCalledWith('option-3');
+  });
+
+  it('does not execute the callback when disabled', () => {
+    const { getByText } = render(
+      <ControlledRadioGroup disabled onChange={onChangeMock} />
+    );
+
+    const option2 = getByText('Option 2');
+
+    fireEvent.press(option2);
+    expect(onChangeMock).not.toHaveBeenCalled();
   });
 
   it.each(directions)('renders as expected when size is %s', (value) => {
@@ -44,11 +91,6 @@ describe('RadioGroup', () => {
 
   it.each(sizes)('renders as expected when size is %s', (value) => {
     const { toJSON } = render(<ControlledRadioGroup size={value} />);
-    expect(toJSON()).toMatchSnapshot();
-  });
-
-  it('renders as expected when disabled', () => {
-    const { toJSON } = render(<ControlledRadioGroup disabled />);
     expect(toJSON()).toMatchSnapshot();
   });
 });
