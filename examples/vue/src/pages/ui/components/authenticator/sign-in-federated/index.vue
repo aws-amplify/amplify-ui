4acdefd2c6ed66f12a1a71a3d3b4d8545d033717
William Lee <43682783+wlee221@users.noreply.github.com>
2022-10-07 15:14:22 -0700

    chore(split-environments): recreate auth-with-federated environment (#2704)
    * Use auth-with-federated again
    
    * Add back auth-with-federated environment
    
    * Back to zeroconfig!
    
    * Skip apple test
    
    * Fix typo

--- examples/vue/src/pages/ui/components/authenticator/sign-in-federated/index.vue
+++ examples/vue/src/pages/ui/components/authenticator/sign-in-federated/index.vue
@@ -8,7 +8,7 @@ Amplify.configure(aws_exports);
 </script>
 
 <template>
-  <authenticator :social-providers="['amazon', 'apple', 'facebook', 'google']">
+  <authenticator>
     <template v-slot="{ user, signOut }">
       <h1>Hello {{ user.username }}!</h1>
       <button @click="signOut">Sign Out</button>
