003105511cbec7b65cc6ec7c1ccbc04deadb4b7d
wlee221 <willeea@amazon.com>
2022-10-06 17:38:16 -0700

    Add back auth-with-federated environment

---
+++ environments/auth/auth-with-federated/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "authwithfederatedd9f06071": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
