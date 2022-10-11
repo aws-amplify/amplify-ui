98045cd93277b705e87860de2293157dd3aabe10
0618 <o.6180339@gmail.com>
2022-10-11 12:47:51 -0400

    feat(docs): update heading demo

--- docs/src/pages/[platform]/components/heading/demo.tsx
+++ docs/src/pages/[platform]/components/heading/demo.tsx
@@ -1,21 +1,42 @@
 import * as React from 'react';
-import { Heading, HeadingLevel } from '@aws-amplify/ui-react';
+import { Heading, HeadingLevel, HeadingProps } from '@aws-amplify/ui-react';
 
 import { HeadingPropControls } from './HeadingPropControls';
 import { Demo } from '@/components/Demo';
 
-const propsToCode = (level: HeadingLevel) => {
-  return `<Heading level={${level}}>Heading</Heading>`;
+const propsToCode = (
+  level: HeadingLevel,
+  isTruncated: boolean,
+  value: string
+) => {
+  return `<Heading
+  width='30vw'
+  level={${level}} ${isTruncated ? `\n  isTruncated={true}` : ''}
+>
+   ${value}
+</Heading>`;
 };
 export const HeadingDemo = () => {
   const [level, setLevel] = React.useState<HeadingLevel>(6);
-
+  const [isTruncated, setIsTruncated] = React.useState<boolean>(false);
+  const [value, setValue] = React.useState<string>('Hello World!');
   return (
     <Demo
-      code={propsToCode(level)}
-      propControls={<HeadingPropControls level={level} setLevel={setLevel} />}
+      code={propsToCode(level, isTruncated, value)}
+      propControls={
+        <HeadingPropControls
+          level={level}
+          setLevel={setLevel}
+          isTruncated={isTruncated}
+          setIsTruncated={setIsTruncated}
+          value={value}
+          setValue={setValue}
+        />
+      }
     >
-      <Heading level={level}>Heading</Heading>
+      <Heading width="30vw" level={level} isTruncated={isTruncated}>
+        {value}
+      </Heading>
     </Demo>
   );
 };
