72b14a49fcdbb4568e004595d01fcdf820d11fcb
Joe Buono <joebuono@amazon.com>
2022-10-12 15:56:06 -0500

    adjust types

--- packages/react-native/src/primitives/Tabs/types.ts
+++ packages/react-native/src/primitives/Tabs/types.ts
@@ -1,4 +1,5 @@
 import { TextStyle, ViewProps, ViewStyle } from 'react-native';
+
 import { ButtonProps } from '../Button';
 
 export interface TabsProps extends ViewProps {
@@ -11,9 +12,11 @@ export interface TabsProps extends ViewProps {
   // should this be required until Tabs can also be an uncontrolled component?
   onChange?: (index: number) => void; // to update the selectedIndex
 
+  // we can add this prop later
   // relative spacing could be accomplished through flexGrow: 1
   spacing?: 'equal' | 'relative';
 
+  // we can add this prop later
   indicatorPosition?: 'top' | 'bottom';
 }
 
