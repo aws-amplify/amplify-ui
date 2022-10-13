0d50811c7c1ba607eee7e01d5deb70f39c99a7ac
wlee221 <willeea@amazon.com>
2022-10-13 14:29:03 -0700

    Remove describe layer and improve wording

--- packages/vue/src/components/__tests__/alias-control.spec.ts
+++ packages/vue/src/components/__tests__/alias-control.spec.ts
@@ -5,75 +5,68 @@ import { render, screen } from '@testing-library/vue';
 // mock Math.random so that ids are statically generated
 jest.spyOn(global.Math, 'random').mockReturnValue(0.123456789);
 
-describe('AliasControl Component', () => {
-  describe('Label', () => {
-    it('label should render default classnames', async () => {
-      render(AliasControl, {
-        global: {
-          components,
-        },
-        props: {
-          label: 'Username',
-          name: 'username',
-          placeholder: 'Username',
-        },
-      });
-      const label = await screen.findByText('Username');
-      expect(label).toHaveClass('amplify-label');
+describe('AliasControl', () => {
+  it('renders a label with the default class', async () => {
+    render(AliasControl, {
+      global: {
+        components,
+      },
+      props: {
+        label: 'Username',
+        name: 'username',
+        placeholder: 'Username',
+      },
     });
-
-    it('label should have `amplify-visually-hidden` class when labelHidden is true', async () => {
-      render(AliasControl, {
-        global: {
-          components,
-        },
-        props: {
-          label: 'Username',
-          name: 'username',
-          placeholder: 'Username',
-          labelHidden: true,
-        },
-      });
-
-      const label = await screen.findByText('Username');
-      expect(label).toHaveClass('amplify-visually-hidden');
-    });
+    const label = await screen.findByText('Username');
+    expect(label).toHaveClass('amplify-label');
   });
 
-  describe('Field', () => {
-    it('field should render default classnames and attributes', async () => {
-      render(AliasControl, {
-        global: {
-          components,
-        },
-        props: {
-          label: 'Username',
-          name: 'username',
-          placeholder: 'Username',
-        },
-      });
-      const field = await screen.findByRole('textbox');
-      expect(field).toHaveClass(
-        'amplify-input',
-        'amplify-field-group__control'
-      );
+  it('label should have `amplify-visually-hidden` class when labelHidden is true', async () => {
+    render(AliasControl, {
+      global: {
+        components,
+      },
+      props: {
+        label: 'Username',
+        name: 'username',
+        placeholder: 'Username',
+        labelHidden: true,
+      },
     });
 
-    it('field should have aria-invalid attribute to input when hasError', async () => {
-      render(AliasControl, {
-        global: {
-          components,
-        },
-        props: {
-          label: 'Username',
-          name: 'username',
-          placeholder: 'Username',
-          hasError: true,
-        },
-      });
+    const label = await screen.findByText('Username');
+    expect(label).toHaveClass('amplify-visually-hidden');
+  });
 
-      const field = await screen.findByRole('textbox');
-      expect(field).toHaveAttribute('aria-invalid');
+  it('renders text field with default classnames and attributes', async () => {
+    render(AliasControl, {
+      global: {
+        components,
+      },
+      props: {
+        label: 'Username',
+        name: 'username',
+        placeholder: 'Username',
+      },
     });
+    const field = await screen.findByRole('textbox');
+    expect(field).toHaveClass('amplify-input', 'amplify-field-group__control');
+  });
+
+  it('field should add aria-invalid attribute to input when hasError is true', async () => {
+    render(AliasControl, {
+      global: {
+        components,
+      },
+      props: {
+        label: 'Username',
+        name: 'username',
+        placeholder: 'Username',
+        hasError: true,
+      },
+    });
+
+    const field = await screen.findByRole('textbox');
+    expect(field).toHaveAttribute('aria-invalid');
   });
 });
