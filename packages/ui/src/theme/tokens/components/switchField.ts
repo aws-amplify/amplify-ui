7a13694017511d7f0c803302eb2c237539ec38b5
Danny Banks <djb@amazon.com>
2022-10-13 15:56:28 -0700

    chore: fixing tests

--- packages/ui/src/theme/tokens/components/switchField.ts
+++ packages/ui/src/theme/tokens/components/switchField.ts
@@ -131,7 +131,7 @@ export const switchfield: SwitchFieldTokens = {
     width: { value: '{space.relative.xl.value}' },
 
     _error: {
-      backgroundColor: { value: '{colors.backgroundColor.error.value}' },
+      backgroundColor: { value: '{colors.background.error.value}' },
     },
   },
 };
