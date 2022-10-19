18a49eeb149e9134e2ff8698d9bcd72516af47be
wlee221 <willeea@amazon.com>
2022-10-19 10:57:48 -0700

    await defaultAuthHubHandler in Angular

--- packages/angular/projects/ui-angular/src/lib/components/authenticator/components/authenticator/authenticator.component.ts
+++ packages/angular/projects/ui-angular/src/lib/components/authenticator/components/authenticator/authenticator.component.ts
@@ -69,27 +69,30 @@ export class AuthenticatorComponent
 
     const { authService, initializeMachine } = this.authenticator;
 
-    this.unsubscribeHub = listenToAuthHub(authService, (data, service) => {
-      defaultAuthHubHandler(data, service);
-      /**
-       * Hub events aren't properly caught by Angular, because they are
-       * synchronous events. Angular tracks async network events and
-       * html events, but not synchronous events like hub.
-       *
-       * On any notable hub events, we run change detection manually.
-       */
-      this.changeDetector.detectChanges();
+    this.unsubscribeHub = listenToAuthHub(
+      authService,
+      async (data, service) => {
+        await defaultAuthHubHandler(data, service);
+        /**
+         * Hub events aren't properly caught by Angular, because they are
+         * synchronous events. Angular tracks async network events and
+         * html events, but not synchronous events like hub.
+         *
+         * On any notable hub events, we run change detection manually.
+         */
+        this.changeDetector.detectChanges();
 
-      /**
-       * Hub events that we handle can lead to multiple state changes:
-       * e.g. `authenticated` -> `signOut` -> initialState.
-       *
-       * We want to ensure change detection runs all the way, until
-       * we reach back to the initial state. Setting the below flag
-       * to true to until we reach initial state.
-       */
-      this.isHandlingHubEvent = true;
-    });
+        /**
+         * Hub events that we handle can lead to multiple state changes:
+         * e.g. `authenticated` -> `signOut` -> initialState.
+         *
+         * We want to ensure change detection runs all the way, until
+         * we reach back to the initial state. Setting the below flag
+         * to true to until we reach initial state.
+         */
+        this.isHandlingHubEvent = true;
+      }
+    );
 
     /**
      * Subscribes to state machine changes and sends INIT event
