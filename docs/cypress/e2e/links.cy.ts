e0561d44d805c6168ae489312e4ef7e2e6041846
0618 <o.6180339@gmail.com>
2022-10-20 15:29:05 -0700

    test: links on each page

--- docs/cypress/e2e/links.cy.ts
+++ docs/cypress/e2e/links.cy.ts
@@ -1,9 +1,18 @@
 describe('Links on Sitemap', () => {
-  it('return 200', async () => {
+  it('should be 118 pages. Links on each page should all return 200', async () => {
     cy.task('readSitemapLinks').then((links: string[]) => {
+      expect(links.length).to.eq(119);
       links.forEach((link) => {
-        cy.request(link ?? '/').then(({ status }) => {
-          expect(status).to.eq(200);
+        cy.visit(link ?? '/');
+        cy.get('a').each((anchor) => {
+          cy.request(anchor.prop('href')).then(({ status }) => {
+            expect(status).to.eq(200);
+          });
+        });
+        cy.get('button').each((button) => {
+          cy.request(button.prop('href')).then(({ status }) => {
+            expect(status).to.eq(200);
+          });
         });
       });
     });
