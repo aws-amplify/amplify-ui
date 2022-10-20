b8a327ca2a90c408489df723ad5d186383898f17
Heather Buchel <buchel@amazon.com>
2022-10-20 15:31:01 -0400

    cleanup branch

--- docs/__tests__/props.ts
+++
@@ -1,9 +0,0 @@
-import * as propsData from '@/data/props';
-
-describe('propsData', () => {
-  describe('alert', () => {
-    it('should match snapshot', () => {
-      expect(propsData.alert).toMatchSnapshot();
-    });
-  });
-});
