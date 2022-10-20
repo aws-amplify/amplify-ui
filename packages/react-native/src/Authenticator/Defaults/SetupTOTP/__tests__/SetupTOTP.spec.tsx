e28c4f292e3975c457b7b68e8d0f10c57ccc2755
Joe Buono <joebuono@amazon.com>
2022-10-20 12:16:20 -0500

    add basic snapshot tests for the sake of coverage

---
+++ packages/react-native/src/Authenticator/Defaults/SetupTOTP/__tests__/SetupTOTP.spec.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import { SetupTOTP } from '..';
+
+const props = {} as any;
+
+describe('SetupTOTP', () => {
+  it('renders as expected', () => {
+    const { toJSON } = render(
+      <>
+        <SetupTOTP {...props} />
+        <SetupTOTP.Header />
+        <SetupTOTP.Footer />
+        <SetupTOTP.FormFields fields={[]} />
+      </>
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
---
+++ packages/react-native/src/Authenticator/Defaults/SignUp/__tests__/SignUp.spec.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import { SignUp } from '..';
+
+const props = {} as any;
+
+describe('SignUp', () => {
+  it('renders as expected', () => {
+    const { toJSON } = render(
+      <>
+        <SignUp {...props} />
+        <SignUp.Header />
+        <SignUp.Footer />
+        <SignUp.FormFields fields={[]} />
+      </>
+    );
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
