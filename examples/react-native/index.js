2510ef0b12863427d7b1803307ee6feb46697809
Caleb Pollman <cpollman@amazon.com>
2022-10-20 18:31:12 -0700

    add a comment abt polyfills

--- examples/react-native/index.js
+++ examples/react-native/index.js
@@ -1,3 +1,5 @@
+// These polyfills are required by Amplify JS,
+// but are commonly found in most React Native apps
 import 'react-native-get-random-values';
 import 'react-native-url-polyfill/auto';
 
