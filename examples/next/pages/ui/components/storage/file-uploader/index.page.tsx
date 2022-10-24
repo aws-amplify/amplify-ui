216ed16fa45f7227bd809cb586d1890d043bba3f
Erik Hanchett <ehhanche@amazon.com>
2022-10-24 14:15:15 -0700

    Stubbed out example

--- examples/next/pages/ui/components/storage/file-uploader/index.page.tsx
+++ examples/next/pages/ui/components/storage/file-uploader/index.page.tsx
@@ -9,41 +9,16 @@ import { SwitchField, View, Card } from '@aws-amplify/ui-react';
 Amplify.configure(awsExports);
 
 function CustomUploaderDrop({ getDropEvents, inDropZone }) {
-  return (
-    <Card
-      style={inDropZone && { backgroundColor: 'purple' }}
-      border={'solid rgb(8,112,152)'}
-      {...getDropEvents}
-    />
-  );
+  // stubbed
 }
 
 export default function FileUploaderEmail() {
-  const [variation, setVariation] = useState(false);
-  const [showPreviewer, setShowPreviewer] = useState(false);
-
   return (
     <>
-      <Button onClick={() => setShowPreviewer(!showPreviewer)}>Press me</Button>
-      <View>
-        <SwitchField
-          isDisabled={false}
-          label="drop?"
-          labelPosition="start"
-          isChecked={variation}
-          onChange={(e) => setVariation(e.target.checked)}
-        />
-      </View>
-
       <FileUploader
         acceptedFileTypes={['.png']}
-        isPreviewerVisible={showPreviewer}
-        components={{
-          UploaderDrop: CustomUploaderDrop,
-        }}
         level="public"
         multiple={true}
-        variation={variation ? 'drop' : 'button'}
       />
     </>
   );
