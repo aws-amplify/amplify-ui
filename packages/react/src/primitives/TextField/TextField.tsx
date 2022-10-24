d71836994a5d2f5b0e524f1ab176a88db18dec46
Danny Banks <djb@amazon.com>
2022-10-24 14:07:34 -0700

    BREAKING CHANGE: React Field component style props split (#2636)

--- packages/react/src/primitives/TextField/TextField.tsx
+++ packages/react/src/primitives/TextField/TextField.tsx
@@ -8,8 +8,8 @@ import { FieldGroup } from '../FieldGroup';
 import { Flex } from '../Flex';
 import { Input } from '../Input';
 import { Label } from '../Label';
-import { splitPrimitiveProps } from '../shared/styleUtils';
 import { Primitive, TextFieldProps } from '../types';
+import { splitPrimitiveProps } from '../shared/styleUtils';
 import { useStableId } from '../utils/useStableId';
 
 const TextFieldPrimitive: Primitive<TextFieldProps, 'input'> = (props, ref) => {
@@ -28,17 +28,7 @@ const TextFieldPrimitive: Primitive<TextFieldProps, 'input'> = (props, ref) => {
     size,
     testId,
     variation,
-
-    alignSelf, // @TODO: remove custom destructuring for 3.0 release
-    bottom, // @TODO: remove custom destructuring for 4.0 release
-    height, // @TODO: remove custom destructuring for 4.0 release
-    left, // @TODO: remove custom destructuring for 4.0 release
-    padding, // @TODO: remove custom destructuring for 4.0 release
-    position, // @TODO: remove custom destructuring for 4.0 release
-    right, // @TODO: remove custom destructuring for 4.0 release
-    top, // @TODO: remove custom destructuring for 4.0 release
-    width, // @TODO: remove custom destructuring for 4.0 release
-
+    inputStyles,
     ..._rest
   } = props;
 
@@ -46,29 +36,19 @@ const TextFieldPrimitive: Primitive<TextFieldProps, 'input'> = (props, ref) => {
   const descriptionId = useStableId();
   const ariaDescribedBy = descriptiveText ? descriptionId : undefined;
 
-  const { flexContainerStyleProps, baseStyleProps, rest } =
-    splitPrimitiveProps(_rest);
+  const { styleProps, rest } = splitPrimitiveProps(_rest);
 
   return (
     <Flex
-      alignSelf={alignSelf}
       className={classNames(
         ComponentClassNames.Field,
         classNameModifier(ComponentClassNames.Field, size),
         ComponentClassNames.TextField,
         className
       )}
-      bottom={bottom}
       data-size={size}
-      height={height}
-      left={left}
-      padding={padding}
-      position={position}
-      right={right}
       testId={testId}
-      top={top}
-      width={width}
-      {...flexContainerStyleProps}
+      {...styleProps}
     >
       <Label htmlFor={fieldId} visuallyHidden={labelHidden}>
         {label}
@@ -92,7 +72,7 @@ const TextFieldPrimitive: Primitive<TextFieldProps, 'input'> = (props, ref) => {
           ref={ref}
           size={size}
           variation={variation}
-          {...baseStyleProps}
+          {...inputStyles}
           {...rest}
         />
       </FieldGroup>
