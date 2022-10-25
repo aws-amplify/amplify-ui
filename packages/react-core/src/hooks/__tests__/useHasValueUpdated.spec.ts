ab520b7ed5d50aec2c67d92d6d0a795e6008df8f
Caleb Pollman <cpollman@amazon.com>
2022-10-24 20:01:06 -0700

    add useHasValueUpdated unit tests

--- packages/react-core/src/hooks/__tests__/useHasValueUpdated.spec.ts
+++ packages/react-core/src/hooks/__tests__/useHasValueUpdated.spec.ts
@@ -1,6 +1,51 @@
-it.todo('return false on initial render and ignoring first render');
-it.todo('return false when current and previous value are equal');
-it.todo(
-  'returns true when initial render and not ignoring first render (default)'
-);
-it.todo('returns true when value has just changed');
+import { renderHook } from '@testing-library/react-hooks';
+
+import useHasValueUpdated from '../useHasValueUpdated';
+
+describe('useHasValueUpdated', () => {
+  it('return true on initial render and ignoring first render', () => {
+    const value = 'value';
+    const { result } = renderHook(() => useHasValueUpdated(value));
+
+    expect(result.current).toBe(true);
+  });
+
+  it('return false on initial render when not ignoring first render', () => {
+    const value = 'value';
+    const { result } = renderHook(() => useHasValueUpdated(value, true));
+
+    expect(result.current).toBe(false);
+  });
+
+  it('return false when current and updated values are equal', () => {
+    const value = 'value';
+
+    const { result, rerender } = renderHook((nextValue = value) =>
+      useHasValueUpdated(nextValue, true)
+    );
+
+    expect(result.current).toBe(false);
+
+    const sameValue = 'value';
+
+    rerender(sameValue);
+
+    expect(result.current).toBe(false);
+  });
+
+  it('returns true when value has updated', () => {
+    const value = 'value';
+
+    const { result, rerender } = renderHook((nextValue = value) =>
+      useHasValueUpdated(nextValue, true)
+    );
+
+    expect(result.current).toBe(false);
+
+    const updatedValue = 'updatedValue';
+
+    rerender(updatedValue);
+
+    expect(result.current).toBe(true);
+  });
+});
