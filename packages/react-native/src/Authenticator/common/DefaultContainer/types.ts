198eedcc22c7419746850c5c165e25e607f2a501
Joe Buono <joebuono724@gmail.com>
2022-10-25 15:50:57 -0500

    chore(rna): Default Container (#2811)
    * basic template for container
    
    * unit tests
    
    * tiny change
    
    * add centering styles, remove check for children
    
    Co-authored-by: Joe Buono <joebuono@amazon.com>
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

---
+++ packages/react-native/src/Authenticator/common/DefaultContainer/types.ts
@@ -0,0 +1,7 @@
+import { ViewProps, ViewStyle } from 'react-native';
+
+export interface DefaultContainerProps extends ViewProps {}
+
+export interface DefaultContainerStyles {
+  container: ViewStyle;
+}
