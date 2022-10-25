f01f44a8af695a221e5f13ab9c4d29d1e4bad9a9
Ioana Brooks <tryioana@amazon.com>
2022-10-25 16:42:48 -0700

    feat(ui-react-native): Add Confirm Signup subcomponent

--- packages/react-native/src/Authenticator/Defaults/ConfirmSignUp/__tests__/ConfirmSignUp.spec.tsx
+++ packages/react-native/src/Authenticator/Defaults/ConfirmSignUp/__tests__/ConfirmSignUp.spec.tsx
@@ -1,22 +1,59 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 
+import { authenticatorTextUtil } from '@aws-amplify/ui';
 import { ConfirmSignUp } from '..';
 
-const props = {} as any;
+const props = {
+  codeDeliveryDetails: undefined,
+  fields: [],
+  Footer: ConfirmSignUp.Footer,
+  FormFields: ConfirmSignUp.FormFields,
+  handleBlur: jest.fn(),
+  handleChange: jest.fn(),
+  handleSubmit: jest.fn(),
+  Header: ConfirmSignUp.Header,
+  isPending: false,
+  resendCode: jest.fn(),
+  socialProviders: undefined,
+};
+
+const { getConfirmingText, getDeliveryMethodText, getResendCodeText } =
+  authenticatorTextUtil;
 
 describe('ConfirmSignUp', () => {
   it('renders as expected', () => {
-    const { toJSON, getByRole } = render(
-      <>
-        <ConfirmSignUp {...props} />
-        <ConfirmSignUp.Header />
-        <ConfirmSignUp.Footer />
-        <ConfirmSignUp.FormFields {...props} />
-      </>
+    const { toJSON } = render(<ConfirmSignUp {...props} />);
+    expect(toJSON()).toMatchSnapshot();
+
+    expect(getDeliveryMethodText(props.codeDeliveryDetails)).toBeDefined();
+  });
+
+  it('resends the code', () => {
+    const { getByText } = render(<ConfirmSignUp {...props} />);
+    const button = getByText(getResendCodeText());
+
+    fireEvent.press(button);
+    expect(props.resendCode).toHaveBeenCalledTimes(1);
+  });
+
+  it('renders as expected with an error', () => {
+    const error = 'An error!';
+    const { getByRole, getByText, toJSON } = render(
+      <ConfirmSignUp {...props} error={error} />
     );
     expect(toJSON()).toMatchSnapshot();
 
-    expect(getByRole('header')).toBeDefined();
+    expect(getByRole('alert')).toBeDefined();
+    expect(getByText(error)).toBeDefined();
+  });
+
+  it('renders as expected when isPending is true', () => {
+    const { toJSON, getByText } = render(
+      <ConfirmSignUp {...props} isPending />
+    );
+    expect(toJSON()).toMatchSnapshot();
+
+    expect(getByText(getConfirmingText())).toBeDefined();
   });
 });
