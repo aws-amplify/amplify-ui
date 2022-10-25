f18a759058981558105baf6167a4ae10e7c0eac8
Joe Buono <joebuono@amazon.com>
2022-10-25 12:58:15 -0500

    unit tests

---
+++ packages/react-native/src/Authenticator/common/DefaultContainer/__tests__/DefaultContainer.spec.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import { DefaultContainer } from '..';
+import { Button } from '../../../../primitives/Button';
+
+describe('DefaultContainer', () => {
+  it('renders as expected with children', () => {
+    const testID = 'testButton';
+    const { toJSON, getByTestId } = render(
+      <DefaultContainer>
+        <Button testID={testID} />
+      </DefaultContainer>
+    );
+    expect(toJSON()).toMatchSnapshot();
+
+    expect(getByTestId(testID)).toBeDefined();
+  });
+
+  it('renders as expected without children', () => {
+    const { toJSON } = render(<DefaultContainer />);
+    expect(toJSON()).toMatchSnapshot();
+
+    expect(toJSON()).toBe(null);
+  });
+});
