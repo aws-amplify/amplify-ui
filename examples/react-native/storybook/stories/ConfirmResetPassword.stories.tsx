b154f747ac37645e77c52416975783e6082de4ac
Joe Buono <joebuono@amazon.com>
2022-10-24 19:34:00 -0500

    update stories

--- examples/react-native/storybook/stories/ConfirmResetPassword.stories.tsx
+++ examples/react-native/storybook/stories/ConfirmResetPassword.stories.tsx
@@ -3,7 +3,20 @@ import { StyleSheet, View } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import { ConfirmResetPassword } from '@aws-amplify/ui-react-native/dist/Authenticator/Defaults';
 
-const props = {} as any;
+const props = {
+  error: undefined,
+  Footer: ConfirmResetPassword.Footer,
+  FormFields: [] as any,
+  Header: ConfirmResetPassword.Header,
+  handleBlur: () => {},
+  handleChange: () => {},
+  handleSubmit: () => {},
+  isPending: false,
+  onBlur: undefined,
+  onChangeText: undefined,
+  onSubmit: undefined,
+  resendCode: () => {},
+};
 
 storiesOf('ConfirmResetPassword', module)
   .add('default', () => (
@@ -11,9 +24,16 @@ storiesOf('ConfirmResetPassword', module)
       <ConfirmResetPassword {...props} />
     </View>
   ))
-  .add('header', () => <ConfirmResetPassword.Header />)
-  .add('footer', () => <ConfirmResetPassword.Footer />)
-  .add('formFields', () => <ConfirmResetPassword.FormFields {...props} />);
+  .add('error', () => (
+    <View style={styles.container}>
+      <ConfirmResetPassword {...props} error="There was an error." />
+    </View>
+  ))
+  .add('isPending', () => (
+    <View style={styles.container}>
+      <ConfirmResetPassword {...props} isPending />
+    </View>
+  ));
 
 const styles = StyleSheet.create({
   container: {
