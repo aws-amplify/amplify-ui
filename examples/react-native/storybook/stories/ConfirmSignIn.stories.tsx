7b9ba22508dbc904467ad7210141a43f1936ada5
Joe Buono <joebuono@amazon.com>
2022-10-26 14:28:49 -0500

    basic template

--- examples/react-native/storybook/stories/ConfirmSignIn.stories.tsx
+++ examples/react-native/storybook/stories/ConfirmSignIn.stories.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react-native';
-import { ConfirmSignIn } from '@aws-amplify/ui-react-native/dist/Authenticator/Defaults';
+import { AuthChallengeName } from '@aws-amplify/ui';
+import { Authenticator } from '@aws-amplify/ui-react-native';
+import noop from 'lodash/noop';
 
-const props = {} as any;
+const code = {
+  name: 'code',
+  label: 'Code',
+  placeholder: 'Code',
+  type: 'default' as const,
+};
 
-storiesOf('ConfirmSignIn', module)
-  .add('default', () => <ConfirmSignIn {...props} />)
-  .add('header', () => <ConfirmSignIn.Header />)
-  .add('footer', () => <ConfirmSignIn.Footer />)
-  .add('formFields', () => <ConfirmSignIn.FormFields {...props} />);
+const fields = [code];
+
+const baseProps = {
+  challengeName: 'SMS_MFA' as AuthChallengeName,
+  error: null as unknown as string,
+  fields,
+  Footer: Authenticator.ConfirmSignIn.Footer,
+  FormFields: Authenticator.ConfirmSignIn.FormFields,
+  Header: Authenticator.ConfirmSignIn.Header,
+  handleBlur: noop,
+  handleSubmit: (values: any) => {
+    console.log('Values', values);
+  },
+  handleChange: noop,
+  isPending: false,
+  toSignIn: noop,
+};
+
+storiesOf('Authenticator.ConfirmSignIn', module)
+  .add('default', () => <Authenticator.ConfirmSignIn {...baseProps} />)
+  .add('with error', () => (
+    <Authenticator.ConfirmSignIn {...baseProps} error="Error!" />
+  ))
+  .add('isPending', () => (
+    <Authenticator.ConfirmSignIn {...baseProps} isPending />
+  ));
