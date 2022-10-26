edf283daf9996570ac6386c954206fcba3179122
Erik Hanchett <ehhanche@amazon.com>
2022-10-26 12:13:48 -0700

    Removed data- attributes

--- packages/react/src/components/Storage/FileUploader/Previewer/index.tsx
+++ packages/react/src/components/Storage/FileUploader/Previewer/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Card } from '../../../../primitives';
 
 export function Previewer(): JSX.Element {
-  return <Card data-amplify-file-previewer>Previewer</Card>;
+  return <Card>Previewer</Card>;
 }
