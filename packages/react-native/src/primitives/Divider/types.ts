023b8de9250b101765d90ff35e85d2218131ab01
Ioana Brooks <tryioana@amazon.com>
2022-10-26 11:25:17 -0700

    address feddback

--- packages/react-native/src/primitives/Divider/types.ts
+++ packages/react-native/src/primitives/Divider/types.ts
@@ -2,13 +2,15 @@ import { StyleProp, TextStyle, ViewProps, ViewStyle } from 'react-native';
 
 export interface DividerProps extends ViewProps {
   /*
-     Styles for the label
-  */
+   * @description
+   * Styles for the label
+   */
   labelStyle?: StyleProp<TextStyle>;
 
   /*
-     Styles for the line
-  */
+   * @description
+   * Styles for the line
+   */
   lineStyle?: StyleProp<ViewStyle>;
 }
 
