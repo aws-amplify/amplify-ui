cda9f507937790aa2a226eefce0d581c21dba83e
Ioana Brooks <tryioana@amazon.com>
2022-10-30 15:04:08 -0700

    Remove usePressableStyles hook

--- packages/react-native/src/hooks/index.ts
+++ packages/react-native/src/hooks/index.ts
@@ -2,5 +2,4 @@ export {
   DeviceOrientation,
   useDeviceOrientation,
 } from './useDeviceOrientation';
-export { PressableStyleProps, usePressableStyles } from './usePressableStyles';
 export { useTheme } from './useTheme';
