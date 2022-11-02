4dacf1134f81553d237d0e03da2df8df6cf866c7
Erik Hanchett <ehhanche@amazon.com>
2022-11-02 10:19:32 -0700

    Added comment on force

--- packages/e2e/cypress/integration/common/fileuploader.ts
+++ packages/e2e/cypress/integration/common/fileuploader.ts
@@ -7,6 +7,9 @@ When('I select a file with file name {string}', (fileName: string) => {
       fileName,
       lastModified: Date.now(),
     },
+    /**
+     *  Since the input is hidden, this will need to be forced through Cypress
+     */
     { force: true }
   );
 });
