906160f86140de7350ee00b1135b0f572f8ced24
wlee221 <willeea@amazon.com>
2022-11-03 04:13:27 -0700

    Remove unrelated types

--- packages/react/src/components/AccountSettings/ChangePassword/types.ts
+++ packages/react/src/components/AccountSettings/ChangePassword/types.ts
@@ -1,20 +1,4 @@
-import {
-  AccountSettingsPasswordField,
-  AccountSettingsSubmitButton,
-} from '../types';
-
-export interface ChangePasswordOverrides {
-  CurrentPassword?: AccountSettingsPasswordField;
-  NewPassword?: AccountSettingsPasswordField;
-  ConfirmPassword?: AccountSettingsPasswordField;
-  Button?: AccountSettingsSubmitButton;
-}
-
-export type PasswordValidator = (password: string) => string;
-
 export interface ChangePasswordProps {
-  // custom validator function if customer want to override default validations
-  validate?: PasswordValidator;
   // callback once password is successfully updated
   onSuccess?: () => void;
   // callback when there's an error
