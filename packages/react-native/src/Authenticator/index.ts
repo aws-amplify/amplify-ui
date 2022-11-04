7211fabc66a2e78e7a508bb7289ee93c97d9eae4
Joe Buono <joebuono@amazon.com>
2022-11-04 15:08:43 -0500

    remove empty whitespace

--- packages/react-native/src/Authenticator/index.ts
+++ packages/react-native/src/Authenticator/index.ts
@@ -1,6 +1,5 @@
 export { default as Authenticator } from './Authenticator';
 export { AuthenticatorProps, WithAuthenticatorOptions } from './types';
-
 export { default as withAuthenticator } from './withAuthenticator';
 
 // re-export shared `Authenticator` exports
