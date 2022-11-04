891014f54b2deafad148809b2a0f54a2c92c4bd1
William Lee <43682783+wlee221@users.noreply.github.com>
2022-11-03 17:09:07 -0700

    Add 'Notifications' to LoggerCategory
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

--- packages/ui/src/helpers/utils.ts
+++ packages/ui/src/helpers/utils.ts
@@ -1,6 +1,6 @@
 import { Logger } from 'aws-amplify';
 
-type LoggerCategory = 'Auth' | 'Storage' | 'Geo';
+type LoggerCategory = 'Auth' | 'Geo' | 'Notifications' | 'Storage';
 
 export const getCategoryLogger = (category: LoggerCategory) =>
   new Logger(`AmplifyUI:${category}`);
