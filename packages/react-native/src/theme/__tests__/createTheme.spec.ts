22049b23ad441c37aa67ab102221f22d51823e81
Ioana Brooks <tryioana@amazon.com>
2022-11-04 16:31:21 -0700

    Add more tests

--- packages/react-native/src/theme/__tests__/createTheme.spec.ts
+++ packages/react-native/src/theme/__tests__/createTheme.spec.ts
@@ -2,10 +2,10 @@ import baseTokens from '@aws-amplify/ui/dist/react-native/tokens';
 import { createTheme } from '../createTheme';
 
 describe('createTheme', () => {
-  describe('without a base theme', () => {
+  describe('without a custom theme', () => {
     const { tokens } = createTheme({ name: 'test-theme' });
 
-    it('should have tokens', () => {
+    it('should have component agnostic tokens', () => {
       expect(tokens).toBeDefined();
 
       expect(tokens.colors).toStrictEqual(baseTokens.colors);
@@ -15,6 +15,36 @@ describe('createTheme', () => {
       expect(tokens.radii).toStrictEqual(baseTokens.radii);
       expect(tokens.space).toStrictEqual(baseTokens.space);
       expect(tokens.time).toStrictEqual(baseTokens.time);
+    });
+
+    it('should not have component tokens', () => {
+      expect(tokens.components).toBeUndefined();
+    });
+  });
+
+  describe('with a custom theme', () => {
+    const theme = createTheme({
+      name: 'test-theme',
+      tokens: {
+        colors: {
+          font: {
+            primary: '{colors.white}',
+            secondary: 'purple',
+          },
+        },
+        components: {
+          label: {
+            text: { color: 'blue' },
+          },
+        },
+      },
+    });
+
+    it('should override the base theme', () => {
+      const { tokens } = theme;
+      expect(tokens.colors.font.primary).toEqual('hsl(0, 0%, 100%)');
+      expect(tokens.colors.font.secondary).toEqual('purple');
+      expect(tokens.components?.label.text.color).toEqual('blue');
     });
   });
   //TODO add more tests once component tokens are added
