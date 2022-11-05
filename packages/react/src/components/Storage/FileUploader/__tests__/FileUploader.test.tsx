e5bb9cd322873cb288a91621f37e7daa1913bf42
Erik Hanchett <ehhanche@amazon.com>
2022-11-04 17:12:58 -0700

    Added more tests

--- packages/react/src/components/Storage/FileUploader/__tests__/FileUploader.test.tsx
+++ packages/react/src/components/Storage/FileUploader/__tests__/FileUploader.test.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 
+import * as UseHooks from '../hooks/useFileUploader';
 import { FileUploader } from '..';
+import * as UIModule from '@aws-amplify/ui';
+import { update, values } from 'lodash';
+const uploadFileSpy = jest.spyOn(UIModule, 'uploadFile');
+const useFileUploaderSpy = jest.spyOn(UseHooks, 'useFileUploader');
+const fakeFile = new File(['hello'], 'hello.png', { type: 'image/png' });
 
 describe('File Uploader', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   it('exists', async () => {
     const comp = render(
       <FileUploader
@@ -13,7 +22,7 @@ describe('File Uploader', () => {
       />
     );
 
-    expect(comp.container).toBeTruthy();
+    expect(comp.container).toMatchSnapshot();
   });
   it('shows a button when variation is set to button', async () => {
     render(
@@ -38,5 +47,122 @@ describe('File Uploader', () => {
     const svg = container.querySelector('svg');
 
     expect(svg).toBeInTheDocument();
+  });
+
+  it('Previewer is not shown on empty file target', async () => {
+    const { container, queryByText } = render(
+      <FileUploader
+        level="public"
+        acceptedFileTypes={['.png']}
+        variation="button"
+      />
+    );
+
+    const input = container.getElementsByTagName('input')[0];
+    await waitFor(() => {
+      fireEvent.change(input, {
+        target: { files: [] },
+      });
+    });
+    const text = await queryByText(/files selected/);
+
+    expect(text).not.toBeInTheDocument();
+  });
+
+  it('it calls uploadFile with expected arguments', async () => {
+    uploadFileSpy.mockResolvedValue({} as never);
+    const { container } = render(
+      <FileUploader
+        level="public"
+        acceptedFileTypes={['.png']}
+        variation="drop"
+      />
+    );
+
+    const input = container.getElementsByTagName('input')[0];
+    await waitFor(() => {
+      fireEvent.change(input, {
+        target: { files: [fakeFile] },
+      });
+    });
+
+    const clickButton = await screen.findByRole('button', {
+      name: 'Upload 1 files',
+    });
+
+    fireEvent.click(clickButton);
+
+    expect(uploadFileSpy).toBeCalledWith({
+      completeCallback: expect.any(Function),
+      errorCallback: expect.any(Function),
+      file: fakeFile,
+      fileName: fakeFile.name,
+      level: 'public',
+      progressCallback: expect.any(Function),
+    });
+  });
+  it('it skips files that are already uploaded', async () => {
+    uploadFileSpy.mockResolvedValue({} as never);
+    const fileName2 = 'hello2.png';
+    const fileStatuses = [
+      {
+        percentage: 100,
+        uploadTask: undefined,
+        fileErrors: undefined,
+        name: 'hello.png',
+        file: fakeFile,
+        fileState: 'success' as any,
+      },
+      {
+        percentage: 0,
+        uploadTask: undefined,
+        fileErrors: undefined,
+        name: fileName2,
+        file: fakeFile,
+        fileState: null,
+      },
+    ];
+
+    useFileUploaderSpy.mockReturnValue({
+      fileStatuses,
+      onDragStart: () => null,
+      onDragEnter: () => null,
+      onDragLeave: () => null,
+      onDrop: () => null,
+      onDragOver: () => null,
+      setFileStatuses: () => null,
+      addTargetFiles: () => 1,
+      showPreviewer: true,
+      setShowPreviewer: () => null,
+    });
+    const { container } = render(
+      <FileUploader
+        level="public"
+        acceptedFileTypes={['.png']}
+        variation="drop"
+      />
+    );
+
+    const input = container.getElementsByTagName('input')[0];
+    await waitFor(() => {
+      fireEvent.change(input, {
+        target: { files: [fakeFile] },
+      });
+    });
+
+    const clickButton = await screen.findByRole('button', {
+      name: 'Upload 1 files',
+    });
+
+    fireEvent.click(clickButton);
+
+    expect(uploadFileSpy).toBeCalledWith({
+      completeCallback: expect.any(Function),
+      errorCallback: expect.any(Function),
+      file: fakeFile,
+      fileName: fileName2,
+      level: 'public',
+      progressCallback: expect.any(Function),
+    });
   });
 });
