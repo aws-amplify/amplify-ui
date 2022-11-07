53c94eb375a0261be47523eb63a31bd80627c10a
Joe Buono <joebuono@amazon.com>
2022-11-07 15:26:55 -0600

    update styles to include components tokens

--- packages/react-native/src/primitives/Radio/styles.ts
+++ packages/react-native/src/primitives/Radio/styles.ts
@@ -6,14 +6,16 @@ import { RadioStyles } from './types';
 const ROUNDED_BORDER_RADIUS = 999;
 
 export const getThemedStyles = (theme: StrictTheme): RadioStyles => {
-  const { colors, opacities, space } = theme.tokens;
+  const { colors, components, opacities, space } = theme.tokens;
 
   return StyleSheet.create({
     container: {
       alignItems: 'center',
+      ...components?.radio.container,
     },
     disabled: {
       opacity: opacities[60],
+      ...components?.radio.disabled,
     },
     radioContainer: {
       alignItems: 'center',
@@ -21,34 +23,42 @@ export const getThemedStyles = (theme: StrictTheme): RadioStyles => {
       borderRadius: ROUNDED_BORDER_RADIUS,
       borderWidth: 2,
       justifyContent: 'center',
+      ...components?.radio.radioContainer,
     },
     radioDot: {
       backgroundColor: colors.teal[80],
       borderRadius: ROUNDED_BORDER_RADIUS,
+      ...components?.radio.radioDot,
     },
     radioContainerLarge: {
       height: space.xxxl,
       width: space.xxxl,
+      ...components?.radio.radioContainerLarge,
     },
     radioContainerMedium: {
       height: space.xxl,
       width: space.xxl,
+      ...components?.radio.radioContainerMedium,
     },
     radioContainerSmall: {
       height: space.xl,
       width: space.xl,
+      ...components?.radio.radioContainerSmall,
     },
     radioDotLarge: {
       height: space.xl,
       width: space.xl,
+      ...components?.radio.radioDotLarge,
     },
     radioDotMedium: {
       height: space.large,
       width: space.large,
+      ...components?.radio.radioDotMedium,
     },
     radioDotSmall: {
       height: space.medium,
       width: space.medium,
+      ...components?.radio.radioDotSmall,
     },
   });
 };
