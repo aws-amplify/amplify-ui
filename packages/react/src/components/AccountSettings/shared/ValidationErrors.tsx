d4952ab7b20b916467a633e21b3667810c96de76
wlee221 <willeea@amazon.com>
2022-11-07 03:08:16 -0800

    Remove unnecessary classname

--- packages/react/src/components/AccountSettings/shared/ValidationErrors.tsx
+++ packages/react/src/components/AccountSettings/shared/ValidationErrors.tsx
@@ -7,16 +7,14 @@ import { Text } from '../../../primitives/Text';
 // TODO: consolidate with Authenticator one
 export interface ValidationErrorsProps {
   errors: string[];
-  id?: string;
 }
 export const ValidationErrors = ({
   errors,
-  id,
 }: ValidationErrorsProps): JSX.Element => {
   if (!(errors?.length > 0)) return null;
 
   return (
-    <View data-amplify-validation-errors id={id}>
+    <View>
       {errors.map((error) => {
         return (
           <Text key={error} role="alert" variation="error">
