1abc2d6ceee6dca3195687c8077aa3e831041f58
Ioana Brooks <tryioana@amazon.com>
2022-11-10 13:12:49 -0800

    Add icon size util

--- packages/react-native/src/primitives/Icon/styles.ts
+++ packages/react-native/src/primitives/Icon/styles.ts
@@ -6,30 +6,34 @@ import { IconProps, iconSizes, IconStyles } from './types';
 
 const logger = new Logger('Icon-logger');
 
+const getIconSize = (size: IconProps['size']): number => {
+  if (!size) {
+    return iconSizes.medium;
+  }
+
+  if (typeof size === 'number') {
+    return size;
+  }
+
+  if (!iconSizes[size]) {
+    logger.warn(
+      `"${size}" is not a valid icon size. Available values are: ${Object.keys(
+        iconSizes
+      )}`
+    );
+    return iconSizes.medium;
+  } else {
+    return iconSizes[size];
+  }
+};
+
 export const getThemedStyles = (
   theme: StrictTheme,
   color?: IconProps['color'],
   size?: IconProps['size']
 ): IconStyles => {
   const { components } = theme.tokens;
-
-  let iconSize: number;
-  if (!size) {
-    iconSize = iconSizes.medium;
-  } else if (typeof size === 'number') {
-    iconSize = size;
-  } else {
-    if (!iconSizes[size]) {
-      logger.warn(
-        `"${size}" is not a valid icon size. Available values are: ${Object.keys(
-          iconSizes
-        )}`
-      );
-      iconSize = iconSizes.medium;
-    } else {
-      iconSize = iconSizes[size];
-    }
-  }
+  const iconSize = getIconSize(size);
 
   return StyleSheet.create({
     icon: {
