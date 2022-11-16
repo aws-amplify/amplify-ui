a10cf981cc30a8a317e27ea58a92a4dea7b8b884
0618 <o.6180339@gmail.com>
2022-11-16 14:44:36 -0800

    refactor(docs): use react-markdown

--- docs/src/components/propsTable/PropsTableBody.tsx
+++ docs/src/components/propsTable/PropsTableBody.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react';
-import { remark } from 'remark';
-import html from 'remark-html';
+import ReactMarkdown from 'react-markdown';
 import { Badge, TableRow } from '@aws-amplify/ui-react';
 import { ResponsiveTableCell } from '@/components/ResponsiveTable';
 import { CodeHighlight } from '@/components/CodeHighlight';
@@ -10,16 +8,6 @@ export const PropsTableBody = ([
   propName,
   { name, type, description, isOptional },
 ]: [string, Property]): JSX.Element => {
-  const [htmlDescription, setHtmlDescription] = useState('-');
-
-  useEffect(() => {
-    const getDesc = async () => {
-      const descriptionHTML = await remark().use(html).process(description);
-      setHtmlDescription(descriptionHTML.toString());
-    };
-    getDesc();
-  }, [description]);
-
   return (
     <TableRow key={name}>
       <ResponsiveTableCell label="Name">
@@ -34,12 +22,7 @@ export const PropsTableBody = ([
         <CodeHighlight code={type} language="typescript" />
       </ResponsiveTableCell>
       <ResponsiveTableCell label="Description">
-        {/**
-         * [`dangerouslySetInnerHTML`](https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml) here only use the pre-generated props-table data,
-         * so there's no cross-site scripting concern.
-         * more details: https://nextjs.org/learn/basics/dynamic-routes/render-markdown
-         */}
-        <div dangerouslySetInnerHTML={{ __html: htmlDescription }} />
+        <ReactMarkdown>{description}</ReactMarkdown>
       </ResponsiveTableCell>
     </TableRow>
   );
