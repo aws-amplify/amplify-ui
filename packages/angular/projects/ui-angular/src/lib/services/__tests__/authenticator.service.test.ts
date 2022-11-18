fd0139a3f4dbfb7be7eb57c712a918f2825a2bde
wlee221 <willeea@amazon.com>
2022-11-18 03:08:12 -0800

    Inline mock service

--- packages/angular/projects/ui-angular/src/lib/services/__tests__/authenticator.service.test.ts
+++ packages/angular/projects/ui-angular/src/lib/services/__tests__/authenticator.service.test.ts
@@ -2,7 +2,28 @@ import { AuthenticatorService } from '../authenticator.service';
 
 import * as UIModule from '@aws-amplify/ui';
 import * as XState from 'xstate';
-import { MockAuthService } from './mockAuthService';
+
+// mock state machine service
+// based on https://github.com/statelyai/xstate/blob/main/packages/core/src/interpreter.ts
+export class MockAuthService {
+  private listeners: (() => void)[] = [];
+
+  subscribe(callback: () => void) {
+    this.listeners.push(callback);
+    const unsubscribe = () => {};
+    return { unsubscribe };
+  }
+
+  start() {
+    return this;
+  }
+
+  send() {
+    for (const listener of this.listeners) {
+      listener();
+    }
+  }
+}
 
 const mockFacade = {} as unknown as ReturnType<
   typeof UIModule['getServiceFacade']
