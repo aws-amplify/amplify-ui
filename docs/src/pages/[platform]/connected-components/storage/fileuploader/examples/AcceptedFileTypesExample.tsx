de29003221b6f0269f28eeefdf79a5e01e993643
Erik Hanchett <ehhanche@amazon.com>
2022-11-18 15:55:53 -0800

    Added props

--- docs/src/pages/[platform]/connected-components/storage/fileuploader/examples/AcceptedFileTypesExample.tsx
+++ docs/src/pages/[platform]/connected-components/storage/fileuploader/examples/AcceptedFileTypesExample.tsx
@@ -3,7 +3,7 @@ import { FileUploader } from '@aws-amplify/ui-react';
 export const AcceptedFileTypesExample = () => {
   return (
     <FileUploader
-      acceptedFileTypes={['.gif', '.bmp', '.doc', '.jpeg', 'jpg']}
+      acceptedFileTypes={['.gif', '.bmp', '.doc', '.jpeg', '.jpg']}
       variation="drop"
       level="public"
       provider="fast" // IGNORE
--- docs/src/pages/[platform]/connected-components/storage/fileuploader/examples/DefaultFileUploaderExample.tsx
+++ docs/src/pages/[platform]/connected-components/storage/fileuploader/examples/DefaultFileUploaderExample.tsx
@@ -1,12 +1,10 @@
 import { FileUploader } from '@aws-amplify/ui-react'; // IGNORE
 export const DefaultFileUploaderExample = () => {
   return (
-    <>
-      <FileUploader
-        acceptedFileTypes={['.png', '.jpg', '.pdf']}
-        level="public"
-        provider="fast" // IGNORE
-      />
-    </>
+    <FileUploader
+      acceptedFileTypes={['.png', '.jpg', '.pdf']}
+      level="public"
+      provider="fast" // IGNORE
+    />
   );
 };
