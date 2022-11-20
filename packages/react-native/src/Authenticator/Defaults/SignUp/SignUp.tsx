e7fe6a91e3844a977a720fc2381d22bc5fc466ca
Caleb Pollman <cpollman@amazon.com>
2022-11-19 16:26:15 -0800

    Update packages/react-native/src/Authenticator/Defaults/SignUp/SignUp.tsx
    Co-authored-by: Ioana Brooks <68251134+ioanabrooks@users.noreply.github.com>

--- packages/react-native/src/Authenticator/Defaults/SignUp/SignUp.tsx
+++ packages/react-native/src/Authenticator/Defaults/SignUp/SignUp.tsx
@@ -37,7 +37,7 @@ const SignUp: DefaultSignUpComponent = ({
   const primaryButtonText = isPending
     ? getCreatingAccountText()
     : getCreateAccountText();
-  const secondaryButtontext = getSignInTabText();
+  const secondaryButtonText = getSignInTabText();
 
   const buttons = useMemo(
     () => ({
