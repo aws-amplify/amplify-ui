de49a5bfedbca1cb938367af17ee4498af3343c2
Danny Banks <djb@amazon.com>
2022-11-21 12:16:23 -0800

    feat(rna): adding default dark mode export

--- examples/react-native/src/features/Authenticator/Styles/Example.tsx
+++ examples/react-native/src/features/Authenticator/Styles/Example.tsx
@@ -1,45 +1,47 @@
 import React, { ReactNode } from 'react';
-import { StyleProp, StyleSheet, Text, View, ViewStyle } from 'react-native';
+import {
+  StyleProp,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  ViewStyle,
+} from 'react-native';
 
-import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
+import {
+  Authenticator,
+  useAuthenticator,
+  defaultDarkModeOverride,
+  ThemeProvider,
+  Theme,
+} from '@aws-amplify/ui-react-native';
 import { Amplify } from 'aws-amplify';
 
 import { Button } from '../../../ui';
 
 Amplify.configure({});
 
-const MyHeader = ({
-  children,
-  style,
-}: {
-  children?: ReactNode;
-  style?: StyleProp<ViewStyle>;
-}) => (
-  <View style={style}>
-    <Text style={{ fontSize: 36 }}>{children}</Text>
-  </View>
-);
-
 function SignOutButton() {
   const { signOut } = useAuthenticator();
   return <Button onPress={signOut}>Sign Out</Button>;
 }
 
+const theme: Theme = {
+  overrides: [defaultDarkModeOverride],
+};
+
 function App() {
+  const colorMode = useColorScheme();
   return (
-    <Authenticator.Provider>
-      <Authenticator
-        components={{
-          SignIn: (props) => (
-            <Authenticator.SignIn {...props} Header={MyHeader} />
-          ),
-        }}
-      >
-        <View style={style.container}>
-          <SignOutButton />
-        </View>
-      </Authenticator>
-    </Authenticator.Provider>
+    <ThemeProvider theme={theme} colorMode={colorMode}>
+      <Authenticator.Provider>
+        <Authenticator>
+          <View style={style.container}>
+            <SignOutButton />
+          </View>
+        </Authenticator>
+      </Authenticator.Provider>
+    </ThemeProvider>
   );
 }
 
--- packages/react-native/src/index.ts
+++ packages/react-native/src/index.ts
@@ -7,4 +7,9 @@ export {
   WithAuthenticatorOptions,
 } from './Authenticator';
 export * from './InAppMessaging';
-export { Theme, ThemeProvider } from './theme';
+export {
+  Theme,
+  ThemeProvider,
+  defaultDarkModeOverride,
+  defaultTheme,
+} from './theme';
