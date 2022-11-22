839ceb3a682bc425719204fae32c1e6d1810f69c
Caleb Pollman <cpollman@amazon.com>
2022-11-21 16:28:37 -0800

    chore(rna): add labelHidden support and example apps (#3043)

--- packages/react-native/src/Authenticator/hooks/types.ts
+++ packages/react-native/src/Authenticator/hooks/types.ts
@@ -17,19 +17,20 @@ export type TextFieldOnBlur = TextFieldProps['onBlur'];
 export type OnChangeText = TextFieldProps['onChangeText'];
 
 type FieldOptions<FieldProps, Type extends AuthenticatorFieldTypeKey> = {
-  type: Type;
   name: string;
-  required?: boolean;
   onBlur?: Type extends 'radio' ? RadioFieldOnBlur : TextFieldOnBlur;
+  required?: boolean;
+  type: Type;
 } & Omit<FieldProps, 'disabled' | 'onBlur'>;
 
 type PasswordFieldOptions = FieldOptions<PasswordFieldProps, 'password'>;
 type PhoneFieldOptions = FieldOptions<PhoneNumberFieldProps, 'phone'>;
 type DefaultFieldOptions = FieldOptions<TextFieldProps, 'default'>;
-export type TextFieldOptionsType =
+export type TextFieldOptionsType = (
   | PasswordFieldOptions
   | PhoneFieldOptions
-  | DefaultFieldOptions;
+  | DefaultFieldOptions
+) & { labelHidden?: boolean };
 
 export type RadioFieldOptions = FieldOptions<RadioProps<string>, 'radio'>;
 
