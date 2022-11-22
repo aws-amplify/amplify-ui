74f62d8deef68ba16b88026869e6a9c98cdec31f
Erik Hanchett <ehhanche@amazon.com>
2022-11-22 13:41:24 -0800

    fixed tests

--- packages/react/src/components/Storage/FileUploader/Tracker/__tests__/FileMessage.test.tsx
+++ packages/react/src/components/Storage/FileUploader/Tracker/__tests__/FileMessage.test.tsx
@@ -1,34 +1,34 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import { FileState } from '../FileState';
+import { FileMessage } from '../FileMessage';
 const fakeFile = new File(['hello'], 'hello.png', { type: 'image/png' });
 
 describe('FileState', () => {
   it('exists', async () => {
     const { container } = render(
-      <FileState fileState="error" errorMessage={''} />
+      <FileMessage fileState="error" errorMessage={''} />
     );
 
     expect(container).toMatchSnapshot();
   });
   it('displays loading message if fileState is loading', async () => {
     const { findByText } = render(
-      <FileState fileState={'loading'} percentage={10} errorMessage={''} />
+      <FileMessage fileState={'loading'} percentage={10} errorMessage={''} />
     );
 
     expect(await findByText('Uploading: 10%')).toBeVisible();
   });
   it('displays paused message if fileState is paused', async () => {
     const { findByText } = render(
-      <FileState fileState={'paused'} percentage={10} errorMessage={''} />
+      <FileMessage fileState={'paused'} percentage={10} errorMessage={''} />
     );
 
     expect(await findByText('Paused: 10%')).toBeVisible();
   });
   it('displays upload success message if fileState is in success', async () => {
     const { findByText } = render(
-      <FileState fileState={'success'} errorMessage={''} />
+      <FileMessage fileState={'success'} errorMessage={''} />
     );
 
     expect(await findByText('Uploaded successfully')).toBeVisible();
@@ -36,7 +36,7 @@ describe('FileState', () => {
   it('displays error message if fileState is in error', async () => {
     const error = 'error message';
     const { findByText } = render(
-      <FileState fileState={'error'} errorMessage={error} />
+      <FileMessage fileState={'error'} errorMessage={error} />
     );
 
     expect(await findByText(error)).toBeVisible();
