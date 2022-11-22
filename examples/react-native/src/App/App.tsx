02f4c65511ce2dd1878a149b6347a318b88b5405
wlee221 <willeea@amazon.com>
2022-11-21 16:49:41 -0800

    Initial commit

--- examples/react-native/src/App/App.tsx
+++ examples/react-native/src/App/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-// import Example from '../features/Authenticator/Demo/Example';
-import Example from '../features/Authenticator/Styles/Example';
+import Example from '../features/Authenticator/InitialState/Example';
+// import Example from '../features/Authenticator/Styles/Example';
 // import { Demo as InAppDemo } from '../features/InAppMessaging';
 
 const App = () => {
