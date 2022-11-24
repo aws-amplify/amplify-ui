37cd3c907d6dfa0241383d6bdcf8aff146e15150
wlee221 <willeea@amazon.com>
2022-11-24 02:26:39 -0800

    Progress!

---
+++ docs/src/pages/[platform]/connected-components/account-settings/change-password/props.ts
@@ -0,0 +1,22 @@
+export const OVERRIDES = [
+  {
+    name: `CurrentPassword?`,
+    description: 'Password field for current password',
+    type: `PasswordFieldProps`,
+  },
+  {
+    name: `NewPassword?`,
+    description: 'Password field for new password',
+    type: `PasswordFieldProps`,
+  },
+  {
+    name: `ConfirmPassword?`,
+    description: 'Password field for confirm password',
+    type: `PasswordFieldProps`,
+  },
+  {
+    name: `SubmitButton?`,
+    description: 'Submit button',
+    type: `SubmitButtonProps`,
+  },
+];
