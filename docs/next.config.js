7286720d605d684285568a83d585770b3da2e5b1
Danny Banks <djb@amazon.com>
2022-11-29 11:16:51 -0800

    chore(docs): add feature flag for fileuploader component props

--- docs/next.config.js
+++ docs/next.config.js
@@ -15,6 +15,8 @@ module.exports = withNextPluginPreval({
     DOCSEARCH_DOCS_APP_ID: process.env.DOCSEARCH_DOCS_APP_ID,
     DOCSEARCH_DOCS_API_KEY: process.env.DOCSEARCH_DOCS_API_KEY,
     DOCSEARCH_DOCS_INDEX_NAME: process.env.DOCSEARCH_DOCS_INDEX_NAME,
+    FF_FILEUPLOADER_COMPONENTS_ENABLED:
+      process.env.FF_FILEUPLOADER_COMPONENTS_ENABLED,
   },
   // Differentiate pages with frontmatter & layout vs. normal MD(X)
   pageExtensions: ['page.mdx', 'page.tsx'],
