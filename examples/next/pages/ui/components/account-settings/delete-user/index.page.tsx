f2475e1ceceedc3b7b2313d6025592518c4a4955
wlee221 <willeea@amazon.com>
2022-12-01 17:33:48 -0800

    Use actual code from examples

--- examples/next/pages/ui/components/account-settings/delete-user/index.page.tsx
+++ examples/next/pages/ui/components/account-settings/delete-user/index.page.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Amplify } from 'aws-amplify';
 
 import {
@@ -7,11 +8,52 @@ import {
   Flex,
   Heading,
   AccountSettings,
+  Text,
+  TextField,
 } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
+
+function CustomWarning({ onCancel, onConfirm, isDisabled }) {
+  const [value, setValue] = React.useState('');
+
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    const { value } = event.target;
+    setValue(value);
+  };
+
+  return (
+    <form>
+      <Flex direction="column">
+        <Text variation="warning">
+          Deleting your account is not reversable. Please type "delete" below if
+          you want to confirm user deletion.
+        </Text>
+        <TextField
+          labelHidden
+          label="Confirm Account Deletion"
+          placeholder="delete"
+          onChange={handleChange}
+          value={value}
+        />
+        <Button onClick={onCancel}>Back</Button>
+        <Button
+          onClick={onConfirm}
+          variation="primary"
+          isDisabled={isDisabled || value !== 'delete'}
+        >
+          Submit
+        </Button>
+      </Flex>
+    </form>
+  );
+}
+
+const components = { Warning: CustomWarning };
 
 export default function App() {
   return (
@@ -21,7 +63,7 @@ export default function App() {
           <Flex direction="column">
             <Card variation="outlined">
               <Heading>Delete Account:</Heading>
-              <AccountSettings.DeleteUser />
+              <AccountSettings.DeleteUser components={components} />
             </Card>
             <Button onClick={signOut}>Sign Out</Button>
           </Flex>
