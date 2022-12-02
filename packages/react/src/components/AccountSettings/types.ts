4d0053b0589d3410b7f34c9b4ef07e86ccb050a6
wlee221 <willeea@amazon.com>
2022-12-02 08:41:01 -0800

    Mark essential types as required

--- packages/react/src/components/AccountSettings/types.ts
+++ packages/react/src/components/AccountSettings/types.ts
@@ -6,27 +6,40 @@ import {
   PrimitiveProps,
 } from '../../primitives/types';
 
-/**
+/*
  * These are primitive prop types that account settings component use.
  *
  * Note that `PrimitiveProps` is used to get native html types, like `onSubmit`.
  */
-type CommonPasswordFieldProps = Partial<
-  PrimitiveProps<PasswordFieldProps, 'input'>
->;
-type CommonButtonProps = Partial<PrimitiveProps<ButtonProps, 'button'>>;
+type PasswordFieldPrimitiveProps = PrimitiveProps<PasswordFieldProps, 'input'>;
+type ButtonPrimtiveProps = PrimitiveProps<ButtonProps, 'button'>;
+
+/*
+ * These are common prop types for component overrides.
+ *
+ * Any essential props for overriding components are marked as required.
+ */
+type CommonPasswordFieldProps = Partial<PasswordFieldPrimitiveProps> &
+  Required<Pick<PasswordFieldPrimitiveProps, 'onBlur' | 'onChange'>>;
+type CommonButtonProps = Partial<ButtonPrimtiveProps> &
+  Required<Pick<ButtonPrimtiveProps, 'onClick'>>;
+type CommonSubmitButtonProps = Partial<PrimitiveProps<ButtonProps, 'button'>>;
 type CommonAlertProps = Partial<PrimitiveProps<AlertProps, 'div'>>;
 
 /**
- * These are overridable component types (e.g. submit button).
+ * These are component override types.
  */
 export type PasswordFieldComponent<Props = {}> = React.ComponentType<
   // `Props` generic allows additional props passed on override components
   Props & CommonPasswordFieldProps & { validationErrors?: string[] }
 >;
 
-export type SubmitButtonComponent<Props = {}> = React.ComponentType<
+export type ButtonComponent<Props = {}> = React.ComponentType<
   Props & CommonButtonProps
+>;
+
+export type SubmitButtonComponent<Props = {}> = React.ComponentType<
+  Props & CommonSubmitButtonProps
 >;
 
 export type ErrorComponent<Props = {}> = React.ComponentType<
