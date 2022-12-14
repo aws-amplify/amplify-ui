f96c033aa15153c4209d859a6ebfa2947613a0ab
Chenwei Zhang <chenwz@amazon.com>
2022-12-13 18:26:58 -0800

    chore: update AccountSettings typings

--- packages/react/src/components/AccountSettings/types.ts
+++ packages/react/src/components/AccountSettings/types.ts
@@ -3,7 +3,7 @@ import {
   AlertProps,
   ButtonProps,
   PasswordFieldProps,
-  PrimitiveProps,
+  PrimitivePropsWithoutRef,
 } from '../../primitives/types';
 
 /*
@@ -11,9 +11,12 @@ import {
  *
  * Note that `PrimitiveProps` is used to get native html types, like `onSubmit`.
  */
-type PasswordFieldPrimitiveProps = PrimitiveProps<PasswordFieldProps, 'input'>;
-type ButtonPrimitiveProps = PrimitiveProps<ButtonProps, 'button'>;
-type AlertPrimitiveProps = PrimitiveProps<AlertProps, 'div'>;
+type PasswordFieldPrimitiveProps = PrimitivePropsWithoutRef<
+  PasswordFieldProps,
+  'input'
+>;
+type ButtonPrimitiveProps = PrimitivePropsWithoutRef<ButtonProps, 'button'>;
+type AlertPrimitiveProps = PrimitivePropsWithoutRef<AlertProps, 'div'>;
 
 /*
  * These are common prop types for component overrides.
@@ -25,7 +28,7 @@ type CommonPasswordFieldProps = Partial<PasswordFieldPrimitiveProps> &
     Pick<PasswordFieldPrimitiveProps, 'onBlur' | 'onChange' | 'name'>
   > & { fieldValidationErrors?: string[] };
 
-type CommonAlertProps = Partial<PrimitiveProps<AlertProps, 'div'>> &
+type CommonAlertProps = Partial<PrimitivePropsWithoutRef<AlertProps, 'div'>> &
   Required<Pick<AlertPrimitiveProps, 'children'>>;
 
 type CommonButtonProps<T extends 'submit' | 'default' = 'default'> =
