d50e7c95a9a26a7d3cec09dbf71ae1c03e955593
Caleb Pollman <cpollman@amazon.com>
2022-12-20 15:35:04 -0800

    chore(ui-react): lint primitives - lint A* primitives

--- packages/react/.eslintrc.js
+++ packages/react/.eslintrc.js
@@ -69,7 +69,7 @@ module.exports = {
         'src/components/**/*',
         'src/helpers/**/*',
         'src/hooks/**/*',
-        'src/primitives/+(shared|utils|E*)/**/*',
+        'src/primitives/+(shared|utils|A*|E*)/**/*',
         'src/studio',
         // 'src/primitives/**/*',
       ],
