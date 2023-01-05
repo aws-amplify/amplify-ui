bf5398d8ed83dbae93a6d98448b029592783d1f3
Caleb Pollman <cpollman@amazon.com>
2023-01-04 16:27:36 -0800

    - remove extra templateJoin util

--- packages/ui/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-const templateJoin = (
-  values: string[],
-  template: (value: string) => string
-): string =>
-  values.reduce(
-    (acc, curr) => `${acc}${typeof curr === 'string' ? template(curr) : ''}`,
-    ''
-  );
