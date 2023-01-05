101341fc49a1ab6beb98275f9a1f25ef9b2fcd21
wlee221 <willeea@amazon.com>
2023-01-04 18:04:54 -0800

    Fix typo

--- packages/vue/src/components/password-control.vue
+++ packages/vue/src/components/password-control.vue
@@ -124,7 +124,7 @@ export default {
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
             viewBox="0 0 24 24"
-            class="ComponentClassName.Icon"
+            :class="ComponentClassName.Icon"
           >
             <path
               d="M0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0z"
