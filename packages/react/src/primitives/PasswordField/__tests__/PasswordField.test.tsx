9edc0ed512f2d867241e9c7d746243e043dd0a01
wlee221 <willeea@amazon.com>
2023-01-04 18:26:25 -0800

    Revert React change

--- packages/react/src/primitives/PasswordField/__tests__/PasswordField.test.tsx
+++ packages/react/src/primitives/PasswordField/__tests__/PasswordField.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { PasswordField } from '../PasswordField';
 import { ComponentClassNames, ComponentText } from '../../shared/constants';
