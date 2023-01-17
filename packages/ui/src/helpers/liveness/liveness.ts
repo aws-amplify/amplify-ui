6be24a973bd6d360ef1fa41944175eb5e44950c3
thaddmt <68032955+thaddmt@users.noreply.github.com>
2023-01-17 10:29:15 -0800

    fix: set liveness width seed to 1.0 (#688)

--- packages/ui/src/helpers/liveness/liveness.ts
+++ packages/ui/src/helpers/liveness/liveness.ts
@@ -112,7 +112,7 @@ export function getRandomLivenessOvalDetails({
   return getStaticLivenessOvalDetails({
     width,
     height,
-    widthSeed: randomScalingAttributes.width,
+    widthSeed: 1.0, // Testing with max value to force closest distance to camera
     centerXSeed: randomScalingAttributes.centerX,
     centerYSeed: randomScalingAttributes.centerY,
   });
