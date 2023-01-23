fe738bcc4864b77fb50c5ed863f271711933909c
0618 <o.6180339@gmail.com>
2023-01-23 14:13:27 -0800

    chore(docs): add comment for regex

--- docs/tests/sitemap.test.ts
+++ docs/tests/sitemap.test.ts
@@ -2,9 +2,9 @@ import * as fs from 'fs';
 
 test('Sitemap Snapshot', () => {
   const sitemapData = fs.readFileSync('./public/sitemap.xml', 'utf8');
-  const regex = new RegExp('(?<=>).+?(?=</loc)', 'gmi');
+  const regexOfLinks = new RegExp('(?<=>).+?(?=</loc)', 'gmi'); // This Regex is to match all the content between `>` and end with `</loc`.
   const paths = sitemapData
-    .match(regex)
+    .match(regexOfLinks)
     .map((link) => link.replace(process.env.SITE_URL, ''))
     .sort();
   expect(paths.join(', \n')).toMatchSnapshot();
