82aad7381da65464085c6aa934a2fb73a6e5130d
0618 <o.6180339@gmail.com>
2023-01-23 12:45:31 -0800

    chore(docs): fit regex

--- docs/tests/sitemap.test.ts
+++ docs/tests/sitemap.test.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 test('Sitemap Snapshot', () => {
   const sitemapData = fs.readFileSync('./public/sitemap.xml', 'utf8');
-  const regex = new RegExp('(?<=<loc>).+?(?=</loc>)', 'gmi');
+  const regex = new RegExp('(?<=>).+?(?=</loc)', 'gmi');
   const paths = sitemapData
     .match(regex)
     .map((link) => link.replace(process.env.SITE_URL, ''))
