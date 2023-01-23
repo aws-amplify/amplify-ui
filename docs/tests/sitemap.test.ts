90b28d08c65b90058655a8c77278644ffb38ebc1
0618 <o.6180339@gmail.com>
2023-01-23 13:28:13 -0800

    chore(docs): remove console log

--- docs/tests/sitemap.test.ts
+++ docs/tests/sitemap.test.ts
@@ -7,9 +7,7 @@ test('Sitemap Snapshot', () => {
     .match(regex)
     .map((link) => link.replace(process.env.SITE_URL, ''))
     .sort();
-  console.log('⭐ :::::SITEMAP::::: ⭐', sitemapData);
   console.log(sitemapData);
-  console.log('⭐ :::::PATHS::::: ⭐');
   console.log(paths.join(', \n'));
   expect(paths.join(', \n')).toMatchSnapshot();
 });
