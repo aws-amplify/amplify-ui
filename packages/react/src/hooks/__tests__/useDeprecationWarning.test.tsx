bf3bfc52dcb39c98bddee37fd6208fda64971c63
Caleb Pollman <cpollman@amazon.com>
2023-01-27 11:29:44 -0800

    chore(ui-react): lint test files (#3343)

--- packages/react/src/hooks/__tests__/useDeprecationWarning.test.tsx
+++ packages/react/src/hooks/__tests__/useDeprecationWarning.test.tsx
@@ -1,52 +1,55 @@
 import { renderHook } from '@testing-library/react-hooks';
 import { useDeprecationWarning } from '../useDeprecationWarning';
 
-const originalWarn = console.warn;
+// add empty mockImplementation to prevent logging output to the console
+const consoleWarnSpy = jest.spyOn(console, 'warn').mockImplementation();
 
 describe('useDeprecationWarning', () => {
-  beforeAll(() => {
-    console.warn = jest.fn();
-  });
-  afterAll(() => {
-    console.warn = originalWarn;
-  });
   beforeEach(() => {
-    jest.clearAllMocks();
+    consoleWarnSpy.mockClear();
   });
 
-  it('should render message (shouldWarn true)', async () => {
+  it('should render message (shouldWarn true)', () => {
     const message = 'This component is deprecated, use X instead';
 
     renderHook(() => useDeprecationWarning({ shouldWarn: true, message }));
-    expect(console.warn).toHaveBeenCalledTimes(1);
-    expect(console.warn).toHaveBeenCalledWith(message);
+    expect(consoleWarnSpy).toHaveBeenCalledTimes(1);
+    expect(consoleWarnSpy).toHaveBeenCalledWith(message);
   });
 
-  it('should not render message if shouldWarn is false', async () => {
+  it('should not render message if shouldWarn is false', () => {
     const message = 'This component is deprecated, use X instead';
 
     renderHook(() => useDeprecationWarning({ message, shouldWarn: false }));
-    expect(console.warn).toHaveBeenCalledTimes(0);
-    expect(console.warn).not.toHaveBeenCalledWith(message);
+    expect(consoleWarnSpy).toHaveBeenCalledTimes(0);
+    expect(consoleWarnSpy).not.toHaveBeenCalledWith(message);
   });
 
-  it('should not render message if shouldWarn is undefined', async () => {
+  it('should not render message if shouldWarn is undefined', () => {
     const message = 'This component is deprecated, use X instead';
 
-    renderHook(() => useDeprecationWarning({ message, shouldWarn: undefined }));
-    expect(console.warn).toHaveBeenCalledTimes(0);
-    expect(console.warn).not.toHaveBeenCalledWith(message);
+    renderHook(() =>
+      useDeprecationWarning({
+        message,
+        shouldWarn: undefined as unknown as boolean,
+      })
+    );
+    expect(consoleWarnSpy).toHaveBeenCalledTimes(0);
+    expect(consoleWarnSpy).not.toHaveBeenCalledWith(message);
   });
 
-  it('should not throw reference error if process is not defined', async () => {
+  it('should not throw reference error if process is not defined', () => {
     const message = 'This component is deprecated, use X instead';
 
     const originalProcess = global.process;
+
+    // ignore the below as this test is meant to test behavior that TS does not support
+    // @ts-ignore
     delete global.process;
 
     renderHook(() => useDeprecationWarning({ shouldWarn: true, message }));
-    expect(console.warn).toHaveBeenCalledTimes(1);
-    expect(console.warn).toHaveBeenCalledWith(message);
+    expect(consoleWarnSpy).toHaveBeenCalledTimes(1);
+    expect(consoleWarnSpy).toHaveBeenCalledWith(message);
 
     global.process = originalProcess;
   });
