7e7490b51009cc57dc8906bf36d1a8fda840e353
0618 <o.6180339@gmail.com>
2023-02-02 12:47:48 -0800

    fix(docs): ThemeExample heading

--- docs/src/components/ThemeExample.tsx
+++ docs/src/components/ThemeExample.tsx
@@ -1,11 +1,16 @@
-import { Link, View } from '@aws-amplify/ui-react';
+import { Heading, Link, View } from '@aws-amplify/ui-react';
 import { ThemeAlert } from '@/components/ThemeAlert';
 import { GITHUB_REPO_FILE } from '@/data/links';
 import { DesignTokenIcon } from '@/components/Icons';
 
 export const ThemeExample = (props) => (
   <>
-    ### Theme
+    <Heading level={3} id="theme">
+      <a aria-hidden="true" tabIndex={-1} href="#theme">
+        <span className="icon icon-link"></span>
+      </a>
+      Theme
+    </Heading>
     <ThemeAlert />
     You can customize the appearance of all {props.component} components in your
     application with a <Link href="/theming">Theme</Link>.
