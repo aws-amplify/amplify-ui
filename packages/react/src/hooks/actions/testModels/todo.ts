1850ff33aea0473dc6b3bf3364cd96b36be15e8e
Danny Banks <djb@amazon.com>
2023-02-02 12:10:29 -0800

    chore: updates

--- packages/react/src/hooks/actions/testModels/todo.ts
+++ packages/react/src/hooks/actions/testModels/todo.ts
@@ -5,16 +5,13 @@ export type TodoMetaData = {
 };
 
 export class Todo {
-  readonly id: string;
+  readonly id!: string;
   readonly name: string;
   readonly description?: string;
   readonly createdAt?: string;
   readonly updatedAt?: string;
   constructor(init: ModelInit<Todo, TodoMetaData>) {
     this.name = init.name;
-    // this is only a test model and this is to make TS happy about
-    // the id attribute being set on the constructor
-    this.id = Date.now().toString();
   }
   static copyOf(
     source: Todo,
