9eee40e1b2784eacac9b866c39b912ba0a64d835
0618 <o.6180339@gmail.com>
2023-02-08 15:59:01 -0800

    chore(docs): refactor sitePaths

--- docs/tests/sitemap.test.ts
+++ docs/tests/sitemap.test.ts
@@ -1,11 +1,6 @@
 import * as fs from 'fs';
+import { sitePaths } from '../src/data/sitePaths';
 
 test('Sitemap Snapshot', () => {
-  const sitemapData = fs.readFileSync('./public/sitemap.xml', 'utf8');
-  const linkRegex = new RegExp('(?<=>).+?(?=</loc)', 'gmi'); // This Regex is to match all the content between `>` and end with `</loc`.
-  const paths = sitemapData
-    .match(linkRegex)
-    .map((link) => link.replace(process.env.SITE_URL, ''))
-    .sort();
-  expect(paths.join(', \n')).toMatchSnapshot();
+  expect(sitePaths.join(', \n')).toMatchSnapshot();
 });
