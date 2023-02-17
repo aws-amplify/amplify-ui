fefb992316de54d04d26729958501d73a5b69eef
Ioana Brooks <68251134+ioanabrooks@users.noreply.github.com>
2023-02-17 12:41:37 -0800

    chore(ui-react): Replace lodash utils that check emptiness (#3376)

--- packages/react-core/src/utils/index.ts
+++ packages/react-core/src/utils/index.ts
@@ -1,20 +1,4 @@
-import { isEmpty, isObject, isString } from '@aws-amplify/ui';
-
-function isEmptyArray<T>(value: T): boolean {
-  return Array.isArray(value) && isEmpty(value);
-}
-
-export function areEmptyArrays<T>(...values: T[]): boolean {
-  return values.every(isEmptyArray);
-}
-
-function isEmptyObject<T>(value: T): boolean {
-  return isObject(value) && isEmpty(value);
-}
-
-export function areEmptyObjects<T>(...values: T[]): boolean {
-  return values.every(isEmptyObject);
-}
+import { isString } from '@aws-amplify/ui';
 
 export function templateJoin(
   values: string[],
--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -3,13 +3,10 @@
  * The ultimate goal is have replacement implementation to remove the entire dependency
  */
 export { default as isEqual } from 'lodash/isEqual.js';
-export { default as isEmpty } from 'lodash/isEmpty.js';
 export { default as debounce } from 'lodash/debounce.js';
-export { default as isNil } from 'lodash/isNil.js';
 export { default as includes } from 'lodash/includes.js';
 export { default as get } from 'lodash/get.js';
 export { default as pickBy } from 'lodash/pickBy.js';
-export { default as has } from 'lodash/has.js';
 export { default as kebabCase } from 'lodash/kebabCase.js';
 export { default as merge } from 'lodash/merge.js';
 
@@ -51,6 +48,30 @@ export function isString(value: unknown): value is string {
 }
 
 /**
+ * Checks if `value` is a Map
+ *
+ * @param {unknown} value The value to check
+ * @returns {boolean} Returns `true` if `value` is a Map, `false` otherwise
+ */
+export function isMap(value: unknown): value is Map<unknown, unknown> {
+  return (
+    isObject(value) && Object.prototype.toString.call(value) === '[object Map]'
+  );
+}
+
+/**
+ * Checks if `value` is a Set
+ *
+ * @param {unknown} value The value to check
+ * @returns {boolean} Returns `true` if `value` is a Set, `false` otherwise
+ */
+export function isSet<T>(value: unknown): value is Set<T> {
+  return (
+    isObject(value) && Object.prototype.toString.call(value) === '[object Set]'
+  );
+}
+
+/**
  * Checks if `value` is undefined
  *
  * @param {unknown} value The value to check
@@ -61,7 +82,79 @@ export function isUndefined(value: unknown): value is undefined {
 }
 
 /**
+ * Checks if `value` is nullish
  *
+ * @param {unknown} value The value to check
+ * @returns {boolean} Returns `true` if `value` is nullish, `false` otherwise
+ */
+export function isNil(value: unknown): value is null | undefined {
+  return value == null;
+}
+
+/**
+ * Checks if `value` is empty
+ *
+ * @param {unknown} value The value to check
+ * @returns {boolean} Returns `true` if `value` is empty, `false` otherwise
+ */
+export function isEmpty<T>(value: T): boolean {
+  if (value === null || value === undefined) return true;
+
+  if (isObject(value) && (isMap(value) || isSet(value))) {
+    return !value.size;
+  }
+  if (isObject(value) && (isString(value) || Array.isArray(value))) {
+    return !value.length;
+  }
+  for (const key in value) {
+    if (has(value, key)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/**
+ * Checks if `value` is an empty array
+ *
+ * @param {unknown} value The value to check
+ * @returns {boolean} Returns `true` if `value` is a empty, `false` otherwise
+ */
+function isEmptyArray<T>(value: T): boolean {
+  return Array.isArray(value) && isEmpty(value);
+}
+
+/**
+ * Checks if all members of the `values` param are empty arrays
+ *
+ * @param {unknown} value The values to check
+ * @returns {boolean} Returns `true` if all members of `values` are empty, `false` otherwise
+ */
+export function areEmptyArrays<T>(...values: T[]): boolean {
+  return values.every(isEmptyArray);
+}
+
+/**
+ * Checks if `value` is an empty object
+ *
+ * @param {unknown} value The value to check
+ * @returns {boolean} Returns `true` if `value` is empty, `false` otherwise
+ */
+function isEmptyObject<T>(value: T): boolean {
+  return isObject(value) && isEmpty(value);
+}
+
+/**
+ * Checks if all members of the `values` param are empty objects
+ *
+ * @param {unknown} value The values to check
+ * @returns {boolean} Returns `true` if all members of the `values` param are empty, `false` otherwise
+ */
+export function areEmptyObjects<T>(...values: T[]): boolean {
+  return values.every(isEmptyObject);
+}
+
+/*
  * @param value capitalizes `value` and its type.
  * @returns Capitalized `value`
  */
@@ -69,4 +162,11 @@ export function capitalize<T extends string>(value: T): Capitalize<T> {
   return (
     isString(value) ? value.charAt(0).toUpperCase() + value.slice(1) : ''
   ) as Capitalize<T>;
+}
+
+/*
+ * Checks if `key` is a direct property of `object`.
+ */
+export function has(object: unknown, key: string): boolean {
+  return object != null && Object.prototype.hasOwnProperty.call(object, key);
 }
