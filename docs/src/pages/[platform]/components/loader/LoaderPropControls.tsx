d55d946a47d8f33cf5121c6d3ded81dd29219ee9
Danny Banks <djb@amazon.com>
2023-02-24 14:47:06 -0800

    chore: updates

--- docs/src/pages/[platform]/components/loader/LoaderPropControls.tsx
+++ docs/src/pages/[platform]/components/loader/LoaderPropControls.tsx
@@ -8,6 +8,7 @@ import {
 } from '@aws-amplify/ui-react';
 
 export interface LoaderPropControlsProps extends LoaderProps {
+  filledColor: string;
   setSize: (value: React.SetStateAction<LoaderProps['size']>) => void;
   setVariation: (value: React.SetStateAction<LoaderProps['variation']>) => void;
   setEmptyColor: (
