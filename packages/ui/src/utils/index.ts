e08e6223473f56cbbc2d0ce4bab85ebd1caf020c
William Lee <43682783+wlee221@users.noreply.github.com>
2023-02-24 14:50:14 -0800

    fix(ui): use lodash imports directly  (#3472)
    * Import directly from lodash
    
    * Update packages/ui/src/validators/index.ts
    
    * Create good-cooks-crash.md

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,16 +1,4 @@
 /**
- * We re-export any lodash imports to avoid appending .js extension across codebase
- * The ultimate goal is have replacement implementation to remove the entire dependency
- */
-export { default as isEqual } from 'lodash/isEqual.js';
-export { default as debounce } from 'lodash/debounce.js';
-export { default as includes } from 'lodash/includes.js';
-export { default as get } from 'lodash/get.js';
-export { default as pickBy } from 'lodash/pickBy.js';
-export { default as kebabCase } from 'lodash/kebabCase.js';
-export { default as merge } from 'lodash/merge.js';
-
-/**
  * Some libraries may not follow Node ES module spec and could be loaded as CommonJS modules,
  * To ensure the interoperability between ESM and CJS, modules from those libraries have to be loaded via namespace import
  * And sanitized by the function below because unlike ESM namespace, CJS namespace set `module.exports` object on the `default` key
