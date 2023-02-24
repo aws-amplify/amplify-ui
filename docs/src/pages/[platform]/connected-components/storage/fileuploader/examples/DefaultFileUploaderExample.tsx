9ac04da24cbba27d550471e216a3f654ff0f6adb
Danny Banks <djb@amazon.com>
2023-02-24 12:52:51 -0800

    chore: updates

--- docs/src/pages/[platform]/connected-components/storage/fileuploader/examples/DefaultFileUploaderExample.tsx
+++ docs/src/pages/[platform]/connected-components/storage/fileuploader/examples/DefaultFileUploaderExample.tsx
@@ -1,43 +1,11 @@
-import { Button, FileUploader } from '@aws-amplify/ui-react'; // IGNORE
-import { useState } from 'react';
-
-const es = {
-  dropFilesText: 'yoink filez',
-  uploadButtonText: (count) => `YOINK ${count} filez`,
-};
-
-const en = {
-  dropFilesText: 'yeet filez',
-  uploadButtonText: (count) => `yeet ${count} filez`,
-};
+import { FileUploader } from '@aws-amplify/ui-react'; // IGNORE
 
 export const DefaultFileUploaderExample = () => {
-  const [text, setText] = useState(en);
-
   return (
-    <>
-      <Button
-        onClick={() => {
-          setText(en);
-        }}
-      >
-        En
-      </Button>
-      <Button
-        onClick={() => {
-          setText(es);
-        }}
-      >
-        Es
-      </Button>
-      <FileUploader
-        acceptedFileTypes={['image/*']}
-        accessLevel="public"
-        displayText={{
-          dropFilesText: text.dropFilesText,
-        }}
-        provider="fast" // IGNORE
-      />
-    </>
+    <FileUploader
+      acceptedFileTypes={['image/*']}
+      accessLevel="public"
+      provider="fast" // IGNORE
+    />
   );
 };
