8e6d9b2db607c7de19b9abb5abf1a3a83198e856
Moritz R <mrtz.dev@gmail.com>
2023-02-27 19:53:54 +0100

    remove edits IconClose

--- packages/react/src/primitives/Icon/icons/IconClose.tsx
+++ packages/react/src/primitives/Icon/icons/IconClose.tsx
@@ -9,13 +9,13 @@ import { InternalIcon } from './types';
  * @internal For internal Amplify UI use only. May be removed in a future release.
  */
 export const IconClose: InternalIcon = (props) => {
-  const { className, ...rest } = props;
+  const { className, size, ...rest } = props;
 
   return (
     <View
       as="span"
-      width="1em"
-      height="1em"
+      width={size || '1em'}
+      height={size || '1em'}
       className={classNames(ComponentClassNames.Icon, className)}
       {...rest}
     >
@@ -25,6 +25,7 @@ export const IconClose: InternalIcon = (props) => {
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        style={size ? { width: size, height: size } : undefined}
       >
         <path
           d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
