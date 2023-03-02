bfe97d00372263be55fb1aedf5d840592c66f2db
MJ☔️ <o.6180339+github@gmail.com>
2023-03-02 21:17:39 +0000

    test(docs): pptr remove pause()

--- docs/scripts/util/index.ts
+++ docs/scripts/util/index.ts
@@ -2,5 +2,4 @@ export { checkLink } from './checkLink';
 export { crawlAllLinksFromAllPages } from './crawlAllLinksFromAllPages';
 export { getAllTypesData } from './getAllTypesData';
 export { getCatalog } from './getCatalog';
-export { pause } from './pause';
 export { runArrayPromiseInOrder } from './runArrayPromiseInOrder';
