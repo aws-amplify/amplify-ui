95d015570fbec1d2748437e68b7ad6670c6f9743
William Lee <43682783+wlee221@users.noreply.github.com>
2023-03-01 18:56:20 -0800

    chore: use personal amplify app and try pulling it (#4)
    * Skeleton workflow
    
    * Add name
    
    * Try assuming credentials
    
    * Fix typo
    
    * try pulling env
    
    * Setup node.js 16
    
    * Fix region
    
    * Fix appid
    
    * Fix envName
    
    * Try updating resources?
    
    * try get-caller-identity
    
    * Unset env variable
    
    * Try profile
    
    * Try environments/auth cwd
    
    * Try just environments
    
    * Remove wd settings
    
    * try get-app
    
    * aws configure list
    
    * More echos
    
    * Try echo n
    
    * Try simplifying the config
    
    * LS
    
    * Reorder steps
    
    * Remove unrelated steps
    
    * Try echo y again
    
    * Try --yes

--- environments/auth/auth-with-email/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ environments/auth/auth-with-email/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,13 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "authwithemail": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
+  "auth": {
+    "authwithemail24754f67": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
+  }
 }
--- environments/pull-environment.sh
+++ environments/pull-environment.sh
@@ -7,27 +7,12 @@ dir=$1
 region=$2
 
 # In development, AWS_PROFILE should be set. In CI, it's not.
-[ "$AWS_PROFILE" ] && useProfile="true" || useProfile="false"
+[ "$AWS_PROFILE" ] || AWS_PROFILE="default"
 
-FRONTENDCONFIG="{\
-\"SourceDir\":\"src\",\
-\"DistributionDir\":\"dist\",\
-\"BuildCommand\":\"npm run-script build\",\
-\"StartCommand\":\"npm run-script start\"\
-}"
-FRONTEND="{\
-\"frontend\":\"javascript\",\
-\"framework\":\"none\",\
-\"config\":$FRONTENDCONFIG}"
-AMPLIFY="{\
-\"defaultEditor\":\"code\",\
-}"
 AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"project\",\
-\"useProfile\":$useProfile,\
+\"useProfile\":\"true\",\
 \"profileName\":\"$AWS_PROFILE\",\
-\"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
-\"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
 \"region\":\""$region\""\
 }"
 PROVIDERS="{\
@@ -35,4 +20,4 @@ PROVIDERS="{\
 
 cd $dir
 
-echo y | yarn pull --amplify $AMPLIFY --frontend $FRONTEND --providers $PROVIDERS
+yarn pull --providers $PROVIDERS -y
