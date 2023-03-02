5dd7519e7a84ec7a3a72ea649a009c42e1a0e3fd
Ioana Brooks <68251134+ioanabrooks@users.noreply.github.com>
2023-03-02 12:28:51 -0800

    chore(docs): Enable type checks for docs package (#3468)

--- docs/src/pages/[platform]/connected-components/storage/fileuploader/examples/MultipleExample.tsx
+++ docs/src/pages/[platform]/connected-components/storage/fileuploader/examples/MultipleExample.tsx
@@ -7,6 +7,7 @@ export const MultipleExample = () => {
       variation="drop"
       acceptedFileTypes={['image/*']}
       accessLevel="public"
+      // @ts-ignore // IGNORE
       provider="fast" // IGNORE
     />
   );
