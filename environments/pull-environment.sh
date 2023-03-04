503c30094e1eb35e450459564436a598d0fc4a23
wlee221 <willeea@amazon.com>
2023-03-03 17:49:09 -0800

    Use echo n instead

--- environments/pull-environment.sh
+++ environments/pull-environment.sh
@@ -10,6 +10,20 @@ region=$2
 # we set it explicitly to default.
 [ -z "$AWS_PROFILE" ] && AWS_PROFILE="default"
 
+FRONTENDCONFIG="{\
+\"SourceDir\":\"src\",\
+\"DistributionDir\":\"dist\",\
+\"BuildCommand\":\"npm run-script build\",\
+\"StartCommand\":\"npm run-script start\"\
+}"
+FRONTEND="{\
+\"frontend\":\"javascript\",\
+\"framework\":\"none\",\
+\"config\":$FRONTENDCONFIG}"
+AMPLIFY="{\
+\"defaultEditor\":\"code\",\
+}"
+
 # We set `useProfile` to true, because Amplify CLI does not support headless
 # pull with temporary credentials when `useProfile` is set to false.
 # See https://github.com/aws-amplify/amplify-cli/issues/11009.
@@ -24,6 +38,6 @@ PROVIDERS="{\
 
 cd $dir
 
-# `--yes` flag is to added to say yes to the CLI prompts.
-# See https://github.com/aws-amplify/amplify-cli/issues/5275.
-yarn pull --providers $PROVIDERS --yes
+# 'echo n' is used to answer "No" to the prompt "Do you plan on modifying this backend?"
+# See https://github.com/aws-amplify/amplify-cli/issues/5275
+echo n | yarn pull --amplify $AMPLIFY --frontend $FRONTEND --providers $PROVIDERS
