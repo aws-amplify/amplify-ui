36e16b1a8115c156f172ee64d81cdae4f0e2bb65
MJ☔️ <o.6180339+github@gmail.com>
2023-03-08 23:17:28 +0000

    Revert "test(docs): change || to ??"
    This reverts commit 7986032862ffbcda9acb183ede72c5e43fd4aaab.

--- docs/scripts/util/checkLink.ts
+++ docs/scripts/util/checkLink.ts
@@ -103,7 +103,7 @@ export async function checkLink(
               ...linkData,
               statusCode,
             })
-          )?.statusCode ?? statusCode;
+          )?.statusCode || statusCode;
         requestedUrl.add(href);
         res({ ...linkData, statusCode });
       });
