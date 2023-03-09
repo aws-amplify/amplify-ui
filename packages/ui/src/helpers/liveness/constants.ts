5f7bfa1307e54cdc7de205845c8d236ecf920bf8
Thadd Trinh <thadtrin@amazon.com>
2023-03-09 15:19:06 -0800

    chore: try updating to .3

--- packages/ui/src/helpers/liveness/constants.ts
+++ packages/ui/src/helpers/liveness/constants.ts
@@ -3,6 +3,6 @@
 // These FACE_DISTNACE_THRESHOLD values are determined by the science team and should only be changed with their approval.
 // We want to ensure at the start of a  check that the user's pupilDistance/ovalWidth is below FACE_DISTANCE_THRESHOLD to ensure that they are starting
 //  a certain distance away from the camera.
-export const FACE_DISTANCE_THRESHOLD = 0.34;
+export const FACE_DISTANCE_THRESHOLD = 0.3;
 export const REDUCED_THRESHOLD = 0.4;
 export const REDUCED_THRESHOLD_MOBILE = 0.37;
