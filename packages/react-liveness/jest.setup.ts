6c686b76f94fdce3fc59369a420b908387657799
Caleb Pollman <cpollman@amazon.com>
2023-03-13 15:45:15 -0700

    remove console.warn spy

--- packages/react-liveness/jest.setup.ts
+++ packages/react-liveness/jest.setup.ts
@@ -1,8 +1,5 @@
 import '@testing-library/jest-dom';
 
-// usage of multiple tensorflow deps causes an excessive amount of console warnings
-jest.spyOn(console, 'warn').mockImplementation();
-
 /**
  * This is a workaround to the problem of the jsdom library not supporting
  * URL.createObjectURL. See https://github.com/jsdom/jsdom/issues/1721.
