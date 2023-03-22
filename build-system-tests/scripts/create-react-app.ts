5bf4ae3c50510e8192cc9dc88ba7861fcbf96dbf
MJ☔️ <o.6180339+github@gmail.com>
2023-03-22 23:28:44 +0000

    test(build system): dynamically create react16 and 18 js

--- build-system-tests/scripts/create-react-app.ts
+++ build-system-tests/scripts/create-react-app.ts
@@ -1,9 +1,10 @@
 import fs from 'fs';
+import path from 'path';
 import cp from 'child_process';
 import { getArgs } from './utils/getArgs';
 
 type Args = {
-  react?: string | 18;
+  react?: string;
   npm?: string | true;
   js?: string | true;
 };
@@ -17,6 +18,33 @@ if (args.js && !['true', 'false', true].includes(args.js)) {
 if (args.npm && !['true', 'false', true].includes(args.npm)) {
   console.log('npm must be "true" or "false".');
 }
+
+const reactVersion = args.react ?? '18';
+const buildTool = args.npm ? 'npm' : 'yarn';
+const language = args.js ? 'js' : 'ts';
+
+console.log(
+  `Reacting app react-${reactVersion}-cra-5-${language} with ${buildTool}...`
+);
+
+const packageJson = {
+  package: {
+    dependencies: {
+      '@aws-amplify/ui-react': 'latest',
+      'aws-amplify': 'latest',
+    },
+  },
+};
+
+if (reactVersion) {
+  packageJson.package.dependencies['react'] = reactVersion;
+  packageJson.package.dependencies['react-dom'] = reactVersion;
+}
+
+fs.writeFileSync(
+  path.join(__dirname, '../templates/cra-template-react-js', `./template.json`),
+  JSON.stringify(packageJson, null, 4)
+);
 
 fs.copyFile(
   './environments/src/aws-exports.js',
@@ -37,7 +65,7 @@ fs.copyFile(
 );
 
 fs.copyFile(
-  './templates/components/react/cra/index-react-16.js',
+  `./templates/components/react/cra/index-react-${reactVersion}.js`,
   './templates/cra-template-react-js/template/src/index.js',
   (err) => {
     if (err) throw err;
@@ -46,9 +74,9 @@ fs.copyFile(
 );
 
 cp.exec(
-  `npx create-react-app ./mega-apps/react-${args.react ?? '18'}-cra-5-js${
+  `npx create-react-app ./mega-apps/react-${reactVersion}-cra-5-${language}${
     args.npm ? ' --use-npm' : ''
-  } --template file:./templates/cra-template-react-${args.js ? 'js' : 'ts'}`,
+  } --template file:./templates/cra-template-react-${language}`,
   (error, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
