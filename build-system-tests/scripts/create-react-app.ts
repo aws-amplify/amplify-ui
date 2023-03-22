5dd6f6c1765b7deb84e5d1b440c8f0a9aff213b9
MJ☔️ <o.6180339+github@gmail.com>
2023-03-22 21:16:40 +0000

    test(build system): add script/create-react-app args

--- build-system-tests/scripts/create-react-app.ts
+++ build-system-tests/scripts/create-react-app.ts
@@ -1,10 +1,24 @@
 import { exec } from 'child_process';
 import { getArgs } from './utils/getArgs';
 
-const args = getArgs();
+type Args = {
+  react?: string | 18;
+  npm?: string | true;
+  js?: string | true;
+};
+const args = getArgs() as Args;
+
+if (args.js && !['true', 'false', true].includes(args.js)) {
+  console.log(args.js);
+  console.log('js must be "true" or "false".');
+}
+
+if (args.npm && !['true', 'false', true].includes(args.npm)) {
+  console.log('npm must be "true" or "false".');
+}
 
 exec(
-  `cp ./environments/src/aws-exports.js ./cra-template-react-js/template/src/aws-exports.js`,
+  `cp ./environments/src/aws-exports.js ./templates/cra-template-react-js/template/src/aws-exports.js`,
   (error, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
@@ -13,8 +27,11 @@ exec(
     }
   }
 );
+
 exec(
-  'npx create-react-app react-18-cra-5-js --use-npm --template file:cra-template-react-js',
+  `npx create-react-app react-${args.react ?? '18'}-cra-5-js${
+    args.npm ? ' --use-npm' : ''
+  } --template file:./templates/cra-template-react-${args.js ? 'js' : 'ts'}`,
   (error, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
