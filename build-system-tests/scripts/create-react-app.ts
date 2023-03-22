3c96cbc897d3804c5dc3a0a0bc3de28eb6475633
MJ☔️ <o.6180339+github@gmail.com>
2023-03-22 23:49:06 +0000

    test(build system): refactor create-react-app script

--- build-system-tests/scripts/create-react-app.ts
+++ build-system-tests/scripts/create-react-app.ts
@@ -11,12 +11,11 @@ type Args = {
 const args = getArgs() as Args;
 
 if (args.js && !['true', 'false', true].includes(args.js)) {
-  console.log(args.js);
-  console.log('js must be "true" or "false".');
+  throw new Error('js must be "true" or "false".');
 }
 
 if (args.npm && !['true', 'false', true].includes(args.npm)) {
-  console.log('npm must be "true" or "false".');
+  throw new Error('npm must be "true" or "false".');
 }
 
 const reactVersion = args.react ?? '18';
@@ -36,6 +35,21 @@ const packageJson = {
   },
 };
 
+const filesToCopy = {
+  'aws-export': {
+    src: '../environments/src/aws-exports.js',
+    dist: '../templates/cra-template-react-js/template/src/aws-exports.js',
+  },
+  app: {
+    src: '../templates/components/react/App.js',
+    dist: '../templates/cra-template-react-js/template/src/App.js',
+  },
+  index: {
+    src: `../templates/components/react/cra/index-react-${reactVersion}.js`,
+    dist: '../templates/cra-template-react-js/template/src/index.js',
+  },
+};
+
 if (reactVersion) {
   packageJson.package.dependencies['react'] = reactVersion;
   packageJson.package.dependencies['react-dom'] = reactVersion;
@@ -46,32 +60,16 @@ fs.writeFileSync(
   JSON.stringify(packageJson, null, 4)
 );
 
-fs.copyFile(
-  './environments/src/aws-exports.js',
-  './templates/cra-template-react-js/template/src/aws-exports.js',
-  (err) => {
-    if (err) throw err;
-    console.log('Copied aws-exports.js to template');
-  }
-);
-
-fs.copyFile(
-  './templates/components/react/App.js',
-  './templates/cra-template-react-js/template/src/App.js',
-  (err) => {
-    if (err) throw err;
-    console.log('Copied App.js to template');
-  }
-);
-
-fs.copyFile(
-  `./templates/components/react/cra/index-react-${reactVersion}.js`,
-  './templates/cra-template-react-js/template/src/index.js',
-  (err) => {
-    if (err) throw err;
-    console.log('Copied index.js to template');
-  }
-);
+Object.entries(filesToCopy).forEach(([fileName, val]) => {
+  fs.copyFile(
+    path.join(__dirname, val.src),
+    path.join(__dirname, val.dist),
+    (err) => {
+      if (err) throw err;
+      console.log(`Copied ${fileName} from ${val.src} to ${val.dist}`);
+    }
+  );
+});
 
 cp.exec(
   `npx create-react-app ./mega-apps/react-${reactVersion}-cra-5-${language}${
