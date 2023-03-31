06b3339b8e09c29cf868f6fa8797c64ca28be954
Scott Rees <6165315+reesscot@users.noreply.github.com>
2023-03-31 10:57:40 -0700

    chore: fix rating test type

--- packages/react/src/primitives/Rating/__test__/Rating.test.tsx
+++ packages/react/src/primitives/Rating/__test__/Rating.test.tsx
@@ -56,7 +56,7 @@ describe('Rating', () => {
     const emptyIcon = container.getElementsByClassName(
       'amplify-rating-icon-empty'
     )[0] as HTMLElement;
-    expect((emptyIcon.style as React.CSSProperties).color).toBe('red');
+    expect(emptyIcon.style.color).toBe('red');
   });
 
   it('should render the filled icon color', () => {
@@ -67,7 +67,7 @@ describe('Rating', () => {
     const filledIcon = container.getElementsByClassName(
       'amplify-rating-icon-filled'
     )[0] as HTMLElement;
-    expect((filledIcon['style'] as React.CSSProperties).color).toBe('blue');
+    expect(filledIcon.style.color).toBe('blue');
   });
 
   it('should render filled icons when the value prop is used', () => {
