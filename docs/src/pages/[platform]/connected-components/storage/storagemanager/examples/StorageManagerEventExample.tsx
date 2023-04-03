fccc93523f7b7cd46add32b72ec50d6d8417f32f
Danny Banks <djb@amazon.com>
2023-04-03 15:04:47 -0700

    chore: updates

--- docs/src/pages/[platform]/connected-components/storage/storagemanager/examples/StorageManagerEventExample.tsx
+++ docs/src/pages/[platform]/connected-components/storage/storagemanager/examples/StorageManagerEventExample.tsx
@@ -2,10 +2,6 @@ import * as React from 'react';
 import { StorageManager } from '@aws-amplify/ui-react-storage';
 
 export const StorageManagerEventExample = () => {
-  // NOTE: you will need to keep a ref of the files because
-  // the StorageManager calls onUploadSuccess and onUploadError asynchronously
-  // when each file is uploaded.
-  const filesRef = React.useRef([]);
   const [files, setFiles] = React.useState([]);
 
   return (
@@ -15,15 +11,10 @@ export const StorageManagerEventExample = () => {
         accessLevel="public"
         maxFileCount={10}
         onFileRemove={({ key }) => {
-          // remove the file from the list
-          filesRef.current = filesRef.current.filter(
-            (file) => file.key !== key
-          );
-          setFiles(filesRef.current);
+          setFiles((prevFiles) => prevFiles.filter((file) => file.key !== key));
         }}
         onUploadSuccess={({ key }) => {
-          filesRef.current = [...filesRef.current, { key }];
-          setFiles(filesRef.current);
+          setFiles((prevFiles) => [...prevFiles, { key }]);
         }}
         provider="fast" // IGNORE
       />
