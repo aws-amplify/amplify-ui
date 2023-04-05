df04dfab771ad6f9ec9e3ea808aeb6a6d4c8995c
Heather Buchel <hbuchel@gmail.com>
2023-04-05 10:02:37 -0400

    chore: add tests for MatchIndicator (#833)
    * chore: add tests for MatchIndicator
    
    * ensure progress bar can't be less than 0 or more than 100, add tests
    
    ---------
    
    Co-authored-by: Heather Buchel <buchel@amazon.com>

--- packages/react-liveness/src/components/FaceLivenessDetector/shared/MatchIndicator.tsx
+++ packages/react-liveness/src/components/FaceLivenessDetector/shared/MatchIndicator.tsx
@@ -3,17 +3,34 @@ import { LivenessClassNames } from '../types/classNames';
 
 interface MatchIndicatorProps {
   percentage: number;
+  initialPercentage?: number;
+  testId?: string;
 }
 
 export const MatchIndicator: React.FC<MatchIndicatorProps> = ({
   percentage,
+  initialPercentage = 25,
+  testId,
 }) => {
+  const [matchPercentage, setMatchPercentage] =
+    React.useState<number>(initialPercentage);
+
+  React.useEffect(() => {
+    if (percentage < 0) {
+      setMatchPercentage(0);
+    } else if (percentage > 100) {
+      setMatchPercentage(100);
+    } else {
+      setMatchPercentage(percentage);
+    }
+  }, [percentage]);
+
   const percentageStyles = {
-    '--percentage': `${percentage}%`,
+    '--percentage': `${matchPercentage}%`,
   } as React.CSSProperties;
 
   return (
-    <div className={LivenessClassNames.MatchIndicator}>
+    <div className={LivenessClassNames.MatchIndicator} data-testid={testId}>
       <div
         className={`${LivenessClassNames.MatchIndicator}__bar`}
         style={percentageStyles}
