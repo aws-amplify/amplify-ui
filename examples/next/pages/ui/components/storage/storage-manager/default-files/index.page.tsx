c4825f853ae03d1cfd1ad5febe6ecf333eea55ed
Ioana Brooks <68251134+ioanabrooks@users.noreply.github.com>
2023-04-05 12:59:16 -0700

    chore(ui-react-storage): Add e2e tests for Storage Manager (#3660)

---
+++ examples/next/pages/ui/components/storage/storage-manager/default-files/index.page.tsx
@@ -0,0 +1,20 @@
+import { Amplify } from 'aws-amplify';
+import { StorageManager } from '@aws-amplify/ui-react-storage';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export function StorageManagerExample() {
+  return (
+    <>
+      <StorageManager
+        acceptedFileTypes={['image/*']}
+        accessLevel="public"
+        maxFileCount={1}
+        showThumbnails
+        defaultFiles={[{ key: 'default.jpg' }]}
+      />
+    </>
+  );
+}
+export default StorageManagerExample;
