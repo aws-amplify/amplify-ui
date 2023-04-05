747516159d504b551dab09cbe8f214fa7b4505df
Danny Banks <djb@amazon.com>
2023-04-05 10:27:05 -0700

    internal: add Field primitive (#3663)

--- packages/react/src/primitives/Field/index.ts
+++ packages/react/src/primitives/Field/index.ts
@@ -1,3 +1,4 @@
 export { FieldClearButton } from './FieldClearButton';
 export { FieldDescription } from './FieldDescription';
 export { FieldErrorMessage } from './FieldErrorMessage';
+export { Field } from './Field';
