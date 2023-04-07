5b26ec27a1a3ba1eaf9b277654ad7015b41f43c0
Thadd Trinh <thadtrin@amazon.com>
2023-04-07 15:27:15 -0700

    chore: remove examples react-native changes

--- examples/react-native/src/ui/RadioGroup/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as RadioGroup } from './RadioGroup';
-export type { RadioGroupProps } from './types';
--- examples/react-native/src/ui/RadioGroup/types.ts
+++
@@ -1,7 +0,0 @@
-import { RadioProps } from '@aws-amplify/ui-react-native/dist/primitives';
-
-export interface RadioGroupProps<T>
-  extends Omit<RadioProps<T>, 'buttonStyle' | 'iconStyle' | 'selected'> {
-  defaultValue?: T;
-  direction?: 'vertical' | 'horizontal';
-}
