3f8aa264ce66292def4be74f42c88763f1796562
thaddmt <68032955+thaddmt@users.noreply.github.com>
2023-04-07 17:03:26 -0700

    Update packages/react-core/src/Authenticator/hooks/useAuthenticator/constants.ts
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

--- packages/react-core/src/Authenticator/hooks/useAuthenticator/constants.ts
+++ packages/react-core/src/Authenticator/hooks/useAuthenticator/constants.ts
@@ -1,14 +1,2 @@
 export const USE_AUTHENTICATOR_ERROR =
   '`useAuthenticator` must be used inside an `Authenticator.Provider`.';
-
-export const COMPONENT_ROUTE_KEYS: AuthenticatorRouteComponentKey[] = [
-  'signIn',
-  'signUp',
-  'forceNewPassword',
-  'confirmResetPassword',
-  'confirmSignIn',
-  'confirmSignUp',
-  'confirmVerifyUser',
-  'resetPassword',
-  'setupTOTP',
-];
