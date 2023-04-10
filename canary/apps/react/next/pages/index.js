981dcb29bfbbed6b237b6f1e562b4957084c0547
Thadd Trinh <thadtrin@amazon.com>
2023-04-10 09:42:54 -0700

    Revert "test(liveness): add liveness build test (#824)"
    This reverts commit 2e0a816e19b345b4d2b3cf6f1b0ea65f9c420018.

--- canary/apps/react/next/pages/index.js
+++ canary/apps/react/next/pages/index.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import {
   AmplifyProvider,
   Button,
@@ -10,7 +9,6 @@ import {
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
 import aws_exports from '../../../../environments/auth-with-email/src/aws-exports.js';
-import { FaceLivenessDetector } from '@aws-amplify/ui-react-liveness';
 Amplify.configure(aws_exports);
 
 function App() {
@@ -22,11 +20,6 @@ function App() {
         <Button variation="primary">Click me!</Button>
         <Button onClick={signOut}>Sign out</Button>
       </Card>
-      <FaceLivenessDetector
-        sessionId={'123'}
-        region={'us-east-1'}
-        handleGetLivenessDetection={async () => {}}
-      />
     </AmplifyProvider>
   );
 }
