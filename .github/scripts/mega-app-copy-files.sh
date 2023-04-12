f29ab6562ff75d1eb96142d98cd92a28790d3762
MJ☔️ <o.6180339+github@gmail.com>
2023-04-12 18:51:35 +0000

    test(build system): add comments and misc changes

--- .github/scripts/mega-app-copy-files.sh
+++ .github/scripts/mega-app-copy-files.sh
@@ -8,6 +8,8 @@ if [ "$BUILD_TOOL" == 'cra' ]; then
     else
         cp templates/components/react/cra/App.js mega-apps/${MEGA_APP_NAME}/src/App.tsx
         if [ "$FRAMEWORK_VERSION" == '16' ]; then
+            # We have to customize the index.tsx file for React 16 because the render API changed since React 18.
+            # See more: https://legacy.reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
             cp templates/components/react/cra/index-react-${FRAMEWORK_VERSION}.js mega-apps/${MEGA_APP_NAME}/src/index.tsx
         fi
     fi
@@ -18,13 +20,15 @@ if [ "$BUILD_TOOL" == 'next' ]; then
     cp templates/template-aws-exports.js mega-apps/${MEGA_APP_NAME}/data/aws-exports.js
     cp templates/components/react/next/App.js mega-apps/${MEGA_APP_NAME}/pages/index.tsx
     if [ "$BUILD_TOOL_VERSION" == '11' ]; then
+        # We have to customize the package.json and tsconfig.json for Next.js 11,
+        # because create-next-app only creates the app with the latest version
         cp templates/components/react/next/template-package-${BUILD_TOOL_VERSION}.json mega-apps/${MEGA_APP_NAME}/package.json
         cp templates/components/react/next/template-tsconfig-${BUILD_TOOL_VERSION}.json mega-apps/${MEGA_APP_NAME}/tsconfig.json
     fi
 fi
 
 if [ "$BUILD_TOOL" == 'gatsby' ]; then
-    templates/template-aws-exports.js mega-apps/${MEGA_APP_NAME}/src/pages/aws-exports.js
+    cp templates/template-aws-exports.js mega-apps/${MEGA_APP_NAME}/src/pages/aws-exports.js
     cp templates/components/react/cra/App.js mega-apps/${MEGA_APP_NAME}/src/pages/index.tsx
 fi
 
