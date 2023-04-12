3b8c87120cb30bf33034079b7895aff4854db685
MJ☔️ <o.6180339+github@gmail.com>
2023-04-12 18:51:35 +0000

    test(build system): add comments and misc changes

--- .github/scripts/mega-app-create-app.sh
+++ .github/scripts/mega-app-create-app.sh
@@ -2,27 +2,24 @@
 
 cd build-system-tests
 mkdir -p mega-apps/
+cd mega-apps
 
 if [[ "$BUILD_TOOL" == 'cra' && "$LANGUAGE" == 'js' ]]; then
-    npx create-react-app mega-apps/${MEGA_APP_NAME}
+    npx create-react-app ${MEGA_APP_NAME}
 fi
 
 if [[ "$BUILD_TOOL" == 'cra' && "$LANGUAGE" == 'ts' ]]; then
-    npx create-react-app mega-apps/${MEGA_APP_NAME} --template typescript
+    npx create-react-app ${MEGA_APP_NAME} --template typescript
 fi
 
 if [ "$BUILD_TOOL" == 'next' ]; then
-    npx create-next-app mega-apps/${MEGA_APP_NAME} --ts --no-src-dir --no-experimental-app --no-eslint
+    npx create-next-app ${MEGA_APP_NAME} --ts --no-src-dir --no-experimental-app --no-eslint
 fi
 
 if [ "$BUILD_TOOL" == 'gatsby' ]; then
-    cd mega-apps
     npx create-gatsby@latest -y $MEGA_APP_NAME
-    cd ..
 fi
 
 if [ "$BUILD_TOOL" == 'vite' ]; then
-    cd mega-apps
     npm create vite@${BUILD_TOOL_VERSION} $MEGA_APP_NAME -- --template react-ts
-    cd ..
 fi
--- .github/scripts/mega-app-install-build.sh
+++ .github/scripts/mega-app-install-build.sh
@@ -15,9 +15,13 @@ if [ "$PKG_MANAGER" == 'yarn' ]; then
     yarn build
 else
     if [[ "$BUILD_TOOL" == 'cra' && "$LANGUAGE" == 'ts' ]]; then
+        # If not testing the latest React, we need to download its types.
+        # CRA is the only framework that we test React 16.
         npm install $DEP_TYPES
     fi
     if [[ "$BUILD_TOOL" == 'next' && "$LANGUAGE" == '11' ]]; then
+        # We have to remove the initial downloaded node_modules for Next.js 11,
+        # because create-next-app only creates the app with the latest version
         rm -rf node_modules
     fi
     npm install $DEPENDENCIES
