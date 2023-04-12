d0101d39d682aa766b3beffd691d71525803e5d7
Thadd Trinh <thadtrin@amazon.com>
2023-04-12 15:37:35 -0700

    chore: poc unstable canaries

--- canary/apps/react/vite3/src/App.tsx
+++ canary/apps/react/vite3/src/App.tsx
@@ -8,7 +8,7 @@ import {
 } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
-import aws_exports from '../../../../environments/auth-with-email/src/aws-exports';
+import aws_exports from '../../../../../environments/auth-with-email/src/aws-exports';
 Amplify.configure(aws_exports);
 
 function App() {
