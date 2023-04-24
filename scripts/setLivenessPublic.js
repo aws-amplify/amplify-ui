f8232126e81b6558fdd2d96845713bb1caab35fc
Thadd Trinh <thadtrin@amazon.com>
2023-04-24 15:11:41 -0700

    chore: switch script to flip private field to true

--- scripts/setLivenessPublic.js
+++ scripts/setLivenessPublic.js
@@ -1,6 +1,6 @@
 /**
  * This is a script that runs before publish latest and sets the `@aws-amplify/ui-react-liveness`
- * package.json private field to true so that it is not published by changesets cli publish.
+ * package.json private field to flase so that it is published to the next tag.
  */
 
 const fs = require('fs');
@@ -8,8 +8,8 @@ const fs = require('fs');
 // Read the package.json file
 const packageJson = require(`../packages/react-liveness/package.json`);
 
-// Set the "private" field to true
-packageJson.private = true;
+// Set the "private" field to false
+packageJson.private = false;
 
 // Write the updated package.json file
 fs.writeFileSync(
