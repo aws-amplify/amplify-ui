30252e551402edf8f2a8b1938226a5b17a218126
wlee221 <willeea@amazon.com>
2023-04-25 18:29:46 -0700

    Extend base configs

--- packages/angular/.eslintrc.js
+++ packages/angular/.eslintrc.js
@@ -1,22 +1,31 @@
 module.exports = {
   root: true,
-  ignorePatterns: ['projects/**/*'],
   overrides: [
     {
-      files: ['*.ts'],
+      files: ['**/*.ts'],
       parserOptions: {
         project: ['tsconfig.json'],
         tsconfigRootDir: __dirname,
         createDefaultProgram: true,
       },
       extends: [
-        'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:@angular-eslint/recommended',
         // This is required if you use inline templates in Components
         'plugin:@angular-eslint/template/process-inline-templates',
+        '../configs/eslint/configs/index.js',
       ],
       rules: {
+        // based on https://angular.io/guide/styleguide#member-sequence
+        // TODO: turn the following settings on and fix linting issues
+        '@typescript-eslint/member-ordering': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/prefer-nullish-coalescing': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        'prefer-destructuring': 'off',
         '@angular-eslint/directive-selector': [
           'error',
           {
@@ -28,7 +37,7 @@ module.exports = {
       },
     },
     {
-      files: ['*.html'],
+      files: ['**/*.html'],
       extends: ['plugin:@angular-eslint/template/recommended'],
       rules: {},
     },
--- packages/angular/projects/ui-angular/.eslintrc.js
+++ packages/angular/projects/ui-angular/.eslintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
   extends: '../../.eslintrc.js',
-  // ignorePatterns: ['!**/*'],
-  // overrides: [
-  //   {
-  //     files: ['*.ts'],
-  //     parserOptions: {
-  //       project: ['projects/ui-angular/tsconfig.lib.json'],
-  //       createDefaultProgram: true,
-  //     },
-  //   },
-  //   {
-  //     files: ['*.html'],
-  //     rules: {},
-  //   },
-  // ],
 };
