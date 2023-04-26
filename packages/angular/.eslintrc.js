1233c37c4915f9becf636681a070c5b0f4e3e189
wlee221 <willeea@amazon.com>
2023-04-25 19:07:52 -0700

    turn off prefer-nullish-coalescing

--- packages/angular/.eslintrc.js
+++ packages/angular/.eslintrc.js
@@ -16,19 +16,29 @@ module.exports = {
         '../configs/eslint/configs/index.js',
       ],
       rules: {
-        // TODO: turn the following settings on and fix linting issues
-        '@typescript-eslint/member-ordering': 'warn',
+        /*
+         * TODO: turn the following settings on after fixing the
+         * corresponding linting issues.
+         */
         '@typescript-eslint/explicit-module-boundary-types': 'warn',
+        '@typescript-eslint/member-ordering': 'warn',
+        '@typescript-eslint/no-empty-function': 'warn',
         '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-inferrable-types': 'warn',
+        '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
+        '@typescript-eslint/no-unsafe-argument': 'warn',
         '@typescript-eslint/no-unsafe-assignment': 'warn',
         '@typescript-eslint/no-unsafe-member-access': 'warn',
-        '@typescript-eslint/prefer-nullish-coalescing': 'warn',
-        '@typescript-eslint/no-unsafe-argument': 'warn',
-        '@typescript-eslint/no-inferrable-types': 'warn',
-        '@typescript-eslint/no-empty-function': 'warn',
-        '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
         'prefer-const': 'warn',
         'prefer-destructuring': 'warn',
+
+        /*
+         * `prefer-nullish-coalescing` requires `strictNullChecks` to be on.
+         * Turn this off until we turn `strict` on in the `tsconfig.json`.
+         */
+        '@typescript-eslint/prefer-nullish-coalescing': 'off',
+
+        // we use `amplify-` prefix for directives.
         '@angular-eslint/directive-selector': [
           'error',
           {
