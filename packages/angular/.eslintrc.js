7fd55521283b96a4fbdd6a9d2d3059f355936287
wlee221 <willeea@amazon.com>
2023-04-25 18:36:42 -0700

    Change all offending rules to warn

--- packages/angular/.eslintrc.js
+++ packages/angular/.eslintrc.js
@@ -18,14 +18,18 @@ module.exports = {
       rules: {
         // based on https://angular.io/guide/styleguide#member-sequence
         // TODO: turn the following settings on and fix linting issues
-        '@typescript-eslint/member-ordering': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-        '@typescript-eslint/prefer-nullish-coalescing': 'off',
-        '@typescript-eslint/no-unsafe-argument': 'off',
-        'prefer-destructuring': 'off',
+        '@typescript-eslint/member-ordering': 'warn',
+        '@typescript-eslint/explicit-module-boundary-types': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/no-unsafe-assignment': 'warn',
+        '@typescript-eslint/no-unsafe-member-access': 'warn',
+        '@typescript-eslint/prefer-nullish-coalescing': 'warn',
+        '@typescript-eslint/no-unsafe-argument': 'warn',
+        '@typescript-eslint/no-inferrable-types': 'warn',
+        '@typescript-eslint/no-empty-function': 'warn',
+        '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
+        'prefer-const': 'warn',
+        'prefer-destructuring': 'warn',
         '@angular-eslint/directive-selector': [
           'error',
           {
