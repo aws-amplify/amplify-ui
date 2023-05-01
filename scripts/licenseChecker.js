933b55807c1a53768d2be8ff0318c1e152782995
Chenwei Zhang <chenwz@amazon.com>
2023-05-01 12:02:41 -0700

    chore: address comments

--- scripts/licenseChecker.js
+++ scripts/licenseChecker.js
@@ -1,43 +1,71 @@
 const checker = require('license-checker');
 const path = require('path');
 
-const prohibitedLicenses = [
-  'AGPL-1.0',
-  'AGPL-3.0',
-  'APSL-2.0',
-  'CDLA-1.0',
-  'CPAL-1.0',
-  'MIT-enna',
-  'EUPL-1.1',
-  'EUPL-1.2',
-  'LGPL-3.0',
-  'GPL-3.0',
-  'HPL-1.0',
-  'NOSA-1.3',
-  'ODbL',
-  'OSL-3.0',
-  'Parity-7.0',
-  'RPSL-1.0',
-  'SSPL-1.0',
-  'BSL-1.1',
-  'Commons-Clause',
-  'CRAPL',
-  'CC-BY-NC-1.0',
-  'CC-BY-NC-2.0',
-  'CC-BY-NC-2.5',
-  'CC-BY-NC-3.0',
-  'CC-BY-NC-4.0',
-  'ELv2',
-  'Prosperity-3.0',
-  'RSALv1',
-  "UC Berkeley's Standard Copyright and Disclaimer Notice",
-  'University of Wisconsin Web Cache Simulator License',
+// A list of approved license
+const approvedLicenses = [
+  '0BSD',
+  'Apache-2.0',
+  'Boost',
+  'BouncyCastle',
+  'BSD-1-Clause',
+  'BSD-2-Clause-FreeBSD',
+  'BSD-2-Clause',
+  'BSD-3-Clause-Attribution',
+  'BSD-3-Clause',
+  'BSD-Source-Code',
+  'BSD',
+  'bzip2',
+  'CC-BY-3.0',
+  'CC-BY-4.0',
+  'CC0-1.0',
+  'curl',
+  'EDL',
+  'HDF5',
+  'ISC',
+  'JSON',
+  'libjpeg',
+  'libpng',
+  'MIT',
+  'MPL-2.0',
+  'NTP',
+  'OFL-1.0',
+  'OLDAP-2.8',
+  'OpenSSL',
+  'PDDL-1.0',
+  'PIL',
+  'PostgreSQL',
+  'Python-2.0',
+  'Python-2.1.1',
+  'Spencer-94',
+  'SQLite',
+  'Unicode-DFS-2015',
+  'Unicode-DFS-2016',
+  'Unlicense',
+  'WTFPL-2.0',
+  'WTFPL',
+  'X11',
+  'zlib-acknowledgement',
+  'Zlib',
+];
+
+// Excluding packages that are either unlicensed or license cannot be inferred correctly
+const excludePackages = [
+  // This is MIT licensed but cannot infer correclty
+  // https://github.com/mapbox/jsonlint
+  '@mapbox/jsonlint-lines-primitives@2.0.2',
+  // This is MIT licensed but cannot infer correclty
+  // https://github.com/tecfu/breakword
+  'breakword@1.0.5',
+  // This package does not exist in Github but still available in npm without being licensed
+  // https://www.npmjs.com/package/buffers
+  'buffers@0.1.1',
 ];
 
 checker.init(
   {
     start: path.resolve(__dirname, '..'),
-    failOn: prohibitedLicenses.join(';'),
+    onlyAllow: approvedLicenses.join(';'),
+    excludePackages: excludePackages.join(';'),
     excludePrivatePackages: true,
   },
   function (error) {
