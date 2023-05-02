cf36bda86a2321d0320b22b32ed53ab46ef59223
Caleb Pollman <cpollman@amazon.com>
2023-05-02 09:52:28 -0700

    fix(ui): add event callbacks to defaultAuthHubHandler to fix stale authStatus values

--- packages/react-core/src/Authenticator/context/AuthenticatorProvider.tsx
+++ packages/react-core/src/Authenticator/context/AuthenticatorProvider.tsx
@@ -1,15 +1,43 @@
 import React, { ReactNode, useContext, useEffect, useMemo } from 'react';
 import { useInterpret } from '@xstate/react';
 
-import { createAuthenticatorMachine, listenToAuthHub } from '@aws-amplify/ui';
+import {
+  AuthStatus,
+  AuthMachineHubHandler,
+  createAuthenticatorMachine,
+  defaultAuthHubHandler,
+  listenToAuthHub,
+} from '@aws-amplify/ui';
 
 import { AuthenticatorContext } from './AuthenticatorContext';
+import { Auth } from 'aws-amplify';
+
+type Options = Parameters<AuthMachineHubHandler>[2];
+
+const createHubHandler =
+  (options: Options): AuthMachineHubHandler =>
+  async (data, service) => {
+    await defaultAuthHubHandler(data, service, options);
+  };
 
 export default function AuthenticatorProvider({
   children,
 }: {
   children: ReactNode;
 }): JSX.Element {
+  const [authStatus, setAuthStatus] = React.useState<AuthStatus>('configuring');
+
+  React.useEffect(() => {
+    Auth.currentAuthenticatedUser()
+      .then((user: { username?: string }) => {
+        if (user?.username) {
+          setAuthStatus('authenticated');
+        }
+      })
+      // prevent `Auth.currentAuthenticatedUser` from logging an error if `user` is `undefined`
+      .catch(() => null);
+  }, []);
+
   /**
    * Based on use cases, developer might already have added another Provider
    * outside Authenticator. In that case, we sync the two providers by just
@@ -21,14 +49,24 @@ export default function AuthenticatorProvider({
   const service = useInterpret(createAuthenticatorMachine);
 
   const value = useMemo(
-    () => (!parentProviderVal ? { service } : parentProviderVal),
-    [parentProviderVal, service]
+    () => (!parentProviderVal ? { authStatus, service } : parentProviderVal),
+    [authStatus, parentProviderVal, service]
   );
 
   const { service: activeService } = value;
 
   useEffect(() => {
-    const unsubscribe = listenToAuthHub(activeService);
+    const onSignIn = () => {
+      setAuthStatus('authenticated');
+    };
+    const onSignOut = () => {
+      setAuthStatus('unauthenticated');
+    };
+
+    const unsubscribe = listenToAuthHub(
+      activeService,
+      createHubHandler({ onSignIn, onSignOut })
+    );
     return unsubscribe;
   }, [activeService]);
 
