596a7bbb6f8c78c2efc3047fc1481bf14b3a11a4
wlee221 <willeea@amazon.com>
2023-05-01 18:15:55 -0700

    Remove empty function

--- packages/angular/.eslintrc.js
+++ packages/angular/.eslintrc.js
@@ -22,7 +22,6 @@ module.exports = {
          */
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/member-ordering': 'off',
-        '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-unsafe-argument': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
