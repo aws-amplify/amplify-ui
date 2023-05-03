6abc3063e11f061ac2c3de4bc063ff650322d6af
Scott Rees <6165315+reesscot@users.noreply.github.com>
2023-05-03 11:26:33 -0700

    chore: fix image width

--- docs/src/pages/[platform]/getting-started/usage/vite/examples/BasicExampleCode.tsx
+++ docs/src/pages/[platform]/getting-started/usage/vite/examples/BasicExampleCode.tsx
@@ -12,8 +12,8 @@ function App() {
       <Image
         alt="Abstract art"
         height="21rem"
+        objectFit="cover"
         src="https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987"
-        width="100%"
       />
       <Flex justifyContent="space-between" direction="column">
         <Heading level={3}>Abstract art</Heading>
