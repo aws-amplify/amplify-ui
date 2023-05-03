6ea653a1005a42ea95996a2669d345c07fe433a0
William Lee <43682783+wlee221@users.noreply.github.com>
2023-05-02 17:06:27 -0700

    Update packages/react-core/src/Authenticator/context/AuthenticatorProvider.tsx

--- packages/react-core/src/Authenticator/context/AuthenticatorProvider.tsx
+++ packages/react-core/src/Authenticator/context/AuthenticatorProvider.tsx
@@ -26,7 +26,7 @@ export default function AuthenticatorProvider({
   children: ReactNode;
 }): JSX.Element {
   // `authStatus` is exposed by `useAuthenticator` but should not be derived directly from the
-  // state machine as the machne only updates on `Authenticator` initiated events, which
+  // state machine as the machine only updates on `Authenticator` initiated events, which
   // leads to scenarios where the state machine `authStatus` gets "stuck". For exmample,
   // if a user was to sign in using `Auth.signIn` directly rather than using `Authenticator`
   const [authStatus, setAuthStatus] = React.useState<AuthStatus>('configuring');
