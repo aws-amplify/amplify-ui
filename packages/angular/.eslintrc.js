105e8ed9c3a9c5b09988557a73c0b4d39cc20043
wlee221 <willeea@amazon.com>
2023-05-03 19:29:47 -0700

    Remove resolved eslint rules

--- packages/angular/.eslintrc.js
+++ packages/angular/.eslintrc.js
@@ -22,9 +22,6 @@ module.exports = {
          */
         '@typescript-eslint/explicit-module-boundary-types': 'warn',
         '@typescript-eslint/member-ordering': 'warn',
-        '@typescript-eslint/no-unsafe-argument': 'warn',
-        '@typescript-eslint/no-unsafe-assignment': 'warn',
-        '@typescript-eslint/no-unsafe-member-access': 'warn',
 
         /*
          * `prefer-nullish-coalescing` requires `strictNullChecks` to be on.
