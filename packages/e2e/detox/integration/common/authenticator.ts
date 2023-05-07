c79ae8edc013b3f45d02564ab5565e588882537c
Ioana Brooks <tryioana@amazon.com>
2023-05-06 17:41:06 -0700

    Increase ram and heap

--- packages/e2e/detox/integration/common/authenticator.ts
+++ packages/e2e/detox/integration/common/authenticator.ts
@@ -92,7 +92,7 @@ When('I type a new {string}', async (field: string) => {
 });
 
 When('I select my country code with status {string}', (status: string) => {
-  // do nothing,  React-Native phonenumber field does not support country code selection yet
+  // do nothing, React-Native phone number field does not support country code selection yet
 });
 
 Then('I will be redirected to the confirm forgot password page', async () => {
