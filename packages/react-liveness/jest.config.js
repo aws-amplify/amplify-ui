2a439bdd7874603a6b6f60927a2f4bed4a37a3d9
Thadd Trinh <thadtrin@amazon.com>
2023-05-11 13:14:03 -0700

    chore: update coverage numbers

--- packages/react-liveness/jest.config.js
+++ packages/react-liveness/jest.config.js
@@ -6,9 +6,9 @@ module.exports = {
   coverageThreshold: {
     global: {
       branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      functions: 85,
+      lines: 89,
+      statements: 89,
     },
   },
   globals: { 'ts-jest': { tsconfig: 'tsconfig.json' } },
