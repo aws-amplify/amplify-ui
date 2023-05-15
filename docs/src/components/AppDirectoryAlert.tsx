d123db5a795cba3f04b3d626599b7bc8aa204919
Scott Rees <6165315+reesscot@users.noreply.github.com>
2023-05-15 14:54:07 -0700

    chore: move alert to tsx

---
+++ docs/src/components/AppDirectoryAlert.tsx
@@ -0,0 +1,30 @@
+import { Alert, Link, Text } from '@aws-amplify/ui-react';
+
+export const AppDirectoryAlert = () => (
+  <Alert variation="info" role="none">
+    <Text>
+      Next.js 13.4+ introduces{' '}
+      <Link href="https://nextjs.org/docs/app/building-your-application/routing#the-app-directory">
+        app directory
+      </Link>{' '}
+      with the usage of{' '}
+      <Link href="https://nextjs.org/docs/getting-started/react-essentials#server-components">
+        Server Components.
+      </Link>{' '}
+      Amplify UI components are interactive and designed to work on the client
+      side. To use them inside of Server Components you must wrap them in a
+      Client Component with <code>&quot;use client&quot;</code>. See{' '}
+      <Link href="/react/getting-started/usage/nextjs#app-router">
+        Using Next.js App Router with Amplify UI
+      </Link>
+      . <br />
+    </Text>
+    <Text>
+      If you are using Next.js{' '}
+      <Link href="https://nextjs.org/docs/app/building-your-application/routing#the-app-directory">
+        pages directory,
+      </Link>{' '}
+      no changes are required to use Amplify UI components.
+    </Text>
+  </Alert>
+);
