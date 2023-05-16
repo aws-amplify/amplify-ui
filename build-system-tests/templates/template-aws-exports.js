4c938c33c2f466d679f88bed7b759ca41764925d
0618 <o.6180339@gmail.com>
2023-05-15 18:15:36 -0700

    fix check-react-native-log

--- build-system-tests/templates/template-aws-exports.js
+++ build-system-tests/templates/template-aws-exports.js
@@ -1,11 +1,5 @@
 /* eslint-disable */
 
-const awsmobile = {
-  aws_project_region: 'us-east-2',
-  aws_cognito_identity_pool_id: 'us-east-2:adsf',
-  aws_cognito_region: 'us-east-2',
-  aws_user_pools_id: 'us-east-2_asdf',
-  aws_user_pools_web_client_id: 'asdf',
-};
+const awsmobile = {};
 
 export default awsmobile;
