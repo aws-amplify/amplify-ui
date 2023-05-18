4da6e9fed6629cbd2897d960692551e83c750ae0
0618 <o.6180339@gmail.com>
2023-05-18 12:42:37 -0700

    clean up checkReactNativelog

--- .github/scripts/checkReactNativeLog.js
+++ .github/scripts/checkReactNativeLog.js
@@ -39,14 +39,16 @@ const checkReactNativeLog = () => {
     'React Native iOS Logger started for XCode project',
   ];
 
-  console.log('\x1b[1;36mChecking log file for start messages...\n\x1b[0m');
+  console.log(
+    `${colors.blueBold}Checking log file for start messages...${colors.colorReset}}`
+  );
   if (logLines.length === 1) {
     for (const startMessage of startMessages) {
       if (logLines[0].includes(startMessage)) {
         console.error(
-          'Failed to get the logging messages. Please increase TIME_TO_WAIT.'
+          `${colors.redBold}Failed to get the logging messages. Please increase TIME_TO_WAIT.${colors.colorReset}`
         );
-        console.log('\x1b[1;36mFull log:\x1b[0m');
+        console.log(`${colors.blueBold}Full log:${colors.colorReset}`);
         console.log(logFile);
         process.exit(1);
       }
@@ -56,20 +58,18 @@ const checkReactNativeLog = () => {
   let hasError = false;
 
   console.log(
-    '\x1b[1;36mChecking log file',
-    process.env.LOG_FILE,
-    'for errors...\x1b[0m'
+    `${colors.blueBold}Checking log file ${process.env.LOG_FILE} for errors...${colors.colorReset}`
   );
 
   for (const line of logLines) {
     if (line === '') {
       continue;
     } else if (line.match(/(([0-9]{2}:){2}[0-9]{2},?).*(Error|ERROR)/)) {
-      console.error('ERROR found:');
+      console.error(`${colors.redBold}ERROR found:${colors.colorReset}`);
       console.error(line);
       hasError = true;
     } else if (line.match(/(([0-9]{2}:){2}[0-9]{2},?).*(fail)/)) {
-      console.error('\x1b[1;31mfail found:\x1b[0m');
+      console.error(`${colors.redBold}fail found:${colors.colorReset}`);
       console.error(line);
       hasError = true;
     } else if (
@@ -77,7 +77,7 @@ const checkReactNativeLog = () => {
         /(([0-9]{2}:){2}[0-9]{2},?).*(Could not connect to development server)/
       )
     ) {
-      console.error('\x1b[1;31mConnection error:\x1b[0m');
+      console.error(`${colors.redBold}Connection error:${colors.colorReset}`);
       console.error(line);
       hasError = true;
     }
@@ -85,19 +85,22 @@ const checkReactNativeLog = () => {
     let isException = false;
     for (const exception of exceptions) {
       if (line.includes(exception)) {
-        console.warn('\x1b[1;33mException found:\x1b[0m');
+        console.warn(
+          `${colors.yellowBold}Exception found:${colors.colorReset}`
+        );
         console.warn(line);
         isException = true;
         break;
       }
     }
 
-    if (
-      !isException &&
-      line.match(
-        /(([0-9]{2}:){2}[0-9]{2},?).*(Error|ERROR|fail|Could not connect to development server)/
-      )
-    ) {
+    /*
+     * `errorRegex` finds a line that starts with NN:NN:NN (N is a number)
+     * and contains the word 'Error' or 'fail' or 'Could not connect to development server'
+     */
+    const errorRegex =
+      '/(([0-9]{2}:){2}[0-9]{2},?).*(Error|ERROR|fail|Could not connect to development server)/';
+    if (!isException && line.match(errorRegex)) {
       hasError = true;
     }
   }
@@ -106,26 +109,22 @@ const checkReactNativeLog = () => {
     console.error(
       `${colors.redBold}Errors found in log file ${process.env.LOG_FILE}${colors.colorReset}`
     );
-    console.log('\x1b[1;36mFull log:\x1b[0m');
+    console.log(`${colors.blueBold}Full log:${colors.colorReset}`);
     console.log(logFile);
     process.exit(1);
   } else if (logFile === '') {
     console.error(
-      '\x1b[1;31mlog file',
-      process.env.LOG_FILE,
-      'is empty:\x1b[0m'
+      `${colors.redBold}Log file ${process.env.LOG_FILE} is empty.${colors.colorReset}`
     );
-    console.log('\x1b[1;36mFull log:\x1b[0m');
+    console.log(`${colors.blueBold}Full log:${colors.colorReset}`);
     console.log(logFile);
     process.exit(1);
   }
 
-  console.log('\x1b[1;36mFull log:\x1b[0m');
+  console.log(`${colors.greenBold}Full log:${colors.colorReset}`);
   console.log(logFile);
   console.log(
-    '\x1b[1;32mNo errors found in log file',
-    process.env.LOG_FILE,
-    '. Exiting successfully.\x1b[0m'
+    `${colors.greenBold}No errors found in log file ${process.env.LOG_FILE}${colors.colorReset}`
   );
   process.exit(0);
 };
