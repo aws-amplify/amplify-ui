f16e4bda80e593252472f5aaed5af49d2f726b04
thaddmt <68032955+thaddmt@users.noreply.github.com>
2023-05-22 13:01:37 -0700

    feat(liveness): remove 3 second countdown from liveness check (#3862)
    * feat(liveness): remove 3 second countdown from liveness check
    
    * Create angry-ladybugs-sing.md
    
    * fix: fix e2e tests after removing countdown
    
    * chore: fix unit tests
    
    * chore: update text and add additional unit tests
    
    * remove testing code
    
    * fix unused var
    
    * Update packages/react-liveness/src/components/FaceLivenessDetector/service/machine/index.ts
    
    Co-authored-by: William Lee <43682783+wlee221@users.noreply.github.com>
    
    * Update packages/react-liveness/src/components/FaceLivenessDetector/service/machine/index.ts
    
    Co-authored-by: William Lee <43682783+wlee221@users.noreply.github.com>
    
    * add yarn.lock
    
    * chore: fix xstate conditionals
    
    ---------
    
    Co-authored-by: William Lee <43682783+wlee221@users.noreply.github.com>

--- packages/react-liveness/src/components/FaceLivenessDetector/shared/Hint.tsx
+++ packages/react-liveness/src/components/FaceLivenessDetector/shared/Hint.tsx
@@ -56,7 +56,6 @@ export const Hint: React.FC<HintProps> = ({ hintDisplayText }) => {
   );
   const isRecording = state.matches('recording');
   const isNotRecording = state.matches('notRecording');
-  const isWaitingForSessionInfo = state.matches('waitForSessionInfo');
   const isUploading = state.matches('uploading');
   const isCheckSuccessful = state.matches('checkSucceeded');
   const isCheckFailed = state.matches('checkFailed');
@@ -103,12 +102,6 @@ export const Hint: React.FC<HintProps> = ({ hintDisplayText }) => {
       }
 
       if (isNotRecording) {
-        return (
-          <Toast>{hintDisplayText.hintHoldFacePositionCountdownText}</Toast>
-        );
-      }
-
-      if (isWaitingForSessionInfo) {
         return (
           <Toast>
             <Flex className={LivenessClassNames.HintText}>
