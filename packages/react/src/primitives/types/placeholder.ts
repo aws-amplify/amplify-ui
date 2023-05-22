0fb611262b34072f1f4e2dd385b090f507c542cf
paras11917 <70134657+paras11917@users.noreply.github.com>
2023-05-23 03:42:14 +0530

    Update placeholder.ts

--- packages/react/src/primitives/types/placeholder.ts
+++ packages/react/src/primitives/types/placeholder.ts
@@ -1,5 +1,8 @@
+import { Property } from 'csstype';
 import { Sizes } from './base';
 import { ViewProps } from './view';
+
+import type { ColorKeys } from './theme';
 
 export type PlaceholderSizes = Sizes;
 
@@ -17,4 +20,16 @@ export interface PlaceholderProps extends ViewProps {
    * Controls the display size of placeholder
    */
   size?: PlaceholderSizes;
+
+  /**
+   * @description
+   * Controls the start color of placeholder
+   */
+  startColor?: ColorKeys<Property.BackgroundColor>;
+
+  /**
+   * @description
+   * Controls the end color of placeholder
+   */
+  endColor?: ColorKeys<Property.BackgroundColor>;
 }
