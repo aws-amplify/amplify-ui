857308892cced8229e1673d9a7538bef655181ea
Thadd Trinh <thadtrin@amazon.com>
2023-05-31 14:44:07 -0700

    added integration test for accepting all file types

---
+++ examples/next/pages/ui/components/storage/storage-manager/accept-all-files/index.page.tsx
@@ -0,0 +1,14 @@
+import { Amplify } from 'aws-amplify';
+import { StorageManager } from '@aws-amplify/ui-react-storage';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export function StorageManagerExample() {
+  return (
+    <>
+      <StorageManager accessLevel="public" maxFileCount={1} showThumbnails />
+    </>
+  );
+}
+export default StorageManagerExample;
