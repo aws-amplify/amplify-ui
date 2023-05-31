38435c9b6403a884b9b98cf4270c2ae9116f7bee
Thadd Trinh <thadtrin@amazon.com>
2023-05-31 14:10:12 -0700

    update docs

--- docs/src/pages/[platform]/connected-components/storage/storagemanager/props.ts
+++ docs/src/pages/[platform]/connected-components/storage/storagemanager/props.ts
@@ -20,11 +20,6 @@ const eventHandler = `(file: {key: string}) => void;`;
 
 export const STORAGE_MANAGER = [
   {
-    name: `acceptedFileTypes`,
-    description: 'List of accepted file types',
-    type: `string[]`,
-  },
-  {
     name: `accessLevel`,
     description:
       'Access level for files in Storage. See https://docs.amplify.aws/lib/storage/configureaccess/q/platform/js/',
@@ -34,6 +29,12 @@ export const STORAGE_MANAGER = [
     name: `maxFileCount`,
     description: '',
     type: 'integer',
+  },
+  {
+    name: `acceptedFileTypes?`,
+    description:
+      "List of accepted file types, values of `['*']` or undefined allow any files",
+    type: `string[]`,
   },
   {
     name: `maxFileSize?`,
