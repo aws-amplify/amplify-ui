b6b8ef51ff7a86e92be7df596b26df31f50cdc7b
Thadd Trinh <thadtrin@amazon.com>
2023-06-02 11:07:47 -0700

    fix: remove mjs entry points for new categorical packages

--- packages/react-geo/rollup.config.js
+++ packages/react-geo/rollup.config.js
@@ -31,7 +31,6 @@ const config = defineConfig([
     output: {
       dir: 'dist/esm',
       format: 'es',
-      entryFileNames: '[name].mjs',
       preserveModules: true,
       preserveModulesRoot: 'src',
     },
