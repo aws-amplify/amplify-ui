bbc76de6755794bfd1e438e25236592d93ec95b7
0618 <o.6180339@gmail.com>
2023-06-06 11:37:01 -0700

    test(build system): next release

--- .github/scripts/mega-app-install-build.sh
+++ .github/scripts/mega-app-install-build.sh
@@ -48,8 +48,8 @@ else
         if [[ "$BUILD_TOOL" == 'angular-lib' ]]; then
             echo "cd projects/my-amplify-ui-lib/"
             cd projects/my-amplify-ui-lib/
-            echo "npm install --save-peer @aws-amplify/ui-angular aws-amplify"
-            npm install --save-peer @aws-amplify/ui-angular aws-amplify
+            echo "npm install --save-peer @aws-amplify/ui-angular@next-release aws-amplify"
+            npm install --save-peer @aws-amplify/ui-angular@next-release aws-amplify
             echo "cd -"
             cd -
             echo "npm install $DEPENDENCIES"
