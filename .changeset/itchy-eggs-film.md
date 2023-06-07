16f81fb3b44f12df1cc8983af514eff20dbd0794
William Lee <43682783+wlee221@users.noreply.github.com>
2023-06-06 23:54:54 -0700

    chore(next-release): revert Angular changes (#4041)
    * Revert "fix(next-release): fix angular canary (#4035)"
    
    This reverts commit 78a0442fde3a5b7605e82be70f2368f460620c74.
    
    * Revert "chore: deprecate Angular 13 and below (#3861)"
    
    This reverts commit a46daca69655c56ac6146f0e5c5d6b489267d3fb.

--- .changeset/itchy-eggs-film.md
+++
@@ -1,5 +0,0 @@
----
-"@aws-amplify/ui-angular": major
----
-
-**Breaking**: bump minimum Angular version to 14. If your application is on Angular 13 or older, please migrate to Angular 14+ as per [official guide](https://update.angular.io/).
--- canary/apps/angular/angularcli/src/test.ts
+++ canary/apps/angular/angularcli/src/test.ts
@@ -21,10 +21,7 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
-  {
-    teardown: { destroyAfterEach: false },
-  }
+  platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
