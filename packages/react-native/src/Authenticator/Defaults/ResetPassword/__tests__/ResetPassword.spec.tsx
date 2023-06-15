156d9a51059b94da865aca1dd2d0e60dfd784c0e
Ioana Brooks <tryioana@amazon.com>
2023-06-15 11:09:28 -0700

    lint

--- packages/react-native/src/Authenticator/Defaults/ResetPassword/__tests__/ResetPassword.spec.tsx
+++ packages/react-native/src/Authenticator/Defaults/ResetPassword/__tests__/ResetPassword.spec.tsx
@@ -21,6 +21,7 @@ const props = {
   handleBlur: jest.fn(),
   handleChange: jest.fn(),
   handleSubmit: jest.fn(),
+  hasValidationErrors: false,
   Header: ResetPassword.Header,
   isPending: false,
   toSignIn: jest.fn(),
