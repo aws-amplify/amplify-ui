11f3221a1ce012b07df706f449c34c4e91d1940c
Thadd Trinh <thadtrin@amazon.com>
2023-06-30 12:53:52 -0700

    chore(liveness): move inline css to classes

--- packages/react-liveness/src/components/FaceLivenessDetector/shared/FaceLivenessErrorModal.tsx
+++ packages/react-liveness/src/components/FaceLivenessDetector/shared/FaceLivenessErrorModal.tsx
@@ -8,6 +8,7 @@ import { LivenessErrorState } from '../service';
 import { Toast } from './Toast';
 import { Overlay } from './Overlay';
 import { defaultErrorDisplayText, ErrorDisplayText } from '../displayText';
+import { LivenessClassNames } from '../types/classNames';
 
 export interface CheckScreenComponents {
   ErrorView?: React.ComponentType<FaceLivenessErrorModalProps>;
@@ -60,14 +61,11 @@ const renderToastErrorModal = (props: {
 
   return (
     <>
-      <Flex
-        gap="xs"
-        alignItems="center"
-        justifyContent="center"
-        color="font.error"
-      >
+      <Flex className={LivenessClassNames.ErrorModal}>
         <AlertIcon ariaHidden variation="error" />
-        <Text fontWeight="bold">{heading}</Text>
+        <Text className={`${LivenessClassNames.ErrorModal}__heading`}>
+          {heading}
+        </Text>
       </Flex>
       {message}
     </>
@@ -113,7 +111,7 @@ export const FaceLivenessErrorModal: React.FC<FaceLivenessErrorModalProps> = (
   const { tryAgainText } = displayText;
 
   return (
-    <Overlay backgroundColor="overlay.40">
+    <Overlay className={LivenessClassNames.OpaqueOverlay}>
       <Toast>
         {children}
         <Flex justifyContent="center">
--- packages/react-liveness/src/components/FaceLivenessDetector/shared/Toast.tsx
+++ packages/react-liveness/src/components/FaceLivenessDetector/shared/Toast.tsx
@@ -17,18 +17,12 @@ export const Toast: React.FC<ToastProps> = ({
 }) => {
   return (
     <View
-      padding="small"
-      borderRadius="medium"
       className={`${LivenessClassNames.Toast} ${LivenessClassNames.Toast}--${variation} ${LivenessClassNames.Toast}--${size}`}
       maxWidth={{ base: '100%', small: '70%' }}
       {...rest}
     >
-      <Flex gap="xs" direction="column" alignItems="center">
-        <Flex
-          className={`${LivenessClassNames.Toast}__message`}
-          textAlign="center"
-          direction="column"
-        >
+      <Flex className={`${LivenessClassNames.Toast}__container`}>
+        <Flex className={`${LivenessClassNames.Toast}__message`}>
           {children}
         </Flex>
       </Flex>
