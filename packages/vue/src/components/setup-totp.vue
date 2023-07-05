fd29d60dda92fdce584cb1d9afc9a0ecbfcfb0ba
William Lee <willeea@amazon.com>
2023-07-05 16:54:45 -0700

    add confirm sign in refactor + tests

--- packages/vue/src/components/setup-totp.vue
+++ packages/vue/src/components/setup-totp.vue
@@ -16,6 +16,7 @@ import BaseFormFields from './primitives/base-form-fields.vue';
 
 const logger = new Logger('SetupTOTP-logger');
 
+// `useAuthenticator` is casted for temporary type safety on this file.
 const props = useAuthenticator() as UseAuthenticator;
 const { updateForm, submitForm, toSignIn } = props;
 const { user, totpSecretCode, isPending, error, QRFields } = toRefs(props);
--- packages/vue/src/composables/__mock__/useAuthenticatorMock.ts
+++ packages/vue/src/composables/__mock__/useAuthenticatorMock.ts
@@ -1,27 +1,29 @@
 import { AuthenticatorServiceFacade } from '@aws-amplify/ui';
+import { UseAuthenticator } from '../../types';
 
-export const baseMockServiceFacade: AuthenticatorServiceFacade = {
+export const baseMockServiceFacade: UseAuthenticator = {
   authStatus: 'authenticated',
   codeDeliveryDetails: {} as AuthenticatorServiceFacade['codeDeliveryDetails'],
   error: undefined as unknown as AuthenticatorServiceFacade['error'],
   hasValidationErrors: false,
-  isPending: false,
-  route: 'idle',
-  socialProviders: [],
-  unverifiedContactMethods: { email: 'test#example.com' },
-  user: {} as AuthenticatorServiceFacade['user'],
-  validationErrors:
-    {} as unknown as AuthenticatorServiceFacade['validationErrors'],
-  totpSecretCode: null,
   initializeMachine: jest.fn(),
+  isPending: false,
+  QRFields: null,
   resendCode: jest.fn(),
+  route: 'idle',
   signOut: jest.fn(),
+  skipVerification: jest.fn(),
+  socialProviders: [],
   submitForm: jest.fn(),
-  updateForm: jest.fn(),
-  updateBlur: jest.fn(),
   toFederatedSignIn: jest.fn(),
   toResetPassword: jest.fn(),
   toSignIn: jest.fn(),
   toSignUp: jest.fn(),
-  skipVerification: jest.fn(),
+  totpSecretCode: null,
+  unverifiedContactMethods: { email: 'test#example.com' },
+  updateBlur: jest.fn(),
+  updateForm: jest.fn(),
+  user: {} as AuthenticatorServiceFacade['user'],
+  validationErrors:
+    {} as unknown as AuthenticatorServiceFacade['validationErrors'],
 };
