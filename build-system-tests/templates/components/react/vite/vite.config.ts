56f630b2799b2bbfbd15bb9b3660f6bbf6654c22
MJ Zhang <0618@users.noreply.github.com>
2023-07-12 15:44:46 -0700

    test: Build System Test (#4139)
    Co-authored-by: William Lee <43682783+wlee221@users.noreply.github.com>
    Co-authored-by: 0618 <o.6180339@gmail.com>
    Co-authored-by: Ioana Brooks <68251134+ioanabrooks@users.noreply.github.com>
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

---
+++ build-system-tests/templates/components/react/vite/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: './runtimeConfig',
+        replacement: './runtimeConfig.browser',
+      },
+    ],
+  },
+});
