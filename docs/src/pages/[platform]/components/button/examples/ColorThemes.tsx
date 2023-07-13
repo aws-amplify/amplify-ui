91dec653f3dbbd8f66a0b52f538c19f1df00e8f0
Heather Buchel <buchel@amazon.com>
2023-07-13 17:04:49 -0400

    add docs example for validating

--- docs/src/pages/[platform]/components/button/examples/ColorThemes.tsx
+++ docs/src/pages/[platform]/components/button/examples/ColorThemes.tsx
@@ -1,17 +1,46 @@
 import * as React from 'react';
 import { Button, Flex } from '@aws-amplify/ui-react';
 
-export const IconButtonExample = () => {
+export const ColorThemes = () => {
   return (
     <Flex direction="column" gap="medium">
       <Flex>
         <Button>default</Button>
-        <Button>info</Button>
-        <Button>success</Button>
-        <Button>error</Button>
-        <Button>war</Button>
+        <Button colorTheme="info">info</Button>
+        <Button colorTheme="success">success</Button>
+        <Button colorTheme="error">error</Button>
+        <Button colorTheme="warning">warning</Button>
       </Flex>
-      <Button></Button>
+      <Flex>
+        <Button variation="primary">default</Button>
+        <Button variation="primary" colorTheme="info">
+          info
+        </Button>
+        <Button variation="primary" colorTheme="success">
+          success
+        </Button>
+        <Button variation="primary" colorTheme="error">
+          error
+        </Button>
+        <Button variation="primary" colorTheme="warning">
+          warning
+        </Button>
+      </Flex>
+      <Flex>
+        <Button variation="link">default</Button>
+        <Button variation="link" colorTheme="info">
+          info
+        </Button>
+        <Button variation="link" colorTheme="success">
+          success
+        </Button>
+        <Button variation="link" colorTheme="error">
+          error
+        </Button>
+        <Button variation="link" colorTheme="warning">
+          warning
+        </Button>
+      </Flex>
     </Flex>
   );
 };
--- docs/src/pages/[platform]/components/button/examples/index.ts
+++ docs/src/pages/[platform]/components/button/examples/index.ts
@@ -1,2 +1,3 @@
 export { IconButtonExample } from './iconButton';
 export { ButtonThemeExample } from './ButtonThemeExample';
+export { ColorThemes } from './ColorThemes';
