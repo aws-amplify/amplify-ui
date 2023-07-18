c2ad3fde1d07ebab990f3fa261002d8bc4a5c6b3
Caleb Pollman <cpollman@amazon.com>
2023-07-17 18:15:02 -0700

    chore(ui-react-core): improve internal documentation of useHasValueUpdated

--- packages/react-core/src/hooks/useHasValueUpdated.ts
+++ packages/react-core/src/hooks/useHasValueUpdated.ts
@@ -2,6 +2,19 @@ import { isUndefined } from '@aws-amplify/ui';
 
 import usePreviousValue from './usePreviousValue';
 
+/**
+ * @param value `value` to track for updates
+ * @param ignoreFirstRender whether to ignore initial render. defaults to `false`
+ * @returns a boolean representing whether the tracked `value` has updated between renders
+ *
+ * Returns `false`:
+ * - on initial render when ignoring first render
+ * - current and previous `value` are equal
+ *
+ * Returns `true`:
+ * - on initial render and not ignoring first render (default behavior)
+ * - current and previous `value` are not equal
+ */
 export default function useHasValueUpdated<Value>(
   value: Value,
   ignoreFirstRender = false
