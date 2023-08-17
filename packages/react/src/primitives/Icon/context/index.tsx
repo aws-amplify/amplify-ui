729cb7560cfd7e63cd1c1e767500da7a6adcf88b
Danny Banks <djb@amazon.com>
2023-08-16 22:31:49 -0700

    fixing

--- packages/react/src/primitives/Icon/context/index.tsx
+++ packages/react/src/primitives/Icon/context/index.tsx
@@ -1,19 +1,2 @@
-import * as React from 'react';
-import { IconsContext, IconsContextInterface } from './IconsContext';
+export { IconsProvider, IconsContextInterface } from './IconsProvider';
 export { useIcons } from './useIcons';
-
-interface IconProviderProps {
-  children: React.ReactNode;
-  icons?: IconsContextInterface;
-}
-
-export { IconsContextInterface };
-
-export function IconsProvider({
-  children,
-  icons,
-}: IconProviderProps): JSX.Element {
-  return (
-    <IconsContext.Provider value={icons}>{children}</IconsContext.Provider>
-  );
-}
