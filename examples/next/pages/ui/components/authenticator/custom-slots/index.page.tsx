4fa702e1d95e008c06e45453f9cede62ca13c605
Thadd Trinh <thadtrin@amazon.com>
2023-08-23 12:05:41 -0700

    chore: remove unused variable

--- examples/next/pages/ui/components/authenticator/custom-slots/index.page.tsx
+++ examples/next/pages/ui/components/authenticator/custom-slots/index.page.tsx
@@ -15,7 +15,7 @@ import {
 } from '@aws-amplify/ui-react';
 
 import awsExports from './aws-exports';
-import { AuthFormFields, FormFields } from '@aws-amplify/ui';
+import { AuthFormFields } from '@aws-amplify/ui';
 Amplify.configure(awsExports);
 
 const components = {
