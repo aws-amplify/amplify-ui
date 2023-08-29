78f183b66fa70073535dd770e6bc0e18787e4bf0
Danny Banks <djb@amazon.com>
2023-08-29 11:31:18 -0700

    one more

--- packages/react-storage/src/components/StorageManager/ui/DropZone/types.ts
+++ packages/react-storage/src/components/StorageManager/ui/DropZone/types.ts
@@ -3,7 +3,7 @@ import { StorageManagerDisplayText } from '../../utils/displayText';
 export interface DropZoneProps {
   children?: React.ReactNode;
   displayText: StorageManagerDisplayText;
-  inDropZone?: boolean;
+  inDropZone: boolean;
   onDragEnter: (event: React.DragEvent<HTMLDivElement>) => void;
   onDragLeave: (event: React.DragEvent<HTMLDivElement>) => void;
   onDragOver: (event: React.DragEvent<HTMLDivElement>) => void;
