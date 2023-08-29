9852e12e42cdd74ab6b149a334470a7c72151af5
Thadd Trinh <thadtrin@amazon.com>
2023-08-29 12:39:26 -0700

    fix unused variables

--- packages/react-liveness/src/components/FaceLivenessDetector/service/utils/__tests__/CustomWebscoketFetchHandler.test.ts
+++ packages/react-liveness/src/components/FaceLivenessDetector/service/utils/__tests__/CustomWebscoketFetchHandler.test.ts
@@ -40,7 +40,7 @@ describe(CustomWebSocketFetchHandler.name, () => {
 
     it('populates socket in socket pool based on handle() requests', async () => {
       const handler = new CustomWebSocketFetchHandler();
-      const server = new WS(mockUrl);
+      new WS(mockUrl);
 
       // @ts-expect-error Property 'sockets' is private and only accessible within class 'WebSocketHandler'.
       expect(handler.sockets[mockUrl]).not.toBeDefined();
@@ -72,7 +72,7 @@ describe(CustomWebSocketFetchHandler.name, () => {
 
     it('closes socket in socket pool on handler.destroy()', async () => {
       const handler = new CustomWebSocketFetchHandler();
-      const server = new WS(mockUrl);
+      new WS(mockUrl);
 
       await handler.handle(
         new HttpRequest({
@@ -113,6 +113,7 @@ describe(CustomWebSocketFetchHandler.name, () => {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         for await (const chunk of responsePayload) {
           /** pass */
+          continue;
         }
       } catch (err) {
         expect(err).toBeDefined();
