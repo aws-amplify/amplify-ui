41a7b4288930f9574d037c0543167b405d62d016
thaddmt <68032955+thaddmt@users.noreply.github.com>
2023-08-31 11:12:13 -0700

    chore(liveness): Custom websocket handler (#4371)
    * WIP
    
    * chore: websocket handler wip
    
    * chore(liveness): add custom websocket fetch handler
    
    * Create soft-singers-study.md
    
    * chore: address comments
    
    * chore: add tests and test coverage for custom websocket handler
    
    * fix unused variables
    
    * fix more unused variables

--- packages/react-liveness/src/components/FaceLivenessDetector/service/utils/__tests__/streamProvider.test.ts
+++ packages/react-liveness/src/components/FaceLivenessDetector/service/utils/__tests__/streamProvider.test.ts
@@ -2,6 +2,7 @@
 import 'web-streams-polyfill';
 import 'blob-polyfill';
 
+import { TextDecoder } from 'util';
 import { Amplify } from '@aws-amplify/core';
 import { RekognitionStreamingClient } from '@aws-sdk/client-rekognitionstreaming';
 import { LivenessStreamProvider } from '../streamProvider';
@@ -12,6 +13,11 @@ import { AwsCredentialProvider } from '../../types';
 jest.mock('../videoRecorder');
 jest.mock('@aws-sdk/client-rekognitionstreaming');
 jest.mock('@aws-amplify/core');
+
+Object.defineProperty(window, 'TextDecoder', {
+  writable: true,
+  value: TextDecoder,
+});
 
 const mockGet = jest.fn().mockImplementation(() => {
   return {
@@ -244,7 +250,7 @@ describe('LivenessStreamProvider', () => {
     });
   });
 
-  describe('endStream', () => {
+  describe('endStreamWithCode', () => {
     test('should stop video and end the stream and return a promise if cancelled successfully', async () => {
       const provider = new LivenessStreamProvider({
         sessionId: 'sessionId',
@@ -252,7 +258,7 @@ describe('LivenessStreamProvider', () => {
         stream: mockVideoMediaStream,
         videoEl: mockVideoEl,
       });
-      const response = await provider.endStream();
+      const response = await provider.endStreamWithCode();
 
       expect(mockVideoRecorder.stop).toHaveBeenCalled();
       expect(response).toBeUndefined();
@@ -266,7 +272,7 @@ describe('LivenessStreamProvider', () => {
         videoEl: mockVideoEl,
       });
       (provider as any)._reader = undefined;
-      const response = await provider.endStream();
+      const response = await provider.endStreamWithCode();
 
       expect(mockVideoRecorder.stop).toHaveBeenCalled();
       expect(response).toBeUndefined();
