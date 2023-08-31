2aa5d67c94e797982da50357ed839ffae667a00d
Scott Rees <6165315+reesscot@users.noreply.github.com>
2023-08-30 17:10:43 -0700

    chore: remove unneeded studio hooks

--- packages/react/src/internal.ts
+++ packages/react/src/internal.ts
@@ -1,36 +1,11 @@
 export * from './hooks/useAuth';
-export * from './hooks/useDataStore';
 export * from './hooks/useStorageURL';
 export * from './hooks/useThemeBreakpoint';
-export {
-  UseAuthSignOutAction,
-  useAuthSignOutAction,
-} from './hooks/actions/useAuthSignOutAction';
-export {
-  useNavigateAction,
-  UseNavigateActionOptions,
-} from './hooks/actions/useNavigateAction';
-export { useStateMutationAction } from './hooks/actions/useStateMutationAction';
-
-export { useDataStoreCreateAction } from './hooks/actions/useDataStoreCreateAction';
-export type { UseDataStoreCreateActionOptions } from './hooks/actions/useDataStoreCreateAction';
-export { useDataStoreDeleteAction } from './hooks/actions/useDataStoreDeleteAction';
-export type { UseDataStoreDeleteActionOptions } from './hooks/actions/useDataStoreDeleteAction';
-export { useDataStoreUpdateAction } from './hooks/actions/useDataStoreUpdateAction';
-export type { UseDataStoreUpdateActionOptions } from './hooks/actions/useDataStoreUpdateAction';
 
 export { AlertIcon } from './primitives/Alert/AlertIcon';
 export * from './primitives/Icon/internal';
 export * from './primitives/shared/datastore';
 export { useDropZone } from './primitives/DropZone/useDropZone';
-export {
-  EscapeHatchProps,
-  Variant,
-  findChildOverrides,
-  getOverridesFromVariants,
-  getOverrideProps,
-  mergeVariantsAndOverrides,
-} from './studio';
 
 export { Field } from './primitives/Field';
 
