1150c945a89607a8753e1ce2854f6e7fd2617887
Thadd Trinh <thadtrin@amazon.com>
2023-09-18 14:28:14 -0700

    address more comments

--- examples/next-liveness/next.config.js
+++ examples/next-liveness/next.config.js
@@ -3,11 +3,6 @@ const { patchWebpackConfig } = require('next-global-css');
 
 module.exports = {
   reactStrictMode: true,
-  /**
-   * Setting trailingSlash to true to resolve known bug with federated sign in redirect + next.js
-   * https://github.com/aws-amplify/amplify-cli/issues/7359#issuecomment-812821315
-   */
-  trailingSlash: true,
   pageExtensions: ['page.tsx'],
   webpack: (config, options) => {
     // allows importing of css files inside modules
--- packages/e2e/features/ui/components/liveness/disable-start-screen.feature
+++ packages/e2e/features/ui/components/liveness/disable-start-screen.feature
@@ -5,11 +5,11 @@ Feature: Disable Start Screen
   Background:
     Given I'm running the example "ui/components/liveness/disable-start-screen/"
   
-  @react @liveness-react
+  @liveness-react
   Scenario: See camera module and close with the close icon
       Then I see "Loading"
 
-  @react @liveness-react
+  @liveness-react
   Scenario: See camera module and instructions
       Then I see "liveness-detector" element
       Then I see "connecting"
