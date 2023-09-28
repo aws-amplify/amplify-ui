27d76a3efd66ace7237baae4725b7aa14fc73c2e
Caleb Pollman <cpollman@amazon.com>
2023-09-27 17:30:15 -0700

    feat(A.Next): add createProvider util and related context (#4490)

---
+++ packages/react-core-auth/src/components/Authenticator/context/DisplayText/index.ts
@@ -0,0 +1,7 @@
+export { DisplayTextProvider, useDisplayText } from './DisplayTextContext';
+export { displayTextEn } from './i18n';
+export {
+  DisplayText,
+  DisplayTextDefault,
+  DisplayTextProviderProps,
+} from './types';
