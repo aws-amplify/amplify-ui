054444d9f604bb5a1a1b868e485956dd1685e916
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2023-10-04 15:52:06 -0400

    updating tests

--- packages/react-liveness/src/components/FaceLivenessDetector/shared/__tests__/LivenessIconWithPopover.test.tsx
+++ packages/react-liveness/src/components/FaceLivenessDetector/shared/__tests__/LivenessIconWithPopover.test.tsx
@@ -7,7 +7,14 @@ describe('LivenessIconWithPopover', () => {
   it('should render the component content appropriately', () => {
     const infoText = 'Test info text';
 
-    render(<LivenessIconWithPopover>{infoText}</LivenessIconWithPopover>);
+    render(
+      <LivenessIconWithPopover
+        labelText="More information about photosensitivity"
+        headingText="Photosensitivity warning"
+      >
+        {infoText}
+      </LivenessIconWithPopover>
+    );
 
     const popover = screen.queryByTestId('popover-icon');
     expect(screen.queryByTestId('popover-icon')).toBeInTheDocument();
