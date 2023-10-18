47513859f1fc1934eba7e243f31d74a8c16c495d
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2023-10-17 22:23:38 -0400

    restoring paths

--- examples/next/pages/ui/components/in-app-messaging/basic-usage-provider/aws-exports.js
+++ examples/next/pages/ui/components/in-app-messaging/basic-usage-provider/aws-exports.js
@@ -2,6 +2,7 @@
 // You must supply the config from your own in-app env.
 // The Demo app will still function without values below,
 // but will throw warnings on some displayMessage calls
-import awsExports from '@environments/in-app-messaging/in-app-messaging-with-pinpoint-campaign/src/aws-exports';
 
-export default awsExports;
+const awsmobile = {};
+
+export default awsmobile;
