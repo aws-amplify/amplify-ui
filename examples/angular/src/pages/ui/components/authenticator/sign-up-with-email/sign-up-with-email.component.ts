988f8306ef5f25d090ea4462f97990a705e907d5
Thadd Trinh <thadtrin@amazon.com>
2023-10-23 15:15:19 -0700

    fix: example apps for vue and angular auto signin

--- examples/angular/src/pages/ui/components/authenticator/sign-up-with-email/sign-up-with-email.component.ts
+++ examples/angular/src/pages/ui/components/authenticator/sign-up-with-email/sign-up-with-email.component.ts
@@ -51,9 +51,7 @@ export class SignUpWithEmailComponent implements OnInit {
         options: {
           userAttributes: attributes,
           // @todo-migration re-enable
-          // autoSignIn: {
-          //   enabled: true,
-          // },
+          autoSignIn: true,
         },
       });
     },
