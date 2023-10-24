60f0c1c23d98269a6e98acd71b675486e10ab6d5
Caleb Pollman <cpollman@amazon.com>
2023-10-24 08:36:16 -0700

    Revert "chore: add data plane tracking  (#4563)" (#4577)
    This reverts commit 4278849b7dd2eb26e993141d6e658bbe5463b50a.

--- packages/ui/src/utils/setUserAgent/constants.ts
+++ packages/ui/src/utils/setUserAgent/constants.ts
@@ -1,72 +1,24 @@
 import {
-  AuthUserAgentInput,
-  GeoUserAgentInput,
-  InAppMessagingUserAgentInput,
-  StorageUserAgentInput,
+  setCustomUserAgent,
+  SetCustomUserAgentInput,
   AuthAction,
-  GeoAction,
-  InAppMessagingAction,
-  StorageAction,
   Category,
 } from '@aws-amplify/core/internals/utils';
 
+// replace type once Category input types are available
 export const ACCOUNT_SETTINGS_INPUT_BASE: Omit<
-  AuthUserAgentInput,
+  SetCustomUserAgentInput,
   'additionalDetails'
 > = {
   apis: [AuthAction.DeleteUser, AuthAction.UpdatePassword],
   category: Category.Auth,
 };
 
-export const AUTHENTICATOR_INPUT_BASE: Omit<
-  AuthUserAgentInput,
-  'additionalDetails'
-> = {
-  apis: [
-    AuthAction.SignUp,
-    AuthAction.ConfirmSignUp,
-    AuthAction.ResendSignUpCode,
-    AuthAction.SignIn,
-    AuthAction.ConfirmSignIn,
-    AuthAction.FetchUserAttributes,
-    AuthAction.SignOut,
-    AuthAction.ResetPassword,
-    AuthAction.ConfirmResetPassword,
-    AuthAction.SignInWithRedirect,
-  ],
-  category: Category.Auth,
-};
-
+// replace type once Category input types are available
 export const IN_APP_MESSAGING_INPUT_BASE: Omit<
-  InAppMessagingUserAgentInput,
+  SetCustomUserAgentInput,
   'additionalDetails'
 > = {
-  apis: [InAppMessagingAction.NotifyMessageInteraction],
+  apis: undefined, // TODO
   category: Category.InAppMessaging,
-};
-
-export const LOCATION_SEARCH_INPUT_BASE: Omit<
-  GeoUserAgentInput,
-  'additionalDetails'
-> = {
-  category: Category.Geo,
-  apis: [
-    GeoAction.SearchByText,
-    GeoAction.SearchForSuggestions,
-    GeoAction.SearchByPlaceId,
-  ],
-};
-
-export const MAP_VIEW_INPUT_BASE: Omit<GeoUserAgentInput, 'additionalDetails'> =
-  {
-    category: Category.Geo,
-    apis: [],
-  };
-
-export const STORAGE_MANAGER_INPUT_BASE: Omit<
-  StorageUserAgentInput,
-  'additionalDetails'
-> = {
-  apis: [StorageAction.UploadData],
-  category: Category.Storage,
 };
