00229bfe6053d1443a5c31fe029ba63dfe2229dd
Danny Banks <djb@amazon.com>
2023-10-25 12:03:11 -0700

    chore(docs): fix social images for now

--- docs/src/utils/previews.ts
+++ docs/src/utils/previews.ts
@@ -1,9 +1,11 @@
-import slugify from 'slugify';
+// import slugify from 'slugify';
 
 import { PREVIEW_VERSION } from '../data/preview';
 
+// For now dynamic social images are disabled
+// hard-coding a preview image based on platform until resolved
 export const getImagePath = (name: string) => {
-  const slug = slugify(name.replace(/\W+/g, ' '));
-
-  return `/previews/${slug.length > 0 ? slug : 'react'}-${PREVIEW_VERSION}.png`;
+  // const slug = slugify(name.replace(/\W+/g, ' '));
+  // return `/previews/${slug.length > 0 ? slug : 'react'}-${PREVIEW_VERSION}.png`;
+  return `/previews/${name}-${PREVIEW_VERSION}.png`;
 };
