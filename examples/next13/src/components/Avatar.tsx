0b6b50a70cefea59ecbcd28e750d7fa22dd23507
Scott Rees <6165315+reesscot@users.noreply.github.com>
2023-10-26 01:30:05 -0700

    chore: remove next13 example app

--- examples/next13/src/components/Avatar.tsx
+++
@@ -1,9 +0,0 @@
-import { createComponentTheme } from '@aws-amplify/ui-react/theme';
-
-const theme = createComponentTheme('avatar', (tokens) => {
-  return {};
-});
-
-export const Avatar = () => {
-  return <div></div>;
-};
