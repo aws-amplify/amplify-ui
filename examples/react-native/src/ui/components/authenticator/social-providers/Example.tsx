16b67fc6dabe400f37c493b3eb26c4f90f6e8911
Caleb Pollman <cpollman@amazon.com>
2023-10-30 12:12:51 -0700

    temp

--- examples/react-native/src/ui/components/authenticator/social-providers/Example.tsx
+++ examples/react-native/src/ui/components/authenticator/social-providers/Example.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { Authenticator } from '@aws-amplify/ui-react-native';
-import { Amplify } from 'aws-amplify';
+import { Amplify, ResourcesConfig } from 'aws-amplify';
+import { ConsoleLogger } from 'aws-amplify/utils';
 
 import { SignOutButton } from '../SignOutButton';
 import awsconfig from './aws-exports';
+
+const maybe: ResourcesConfig = awsconfig;
+
+console.log('maybe', maybe);
+
+// @ts-expect-error
+ConsoleLogger.LOG_LEVEL = 'DEBUG';
 Amplify.configure(awsconfig);
 
 function App() {
