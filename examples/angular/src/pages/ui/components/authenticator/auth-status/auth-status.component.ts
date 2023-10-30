31e2a397ab0aeaf38f9503eb9161aef960de7c09
Caleb Pollman <cpollman@amazon.com>
2023-10-30 12:05:16 -0700

    fix: revert angular example chnages

--- examples/angular/src/pages/ui/components/authenticator/auth-status/auth-status.component.ts
+++ examples/angular/src/pages/ui/components/authenticator/auth-status/auth-status.component.ts
@@ -5,14 +5,15 @@ import { Amplify } from 'aws-amplify';
 import { signIn, signOut } from 'aws-amplify/auth';
 
 import awsExports from './aws-exports';
-Amplify.configure(awsExports);
 
 @Component({
   selector: 'auth-status',
   templateUrl: 'auth-status.component.html',
 })
 export class AuthStatusComponent {
-  constructor(public authenticator: AuthenticatorService) {}
+  constructor(public authenticator: AuthenticatorService) {
+    Amplify.configure(awsExports);
+  }
 
   get isAuthenticated(): boolean {
     return this.authenticator.authStatus === 'authenticated';
