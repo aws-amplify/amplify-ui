30f5b7a5cbe67b28083f306dd66b1fb32a2eb786
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2023-10-30 14:17:43 -0400

    fix: removing phone number from example

--- examples/vue/src/pages/ui/components/authenticator/sign-in-with-username/index.vue
+++ examples/vue/src/pages/ui/components/authenticator/sign-in-with-username/index.vue
@@ -5,21 +5,10 @@ import '@aws-amplify/ui-vue/styles.css';
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
-const formFields = {
-  signUp: {
-    phone_number: {
-      dialCode: '+227',
-      dialCodeList: ['+1', '+82', '+227', '+100', '+227'],
-    },
-  },
-};
 </script>
 
 <template>
-  <authenticator
-    :form-fields="formFields"
-    :sign-up-attributes="['phone_number']"
-  >
+  <authenticator>
     <template v-slot="{ user, signOut }">
       <h1>Hello {{ user.username }}!</h1>
       <button @click="signOut">Sign Out</button>
