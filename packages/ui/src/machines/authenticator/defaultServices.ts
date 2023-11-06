229aed6a82591c833ac8c09a900ae572e79e335b
Scott Rees <6165315+reesscot@users.noreply.github.com>
2023-11-06 14:31:19 -0700

    chore: fix lodash import (#4642)
    * chore: fix lodash import
    
    * Update packages/ui/src/machines/authenticator/defaultServices.ts
    
    ---------
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

--- packages/ui/src/machines/authenticator/defaultServices.ts
+++ packages/ui/src/machines/authenticator/defaultServices.ts
@@ -30,7 +30,8 @@ import {
   ValidatorResult,
 } from '../../types';
 import { groupLog } from '../../utils';
-import { uniqueId } from 'lodash';
+// import .js directly to satisfy ESM
+import { uniqueId } from 'lodash/uniqueId.js';
 
 // Cognito does not allow a password length lower then 8 characters
 const DEFAULT_COGNITO_PASSWORD_MIN_LENGTH = 8;
