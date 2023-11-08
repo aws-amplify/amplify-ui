bc4831cc2756c767a56e125af1f03b665dcd5396
Danny Banks <djb@amazon.com>
2023-11-08 13:52:10 -0800

    fix(storageimage): handle onStorageGetError properly (#4667)
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

--- packages/react-storage/src/components/StorageImage/StorageImage.tsx
+++ packages/react-storage/src/components/StorageImage/StorageImage.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { classNames } from '@aws-amplify/ui';
 
-import { ComponentClassName } from '@aws-amplify/ui';
+import { classNames, ComponentClassName, isUndefined } from '@aws-amplify/ui';
 import { Image } from '@aws-amplify/ui-react';
 import { useStorageURL } from '@aws-amplify/ui-react/internal';
+import { useSetUserAgent } from '@aws-amplify/ui-react-core';
 
+import { VERSION } from '../../version';
 import type { StorageImageProps } from './types';
 
 export const StorageImage = ({
@@ -14,12 +15,26 @@ export const StorageImage = ({
   identityId,
   imgKey,
   onStorageGetError,
+  validateObjectExistence,
   ...rest
 }: StorageImageProps): JSX.Element => {
+  const resolvedValidateObjectExistence = isUndefined(validateObjectExistence)
+    ? true
+    : validateObjectExistence;
   const options = React.useMemo(
-    () => ({ accessLevel, identityId }),
-    [accessLevel, identityId]
+    () => ({
+      accessLevel,
+      identityId,
+      validateObjectExistence: resolvedValidateObjectExistence,
+    }),
+    [accessLevel, identityId, resolvedValidateObjectExistence]
   );
+
+  useSetUserAgent({
+    componentName: 'StorageImage',
+    packageName: 'react-storage',
+    version: VERSION,
+  });
 
   const url = useStorageURL({
     key: imgKey,
