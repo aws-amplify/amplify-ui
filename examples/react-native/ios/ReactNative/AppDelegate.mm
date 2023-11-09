d27d5247df29d77eeaebfa836f27f2a8e7842cbf
Danny Banks <djb@amazon.com>
2023-11-08 23:19:47 -0800

    nm

--- examples/react-native/ios/ReactNative/AppDelegate.mm
+++ examples/react-native/ios/ReactNative/AppDelegate.mm
@@ -1,7 +1,6 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
@@ -22,13 +21,6 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
-}
-
-- (BOOL)application:(UIApplication *)application
-   openURL:(NSURL *)url
-   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
-{
-  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 /// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
