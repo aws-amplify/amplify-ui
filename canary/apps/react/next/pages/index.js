9effe1d3d5b9e0aee5f2ca27c6252a98afeb3a70
thaddmt <68032955+thaddmt@users.noreply.github.com>
2023-11-15 08:59:57 -0800

    chore: remove liveness from canaries (#4741)

--- canary/apps/react/next/pages/index.js
+++ canary/apps/react/next/pages/index.js
@@ -7,7 +7,8 @@ import {
   withAuthenticator,
   useAuthenticator,
 } from '@aws-amplify/ui-react';
-import { FaceLivenessDetector } from '@aws-amplify/ui-react-liveness';
+// Todo-migration re-enable once liveness has been updated
+// import { FaceLivenessDetector } from '@aws-amplify/ui-react-liveness';
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify';
 import aws_exports from '../../../../environments/auth-with-email/src/aws-exports.js';
@@ -22,11 +23,11 @@ function App() {
         <Button variation="primary">Click me!</Button>
         <Button onClick={signOut}>Sign out</Button>
       </Card>
-      <FaceLivenessDetector
+      {/* <FaceLivenessDetector
         sessionId="123"
         region="us-east-1"
         handleGetLivenessDetection={async () => {}}
-      />
+      /> */}
     </ThemeProvider>
   );
 }
