69cbc9ba9bb2bdeaee0e760642dd42c1798c2038
Danny Banks <djb@amazon.com>
2023-11-15 14:43:56 -0800

    updates

--- docs/src/components/Layout/LogoLink.tsx
+++ docs/src/components/Layout/LogoLink.tsx
@@ -1,10 +1,5 @@
 import NextLink from 'next/link';
 
-import { VisuallyHidden, Text } from '@aws-amplify/ui-react';
-
-import { Logo } from '../Logo';
-import { AmplifyIcon } from '../Icons';
-
 interface LogoLinkProps {
   platform: string;
   onClick?: () => void;
@@ -15,12 +10,6 @@ export const LogoLink = ({ platform, onClick }: LogoLinkProps) => {
     <NextLink href={`/${platform}`} passHref legacyBehavior>
       <a onClick={onClick} className="docs-logo-link">
         Amplify UI
-        <VisuallyHidden>Amplify UI Home</VisuallyHidden>
-        {/* <AmplifyIcon color="primary.60" />{' '} */}
-        {/* <Text as="span" fontWeight="medium">
-          Amplify
-        </Text>{' '}
-        UI */}
       </a>
     </NextLink>
   );
