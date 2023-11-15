d5934618adbb965cc2052862974d2c338f45ccf1
Danny Banks <djb@amazon.com>
2023-11-14 23:32:04 -0800

    Update examples/react-native/src/features/Authenticator/Federated/aws-exports.js
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

--- examples/react-native/src/features/Authenticator/Federated/aws-exports.js
+++ examples/react-native/src/features/Authenticator/Federated/aws-exports.js
@@ -1,10 +1,3 @@
 import awsExports from '@aws-amplify/ui-environments/auth/auth-with-federated/src/aws-exports';
 
-export default {
-  ...awsExports,
-  oauth: {
-    ...awsExports.oauth,
-    redirectSignIn: 'myapp://ui/components/authenticator/sign-in-federated/',
-    redirectSignOut: 'myapp://ui/components/authenticator/sign-in-federated/',
-  },
-};
+export default awsExports;
