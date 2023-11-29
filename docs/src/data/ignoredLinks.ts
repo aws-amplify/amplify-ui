2758f2b086f67fee3c720ada8ff594e732c747e2
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2023-11-29 16:27:15 -0500

    removing ignored links

--- docs/src/data/ignoredLinks.ts
+++
@@ -1,8 +0,0 @@
-/**
- * IGNORED_LINKS is a manually maintained allowlist.
- * Any links on this list will not be tested, so KEEP THIS LIST AS SHORT AS POSSIBLE.
- * External Links added to this list because they are good links returning a bad status code (not 200, 301, 303).
- * e.g. 403 error may caused by WAF blocking NodeJS HTTP(s) request from linux.
- */
-
-export const IGNORED_LINKS = [];
