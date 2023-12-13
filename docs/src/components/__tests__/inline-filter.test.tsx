8acc27d6ebc619ef53fbb05219f1ce99d33551e3
Jacob Logan <lognjc@amazon.com>
2023-12-13 13:32:31 -0700

    update test to use it.each

--- docs/src/components/__tests__/inline-filter.test.tsx
+++ docs/src/components/__tests__/inline-filter.test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import { InlineFilter } from '../src/components/InlineFilter';
+import { InlineFilter } from '../InlineFilter';
 import { useRouter } from 'next/router';
 
 jest.mock('next/router', () => ({
@@ -79,42 +79,20 @@ describe('InlineFilter', () => {
     expect(filter2).toBeNull();
   });
 
-  it('should not render anything if no or an invalid filter is passed in', () => {
-    const { container } = render(
-      <div>
-        <InlineFilter>
-          <span id="filter1">Content</span>
-        </InlineFilter>
-        <InlineFilter filters>
-          <span id="filter2">Content</span>
-        </InlineFilter>
-        <InlineFilter filters={{}}>
-          <span id="filter3">Content</span>
-        </InlineFilter>
-        <InlineFilter filters={'string'}>
-          <span id="filter4">Content</span>
-        </InlineFilter>
-        <InlineFilter filters={1}>
-          <span id="filter5">Content</span>
-        </InlineFilter>
-        <InlineFilter filters={[]}>
-          <span id="filter6">Content</span>
-        </InlineFilter>
-      </div>
-    );
-    const filter1 = container.querySelector('#filter1');
-    const filter2 = container.querySelector('#filter2');
-    const filter3 = container.querySelector('#filter3');
-    const filter4 = container.querySelector('#filter4');
-    const filter5 = container.querySelector('#filter5');
-    const filter6 = container.querySelector('#filter6');
-    expect(filter1).toBeNull();
-    expect(filter2).toBeNull();
-    expect(filter3).toBeNull();
-    expect(filter4).toBeNull();
-    expect(filter5).toBeNull();
-    expect(filter6).toBeNull();
-  });
+  it.each([undefined, true, {}, 'string', 1, []])(
+    'should not render anything if no or an invalid filter is passed in',
+    (filter) => {
+      const { container } = render(
+        <div>
+          <InlineFilter filters={filter}>
+            <span id="filter1">Content</span>
+          </InlineFilter>
+        </div>
+      );
+      const filter1 = container.querySelector('#filter1');
+      expect(filter1).toBeNull();
+    }
+  );
 
   it('should display content when matching an element of the filter array', () => {
     const { container } = render(
