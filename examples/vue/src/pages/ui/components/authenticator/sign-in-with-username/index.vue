977b7c58bf8cc613f73ea5f1b182b74a38e37fdc
Emma Sauerborn <70536670+esauerbo@users.noreply.github.com>
2023-12-18 14:20:33 -0500

    remove duplicate phone numbers from examples (#4860)

--- examples/vue/src/pages/ui/components/authenticator/sign-in-with-username/index.vue
+++ examples/vue/src/pages/ui/components/authenticator/sign-in-with-username/index.vue
@@ -9,17 +9,14 @@ const formFields = {
   signUp: {
     phone_number: {
       dialCode: '+227',
-      dialCodeList: ['+1', '+82', '+227', '+100', '+227'],
+      dialCodeList: ['+1', '+82', '+227', '+100'],
     },
   },
 };
 </script>
 
 <template>
-  <authenticator
-    :form-fields="formFields"
-    :sign-up-attributes="['phone_number']"
-  >
+  <authenticator :form-fields="formFields" :sign-up-attributes="['phone_number']">
     <template v-slot="{ user, signOut }">
       <h1>Hello {{ user.username }}!</h1>
       <button @click="signOut">Sign Out</button>
