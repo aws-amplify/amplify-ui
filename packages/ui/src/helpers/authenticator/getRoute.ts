40a9d484ef1baf10540b219d44620d0a30123f9a
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-01-04 15:11:07 -0500

    add handling for runActor

--- packages/ui/src/helpers/authenticator/getRoute.ts
+++ packages/ui/src/helpers/authenticator/getRoute.ts
@@ -7,6 +7,9 @@ export const getRoute = (
   state: AuthMachineState,
   actorState: AuthActorState
 ) => {
+  if (state?.matches('signUpActor.runActor')) {
+    console.log(state.value);
+  }
   switch (true) {
     case state.matches('idle'):
       return 'idle';
@@ -26,12 +29,16 @@ export const getRoute = (
     case actorState?.matches('setupTotp.submit'):
       return 'setupTotp';
     case actorState?.matches('signIn'):
+    case state?.matches('signInActor.runActor'):
       return 'signIn';
     case actorState?.matches('signUp'):
+    case actorState?.matches('autoSignIn'):
+    case state?.matches('signUpActor.runActor'):
       return 'signUp';
     case actorState?.matches('forceChangePassword'):
       return 'forceNewPassword';
     case actorState?.matches('forgotPassword'):
+    case state?.matches('forgotPasswordActor.runActor'):
       return 'forgotPassword';
     case actorState?.matches('confirmResetPassword'):
       return 'confirmResetPassword';
@@ -40,8 +47,7 @@ export const getRoute = (
     case actorState?.matches('confirmVerifyUserAttribute'):
       return 'confirmVerifyUser';
     case state.matches('getCurrentUser'):
-    case actorState.matches('fetchUserAttributes'):
-    case actorState.matches('autoSignIn'):
+    case actorState?.matches('fetchUserAttributes'):
       /**
        * This route is needed for autoSignIn to capture both the
        * autoSignIn.pending and the resolved states when the
