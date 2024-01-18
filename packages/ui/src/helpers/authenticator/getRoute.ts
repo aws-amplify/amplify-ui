9dc52eb1b2f4ad8c1f2956c7ad8a5073cf203172
Emma Sauerborn <70536670+esauerbo@users.noreply.github.com>
2024-01-18 10:24:41 -0500

    fix(authenticator): unhandled authenticator route console warning  (#4892)
    * adding handling for autoSignIn and resendSignUpCode
    
    * add handling for runActor
    
    * adding addtl cases
    
    * move sign in down
    
    * Adding case for federated
    
    * remove general cases and console warning
    
    * Adding unit tests
    
    * add unit tests
    
    * Create friendly-pots-switch.md
    
    ---------
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

--- packages/ui/src/helpers/authenticator/getRoute.ts
+++ packages/ui/src/helpers/authenticator/getRoute.ts
@@ -17,7 +17,7 @@ export const getRoute = (
     case state.matches('authenticated'):
       return 'authenticated';
     case actorState?.matches('confirmSignUp'):
-    case actorState?.matches('confirmSignUp.resendConfirmationCode'):
+    case actorState?.matches('resendSignUpCode'):
       return 'confirmSignUp';
     case actorState?.matches('confirmSignIn'):
       return 'confirmSignIn';
@@ -25,8 +25,10 @@ export const getRoute = (
     case actorState?.matches('setupTotp.submit'):
       return 'setupTotp';
     case actorState?.matches('signIn'):
+    case actorState?.matches('federatedSignIn'):
       return 'signIn';
     case actorState?.matches('signUp'):
+    case actorState?.matches('autoSignIn'):
       return 'signUp';
     case actorState?.matches('forceChangePassword'):
       return 'forceNewPassword';
@@ -39,7 +41,7 @@ export const getRoute = (
     case actorState?.matches('confirmVerifyUserAttribute'):
       return 'confirmVerifyUser';
     case state.matches('getCurrentUser'):
-    case actorState.matches('fetchUserAttributes'):
+    case actorState?.matches('fetchUserAttributes'):
       /**
        * This route is needed for autoSignIn to capture both the
        * autoSignIn.pending and the resolved states when the
@@ -47,10 +49,6 @@ export const getRoute = (
        */
       return 'transition';
     default:
-      console.debug(
-        'Cannot infer `route` from Authenticator state:',
-        state.value
-      );
       return null;
   }
 };
