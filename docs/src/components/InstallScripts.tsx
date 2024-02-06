0c10f7cf38966a84c63ce4cc6abaddc28ee47131
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-02-06 13:24:55 -0500

    remove alert from install scripts

--- docs/src/components/InstallScripts.tsx
+++ docs/src/components/InstallScripts.tsx
@@ -1,12 +1,7 @@
 import { CopyButton } from '@/components/CopyButton';
-import { Alert, Tabs, Text } from '@aws-amplify/ui-react';
+import { Tabs } from '@aws-amplify/ui-react';
 import { useRouter } from 'next/router';
-import {
-  Framework,
-  FRAMEWORK_DISPLAY_NAMES,
-  REACT_NATIVE_DEPENDENCIES,
-  MAJOR_VERSIONS,
-} from '../data/frameworks';
+import { Framework, REACT_NATIVE_DEPENDENCIES } from '../data/frameworks';
 
 type PackageManager = 'npm' | 'yarn';
 
@@ -36,48 +31,6 @@ const frameworkInstallScript = (
   const componentSubpackage = component ? `-${component}` : '';
 
   return `${packageManagerPrefix} @aws-amplify/ui-${framework}${componentSubpackage} aws-amplify${extraDependencies}`;
-};
-
-export const PrevVersionInstallAlert = ({ framework }) => {
-  const {
-    query: { platform = 'react' },
-  } = useRouter();
-
-  if (!framework) {
-    framework = platform as Framework;
-  }
-
-  const prevFrameworkVersion = MAJOR_VERSIONS[framework][1];
-  const heading = `${FRAMEWORK_DISPLAY_NAMES[framework]} ${prevFrameworkVersion}`;
-
-  const isReactNative = framework === 'react-native';
-  if (isReactNative) {
-    return (
-      <Alert role="none" variation="info" heading={heading}>
-        <Text>
-          Using version {prevFrameworkVersion} of{' '}
-          <code>@aws-amplify/ui-{framework}</code>? *Omit* `@aws-amplify/
-          {framework} and *specify* versions{' '}
-          <code>
-            @aws-amplify/ui-{framework}@{prevFrameworkVersion}.x
-          </code>{' '}
-          and <code>aws-amplify@{MAJOR_VERSIONS['aws-amplify'][1]}.x</code>.
-        </Text>
-      </Alert>
-    );
-  }
-  return (
-    <Alert role="none" variation="info" heading={heading}>
-      <Text>
-        Using version {prevFrameworkVersion} of{' '}
-        <code>@aws-amplify/ui-{framework}</code>? Specify versions{' '}
-        <code>
-          @aws-amplify/ui-{framework}@{prevFrameworkVersion}.x
-        </code>{' '}
-        and <code>aws-amplify@{MAJOR_VERSIONS['aws-amplify'][1]}.x</code>.
-      </Text>
-    </Alert>
-  );
 };
 
 export const TerminalCommand = ({
