72940316465bf4bba788e22b74667d1a0c0301e1
Heather <hbuchel@gmail.com>
2024-02-08 15:02:58 -0500

    fix(react-storage): use targetIdentityId to get url

--- packages/react-storage/src/components/StorageImage/StorageImage.tsx
+++ packages/react-storage/src/components/StorageImage/StorageImage.tsx
@@ -24,7 +24,7 @@ export const StorageImage = ({
   const options = React.useMemo(
     () => ({
       accessLevel,
-      identityId,
+      targetIdentityId: identityId,
       validateObjectExistence: resolvedValidateObjectExistence,
     }),
     [accessLevel, identityId, resolvedValidateObjectExistence]
