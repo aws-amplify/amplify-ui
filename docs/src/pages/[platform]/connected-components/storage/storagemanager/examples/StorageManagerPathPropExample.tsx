6b5380db51bb1679baa74af185a2993d11f9b840
Caleb Pollman <cpollman@amazon.com>
2024-02-15 13:38:45 -0800

    Update docs/src/pages/[platform]/connected-components/storage/storagemanager/examples/StorageManagerPathPropExample.tsx

--- docs/src/pages/[platform]/connected-components/storage/storagemanager/examples/StorageManagerPathPropExample.tsx
+++ docs/src/pages/[platform]/connected-components/storage/storagemanager/examples/StorageManagerPathPropExample.tsx
@@ -4,7 +4,7 @@ export const StorageManagerPathPropExample = () => {
   return (
     <StorageManager
       accessLevel="guest"
-      // uploaded files can be found in S3 inside `guest/images/`
+      // uploaded files can be found in S3 inside `public/images/`
       path="images/"
       acceptedFileTypes={['image/*']}
       maxFileCount={1}
