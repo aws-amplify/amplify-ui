7dea519e540e16a37fecd0616578f660e45e3381
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-03-05 12:24:20 -0500

    Get rid of reused code

--- docs/src/components/MigrationChanges/ForgotPasswordRenameAlert.tsx
+++ docs/src/components/MigrationChanges/ForgotPasswordRenameAlert.tsx
@@ -14,33 +14,26 @@ export const ForgotPasswordRenameAlert = ({ framework }) => {
   const isReactNative = framework === 'react-native';
   const prevFrameworkVersion = PREV_MAJOR_VERSIONS[framework];
 
-  if (isReactNative) {
-    return (
-      <Alert
-        role="none"
-        variation="info"
-        heading={`ui-${framework}@${prevFrameworkVersion}.x`}
-      >
-        <Text>
-          Use <code>resetPassword</code> in place of <code>forgotPassword</code>{' '}
-          in version {prevFrameworkVersion} of{' '}
-          <code>@aws-amplify/ui-{framework}</code>.
-        </Text>
-      </Alert>
-    );
-  }
-
+  const textContent = isReactNative ? (
+    <Text>
+      Use <code>resetPassword</code> in place of <code>forgotPassword</code> in
+      version {prevFrameworkVersion} of <code>@aws-amplify/ui-{framework}</code>
+      .
+    </Text>
+  ) : (
+    <Text>
+      Use <code>resetPassword</code> in place of <code>forgotPassword</code> in
+      versions {prevFrameworkVersion} and earlier of{' '}
+      <code>@aws-amplify/ui-{framework}</code>.
+    </Text>
+  );
   return (
     <Alert
       role="none"
       variation="info"
       heading={`ui-${framework}@${prevFrameworkVersion}.x`}
     >
-      <Text>
-        Use <code>resetPassword</code> in place of <code>forgotPassword</code>{' '}
-        in versions {prevFrameworkVersion} and earlier of{' '}
-        <code>@aws-amplify/ui-{framework}</code>.
-      </Text>
+      {textContent}
     </Alert>
   );
 };
