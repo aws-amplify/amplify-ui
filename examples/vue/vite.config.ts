6b995c0f743cdd105e70d2c1a22846f359ec5545
Caleb Pollman <cpollman@amazon.com>
2024-04-03 13:43:46 -0700

    chore(deps): bump vite and related deps

--- examples/vue/vite.config.ts
+++ examples/vue/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import Pages from 'vite-plugin-pages';
-
-const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
