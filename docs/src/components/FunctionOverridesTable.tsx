251c99ac617920e9efe93bd268dccbfb40ee0d11
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-04-11 16:36:52 -0400

    Add table showing differences in overriding functions in current and prev major versions

---
+++ docs/src/components/FunctionOverridesTable.tsx
@@ -0,0 +1,204 @@
+import {
+  ResponsiveTable,
+  ResponsiveTableCell,
+} from '@/components/ResponsiveTable';
+import {
+  Tabs,
+  TableCell,
+  TableBody,
+  TableHead,
+  TableRow,
+} from '@aws-amplify/ui-react';
+import { useRouter } from 'next/router';
+import { Framework, CURRENT_MAJOR_VERSIONS } from '../data/frameworks';
+
+const PREV_MAJOR_VERSIONS: Record<string, number> = {
+  angular: 4,
+  'aws-amplify': 5,
+  react: 5,
+  'react-native': 1,
+  vue: 3,
+};
+
+export const FunctionOverridesTable = ({ framework }) => {
+  const {
+    query: { platform = 'react' },
+  } = useRouter();
+
+  if (!framework) {
+    framework = platform as Framework;
+  }
+
+  const latestVersion = CURRENT_MAJOR_VERSIONS[framework].toString();
+  const prevVersion = PREV_MAJOR_VERSIONS[framework].toString();
+  return (
+    <Tabs.Container defaultValue={latestVersion}>
+      <Tabs.List>
+        <Tabs.Item value={latestVersion}>
+          <code>@aws-amplify/ui-{framework}</code> v{latestVersion} (latest)
+        </Tabs.Item>
+        <Tabs.Item value={prevVersion}>
+          <code>@aws-amplify/ui-{framework}</code> v{prevVersion}
+        </Tabs.Item>
+      </Tabs.List>
+      <Tabs.Panel value={latestVersion}>
+        <ResponsiveTable labelWidth="10rem">
+          <TableHead>
+            <TableRow>
+              <TableCell as="th">Auth Function Call</TableCell>
+              <TableCell as="th">Override Name</TableCell>
+              <TableCell as="th">input Properties</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>signUp</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleSignUp</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="input Properties">
+                <code>{`{username, password}`}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>signIn</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleSignIn</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="input Properties">
+                <code>{'{username, password}'}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>confirmSignIn</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleConfirmSignIn</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="input Properties">
+                <code>{'{challengeResponse}'}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>confirmSignUp</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleConfirmSignUp</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="input Properties">
+                <code>{'{username, confirmationCode}'}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>resetPassword</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleResetPassword</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="input Properties">
+                <code>{'{username}'}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>confirmResetPassword</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleConfirmResetPassword</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="input Properties">
+                <code>{'{username, newPassword, confirmationCode}'}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+          </TableBody>
+        </ResponsiveTable>
+      </Tabs.Panel>
+      <Tabs.Panel value={prevVersion}>
+        <ResponsiveTable labelWidth="10rem">
+          <TableHead>
+            <TableRow>
+              <TableCell as="th">Function Call</TableCell>
+              <TableCell as="th">Override Name</TableCell>
+              <TableCell as="th">formData Properties</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>Auth.signUp</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleSignUp</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="formData Properties">
+                <code>{'{username, password, attributes}'}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>Auth.signIn</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleSignIn</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="formData Properties">
+                <code>{'{username, password}'}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>Auth.confirmSignIn</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleConfirmSignIn</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="formData Properties">
+                <code>{'{user, code, mfaType}'}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>Auth.confirmSignUp</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleConfirmSignUp</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="formData Properties">
+                <code>{'{username, code}'}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>Auth.forgotPassword</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleForgotPassword</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="formData Properties">
+                <code>{`{username}`}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+            <TableRow>
+              <ResponsiveTableCell label="Function Call">
+                <code>Auth.forgotPasswordSubmit</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="Override Name">
+                <code>handleForgotPasswordSubmit</code>
+              </ResponsiveTableCell>
+              <ResponsiveTableCell label="formData Properties">
+                <code>{'{username, code, password}'}</code>
+              </ResponsiveTableCell>
+            </TableRow>
+          </TableBody>
+        </ResponsiveTable>
+      </Tabs.Panel>
+    </Tabs.Container>
+  );
+};
