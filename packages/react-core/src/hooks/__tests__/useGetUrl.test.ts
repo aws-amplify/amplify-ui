6220f50f0df22798b776bb614a67f20795738ba3
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-04-18 12:57:02 -0400

    Update useGetUrl to ignore props type within hook

--- packages/react-core/src/hooks/__tests__/useGetUrl.test.ts
+++ packages/react-core/src/hooks/__tests__/useGetUrl.test.ts
@@ -16,24 +16,25 @@ describe('useGetUrl', () => {
   const onStorageGetError = jest.fn();
   const onGetUrlError = jest.fn();
 
-  const useGetUrlKeyParams: UseGetUrlInput = {
+  const KEY_INPUT: UseGetUrlInput = {
     key: 'file.jpg',
     options: { accessLevel: 'guest' },
     onError: onStorageGetError,
   };
-  const useGetUrlPathParams: UseGetUrlInput = {
+
+  const PATH_INPUT: UseGetUrlInput = {
     path: 'guest/file.jpg',
     onError: onGetUrlError,
   };
 
   const paramType = [
     {
-      useGetUrlParams: useGetUrlKeyParams,
+      useGetUrlParams: KEY_INPUT,
       description: 'with key params',
       errorHandler: 'onStorageGetError',
     },
     {
-      useGetUrlParams: useGetUrlPathParams,
+      useGetUrlParams: PATH_INPUT,
       description: 'with path params',
       errorHandler: 'onGetUrlError',
     },
@@ -47,6 +48,7 @@ describe('useGetUrl', () => {
       const { result } = renderHook(() => useGetUrl(useGetUrlParams));
       await waitFor(() => {
         expect(result.current.isLoading).toBe(true);
+        expect(result.current.url).toBe(undefined);
       });
     }
   );
@@ -65,6 +67,7 @@ describe('useGetUrl', () => {
 
       expect(getUrl).toHaveBeenCalledWith(getUrlParams);
       expect(result.current.isLoading).toBe(true);
+      expect(result.current.url).toBe(undefined);
 
       // Next update will happen when getUrl resolves
       await waitForNextUpdate();
@@ -83,7 +86,7 @@ describe('useGetUrl', () => {
       const { onError, ...getUrlParams } = useGetUrlParams;
       (getUrl as jest.Mock).mockRejectedValue(customError);
 
-      const { waitForNextUpdate } = renderHook(() =>
+      const { result, waitForNextUpdate } = renderHook(() =>
         useGetUrl(useGetUrlParams)
       );
 
@@ -91,8 +94,102 @@ describe('useGetUrl', () => {
 
       // Next update will happen when getUrl resolves
       await waitForNextUpdate();
+
+      expect(result.current.isLoading).toBe(false);
+      expect(result.current.url).toBe(undefined);
       expect(onError).toHaveBeenCalledTimes(1);
       expect(onError).toHaveBeenCalledWith(customError);
     }
   );
+
+  it('ignores the first response if rerun a second time before the first call resolves in the happy path', async () => {
+    const secondUrl =
+      'https://amplify.s3.amazonaws.com/path/to/the/second-file.jpg';
+
+    (getUrl as jest.Mock)
+      .mockResolvedValueOnce({ url })
+      .mockResolvedValueOnce({ url: secondUrl });
+
+    const { onError, ...getUrlParams } = KEY_INPUT;
+
+    const { result, waitForNextUpdate, rerender } = renderHook(
+      (input: UseGetUrlInput = KEY_INPUT) => useGetUrl(input)
+    );
+
+    expect(getUrl).toHaveBeenCalledWith(getUrlParams);
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.url).toBe(undefined);
+
+    rerender({ ...KEY_INPUT, key: 'second-file.jpg' });
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.url).toBe(undefined);
+
+    // Next update will happen when getUrl resolves
+    await waitForNextUpdate();
+
+    expect(getUrl).toHaveBeenCalledWith({
+      ...getUrlParams,
+      key: 'second-file.jpg',
+    });
+
+    expect(getUrl).toHaveBeenCalledTimes(2);
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.url).toBe(secondUrl);
+  });
+
+  it('ignores the first response if rerun a second time before the first call resolves in the unhappy path', async () => {
+    const firstError = new Error('Something went wrong');
+    const secondError = new Error('Something went wrong again');
+
+    const { onError } = PATH_INPUT;
+    (getUrl as jest.Mock)
+      .mockRejectedValueOnce(firstError)
+      .mockRejectedValueOnce(secondError);
+
+    const { result, waitForNextUpdate, rerender } = renderHook(
+      (input: UseGetUrlInput = PATH_INPUT) => useGetUrl(input)
+    );
+
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.url).toBe(undefined);
+    expect(onError).toHaveBeenCalledTimes(0);
+
+    rerender({ ...PATH_INPUT, path: 'guest/second-file.jpg' });
+
+    expect(result.current.isLoading).toBe(true);
+    expect(result.current.url).toBe(undefined);
+    expect(onError).toHaveBeenCalledTimes(0);
+
+    await waitForNextUpdate();
+
+    expect(result.current.isLoading).toBe(false);
+    expect(getUrl).toHaveBeenCalledTimes(2);
+    expect(result.current.url).toBe(undefined);
+    expect(onError).toHaveBeenCalledTimes(1);
+    expect(onError).toHaveBeenCalledWith(secondError);
+  });
+
+  it('does not call `onError` if it is not a function', async () => {
+    const customError = new Error('Something went wrong');
+    const { onError, ...getUrlParams } = KEY_INPUT;
+
+    (getUrl as jest.Mock).mockRejectedValueOnce(customError);
+
+    const input = { ...KEY_INPUT, onError: null };
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      // @ts-expect-error test against invalid input
+      useGetUrl(input)
+    );
+
+    expect(result.current.isLoading).toBe(true);
+    expect(getUrl).toHaveBeenCalledWith(getUrlParams);
+    expect(result.current.url).toBe(undefined);
+
+    await waitForNextUpdate();
+
+    expect(result.current.isLoading).toBe(false);
+    expect(result.current.url).toBe(undefined);
+    expect(onError).toHaveBeenCalledTimes(0);
+  });
 });
--- packages/react-core/src/hooks/useGetUrl.ts
+++ packages/react-core/src/hooks/useGetUrl.ts
@@ -3,42 +3,26 @@ import * as React from 'react';
 import { getUrl, GetUrlInput } from 'aws-amplify/storage';
 
 import { isFunction } from '@aws-amplify/ui';
-import { useHasValueUpdated } from '@aws-amplify/ui-react-core';
 
 export type UseGetUrlInput = GetUrlInput & {
   onError?: (error: Error) => void;
 };
-
 interface UseGetUrlOutput {
   isLoading: boolean;
   url: URL | undefined;
   expiresAt: Date | undefined;
 }
 
-function getKeyOrPath(
-  input: UseGetUrlInput
-): string | (({ identityId }: { identityId?: string }) => string) {
-  if (input.key !== undefined) {
-    return input.key;
-  }
-  return input.path;
-}
+const INIT_STATE: UseGetUrlOutput = {
+  url: undefined,
+  expiresAt: undefined,
+  isLoading: true,
+};
 
 export const useGetUrl = (input: UseGetUrlInput): UseGetUrlOutput => {
-  const [result, setResult] = React.useState<UseGetUrlOutput>({
-    url: undefined,
-    expiresAt: undefined,
-    isLoading: false,
-  });
-  const keyOrPath = getKeyOrPath(input);
-  const hasImgUpdated = useHasValueUpdated(keyOrPath);
-
+  const [result, setResult] = React.useState(() => INIT_STATE);
   React.useEffect(() => {
-    if (!hasImgUpdated) {
-      return;
-    }
     const { onError, ...getUrlInput } = input;
-    setResult((prevResult) => ({ ...prevResult, isLoading: true }));
     let ignore = false;
 
     getUrl(getUrlInput)
@@ -46,24 +30,25 @@ export const useGetUrl = (input: UseGetUrlInput): UseGetUrlOutput => {
         if (ignore) {
           return;
         }
-        setResult((prevResult) => ({
-          ...prevResult,
-          ...response,
-          isLoading: false,
-        }));
+
+        setResult({ ...response, isLoading: false });
       })
       .catch((error: Error) => {
         if (ignore) {
           return;
         }
+
         if (isFunction(onError)) {
           onError(error);
         }
-        setResult((prevResult) => ({ ...prevResult, isLoading: false }));
-        return () => {
-          ignore = true;
-        };
+
+        setResult({ ...INIT_STATE, isLoading: false });
       });
-  }, [input, hasImgUpdated, keyOrPath, result]);
+
+    return () => {
+      ignore = true;
+    };
+  }, [input]);
+
   return result;
 };
