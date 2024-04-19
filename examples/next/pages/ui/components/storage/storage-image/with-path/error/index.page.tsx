44da897d23b219e4402d45b8f4222bee2a70e9f8
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-04-19 15:04:18 -0400

    Add path examples and e2e tests

---
+++ examples/next/pages/ui/components/storage/storage-image/with-path/error/index.page.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+
+import { Amplify } from 'aws-amplify';
+import { Text, Loader } from '@aws-amplify/ui-react';
+import { StorageImage } from '@aws-amplify/ui-react-storage';
+
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
+export function StorageImageExample() {
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
+  const onLoad = () => {
+    setIsLoaded(true);
+  };
+
+  return (
+    <>
+      <StorageImage
+        alt="private cat"
+        path="guest/this-image-does-not-exist.jpeg"
+        onLoad={onLoad}
+        onGetUrlError={(error) => {
+          console.log('onGetUrlError implementation');
+          console.log(error);
+        }}
+        onError={(error) => {
+          console.log('onError implementation');
+          console.log(error);
+        }}
+      />
+      {isLoaded ? (
+        <Text>The image is loaded.</Text>
+      ) : (
+        <Loader testId="Loader" />
+      )}
+    </>
+  );
+}
+export default StorageImageExample;
---
+++ packages/e2e/features/ui/components/storage/storage-image/with-path/load-public-image-from-s3.feature
@@ -0,0 +1,13 @@
+Feature: Load an image from S3 with public access level settings
+
+  Background:
+    Given I'm running the example "ui/components/storage/storage-image/with-path/guest-access-level"
+
+  @react
+  Scenario: I successfully load a public image
+    Then I see "Loader1" element
+    Then I see "Loader2" element
+    Then I see the "public cat 1" image
+    Then I see the "public cat 2" image
+    Then I see "The first public image is loaded."
+    Then I see "The second public image is loaded."
