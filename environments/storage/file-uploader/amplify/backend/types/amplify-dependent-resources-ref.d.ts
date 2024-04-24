e2cac8e2aee5260ecac6a1138186ae4143398a2b
Tom Burton <Tom.Burton@Outlook.com>
2024-04-24 08:57:13 -0400

    Add Example to locally test rejecting an upload in ProcessFile.

--- environments/storage/file-uploader/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ environments/storage/file-uploader/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,19 +1,19 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "fileuploadera29c8ca6": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "storage": {
-        "fileuploader": {
-            "BucketName": "string",
-            "Region": "string"
-        }
+  "auth": {
+    "fileuploadera29c8ca6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "fileuploader": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
 }
