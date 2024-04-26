d5b4263f560a853454574a38f15b98d54245fc43
Emma Sauerborn <70536670+esauerbo@users.noreply.github.com>
2024-04-26 18:29:06 -0400

    Update DefaultStorageImageExample.tsx
    Co-authored-by: Scott Rees <6165315+reesscot@users.noreply.github.com>

--- docs/src/pages/[platform]/connected-components/storage/storageimage/examples/DefaultStorageImageExample.tsx
+++ docs/src/pages/[platform]/connected-components/storage/storageimage/examples/DefaultStorageImageExample.tsx
@@ -1,5 +1,5 @@
 import { StorageImage } from '@aws-amplify/ui-react-storage';
 
 export const DefaultStorageImageExample = () => {
-  return <StorageImage alt="sleepy-cat" path="guest/cat.jpg" />;
+  return <StorageImage alt="sleepy-cat" path="public/cat.jpg" />;
 };
