85769f3d08d1641567199afd1fea7526a016794f
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-04-26 17:20:38 -0400

    Fix default example

--- docs/src/pages/[platform]/connected-components/storage/storageimage/examples/DefaultStorageImageExample.tsx
+++ docs/src/pages/[platform]/connected-components/storage/storageimage/examples/DefaultStorageImageExample.tsx
@@ -1,13 +1,5 @@
 import { StorageImage } from '@aws-amplify/ui-react-storage';
 
 export const DefaultStorageImageExample = () => {
-  return (
-    <>
-      <StorageImage alt="sleepy-cat" path="guest/cat.jpg" />
-      <StorageImage
-        alt="protected-sleepy-cat"
-        path={({ identityId }) => `protected/${identityId}/cat.jpg`}
-      />
-    </>
-  );
+  return <StorageImage alt="sleepy-cat" path="guest/cat.jpg" />;
 };
