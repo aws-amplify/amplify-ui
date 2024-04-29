989565ed47a340fa4972ace6ecf272aad81596b3
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-04-29 19:14:34 -0400

    Add examples and e2e tests for gen2

---
+++ examples/next/pages/ui/components/storage/storage-manager/gen2/metadata/index.page.tsx
@@ -0,0 +1,33 @@
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+import {
+  StorageManager,
+  StorageManagerProps,
+} from '@aws-amplify/ui-react-storage';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+const processFile: StorageManagerProps['processFile'] = ({ file, key }) => {
+  return {
+    file,
+    key,
+    metadata: {
+      id: key,
+    },
+  };
+};
+
+export function StorageManagerExample() {
+  return (
+    <StorageManager
+      acceptedFileTypes={['image/*']}
+      path="private/"
+      maxFileCount={3}
+      showThumbnails={true}
+      processFile={processFile}
+    />
+  );
+}
+export default withAuthenticator(StorageManagerExample);
