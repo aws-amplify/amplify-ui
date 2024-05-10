90f410d9b19baa8810cc2dcbe47063b20982fed0
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-05-10 12:37:09 -0400

    Add backend env for no lights

---
+++ environments/liveness/liveness-environment/amplify/backend/function/livenessenvironment6105cfac/src/index.js
@@ -0,0 +1,35 @@
+import { RekognitionLiveness } from '@amzn/aws-rekognition-liveness-js-v2-client';
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+
+export const handler = async (event, req) => {
+  const client = new RekognitionLiveness({
+    region: 'us-east-1',
+    endpoint: `https://us-east-1.gamma.frontend.reventlov.rekognition.aws.dev`,
+  });
+
+  const response = await client
+    .createFaceLivenessSession({
+      AwsAccountId: '845721723350',
+      Settings: {
+        ChallengePreferences: [
+          { Type: 'FaceMovementChallenge' },
+          { Type: 'FaceMovementAndLightChallenge' },
+        ],
+      },
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+    },
+    body: JSON.stringify({ sessionId: response.SessionId }),
+  };
+};
+
+handler({});
