d5be098e6224202b0b62a49f65f12ddc5595cf3d
Chenwei Zhang <chenwz@amazon.com>
2024-05-15 12:48:31 -0700

    fix angular examples

--- examples/angular/src/app/app.module.ts
+++ examples/angular/src/app/app.module.ts
@@ -29,6 +29,27 @@ import { SignUpWithUsernameComponent } from 'src/pages/ui/components/authenticat
 import { UseAuthenticatorComponent } from 'src/pages/ui/components/authenticator/useAuthenticator/useAuthenticator.component';
 import { UseAuthenticatorHomeComponent } from 'src/pages/ui/components/authenticator/useAuthenticator/home/useAuthenticatorHome.component';
 
+import { AuthStatusComponent as Gen2AuthStatusComponent } from 'src/pages/ui/components/authenticator/gen2/auth-status/auth-status.component';
+import { CustomSignUpFieldsComponent as Gen2CustomSignUpFieldsComponent } from 'src/pages/ui/components/authenticator/gen2/custom-sign-up-fields/custom-sign-up-fields.component';
+import { CustomSlotsComponent as Gen2CustomSlotsComponent } from 'src/pages/ui/components/authenticator/gen2/custom-slots/custom-slots.component';
+import { HubEventsComponent as Gen2HubEventsComponent } from 'src/pages/ui/components/authenticator/gen2/hub-events/hub-events.component';
+import { I18nComponent as Gen2I18nComponent } from 'src/pages/ui/components/authenticator/gen2/i18n/i18n.component';
+import { ModalComponent as Gen2ModalComponent } from 'src/pages/ui/components/authenticator/gen2/modal/modal.component';
+import { OverrideFunctionCallsComponent as Gen2OverrideFunctionCallsComponent } from 'src/pages/ui/components/authenticator/gen2/override-function-calls/override-function-calls.component';
+import { ForgotPasswordComponent as Gen2ForgotPasswordComponent } from 'src/pages/ui/components/authenticator/gen2/forgot-password/forgot-password.component';
+import { SignInFederatedComponent as Gen2SignInFederatedComponent } from 'src/pages/ui/components/authenticator/gen2/sign-in-federated/sign-in-federated.component';
+import { SignInSMSMFAComponent as Gen2SignInSMSMFAComponent } from 'src/pages/ui/components/authenticator/gen2/sign-in-sms-mfa/sign-in-sms-mfa.component';
+import { SignInTOTPMFAComponent as Gen2SignInTOTPMFAComponent } from 'src/pages/ui/components/authenticator/gen2/sign-in-totp-mfa/sign-in-totp-mfa.component';
+import { SignInTOTPSMSComponent as Gen2SignInTOTPSMSComponent } from 'src/pages/ui/components/authenticator/gen2/sign-in-totp-sms/sign-in-totp-sms.component';
+import { SignInWithEmailComponent as Gen2SignInWithEmailComponent } from 'src/pages/ui/components/authenticator/gen2/sign-in-with-email/sign-in-with-email.component';
+import { SignInWithPhoneComponent as Gen2SignInWithPhoneComponent } from 'src/pages/ui/components/authenticator/gen2/sign-in-with-phone/sign-in-with-phone.component';
+import { SignUpWithAttributesComponent as Gen2SignUpWithAttributesComponent } from 'src/pages/ui/components/authenticator/gen2/sign-up-with-attributes/sign-up-with-attributes.component';
+import { SignUpWithEmailComponent as Gen2SignUpWithEmailComponent } from 'src/pages/ui/components/authenticator/gen2/sign-up-with-email/sign-up-with-email.component';
+import { SignUpWithEmailLambdaComponent as Gen2SignUpWithEmailLambdaComponent } from 'src/pages/ui/components/authenticator/gen2/sign-up-with-email-lambda/sign-up-with-email-lambda.component';
+import { SignUpWithPhoneComponent as Gen2SignUpWithPhoneComponent } from 'src/pages/ui/components/authenticator/gen2/sign-up-with-phone/sign-up-with-phone.component';
+import { UseAuthenticatorComponent as Gen2UseAuthenticatorComponent } from 'src/pages/ui/components/authenticator/gen2/useAuthenticator/useAuthenticator.component';
+import { UseAuthenticatorHomeComponent as Gen2UseAuthenticatorHomeComponent } from 'src/pages/ui/components/authenticator/gen2/useAuthenticator/home/useAuthenticatorHome.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -49,12 +70,31 @@ import { UseAuthenticatorHomeComponent } from 'src/pages/ui/components/authentic
     SignInWithUsernameComponent,
     SignUpWithAttributesComponent,
     SignUpWithEmailComponent,
-    SignUpWithEmailComponent,
     SignUpWithEmailLambdaComponent,
     SignUpWithPhoneComponent,
     SignUpWithUsernameComponent,
     UseAuthenticatorComponent,
     UseAuthenticatorHomeComponent,
+    Gen2AuthStatusComponent,
+    Gen2CustomSignUpFieldsComponent,
+    Gen2CustomSlotsComponent,
+    Gen2HubEventsComponent,
+    Gen2I18nComponent,
+    Gen2ModalComponent,
+    Gen2OverrideFunctionCallsComponent,
+    Gen2ForgotPasswordComponent,
+    Gen2SignInFederatedComponent,
+    Gen2SignInSMSMFAComponent,
+    Gen2SignInTOTPMFAComponent,
+    Gen2SignInTOTPSMSComponent,
+    Gen2SignInWithEmailComponent,
+    Gen2SignInWithPhoneComponent,
+    Gen2SignUpWithAttributesComponent,
+    Gen2SignUpWithEmailComponent,
+    Gen2SignUpWithEmailLambdaComponent,
+    Gen2SignUpWithPhoneComponent,
+    Gen2UseAuthenticatorComponent,
+    Gen2UseAuthenticatorHomeComponent,
   ],
   imports: [
     BrowserModule,
