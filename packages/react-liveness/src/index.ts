78a9eccdf7a401d3ae34ada94ed4019e1886a04a
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-06-03 16:00:17 -0400

    update liveness exports

--- packages/react-liveness/src/index.ts
+++ packages/react-liveness/src/index.ts
@@ -7,4 +7,7 @@ export {
   AwsCredentials,
   AwsTemporaryCredentials,
   ErrorState,
+  FACE_MOVEMENT_AND_LIGHT_CHALLENGE,
+  FACE_MOVEMENT_CHALLENGE,
+  SUPPORTED_CHALLENGES,
 } from './components';
