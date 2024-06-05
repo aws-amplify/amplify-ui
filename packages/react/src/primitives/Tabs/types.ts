a2096f64aacad81709238b4b5215279b182e4cf9
Axel Andrews <axeland@amazon.com>
2024-06-05 14:45:55 -0700

    fixed breaking change by removing groupID from BaseTabProps - fragility in idValue still needs to be addressed

--- packages/react/src/primitives/Tabs/types.ts
+++ packages/react/src/primitives/Tabs/types.ts
@@ -38,7 +38,6 @@ export interface BaseTabsProps extends BaseTabsListProps {
   items?: Array<{
     value: string;
     isDisabled?: boolean;
-    groupID: string;
     label: React.ReactNode;
     content: React.ReactNode;
   }>;
