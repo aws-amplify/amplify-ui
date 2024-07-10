0ec62902892d25af5d8de33704a0b374a07a627e
AhmetAhunbayAWS <ahmetahu@amazon.com>
2024-07-10 16:03:21 -0700

    feat: defines useIsSignedIn hook

---
+++ packages/react-core/src/hooks/useIsSignedIn.ts
@@ -0,0 +1,45 @@
+import { Hub } from 'aws-amplify/utils';
+import { useEffect, useState } from 'react';
+import { HubCallback } from '@aws-amplify/core';
+import { getCurrentUser } from 'aws-amplify/auth';
+
+/**
+ * Returns `false`:
+ * - if user has no credentials cached in browser storage
+ * - if user signs out of account
+ *
+ * Returns `true`:
+ * - if user has cached credentials
+ * - if user signs in to account
+ */
+export default function useIsSignedIn(): boolean {
+  const [isSigned, setIsSigned] = useState<boolean>(false);
+
+  useEffect(() => {
+    getCurrentUser()
+      .then(() => {
+        setIsSigned(true);
+      })
+      .catch(() => {
+        setIsSigned(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    const listener: HubCallback = ({ payload }) => {
+      if (payload.event === 'signedIn') {
+        setIsSigned(true);
+      } else if (payload.event === 'signedOut') {
+        setIsSigned(false);
+      }
+    };
+
+    const unmount = Hub.listen('auth', listener);
+
+    return () => {
+      unmount();
+    };
+  }, []);
+
+  return isSigned;
+}
