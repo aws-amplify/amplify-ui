b0b0c8a2afaad247331265085c8d6db47c7bbd82
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-07-11 12:05:25 -0400

    add element definitions

---
+++ packages/react-ai/src/components/AIConversation/context/elements/definitions.ts
@@ -0,0 +1,48 @@
+import { defineBaseElement } from '@aws-amplify/ui-react-core/elements';
+
+export interface AIConversationElements {
+  Button: typeof ButtonElement;
+  Icon: typeof IconElement;
+  Image: typeof ImageElement;
+  Text: typeof TextElement;
+  View: typeof ViewElement;
+}
+
+type ButtonElementProps = 'onClick' | 'type';
+type ButtonElementVariant = 'custom-action' | 'message-submit';
+
+export const ButtonElement = defineBaseElement<
+  'button',
+  ButtonElementProps,
+  ButtonElementVariant
+>({ type: 'button', displayName: 'Button' });
+
+type IconVariant = 'avatar' | 'custom-action';
+
+export const IconElement = defineBaseElement<'svg', never, IconVariant>({
+  type: 'svg',
+  displayName: 'Icon',
+});
+
+export const ImageElement = defineBaseElement({
+  type: 'img',
+  displayName: 'Image',
+});
+
+export const TextElement = defineBaseElement({
+  type: 'p',
+  displayName: 'Text',
+});
+
+export const ViewElement = defineBaseElement({
+  type: 'div',
+  displayName: 'View',
+});
+
+export const AIConversationElements: AIConversationElements = {
+  Button: ButtonElement,
+  Icon: IconElement,
+  Image: ImageElement,
+  Text: TextElement,
+  View: ViewElement,
+};
