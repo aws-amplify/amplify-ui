527b6a059406cee9703b7daf130e3d61648f7376
Danny Banks <djb@amazon.com>
2024-07-11 09:00:46 -0700

    fix(authenticator): fixing visual inconsistencies

--- packages/react/src/components/Authenticator/shared/ConfirmSignInFooter.tsx
+++ packages/react/src/components/Authenticator/shared/ConfirmSignInFooter.tsx
@@ -20,20 +20,13 @@ export const ConfirmSignInFooter = (): JSX.Element => {
         isDisabled={isPending}
         type="submit"
         variation="primary"
-        fontWeight="normal"
         isLoading={isPending}
         loadingText={getConfirmingText()}
       >
         {getConfirmText()}
       </Button>
 
-      <Button
-        onClick={toSignIn}
-        type="button"
-        variation="link"
-        fontWeight="normal"
-        size="small"
-      >
+      <Button onClick={toSignIn} type="button" variation="link" size="small">
         {getBackToSignInText()}
       </Button>
     </Flex>
