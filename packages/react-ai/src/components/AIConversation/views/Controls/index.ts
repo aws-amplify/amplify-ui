d8fc0533d9c1085ba892f94d7289b78fa6757774
Emma Sauerborn <70536670+esauerbo@users.noreply.github.com>
2024-07-18 16:37:33 -0400

    Remove circular dep (#5422)

--- packages/react-ai/src/components/AIConversation/views/Controls/index.ts
+++ packages/react-ai/src/components/AIConversation/views/Controls/index.ts
@@ -1,14 +1,12 @@
 import { PromptControl, AutoHidablePromptControl } from './PromptControl';
 import { ActionsBarControl } from './ActionsBarControl';
 import { AvatarControl } from './AvatarControl';
-import Conversation from '../ConversationView';
 import { HeaderControl } from './HeaderControl';
 import { MessagesControl } from './MessagesControl';
 
 export {
   ActionsBarControl,
   AvatarControl,
-  Conversation,
   HeaderControl,
   MessagesControl,
   PromptControl,
