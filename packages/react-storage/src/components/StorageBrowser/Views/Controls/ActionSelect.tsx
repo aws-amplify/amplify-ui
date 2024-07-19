d3a54103390a0fc4bf59c7983ad38bc57e240731
Heather <hbuchel@gmail.com>
2024-07-19 12:25:37 -0400

    test ouput

--- packages/react-storage/src/components/StorageBrowser/Views/Controls/ActionSelect.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/Controls/ActionSelect.tsx
@@ -42,15 +42,15 @@ type RenderActionItem = (props: { action: Action }) => React.JSX.Element;
 
 /* <ActionItem /> */
 
-const UploadFileIcon = withBaseElementProps(Icon, {
-  children: (
-    <path
-      d="M10.8 20.4H13.2V15.39L15.12 17.31L16.8 15.6L12 10.8L7.2 15.6L8.91 17.28L10.8 15.39V20.4ZM4.8 24C4.14 24 3.575 23.765 3.105 23.295C2.635 22.825 2.4 22.26 2.4 21.6V2.4C2.4 1.74 2.635 1.175 3.105 0.705C3.575 0.235 4.14 0 4.8 0H14.4L21.6 7.2V21.6C21.6 22.26 21.365 22.825 20.895 23.295C20.425 23.765 19.86 24 19.2 24H4.8ZM13.2 8.4V2.4H4.8V21.6H19.2V8.4H13.2Z"
-      fill="currentColor"
-    />
-  ),
-  ...ICON_ATTRIBUTES,
-});
+// const UploadFileIcon = withBaseElementProps(Icon, {
+//   children: (
+//     <path
+//       d="M10.8 20.4H13.2V15.39L15.12 17.31L16.8 15.6L12 10.8L7.2 15.6L8.91 17.28L10.8 15.39V20.4ZM4.8 24C4.14 24 3.575 23.765 3.105 23.295C2.635 22.825 2.4 22.26 2.4 21.6V2.4C2.4 1.74 2.635 1.175 3.105 0.705C3.575 0.235 4.14 0 4.8 0H14.4L21.6 7.2V21.6C21.6 22.26 21.365 22.825 20.895 23.295C20.425 23.765 19.86 24 19.2 24H4.8ZM13.2 8.4V2.4H4.8V21.6H19.2V8.4H13.2Z"
+//       fill="currentColor"
+//     />
+//   ),
+//   ...ICON_ATTRIBUTES,
+// });
 
 const UploadFolderIcon = withBaseElementProps(Icon, {
   children: (
@@ -75,12 +75,12 @@ const ActionButton = withBaseElementProps(Button, {
 
 const ActionItem: ActionItem = () => (
   <ActionButton>
-    <ActionIcon />
+    <UploadFolderIcon />
   </ActionButton>
 );
 
 ActionItem.Button = ActionButton;
-ActionItem.Icon = ActionIcon;
+ActionItem.Icon = UploadFolderIcon;
 
 /* <ActionsMenu /> */
 interface ActionsMenu<
