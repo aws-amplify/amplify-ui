6cff6b4ef6e8bc5e11e8442d65fb76f4c029b66d
Heather <hbuchel@gmail.com>
2024-07-23 13:24:15 -0400

    updates

--- packages/react-storage/src/components/StorageBrowser/Views/Controls/Navigate.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/Controls/Navigate.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { StorageBrowserElements } from '../../context/elements';
+import { withBaseElementProps } from '@aws-amplify/ui-react-core/elements';
 
-const { Span, Button, Nav, ListItem } = StorageBrowserElements;
+import { StorageBrowserElements } from '../../context/elements';
+import { CLASS_BASE } from '../constants';
+
+const { Span, Button, Nav, OrderedList, ListItem } = StorageBrowserElements;
+const BLOCK_NAME = `${CLASS_BASE}__paginate`;
 
 type PermissionType = 'READ' | 'READWRITE' | 'WRITE';
 type LocationType = 'OBJECT' | 'PREFIX' | 'BUCKET';
@@ -9,9 +13,21 @@ interface LocationData {
   name: string;
   permissionType: PermissionType;
   locationType: LocationType;
+  current: boolean;
 }
 
+/* <Separator /> */
+const Separator = withBaseElementProps(
+  Span,
+  ({ className = `${BLOCK_NAME}__separator`, ...props }) => ({
+    ...props,
+    className,
+  })
+);
+
+/* <NavigateItem /> */
 type RenderNavigateItem = (props: { item: LocationData }) => React.JSX.Element;
+
 interface NavigateItem<
   T extends StorageBrowserElements = StorageBrowserElements,
 > extends RenderNavigateItem,
@@ -19,6 +35,51 @@ interface NavigateItem<
   Separator: T['Span'];
 }
 
+const NavigateItemContainer = withBaseElementProps(
+  ListItem,
+  ({ className = `${BLOCK_NAME}__item`, ...props }) => ({ ...props, className })
+);
+
+const NavigateButton = withBaseElementProps(
+  Button,
+  ({ className = `${BLOCK_NAME}__button`, ...props }) => ({
+    ...props,
+    className,
+  })
+);
+
+const NavigateItem: NavigateItem = ({ item }) => {
+  return (
+    <NavigateItemContainer>
+      <NavigateButton />
+      {item.current ? null : <Separator />}
+    </NavigateItemContainer>
+  );
+};
+
+NavigateItem.Button = Button;
+NavigateItem.Separator = Separator;
+NavigateItem.ListItem = ListItem;
+
+/* <NavigateContainer /> */
+const NavigateContainer: NavigateControl['Container'] = React.forwardRef(
+  function Container({ children, ...props }, ref) {
+    return (
+      <Nav
+        {...props}
+        aria-label={props['aria-label'] ?? 'Breadcrumbs'}
+        className={props.className ?? `${BLOCK_NAME}__container`}
+        ref={ref}
+      >
+        <OrderedList className={`${props.className ?? BLOCK_NAME}__list`}>
+          {children}
+        </OrderedList>
+      </Nav>
+    );
+  }
+);
+
+/* <NavigateControl /> */
 export interface NavigateControl<
   T extends StorageBrowserElements = StorageBrowserElements,
 > {
@@ -27,13 +88,12 @@ export interface NavigateControl<
   NavigateItem: NavigateItem<T>;
 }
 
-const NavigateItem: NavigateItem = () => <>Hi</>;
-NavigateItem.Button = Button;
-NavigateItem.Separator = Span;
-NavigateItem.ListItem = ListItem;
-
 export const NavigateControl: NavigateControl = (_props) => {
-  return <>Navigate</>;
+  return (
+    <NavigateContainer>
+      <NavigateButton>Test</NavigateButton>
+    </NavigateContainer>
+  );
 };
-NavigateControl.Container = Nav;
+NavigateControl.Container = NavigateContainer;
 NavigateControl.NavigateItem = NavigateItem;
