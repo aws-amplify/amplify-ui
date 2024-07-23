ac5596761dd6fa810ce8360f9ff05a08fc8d4427
timngyn <timngyn@gmail.com>
2024-07-23 15:08:25 -0700

    Add reducer scaffold for ActionSelect

---
+++ packages/react-storage/src/components/StorageBrowser/context/controls/ActionSelect/__tests__/ActionSelect.spec.tsx
@@ -0,0 +1,8 @@
+describe('actionSelectReducer', () => {
+  it.todo('handles a SELECT_ACTION_TYPE as expected');
+  it.todo('handles a DESELECT_ACTION_TYPE as expected');
+  it.todo('handles a SET_UPLOAD_ITEMS as expected');
+  it.todo('handles a SELECT_LOCATION_ITEM as expected');
+  it.todo('handles a DESELECT_LOCATION_ITEM as expected');
+  it.todo('handles a DESELECT_ALL_LOCATION_ITEMS as expected');
+});
---
+++ packages/react-storage/src/components/StorageBrowser/context/controls/ActionSelect/index.ts
@@ -0,0 +1,5 @@
+export {
+  ActionSelectProvider,
+  ActionSelectContext,
+  ActionSelectStateContext,
+} from './ActionSelect';
