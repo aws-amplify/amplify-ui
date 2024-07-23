7e54a17cc80645672e908d86cf6b69631a276a59
AhmetAhunbayAWS <ahmetahu@amazon.com>
2024-07-22 22:31:28 -0700

    fix: updates jest usage in tests

--- packages/react-core/src/hooks/__tests__/useIsSignedIn.test.ts
+++ packages/react-core/src/hooks/__tests__/useIsSignedIn.test.ts
@@ -1,72 +1,69 @@
 import useIsSignedIn from '../useIsSignedIn';
 import { act, renderHook } from '@testing-library/react-hooks';
-import { getCurrentUser } from 'aws-amplify/auth';
+import * as AuthModule from 'aws-amplify/auth';
 import { GetCurrentUserOutput } from 'aws-amplify/auth';
 import { Hub } from 'aws-amplify/utils';
 
-jest.mock('aws-amplify/auth', () => ({
-  getCurrentUser: jest.fn(),
-}));
+const getCurrentUserSpy = jest.spyOn(AuthModule, 'getCurrentUser');
+
+const mockError = new Error('Authorization error');
+const mockSuccess: GetCurrentUserOutput = {
+  username: 'username',
+  userId: '123',
+};
 
 describe('useIsSignedIn', () => {
-  it('should be falsy if user is not signed in', () => {
-    const mockError = new Error('Authorization error');
-    (getCurrentUser as jest.Mock).mockRejectedValueOnce(mockError);
+  it('should be false if user is not signed in', () => {
+    getCurrentUserSpy.mockRejectedValueOnce(mockError);
 
     const { result } = renderHook(() => useIsSignedIn());
-    expect(result.current).toBeFalsy();
+    expect(result.current).toBe(false);
   });
 
-  it('should be truthy if user is already signed in', async () => {
+  it('should be truth if user is already signed in', async () => {
     const mockUserCredentials: GetCurrentUserOutput = {
       username: 'username',
       userId: '123',
     };
-    (getCurrentUser as jest.Mock).mockResolvedValueOnce(mockUserCredentials);
+    getCurrentUserSpy.mockResolvedValueOnce(mockUserCredentials);
 
     const { result, waitForNextUpdate } = renderHook(() => useIsSignedIn());
-    expect(result.current).toBeFalsy();
+    expect(result.current).toBe(false);
 
     await waitForNextUpdate();
-    expect(result.current).toBeTruthy();
+    expect(result.current).toBe(true);
   });
 
-  it('should be truthy if receiving a signedIn event', () => {
-    const mockError = new Error('Authorization error');
-    (getCurrentUser as jest.Mock).mockRejectedValueOnce(mockError);
+  it('should be truth if receiving a signedIn event', () => {
+    getCurrentUserSpy.mockRejectedValueOnce(mockError);
 
     const { result, waitForNextUpdate } = renderHook(() => useIsSignedIn());
-    expect(result.current).toBeFalsy();
+    expect(result.current).toBe(false);
 
     waitForNextUpdate();
 
     act(() => {
       Hub.dispatch('auth', { event: 'signedIn' });
     });
-    expect(result.current).toBeTruthy();
+    expect(result.current).toBe(true);
   });
 
-  it('should be falsy if receiving a signedOut event', async () => {
-    const mockUserCredentials: GetCurrentUserOutput = {
-      username: 'username',
-      userId: '123',
-    };
-    (getCurrentUser as jest.Mock).mockResolvedValueOnce(mockUserCredentials);
+  it('should be false if receiving a signedOut event', async () => {
+    getCurrentUserSpy.mockResolvedValueOnce(mockSuccess);
 
     const { result, waitForNextUpdate } = renderHook(() => useIsSignedIn());
 
     await waitForNextUpdate();
-    expect(result.current).toBeTruthy();
+    expect(result.current).toBe(true);
 
     act(() => {
       Hub.dispatch('auth', { event: 'signedOut' });
     });
-    expect(result.current).toBeFalsy();
+    expect(result.current).toBe(false);
   });
 
   it('should be able to listen to multiple events after one call', () => {
-    const mockError = new Error('Authorization error');
-    (getCurrentUser as jest.Mock).mockRejectedValueOnce(mockError);
+    getCurrentUserSpy.mockRejectedValueOnce(mockError);
 
     const { result, waitForNextUpdate } = renderHook(() => useIsSignedIn());
 
@@ -75,16 +72,16 @@ describe('useIsSignedIn', () => {
     act(() => {
       Hub.dispatch('auth', { event: 'signedIn' });
     });
-    expect(result.current).toBeTruthy();
+    expect(result.current).toBe(true);
 
     act(() => {
       Hub.dispatch('auth', { event: 'signedOut' });
     });
-    expect(result.current).toBeFalsy();
+    expect(result.current).toBe(false);
 
     act(() => {
       Hub.dispatch('auth', { event: 'signedIn' });
     });
-    expect(result.current).toBeTruthy();
+    expect(result.current).toBe(true);
   });
 });
