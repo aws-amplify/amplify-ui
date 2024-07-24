6358e74a58f82ffe85b339396de25246b8e4b241
Heather <hbuchel@gmail.com>
2024-07-24 12:51:45 -0400

    update tests

--- packages/react-storage/src/components/StorageBrowser/Views/Controls/__tests__/ActionsSelect.spec.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/Controls/__tests__/ActionsSelect.spec.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ActionSelectControl } from '../ActionSelect';
 
 describe('ActionSelectControl', () => {
   it('renders a `ActionSelectControl`', () => {
-    expect(render(<ActionSelectControl />).container).toBeDefined();
+    render(<ActionSelectControl />);
+    const toggle = screen.getByRole('button', { name: 'Actions' });
+    const menu = screen.getByRole('menu', {
+      name: 'Actions',
+    });
+    expect(menu).toBeInTheDocument();
+    expect(toggle).toBeInTheDocument();
+  });
+
+  it('renders an `ActionItem`', () => {
+    render(
+      <ActionSelectControl.ActionsMenu.ActionItem
+        action={{ displayName: 'Upload folder', type: 'FOLDER' }}
+      />
+    );
+    const actionItem = screen.getByRole('menuitem', { name: 'Upload folder' });
+
+    expect(actionItem).toBeInTheDocument();
+  });
+
+  it('renders an `ActionItem with no variant`', () => {
+    render(
+      <ActionSelectControl.ActionsMenu.ActionItem
+        action={{ displayName: 'Upload folder', type: 'FOLDER' }}
+      />
+    );
+    const actionItem = screen.getByRole('menuitem', { name: 'Upload folder' });
+    const svg = actionItem.querySelector('svg');
+    expect(svg).not.toBeInTheDocument();
   });
 });
