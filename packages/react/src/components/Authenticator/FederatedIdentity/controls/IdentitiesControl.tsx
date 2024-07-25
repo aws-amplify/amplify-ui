f9578c89e88e54afdacd718fde2f60a4f9c26091
AhmetAhunbayAWS <ahmetahu@amazon.com>
2024-07-25 10:53:24 -0700

    fix: cleans imports and comment

--- packages/react/src/components/Authenticator/FederatedIdentity/controls/IdentitiesControl.tsx
+++ packages/react/src/components/Authenticator/FederatedIdentity/controls/IdentitiesControl.tsx
@@ -1,10 +1,9 @@
+import { IdentityControl } from './IdentityControl';
 import { FederatedIdentityElements } from '../context/elements';
 import { ProviderData } from '../types';
-import { IdentityControl } from './IdentityControl';
 
 export interface IdentitiesControl<
   T extends Partial<FederatedIdentityElements>,
-  // Button, Icon are ControlElemnts
 > {
   (props: {
     renderListItem?: { (data: ProviderData): React.JSX.Element };
