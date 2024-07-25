2479376166aa1984670b9cbfb58232291118e36b
Heather Buchel <hbuchel@gmail.com>
2024-07-25 18:06:05 -0400

    remove composability for Message, move OmitElements to types (#5472)

--- packages/react-storage/src/components/StorageBrowser/Views/types.ts
+++ packages/react-storage/src/components/StorageBrowser/Views/types.ts
@@ -4,6 +4,11 @@ import { Controls } from './Controls';
 
 export type CommonControl = 'Message' | 'Navigate' | 'Title';
 
+export type OmitElements<T, K extends string = never> = Omit<
+  T,
+  keyof StorageBrowserElements | K
+>;
+
 export interface ViewComponent<T extends StorageBrowserElements, C> {
   (): React.JSX.Element;
   Controls: C;
