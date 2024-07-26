ccde35cfecfb7f39eabfc15f4aed1248751cc06d
AhmetAhunbayAWS <ahmetahu@amazon.com>
2024-07-26 16:38:07 -0700

    feat: creates TS error for conflicting render

--- packages/react/src/components/Authenticator/FederatedIdentity/controls/IdentitiesControl.tsx
+++ packages/react/src/components/Authenticator/FederatedIdentity/controls/IdentitiesControl.tsx
@@ -2,9 +2,16 @@ import { IdentityControl } from './IdentityControl';
 import { ProviderData } from '../types';
 
 export interface IdentitiesControl<T extends string = string> {
-  (props: {
-    renderListItem?: { (data: ProviderData): React.JSX.Element };
-    children?: React.ReactNode;
-  }): JSX.Element;
+  (props: ChildrenProps | RenderIdentityProps): JSX.Element;
   Identity: IdentityControl<T>;
+}
+
+interface ChildrenProps {
+  children?: React.ReactNode;
+  renderIdentity?: never;
+}
+
+interface RenderIdentityProps {
+  children?: never;
+  renderIdentity?: { (providerData: ProviderData): JSX.Element };
 }
