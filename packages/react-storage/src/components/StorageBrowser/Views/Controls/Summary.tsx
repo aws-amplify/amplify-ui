5e944ffd59aaaba146484e8344892a1f44d98ff7
Heather <hbuchel@gmail.com>
2024-07-29 14:59:46 -0400

    Simplify interfaces, removed named instances

--- packages/react-storage/src/components/StorageBrowser/Views/Controls/Summary.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/Controls/Summary.tsx
@@ -5,108 +5,78 @@ import type { OmitElements } from '../types';
 import { StorageBrowserElements } from '../../context/elements';
 import { CLASS_BASE } from '../constants';
 
-const { DefinitionDetail, DefinitionList, DefinitionTerm, View } =
-  StorageBrowserElements;
+const {
+  DefinitionDetail: DefinitionDetailElement,
+  DefinitionList,
+  DefinitionTerm: DefinitionTermElement,
+  View,
+} = StorageBrowserElements;
 
-const BLOCK_NAME = `${CLASS_BASE}__details`;
+const BLOCK_NAME = `${CLASS_BASE}__definitions`;
 
 /* <Detail /> */
-const DetailContainer = withBaseElementProps(View, {
-  className: `${CLASS_BASE}__detail`,
+const DefinitionContainer = withBaseElementProps(View, {
+  className: `${CLASS_BASE}__definition`,
 });
 
-const DetailLabel = withBaseElementProps(DefinitionTerm, {
-  className: `${CLASS_BASE}__detail__label`,
+const DefinitionTerm = withBaseElementProps(DefinitionTermElement, {
+  className: `${CLASS_BASE}__definition__label`,
 });
-const DetailValue = withBaseElementProps(DefinitionDetail, {
-  className: `${CLASS_BASE}__detail__value`,
+const DefinitionDetail = withBaseElementProps(DefinitionDetailElement, {
+  className: `${CLASS_BASE}__definition__value`,
 });
 
-interface DetailProps {
+interface DefinitionProps {
   label?: string;
   value?: string;
 }
-interface Detail<T extends StorageBrowserElements = StorageBrowserElements> {
-  (props: DetailProps): React.JSX.Element;
+interface Definition<
+  T extends StorageBrowserElements = StorageBrowserElements,
+> {
+  (props: DefinitionProps): React.JSX.Element;
   Container: T['View'];
-  Label: T['DefinitionTerm'];
-  Value: T['DefinitionDetail'];
+  Term: T['DefinitionTerm'];
+  Detail: T['DefinitionDetail'];
 }
 
-const Detail: Detail = (props: DetailProps) => {
+const Definition: Definition = (props: DefinitionProps) => {
   const { label, value } = props;
 
   return (
-    <DetailContainer>
-      <DetailLabel>{label}</DetailLabel>
-      <DetailValue>{value}</DetailValue>
-    </DetailContainer>
+    <DefinitionContainer>
+      <DefinitionTerm>{label}</DefinitionTerm>
+      <DefinitionDetail>{value}</DefinitionDetail>
+    </DefinitionContainer>
   );
 };
-Detail.Container = DetailContainer;
-Detail.Label = DetailLabel;
-Detail.Value = DetailValue;
-
-const CompletedControl = ({
-  label = 'Completed:',
-  value = '0/5',
-}: DetailProps) => <Detail label={label} value={value} />;
-
-const NotStartedControl = ({
-  label = 'Not started:',
-  value = '0/5',
-}: DetailProps) => <Detail label={label} value={value} />;
-
-const FailedControl = ({ label = 'Failed', value = '0/5' }: DetailProps) => (
-  <Detail label={label} value={value} />
-);
-
-const CanceledControl = ({
-  label = 'Canceled:',
-  value = '0/5',
-}: DetailProps) => <Detail label={label} value={value} />;
-
-const DestinationControl = ({
-  label = 'Destination:',
-  value = '/',
-}: DetailProps) => <Detail label={label} value={value} />;
+Definition.Container = DefinitionContainer;
+Definition.Term = DefinitionTerm;
+Definition.Detail = DefinitionDetail;
 
 const Container = withBaseElementProps(DefinitionList, {
   className: BLOCK_NAME,
 });
 
+type RenderSummaryItem = (props: {
+  label: string;
+  value: string;
+}) => React.JSX.Element;
 interface _SummaryControl<
   T extends StorageBrowserElements = StorageBrowserElements,
 > {
-  (): React.JSX.Element;
+  (props: { renderSummaryItem?: RenderSummaryItem }): React.JSX.Element;
   Container: T['DefinitionList'];
-  Destination: Detail<T>;
-  Completed: Detail<T>;
-  Failed: Detail<T>;
-  Canceled: Detail<T>;
-  NotStarted: Detail<T>;
 }
 
 export interface SummaryControl<
   T extends StorageBrowserElements = StorageBrowserElements,
-> extends OmitElements<
-    _SummaryControl<T>,
-    | 'Container'
-    | 'Destination'
-    | 'Completed'
-    | 'Failed'
-    | 'Canceled'
-    | 'NotStarted'
-  > {
+> extends OmitElements<_SummaryControl<T>, 'Container'> {
   (): React.JSX.Element;
 }
 
 export const SummaryControl: SummaryControl = () => (
   <Container>
-    <DestinationControl />
-    <CompletedControl />
-    <CanceledControl />
-    <FailedControl />
-    <NotStartedControl />
+    {/* Example */}
+    <Definition label="Completed" value="0/5" />
   </Container>
 );
