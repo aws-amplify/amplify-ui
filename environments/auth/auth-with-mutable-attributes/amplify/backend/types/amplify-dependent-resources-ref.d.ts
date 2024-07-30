34556c2699de9542b6c0c3e6616d6b50f4e1b979
Axel Andrews <axeland@amazon.com>
2024-07-30 12:20:28 -0700

    feat: new environment with mutable attributes

---
+++ environments/auth/auth-with-mutable-attributes/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "authwithmutableattri485a0894485a0894": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
