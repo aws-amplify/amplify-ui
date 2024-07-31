9f6f766f463a9344d473cb9a04e23e82262fa148
AhmetAhunbayAWS <ahmetahu@amazon.com>
2024-07-31 16:23:46 -0700

    fix: overlaps federatedIdentities with GroupElement

---
+++ packages/react/src/components/Authenticator/FederatedIdentities/controls/FederatedIdentitiesControl.tsx
@@ -0,0 +1,24 @@
+import { IdentityControl } from './IdentityControl';
+import { RenderButton } from '../types';
+import { ForwardRefExoticComponent } from 'react';
+
+interface ChildrenProps {
+  children?: React.ReactNode;
+  RenderButton?: never;
+}
+
+interface RenderButtonProps<T extends string = string> {
+  children?: never;
+  RenderButton: RenderButton<T>;
+}
+
+type FederatedIdentitiesProps<T extends string = string> = (
+  | ChildrenProps
+  | RenderButtonProps<T>
+) &
+  React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+
+export interface FederatedIdentities<T extends string = string>
+  extends ForwardRefExoticComponent<FederatedIdentitiesProps<T>> {
+  Identity: IdentityControl<T>;
+}
