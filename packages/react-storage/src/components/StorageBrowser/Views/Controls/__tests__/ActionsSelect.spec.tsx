df21c774b1583560feeca640a5e3800f906ade34
Heather <hbuchel@gmail.com>
2024-07-31 16:25:56 -0400

    fix tests

--- packages/react-storage/src/components/StorageBrowser/Views/Controls/__tests__/ActionsSelect.spec.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/Controls/__tests__/ActionsSelect.spec.tsx
@@ -12,26 +12,4 @@ describe('ActionSelectControl', () => {
     expect(menu).toBeInTheDocument();
     expect(toggle).toBeInTheDocument();
   });
-
-  it('renders an `ActionItem`', () => {
-    render(
-      <ActionSelectControl.ActionsMenu.ActionItem
-        action={{ displayName: 'Upload folder', type: 'FOLDER' }}
-      />
-    );
-    const actionItem = screen.getByRole('menuitem', { name: 'Upload folder' });
-
-    expect(actionItem).toBeInTheDocument();
-  });
-
-  it('renders an `ActionItem with no variant`', () => {
-    render(
-      <ActionSelectControl.ActionsMenu.ActionItem
-        action={{ displayName: 'Upload folder', type: 'FOLDER' }}
-      />
-    );
-    const actionItem = screen.getByRole('menuitem', { name: 'Upload folder' });
-    const svg = actionItem.querySelector('svg');
-    expect(svg).not.toBeInTheDocument();
-  });
 });
