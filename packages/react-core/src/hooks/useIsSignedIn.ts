d82f6a29e5d389e8cd0c7e63ccd79ca5c1c8948f
AhmetAhunbayAWS <ahmetahu@amazon.com>
2024-08-01 10:41:39 -0700

    fix: removes multiple state variables

--- packages/react-core/src/hooks/useIsSignedIn.ts
+++ packages/react-core/src/hooks/useIsSignedIn.ts
@@ -1,50 +1,35 @@
 import { getCurrentUser } from 'aws-amplify/auth';
 import { Hub, HubCallback } from '@aws-amplify/core';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import useDataState, { DataState } from './useDataState';
-import { AuthError } from 'aws-amplify/auth';
 
-const USER_UNAUTHENTICATED_EXCEPTION = 'UserUnAuthenticatedException';
-
-const action = async (_: { isSignedIn: boolean }, __: undefined) => {
+const action = async (
+  _: { isSignedIn: boolean },
+  input: { setState?: boolean }
+) => {
   try {
     await getCurrentUser();
     return { isSignedIn: true };
   } catch (error) {
-    if (
-      error instanceof AuthError &&
-      error.name === USER_UNAUTHENTICATED_EXCEPTION
-    ) {
+    if (input?.setState) {
       return { isSignedIn: false };
     }
     throw error;
   }
 };
 
-const defaultState: DataState<{ isSignedIn: boolean }> = {
-  hasError: false,
-  isLoading: false,
-  message: undefined,
-  data: { isSignedIn: false },
-};
-
 export default function useIsSignedIn(): DataState<{ isSignedIn: boolean }> {
   const [state, handler] = useDataState(action, { isSignedIn: false });
-  const [effectState, setEffectState] =
-    useState<DataState<{ isSignedIn: boolean }>>(state);
-
-  useEffect(() => {
-    setEffectState(state);
-  }, [state]);
 
   useEffect(() => {
     handler();
 
     const listener: HubCallback = ({ payload }) => {
       if (payload.event === 'signedIn') {
-        setEffectState({ ...defaultState, data: { isSignedIn: true } });
+        handler();
       } else if (payload.event === 'signedOut') {
-        setEffectState(defaultState);
+        handler({ setState: true });
+        handler();
       }
     };
 
@@ -55,5 +40,5 @@ export default function useIsSignedIn(): DataState<{ isSignedIn: boolean }> {
     };
   }, [handler]);
 
-  return effectState;
+  return state;
 }
