85ac4a490860a85b91b8b043d9d55e96d9468598
Thadd Trinh <thadtrin@amazon.com>
2024-08-01 10:17:01 -0700

    add unit test stubs for conversation

--- packages/react-ai/src/hooks/__tests__/createAIHooks.test.tsx
+++ packages/react-ai/src/hooks/__tests__/createAIHooks.test.tsx
@@ -1,5 +1,7 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import { createAIHooks } from '../createAIHooks';
+import { AIContextProvider } from '../AIContextProvider';
+import React from 'react';
 
 const mockClient = jest.fn();
 
@@ -29,6 +31,18 @@ describe('createAIHooks', () => {
       ] = result.current;
       expect(messages).toHaveLength(3);
     });
+
+    it.todo('hook can send a message which updates state');
+
+    it.todo('hook can receive new messages from the conversation subscription');
+
+    it.todo(
+      'multiple hook uses with the same route and id combination have shared message state'
+    );
+    // const wrapper = ({ children }: { children: React.ReactNode }) => (
+    //   <AIContextProvider>{children}</AIContextProvider>
+    // )
+    // const { result } = renderHook(() => useAIConversation('pirateChat'), { wrapper });
   });
 
   describe('useAIGeneration', () => {
