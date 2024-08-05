49c178fb36200298caea3b6575a2aba175908b24
Tim Nguyen <54393192+timngyn@users.noreply.github.com>
2024-08-05 12:59:12 -0700

    chore(storage-browser): set up LocationDetailViewTable (#5528)
    * chore(storage-browser): set up LocationDetailViewTable
    
    * update test to render location items
    
    * add comment for not rendering current prefix as a row item in the table

--- packages/react-storage/src/components/StorageBrowser/Views/LocationDetailView/LocationDetailView.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/LocationDetailView/LocationDetailView.tsx
@@ -1,78 +1,19 @@
 import React from 'react';
 
-import { LocationData, useAction } from '../../context/actions';
-import { parseLocationAccess } from '../../context/controls/Navigate/utils';
 import { StorageBrowserElements } from '../../context/elements';
 import { ViewComponent } from '../types';
 import { LocationDetailViewControls } from './Controls';
 import { CLASS_BASE } from '../constants';
-import { useControl } from '../../context/controls';
 
 export interface LocationDetailView<
   T extends StorageBrowserElements = StorageBrowserElements,
 > extends ViewComponent<LocationDetailViewControls<T>> {}
 
 export const LocationDetailView: LocationDetailView = () => {
-  const [{ history, location }, handleUpdateState] = useControl({
-    type: 'NAVIGATE',
-  });
-
-  const [{ data, isLoading }, handleList] = useAction({
-    type: 'LIST_LOCATION_ITEMS',
-  });
-
-  const { prefix: initialPrefix } = location
-    ? parseLocationAccess(location)
-    : ({} as LocationData);
-
-  const prefix =
-    history.length === 1 ? initialPrefix : history[history.length - 1];
-
-  const hasItems = !!data.result?.length;
-  const shouldReset = hasItems && !location && !history.length;
-
-  React.useEffect(() => {
-    if (shouldReset) {
-      handleList({ prefix: '', options: { reset: true } });
-    }
-
-    if (typeof prefix !== 'string') return;
-
-    handleList({ prefix, options: { pageSize: 1000, refresh: true } });
-  }, [handleList, prefix, shouldReset]);
-
-  const listItems = !hasItems
-    ? null
-    : data.result.map(({ key, type }) => {
-        if (key === prefix) {
-          return null;
-        }
-        if (type === 'FOLDER') {
-          return (
-            <button
-              onClick={() => {
-                handleUpdateState({
-                  type: 'NAVIGATE',
-                  prefix: key,
-                });
-              }}
-              key={key}
-            >
-              {key}
-            </button>
-          );
-        } else {
-          return <p key={key}>{key}</p>;
-        }
-      });
-
   return (
-    <>
-      <div className={CLASS_BASE} data-testid="LOCATION_DETAIL_VIEW">
-        <LocationDetailViewControls />
-      </div>
-      {isLoading && !hasItems ? <span>loading...</span> : listItems}
-    </>
+    <div className={CLASS_BASE} data-testid="LOCATION_DETAIL_VIEW">
+      <LocationDetailViewControls />
+    </div>
   );
 };
 
