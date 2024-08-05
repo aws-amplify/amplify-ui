bf6828e32ffc244a778b3ab4d460ef5176c6dce4
Tim Nguyen <54393192+timngyn@users.noreply.github.com>
2024-08-04 19:51:24 -0700

    chore(storage-browser): set up create folder action view  (#5520)
    * chore(storage-browser): set up create folder action view
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

---
+++ packages/react-storage/src/components/StorageBrowser/Views/CreateFolderActionView/Controls.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Controls } from '../Controls';
+import { StorageBrowserElements } from '../../context/elements';
+
+export interface CreateFolderActionViewControls<
+  T extends StorageBrowserElements = StorageBrowserElements,
+> extends Pick<Controls<T>, 'Target'> {
+  (): React.JSX.Element;
+}
+
+const { Target } = Controls;
+
+// @ts-expect-error TODO: add Controls assignment
+export const CreateFolderActionViewControls: CreateFolderActionViewControls =
+  () => {
+    const [_folderName, setFolderName] = React.useState('');
+    const inputRef = React.useRef<HTMLInputElement>(null);
+
+    return (
+      <Target.Field.Container>
+        <Target.Field.Label htmlFor="folder-name-input">
+          Enter folder name:
+        </Target.Field.Label>
+        <Target.Field.Input type="text" id="folder-name-input" ref={inputRef} />
+        <Target.Field.Submit
+          onClick={() => {
+            setFolderName(() => {
+              return inputRef.current?.value ?? '';
+            });
+          }}
+        >
+          Create folder
+        </Target.Field.Submit>
+      </Target.Field.Container>
+    );
+  };
