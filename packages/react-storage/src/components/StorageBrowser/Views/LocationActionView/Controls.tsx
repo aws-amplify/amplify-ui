5fd4dd8e8587f0d72b4ac2a17b3c1ec1603d7819
Heather Buchel <hbuchel@gmail.com>
2024-08-09 14:01:32 -0400

    chore(storage-browser): create ActionStart/ActionCancel controls, update styles (#5540)
    * initial commit
    
    * update control styles, fix Summary dt/dd
    
    * update create folder
    
    * updates
    
    * update Exit usage
    
    * update Exit usage, fix css, consolidate custom element buttons
    
    * Rename ActionStart -> Primary
    
    * update tests
    
    * remove conditional
    
    * Update order
    
    * fix primary variant
    
    * fix primary variant
    
    * rename primaryProps
    
    * set done state to disabled
    
    * update styles, button text

--- packages/react-storage/src/components/StorageBrowser/Views/LocationActionView/Controls.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/LocationActionView/Controls.tsx
@@ -3,11 +3,8 @@ import React from 'react';
 import { useControl } from '../../context/controls';
 import { StorageBrowserElements } from '../../context/elements';
 
-import { CLASS_BASE } from '../constants';
-import { Controls, NavigateItem } from '../Controls';
+import { Controls } from '../Controls';
 import { CommonControl } from '../types';
-
-const { Button } = StorageBrowserElements;
 
 const { Title: TitleElement } = Controls;
 
@@ -15,28 +12,6 @@ export interface LocationActionViewControls<
   T extends StorageBrowserElements = StorageBrowserElements,
 > extends Pick<Controls<T>, CommonControl | 'Summary' | 'Title'> {
   (): React.JSX.Element;
-}
-
-export const Navigate = (): React.JSX.Element => {
-  const [, handleUpdateState] = useControl({ type: 'ACTION_SELECT' });
-
-  return (
-    <NavigateItem.Button
-      className={`${CLASS_BASE}__navigate__container`}
-      onClick={() => {
-        handleUpdateState({ type: 'EXIT' });
-      }}
-    >
-      Exit
-    </NavigateItem.Button>
-  );
-};
-
-export function Primary(props: {
-  children?: React.ReactNode;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
-}): React.JSX.Element {
-  return <Button {...props} className={`${CLASS_BASE}__refresh`} />;
 }
 
 export const Title = (): React.JSX.Element => {
