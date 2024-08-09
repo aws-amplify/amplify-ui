ef36d005b70fc8a1e8b99d81eb427d58c8b4d706
Heather <hbuchel@gmail.com>
2024-08-09 11:40:56 -0400

    update message control

--- examples/next/pages/ui/components/storage/storage-browser/default-auth/custom-elements.tsx
+++ examples/next/pages/ui/components/storage/storage-browser/default-auth/custom-elements.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {
   Button as _Button,
+  Flex,
   Table as _Table,
   TableBody as _TableBody,
   TableCell as _TableCell,
   TableHead as _TableHead,
   TableRow as _TableRow,
   Heading as _Heading,
+  Message as _Message,
   View as _View,
 } from '@aws-amplify/ui-react';
 
@@ -26,6 +28,16 @@ const Button = React.forwardRef<HTMLButtonElement>(function Button(props, ref) {
           borderRadius="0"
           justifyContent="flex-start"
           ref={ref as any}
+        />
+      );
+    case 'message-dismiss':
+      return (
+        <_Button
+          {...props}
+          size="small"
+          variation="link"
+          colorTheme="overlay"
+          ref={ref}
         />
       );
     case 'navigate':
@@ -121,6 +133,18 @@ const View = React.forwardRef<HTMLDivElement>(function View(props, ref) {
           padding="small"
           ref={ref as any}
         />
+      );
+    case 'info':
+    case 'warning':
+    case 'success':
+    case 'error':
+      const { children } = props as any;
+      return (
+        <_Message {...props} hasIcon={false} colorTheme={variant} ref={ref}>
+          <Flex gap="small" alignItems="center">
+            {children}
+          </Flex>
+        </_Message>
       );
     default:
       return <_View {...props} ref={ref as any} />;
