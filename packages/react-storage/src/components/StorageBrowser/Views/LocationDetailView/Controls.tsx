9b90809cf58c436b21e9cb815cce4e84a872ece1
Tim Nguyen <54393192+timngyn@users.noreply.github.com>
2024-08-16 12:15:27 -0700

    Feat storage browser/refresh location detail view (#5572)
    * chore(storage-browser): add refresh button to locations view
    
    * make props optional
    
    * Update packages/react-storage/src/components/StorageBrowser/Views/LocationsView/LocationsView.tsx
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>
    
    * chore(storage-browser): add refresh for location detail view
    
    * Add action controls container
    
    * remove unncessary grid-template-columns
    
    * use self closing tag
    
    * disable refresh button when there are no rows
    
    * Disable refresh when no rows
    
    * lower the testing coverage by .16
    
    * Update packages/react-storage/src/components/StorageBrowser/Views/Controls/Refresh.tsx
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>
    
    * add test for location detail view refresh button
    
    * fix typo, add call to userEvent.setup()
    
    * remove unnecessary div
    
    * remove unused import
    
    * add pseudo-class to position refresh when it has action-menu sibling
    
    ---------
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

--- packages/react-storage/src/components/StorageBrowser/Views/LocationDetailView/Controls.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/LocationDetailView/Controls.tsx
@@ -4,8 +4,9 @@ import { StorageBrowserElements } from '../../context/elements';
 import { useControl } from '../../context/controls';
 import { Controls, LocationDetailViewTable } from '../Controls';
 import { CommonControl } from '../types';
+import { useAction } from '../../context/actions';
 
-const { ActionSelect, Navigate, Title: TitleElement } = Controls;
+const { ActionSelect, Navigate, Refresh, Title: TitleElement } = Controls;
 
 export interface LocationDetailViewControls<
   T extends StorageBrowserElements = StorageBrowserElements,
@@ -24,12 +25,34 @@ export const Title = (): React.JSX.Element => {
   return <TitleElement>{title}</TitleElement>;
 };
 
+const LocationDetailViewRefresh = () => {
+  const [{ history }] = useControl({
+    type: 'NAVIGATE',
+  });
+
+  const [{ data, isLoading }, handleList] = useAction({
+    type: 'LIST_LOCATION_ITEMS',
+  });
+
+  const prefix = history.join('');
+
+  return (
+    <Refresh
+      disabled={isLoading || data.result.length <= 0}
+      onClick={() =>
+        handleList({ prefix, options: { refresh: true, pageSize: 1000 } })
+      }
+    />
+  );
+};
+
 // @ts-expect-error TODO: add Controls assignment
 export const LocationDetailViewControls: LocationDetailViewControls = () => {
   return (
     <>
       <Navigate />
       <Title />
+      <LocationDetailViewRefresh />
       <ActionSelect />
       <LocationDetailViewTable />
     </>
