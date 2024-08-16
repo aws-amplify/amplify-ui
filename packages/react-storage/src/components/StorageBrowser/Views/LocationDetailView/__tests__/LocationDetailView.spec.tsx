7d7f769c33a14800a71b20e77e4ce7fa4f335ed6
Tim Nguyen <54393192+timngyn@users.noreply.github.com>
2024-08-16 12:15:27 -0700

    Feat storage browser/refresh location detail view (#5572)
    * chore(storage-browser): add refresh button to locations view
    
    * make props optional
    
    * Update packages/react-storage/src/components/StorageBrowser/Views/LocationsView/LocationsView.tsx
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>
    
    * chore(storage-browser): add refresh for location detail view
    
    * Add action controls container
    
    * remove unncessary grid-template-columns
    
    * use self closing tag
    
    * disable refresh button when there are no rows
    
    * Disable refresh when no rows
    
    * lower the testing coverage by .16
    
    * Update packages/react-storage/src/components/StorageBrowser/Views/Controls/Refresh.tsx
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>
    
    * add test for location detail view refresh button
    
    * fix typo, add call to userEvent.setup()
    
    * remove unnecessary div
    
    * remove unused import
    
    * add pseudo-class to position refresh when it has action-menu sibling
    
    ---------
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

--- packages/react-storage/src/components/StorageBrowser/Views/LocationDetailView/__tests__/LocationDetailView.spec.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/LocationDetailView/__tests__/LocationDetailView.spec.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 
 import createProvider from '../../../createProvider';
+import * as ActionsModule from '../../../context/actions';
 
 import { LocationDetailView } from '../LocationDetailView';
+import userEvent from '@testing-library/user-event';
 
 const listLocations = jest.fn(() =>
   Promise.resolve({ locations: [], nextToken: undefined })
@@ -17,6 +19,18 @@ const config = {
 
 const Provider = createProvider({ config });
 
+const handleList = jest.fn();
+
+jest.spyOn(ActionsModule, 'useAction').mockReturnValue([
+  {
+    data: { result: [{ key: 'test1', type: 'FOLDER' }], nextToken: undefined },
+    hasError: false,
+    isLoading: false,
+    message: undefined,
+  },
+  handleList,
+]);
+
 describe('LocationDetailView', () => {
   it('renders a `LocationDetailView`', async () => {
     await waitFor(() => {
@@ -27,6 +41,26 @@ describe('LocationDetailView', () => {
           </Provider>
         ).container
       ).toBeDefined();
+    });
+  });
+
+  it('refreshes table when refresh button is clicked', () => {
+    const user = userEvent.setup();
+
+    render(
+      <Provider>
+        <LocationDetailView />
+      </Provider>
+    );
+
+    const refreshButton = screen.getByLabelText('Refresh table');
+
+    act(() => {
+      user.click(refreshButton);
+    });
+
+    waitFor(() => {
+      expect(handleList).toHaveBeenCalled();
     });
   });
 });
