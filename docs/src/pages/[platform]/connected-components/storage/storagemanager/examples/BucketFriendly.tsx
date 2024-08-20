6d200a99930a5b99f13672fc7bb363ccc49e669f
Jordan Van Ness <jordvn@amazon.com>
2024-08-20 10:46:31 -0700

    docs: include references to new 'bucket' prop and its usage

---
+++ docs/src/pages/[platform]/connected-components/storage/storagemanager/examples/BucketFriendly.tsx
@@ -0,0 +1,12 @@
+import { StorageManager } from '@aws-amplify/ui-react-storage';
+
+export const App = () => {
+  return (
+    <StorageManager
+      acceptedFileTypes={['image/*']}
+      bucket={'my-bucket'}
+      path="public/"
+      maxFileCount={1}
+    />
+  );
+};
