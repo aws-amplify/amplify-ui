6f40fac48d1e6707c705fadbaad3d7929b5f3f46
Danny Banks <djb@amazon.com>
2024-08-20 11:12:38 -0700

    chore(utils): moving humanFileSize to ui package

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -2,3 +2,4 @@ export { setUserAgent, SetUserAgentOptions } from './setUserAgent';
 // generic utils
 export * from './utils';
 export { classNames } from './classNames';
+export { humanFileSize } from './humanFileSize';
