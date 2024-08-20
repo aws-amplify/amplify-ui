8b037516e18ca8970f7713dfe68ef0aa8e6ead19
AhmetAhunbayAWS <ahmetahu@amazon.com>
2024-08-20 13:53:57 -0700

    test: tests getProviderConfig utility

--- packages/react/src/components/FederatedIdentities/controls/__tests__/utils.test.tsx
+++ packages/react/src/components/FederatedIdentities/controls/__tests__/utils.test.tsx
@@ -2,6 +2,82 @@ import React from 'react';
 import { FederatedProvider } from '@aws-amplify/ui';
 import { ProviderType, ProviderData, DefaultFederatedProviderList } from '..';
 import { toProviderData } from '../utils';
+import { getProviderConfig } from '../utils';
+
+describe('getProviderConfig', () => {
+  const mockResourcesConfig = {
+    Auth: {
+      Cognito: {
+        loginWith: {
+          oauth: {
+            providers: ['Google', 'Facebook', 'Amazon'],
+          },
+        },
+      },
+    },
+  };
+
+  const messyConfig = {
+    Auth: {
+      Cognito: {
+        loginWith: {
+          oauth: {
+            providers: ['Google', 'okta', { custom: 'okta' }],
+          },
+        },
+      },
+    },
+  };
+
+  const emptyConfig = {};
+
+  const configWithoutProviders = {
+    Auth: {
+      Cognito: {
+        loginWith: {
+          oauth: {
+            providers: null,
+          },
+        },
+      },
+    },
+  };
+
+  it('returns a list of supported providers in lowercase', () => {
+    const result = getProviderConfig(mockResourcesConfig);
+
+    expect(result).toEqual([
+      'google',
+      'facebook',
+      'amazon',
+    ] as FederatedProvider[]);
+  });
+
+  it('returns a list of supported providers in lowercase if there is unuseful information in config', () => {
+    const result = getProviderConfig(messyConfig);
+
+    expect(result).toEqual(['google'] as FederatedProvider[]);
+  });
+
+  it('returns an empty array if providers are null', () => {
+    const result = getProviderConfig(configWithoutProviders);
+    expect(result).toEqual([]);
+  });
+
+  it('returns an empty array if Auth.Cognito.loginWith.oauth is missing', () => {
+    const result = getProviderConfig(emptyConfig);
+    expect(result).toEqual([]);
+  });
+
+  it('works with generic config input', () => {
+    const genericConfig = {
+      someKey: 'someValue',
+    };
+
+    const result = getProviderConfig(genericConfig);
+    expect(result).toEqual([]);
+  });
+});
 
 describe('toProviderData utility', () => {
   beforeEach(() => {
