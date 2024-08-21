ae2cf6b6075552dc326a3978450c15c7da2842f4
Caleb Pollman <cpollman@amazon.com>
2024-08-21 11:01:12 -0700

    fix(storage-browser): fix same folder name navigation (#5644)

--- packages/react-storage/src/components/StorageBrowser/context/controls/Navigate/__tests__/Navigate.spec.tsx
+++ packages/react-storage/src/components/StorageBrowser/context/controls/Navigate/__tests__/Navigate.spec.tsx
@@ -1,6 +1,11 @@
 import { LocationAccess, LocationData } from '../../../types';
 
-import { NavigateAction, navigateReducer, NavigateState } from '../Navigate';
+import {
+  INITIAL_STATE,
+  NavigateAction,
+  navigateReducer,
+  NavigateState,
+} from '../Navigate';
 
 const location: LocationData = {
   bucket: 'bucket-name',
@@ -24,10 +29,7 @@ const locationAccess: LocationAccess = {
 
 describe('navigateReducer', () => {
   it('ACCESS_LOCATION action sets the location and bucket as the initial entry value', () => {
-    const initialState: NavigateState = {
-      history: [''],
-      location: undefined,
-    };
+    const initialState: NavigateState = INITIAL_STATE;
 
     const action: NavigateAction = {
       type: 'ACCESS_LOCATION',
@@ -35,8 +37,9 @@ describe('navigateReducer', () => {
     };
 
     const expectedState: NavigateState = {
-      history: [''],
+      history: [{ prefix: location.prefix, position: 0 }],
       location: locationAccess,
+      path: location.prefix,
     };
 
     const newState = navigateReducer(initialState, action);
@@ -44,20 +47,25 @@ describe('navigateReducer', () => {
     expect(newState).toStrictEqual(expectedState);
   });
 
-  it('handles a NAVIGATE action as expected when target location is root', () => {
+  it('handles a NAVIGATE action as expected when target prefix is root', () => {
     const state: NavigateState = {
-      history: [location.bucket, location.prefix],
+      history: [
+        { prefix: location.prefix, position: 0 },
+        { prefix: locationTwo.prefix, position: 1 },
+      ],
       location: locationAccess,
+      path: `${location.prefix}${locationTwo.prefix}`,
     };
 
     const action: NavigateAction = {
       type: 'NAVIGATE',
-      prefix: location.bucket,
+      entry: { prefix: location.prefix, position: 0 },
     };
 
     const expectedState: NavigateState = {
-      history: [location.bucket],
+      history: [{ prefix: location.prefix, position: 0 }],
       location: locationAccess,
+      path: location.prefix,
     };
 
     const newState = navigateReducer(state, action);
@@ -65,20 +73,25 @@ describe('navigateReducer', () => {
     expect(newState).toStrictEqual(expectedState);
   });
 
-  it('handles a NAVIGATE action to a new location', () => {
+  it('handles a NAVIGATE action to a new prefix', () => {
     const state: NavigateState = {
-      history: [location.bucket],
+      history: [{ prefix: location.prefix, position: 0 }],
       location: locationAccess,
+      path: location.prefix,
     };
 
     const action: NavigateAction = {
       type: 'NAVIGATE',
-      prefix: locationTwo.prefix,
+      entry: { prefix: locationTwo.prefix, position: 1 },
     };
 
     const expectedState: NavigateState = {
-      history: [location.bucket, locationTwo.prefix],
+      history: [
+        { prefix: location.prefix, position: 0 },
+        { prefix: locationTwo.prefix, position: 1 },
+      ],
       location: locationAccess,
+      path: `${location.prefix}${locationTwo.prefix}`,
     };
 
     const newState = navigateReducer(state, action);
@@ -86,25 +99,72 @@ describe('navigateReducer', () => {
     expect(newState).toEqual(expectedState);
   });
 
-  it('handles a NAVIGATE action to a previous location', () => {
+  it('handles a NAVIGATE action between entries with the same prefix', () => {
     const state: NavigateState = {
+      history: [{ prefix: location.prefix, position: 0 }],
+      location: locationAccess,
+      path: location.prefix,
+    };
+
+    const initialAction: NavigateAction = {
+      type: 'NAVIGATE',
+      entry: { prefix: location.prefix, position: 1 },
+    };
+
+    const initialExpectedState: NavigateState = {
       history: [
-        location.bucket,
-        locationTwo.prefix,
-        'public/folder1/',
-        'public/folder1/nestedFolder/',
+        { prefix: location.prefix, position: 0 },
+        { prefix: location.prefix, position: 1 },
       ],
       location: locationAccess,
+      path: `${location.prefix}${location.prefix}`,
+    };
+
+    const newState = navigateReducer(state, initialAction);
+
+    expect(newState).toEqual(initialExpectedState);
+
+    const nextAction: NavigateAction = {
+      type: 'NAVIGATE',
+      entry: { prefix: location.prefix, position: 0 },
+    };
+
+    const nextState = navigateReducer(initialExpectedState, nextAction);
+
+    const nextExpectedState: NavigateState = {
+      history: [{ prefix: location.prefix, position: 0 }],
+      location: locationAccess,
+      path: location.prefix,
+    };
+
+    expect(nextState).toEqual(nextExpectedState);
+  });
+
+  it('handles a NAVIGATE action to a previous prefix', () => {
+    const path = `${location.prefix}${locationTwo.prefix}public/folder1/public/folder1/nestedFolder/`;
+    const state: NavigateState = {
+      history: [
+        { prefix: location.prefix, position: 0 },
+        { prefix: locationTwo.prefix, position: 1 },
+        { prefix: 'public/folder1/', position: 2 },
+        { prefix: 'public/folder1/nestedFolder/', position: 3 },
+      ],
+      location: locationAccess,
+      path,
     };
 
     const action: NavigateAction = {
       type: 'NAVIGATE',
-      prefix: locationTwo.prefix,
+      entry: { prefix: locationTwo.prefix, position: 1 },
     };
 
     const expectedState: NavigateState = {
-      history: [location.bucket, locationTwo.prefix],
+      history: [
+        { prefix: location.prefix, position: 0 },
+        { prefix: locationTwo.prefix, position: 1 },
+      ],
       location: locationAccess,
+      path: `${location.prefix}${locationTwo.prefix}`,
     };
 
     const newState = navigateReducer(state, action);
@@ -114,19 +174,15 @@ describe('navigateReducer', () => {
 
   it('handles an EXIT action as expected', () => {
     const state: NavigateState = {
-      history: [location.bucket],
+      history: [{ prefix: location.prefix, position: 0 }],
       location: locationAccess,
+      path: location.prefix,
     };
 
     const action: NavigateAction = { type: 'EXIT' };
 
-    const expectedState: NavigateState = {
-      history: [],
-      location: undefined,
-    };
-
     const newState = navigateReducer(state, action);
 
-    expect(newState).toStrictEqual(expectedState);
+    expect(newState).toStrictEqual(INITIAL_STATE);
   });
 });
