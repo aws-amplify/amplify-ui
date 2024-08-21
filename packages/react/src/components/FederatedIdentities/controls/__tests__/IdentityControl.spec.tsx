83deae1a69aad59add470c97b962cbac33b04cc8
AhmetAhunbayAWS <ahmetahu@amazon.com>
2024-08-21 09:21:32 -0700

    fix: unnecessary import

--- packages/react/src/components/FederatedIdentities/controls/__tests__/IdentityControl.spec.tsx
+++ packages/react/src/components/FederatedIdentities/controls/__tests__/IdentityControl.spec.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useRef } from 'react';
+import React, { forwardRef } from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import { CreateProviderInput, ProviderData } from '../types';
 import createProvider from '../createProvider';
