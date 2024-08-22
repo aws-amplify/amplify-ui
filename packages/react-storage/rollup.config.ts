2cb8e0e480829f8aed652ef88ff46a0b1d648c1a
Allan Zheng <zheallan@amazon.com>
2024-08-22 12:50:43 -0700

    fix: address feedbacks

--- packages/react-storage/rollup.config.ts
+++ packages/react-storage/rollup.config.ts
@@ -4,13 +4,12 @@ import styles from 'rollup-plugin-styles';
 import externals from 'rollup-plugin-node-externals';
 
 // common config settings
-const input = ['src/index.ts'];
+const input = {
+  index: 'src/index.ts',
+  browser: 'src/components/StorageBrowser/index.ts',
+};
 const sourceMap = false;
 const tsconfig = 'tsconfig.dist.json';
-
-// storage browser settings
-const storageBrowserInput = 'src/components/StorageBrowser/index.ts';
-const storageBrowserOutputEntryFile = 'storage-browser.js';
 
 const config = defineConfig([
   // CJS config
@@ -18,18 +17,6 @@ const config = defineConfig([
     input,
     output: {
       dir: 'dist',
-      format: 'cjs',
-    },
-    plugins: [
-      externals({ include: /^@aws-amplify/ }),
-      typescript({ declarationDir: 'dist/types', sourceMap, tsconfig }),
-    ],
-  },
-  {
-    input: storageBrowserInput,
-    output: {
-      dir: 'dist',
-      entryFileNames: storageBrowserOutputEntryFile,
       format: 'cjs',
     },
     plugins: [
