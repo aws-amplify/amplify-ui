eb49a96149f5432e15ff44a3e1072a441811ec2e
Emma Sauerborn <70536670+esauerbo@users.noreply.github.com>
2024-08-22 17:30:56 -0400

    Update build-android.sh

--- build-system-tests/scripts/build-android.sh
+++ build-system-tests/scripts/build-android.sh
@@ -31,7 +31,6 @@ if [ $BUILD_TOOL == 'expo' ]; then
   # Run npm run android in the background
   npm run android -- -p 19000 >$LOG_FILE &
   npx wait-on -t 20000 tcp:19000
-  
 else
   log "command" "cd android"
   cd android
