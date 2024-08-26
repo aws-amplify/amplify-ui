aa3d36b325ffc44b7782c609c0d80fe1b1c10bd7
Heather <hbuchel@gmail.com>
2024-08-26 10:22:25 -0400

    remove test Search from view

--- packages/react-storage/src/components/StorageBrowser/Views/LocationsView/LocationsView.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/LocationsView/LocationsView.tsx
@@ -67,7 +67,6 @@ const LocationsViewControls: LocationsViewControls = () => {
       <Title>Home</Title>
       <LocationsViewRefresh />
       <Paginate />
-      <Search />
       <LocationsMessage />
       <Loading />
       <LocationsViewTable />
