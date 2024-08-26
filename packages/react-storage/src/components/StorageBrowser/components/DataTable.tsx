c292a6c7021e35cd7f87aa4970c3e4745aaf06fa
Tim Nguyen <54393192+timngyn@users.noreply.github.com>
2024-08-25 17:08:49 -0700

    chore(storage-browser): refactor table control (#5670)
    * chore(storage-browser): implement table control refactor
    
    * add tests and aria label to table
    
    * update tablerow key in tbody
    
    * remove unused variable
    
    * export TableV2
    
    * Update TableV2.tsx
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>
    
    * destructure getByText
    
    * remove unnecessary interfaces
    
    * address feedback
    
    * create default components for TableData and TableHeader
    
    * fix typo
    
    * add DataTable component
    
    * remove null from return type of DataTable
    
    * use DataTable
    
    ---------
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

---
+++ packages/react-storage/src/components/StorageBrowser/components/DataTable.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import {
+  TableBodyElement,
+  TableDataElement,
+  TableElement,
+  TableHeadElement,
+  TableHeaderElement,
+  TableRowElement,
+} from '../context/elements';
+
+import { CLASS_BASE } from '../Views/constants';
+
+export const TABLE_CLASS_NAME = `${CLASS_BASE}__table`;
+export const TABLE_HEADER_CLASS_NAME = `${TABLE_CLASS_NAME}__header`;
+export const TABLE_ROW_CLASS_NAME = `${TABLE_CLASS_NAME}__row`;
+export const TABLE_DATA_CLASS_NAME = `${TABLE_CLASS_NAME}__data`;
+
+export interface ColumnHeaderItemProps
+  extends React.ComponentProps<typeof TableHeaderElement> {}
+
+export interface RowDataItemProps
+  extends React.ComponentProps<typeof TableDataElement> {}
+
+export interface Data<T, K = {}> {
+  columns?: K[];
+  rows: T[][];
+}
+
+export interface DataTableProps<
+  T extends RowDataItemProps,
+  K extends ColumnHeaderItemProps,
+> {
+  data?: Data<T, K>;
+  renderColumnHeaderItem?: (props: K, index: number) => React.JSX.Element;
+  renderRowDataItem?: (props: T, index: number) => React.JSX.Element;
+}
+
+export function ColumnHeaderItem({
+  className = TABLE_HEADER_CLASS_NAME,
+  ...props
+}: ColumnHeaderItemProps): React.JSX.Element {
+  return <TableHeaderElement {...props} className={className} />;
+}
+
+export function RowDataItem({
+  className = TABLE_DATA_CLASS_NAME,
+  ...props
+}: RowDataItemProps): React.JSX.Element {
+  return <TableDataElement {...props} className={className} />;
+}
+
+export function DataTable<
+  T extends RowDataItemProps,
+  K extends ColumnHeaderItemProps = {},
+>({
+  data,
+  renderColumnHeaderItem = ColumnHeaderItem,
+  renderRowDataItem = RowDataItem,
+}: DataTableProps<T, K>): JSX.Element {
+  const { columns, rows } = data ?? {};
+
+  return (
+    <TableElement aria-label="Table" className={TABLE_CLASS_NAME}>
+      <TableHeadElement className={`${TABLE_CLASS_NAME}__head`}>
+        <TableRowElement className={TABLE_ROW_CLASS_NAME}>
+          {columns?.map(renderColumnHeaderItem)}
+        </TableRowElement>
+      </TableHeadElement>
+      <TableBodyElement className={`${TABLE_CLASS_NAME}__body`}>
+        {rows?.map((row, index) => (
+          <TableRowElement
+            key={`row-${index}`}
+            className={TABLE_ROW_CLASS_NAME}
+          >
+            {row.map(renderRowDataItem)}
+          </TableRowElement>
+        ))}
+      </TableBodyElement>
+    </TableElement>
+  );
+}
