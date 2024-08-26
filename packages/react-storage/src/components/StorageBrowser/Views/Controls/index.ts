599af992912b979c9a266b635e6cfde7728de2c9
Tim Nguyen <54393192+timngyn@users.noreply.github.com>
2024-08-26 12:46:22 -0700

    chore(storage-browser): remove old locations view table (#5679)

--- packages/react-storage/src/components/StorageBrowser/Views/Controls/index.ts
+++ packages/react-storage/src/components/StorageBrowser/Views/Controls/index.ts
@@ -11,9 +11,5 @@ export { RefreshControl } from './Refresh';
 export { SearchControl } from './Search';
 export { TargetControl } from './Target';
 export { TitleControl } from './Title';
-export {
-  TableControl,
-  LocationsViewTable,
-  LocationDetailViewTable,
-} from './Table';
+export { TableControl, LocationDetailViewTable } from './Table';
 export { Controls } from './Controls';
