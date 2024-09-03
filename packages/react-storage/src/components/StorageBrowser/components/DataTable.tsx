20a69e1466df5ed02cab697034ad6e4e9681d270
timngyn <timngyn@gmail.com>
2024-09-03 12:17:25 -0700

    update table classes to build off of one another

--- packages/react-storage/src/components/StorageBrowser/components/DataTable.tsx
+++ packages/react-storage/src/components/StorageBrowser/components/DataTable.tsx
@@ -13,12 +13,12 @@ import { CLASS_BASE } from '../Views/constants';
 
 export const TABLE_CLASS_NAME = `${CLASS_BASE}__table`;
 export const TABLE_HEADER_CLASS_NAME = `${TABLE_CLASS_NAME}__header`;
-export const TABLE_HEADER_BUTTON_CLASS_NAME = `${TABLE_CLASS_NAME}__header__button`;
+export const TABLE_HEADER_BUTTON_CLASS_NAME = `${TABLE_HEADER_CLASS_NAME}__button`;
 export const TABLE_ROW_CLASS_NAME = `${TABLE_CLASS_NAME}__row`;
 export const TABLE_DATA_CLASS_NAME = `${TABLE_CLASS_NAME}__data`;
 export const TABLE_DATA_TEXT_CLASS_NAME = `${TABLE_DATA_CLASS_NAME}__text`;
-export const TABLE_DATA_BUTTON_CLASS = `${TABLE_CLASS_NAME}__data__button`;
-export const TABLE_DATA_ICON_CLASS = `${TABLE_CLASS_NAME}__data__icon`;
+export const TABLE_DATA_BUTTON_CLASS = `${TABLE_DATA_CLASS_NAME}__button`;
+export const TABLE_DATA_ICON_CLASS = `${TABLE_DATA_CLASS_NAME}__icon`;
 
 export interface ColumnHeaderItemProps
   extends React.ComponentProps<typeof TableHeaderElement> {}
