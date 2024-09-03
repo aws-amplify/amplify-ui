6ae52330576caa4bf8f8e77e7873ee0c5812dc12
Heather Buchel <hbuchel@gmail.com>
2024-09-03 15:44:34 -0400

    add test for error Message on LocationsView (#5733)

--- packages/react-storage/src/components/StorageBrowser/Views/LocationsView/__tests__/LocationsView.spec.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/LocationsView/__tests__/LocationsView.spec.tsx
@@ -6,7 +6,7 @@ import * as ActionsModule from '../../../context/actions';
 import * as ControlsModule from '../../../context/controls';
 
 import { LocationsView } from '..';
-import { DEFAULT_LIST_OPTIONS } from '../LocationsView';
+import { DEFAULT_LIST_OPTIONS, DEFAULT_ERROR_MESSAGE } from '../LocationsView';
 
 const INITIAL_PAGINATE_STATE = [
   {
@@ -107,6 +107,58 @@ describe('LocationsListView', () => {
         ).container
       ).toBeDefined();
     });
+  });
+
+  it('renders a returned error message for `LocationsListView`', () => {
+    const errorMessage = 'Something went wrong.';
+
+    useLocationsDataSpy.mockReturnValue([
+      {
+        data: {
+          result: [],
+          nextToken: undefined,
+        },
+        hasError: true,
+        isLoading: false,
+        message: errorMessage,
+      },
+      handleListLocations,
+    ]);
+
+    render(
+      <Provider>
+        <LocationsView />
+      </Provider>
+    );
+
+    const message = screen.getByRole('alert');
+    const messageText = screen.getByText(errorMessage);
+    expect(message).toBeInTheDocument();
+    expect(messageText).toBeInTheDocument();
+  });
+
+  it('renders a fallback error message for `LocationsListView`', () => {
+    useLocationsDataSpy.mockReturnValue([
+      {
+        data: {
+          result: [],
+          nextToken: undefined,
+        },
+        hasError: true,
+        isLoading: false,
+        message: undefined,
+      },
+      handleListLocations,
+    ]);
+
+    render(
+      <Provider>
+        <LocationsView />
+      </Provider>
+    );
+
+    const messageText = screen.getByText(DEFAULT_ERROR_MESSAGE);
+    expect(messageText).toBeInTheDocument();
   });
 
   it('renders a Locations View table', () => {
