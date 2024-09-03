1563865013ae22b84788c0e8e01b56e0e2d5fc30
timngyn <timngyn@gmail.com>
2024-09-03 12:13:49 -0700

    remove LocationDetailViewTable

--- packages/react-storage/src/components/StorageBrowser/Views/Controls/Table.tsx
+++ packages/react-storage/src/components/StorageBrowser/Views/Controls/Table.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { humanFileSize } from '@aws-amplify/ui';
 
 import {
   SpanElementProps,
@@ -7,17 +6,7 @@ import {
   TableDataElementProps,
   TableHeaderElementProps,
 } from '../../context/elements';
-import { DownloadControl } from './Download';
 import { CLASS_BASE } from '../constants';
-import { useControl } from '../../context/controls';
-import { FileItem, FolderItem, LocationItem } from '../../context/types';
-import { useAction } from '../../context/actions';
-import {
-  compareDates,
-  compareNumbers,
-  compareStrings,
-} from '../../context/controls/Table';
-import { TABLE_HEADER_BUTTON_CLASS_NAME } from '../../components/DataTable';
 
 export type SortDirection = 'ascending' | 'descending' | 'none';
 
@@ -33,8 +22,6 @@ const {
   TableHead,
   TableHeader: BaseTableHeader,
   TableRow,
-  Button,
-  Icon,
   Span,
 } = StorageBrowserElements;
 
@@ -84,29 +71,6 @@ export function TableDataText({
   return <Span {...props} className={className} />;
 }
 
-const LOCATION_DETAIL_VIEW_COLUMNS: Column<LocationItem>[] = [
-  {
-    key: 'key',
-    header: 'Name',
-  },
-  {
-    key: 'type',
-    header: 'Type',
-  },
-  {
-    key: 'lastModified' as keyof LocationItem,
-    header: 'Last Modified',
-  },
-  {
-    key: 'size' as keyof LocationItem,
-    header: 'Size',
-  },
-  {
-    key: 'download' as keyof LocationItem,
-    header: 'Download',
-  },
-];
-
 export interface Column<T> {
   header: string;
   key: keyof T;
@@ -149,215 +113,3 @@ export function TableControl<U>({
 TableControl.TableRow = TableRow;
 TableControl.TableData = TableData;
 TableControl.TableHeader = TableHeader;
-
-const LocationDetailViewColumnSortMap = {
-  key: compareStrings,
-  type: compareStrings,
-  lastModified: compareDates,
-  size: compareNumbers,
-};
-
-export const LocationDetailViewTable = ({
-  range,
-}: {
-  range: [start: number, end: number];
-}): JSX.Element | null => {
-  const [start, end] = range;
-
-  const [{ history, path }, handleUpdateState] = useControl({
-    type: 'NAVIGATE',
-  });
-
-  const [{ data, isLoading }] = useAction({
-    type: 'LIST_LOCATION_ITEMS',
-  });
-
-  const currentPosition = history.length;
-  const hasItems = !!data.result?.length;
-
-  const [compareFn, setCompareFn] = React.useState(() => compareStrings);
-  const [sortState, setSortState] = React.useState<SortState<LocationItem>>({
-    selection: 'key',
-    direction: 'ascending',
-  });
-
-  const { direction, selection } = sortState;
-
-  // Use range prop values to get the current page of data
-  const pagedData = data.result.slice(start, end);
-
-  const tableData =
-    direction === 'ascending'
-      ? pagedData.sort((a, b) => compareFn(a[selection], b[selection]))
-      : pagedData.sort((a, b) => compareFn(b[selection], a[selection]));
-
-  const renderHeaderItem = React.useCallback(
-    (column: Column<LocationItem>) => {
-      // Defining this function inside the `LocationDetailViewTable` to get access
-      // to the current sort state
-
-      const { header, key } = column;
-
-      return (
-        <TableHeader
-          key={header}
-          variant={key}
-          aria-label={
-            key == ('download' as keyof LocationItem)
-              ? column.header
-              : undefined
-          }
-          aria-sort={selection === key ? direction : 'none'}
-        >
-          {LocationDetailViewColumnSortMap[column.key] ? (
-            <Button
-              variant="sort"
-              className={TABLE_HEADER_BUTTON_CLASS_NAME}
-              onClick={() => {
-                setCompareFn(() => LocationDetailViewColumnSortMap[column.key]);
-
-                setSortState((prevState) => ({
-                  selection: column.key,
-                  direction:
-                    prevState.direction === 'ascending'
-                      ? 'descending'
-                      : 'ascending',
-                }));
-              }}
-            >
-              {column.header}
-              {selection === column.key ? (
-                <Icon
-                  variant={
-                    direction === 'none'
-                      ? 'sort-indeterminate'
-                      : `sort-${direction}`
-                  }
-                />
-              ) : (
-                <Icon variant="sort-indeterminate" />
-              )}
-            </Button>
-          ) : column.key !== ('download' as keyof LocationItem) ? (
-            column.header
-          ) : null}
-        </TableHeader>
-      );
-    },
-    [direction, selection]
-  );
-
-  // @TODO: This should be it's own component instead of using `useCallback`
-  const renderRowItem: RenderRowItem<LocationItem> = React.useCallback(
-    (row, index) => {
-      const renderTableData = (
-        row: LocationItem,
-        column: Column<LocationItem>
-      ) => {
-        const { type } = row;
-
-        switch (type) {
-          case 'FILE': {
-            // Casting column as Column<FileItem> to assert that we're only working with FileItems
-            // since the type is 'FILE'
-            const { key } = column as Column<FileItem>;
-
-            switch (key) {
-              case 'size': {
-                return (
-                  <TableDataText>
-                    {humanFileSize(row.size ?? 0, true)}
-                  </TableDataText>
-                );
-              }
-              case 'lastModified': {
-                return (
-                  <TableDataText>
-                    {new Date(row.lastModified).toLocaleString()}
-                  </TableDataText>
-                );
-              }
-              case 'download' as keyof LocationItem: {
-                return <DownloadControl fileKey={`${path}${row.key}`} />;
-              }
-              case 'type': {
-                const indexOfDot = row.key.lastIndexOf('.');
-
-                return indexOfDot > -1 ? (
-                  <TableDataText>{row.key.slice(indexOfDot + 1)}</TableDataText>
-                ) : (
-                  '-'
-                );
-              }
-              case 'key': {
-                return (
-                  <TableDataText>
-                    <Icon className={ICON_CLASS} variant="file" />
-                    {row.key}
-                  </TableDataText>
-                );
-              }
-              default:
-                return <TableDataText>{row[column.key]}</TableDataText>;
-            }
-          }
-          case 'FOLDER': {
-            // Casting column as Column<FolderItem> to assert that we're only working with FileItems
-            // since the type is 'FOLDER'
-            const { key } = column as Column<FolderItem>;
-
-            switch (key) {
-              case 'key': {
-                return (
-                  <Button
-                    className={`${BLOCK_NAME}__data__button`}
-                    variant="table-data"
-                    onClick={() => {
-                      handleUpdateState({
-                        type: 'NAVIGATE',
-                        entry: {
-                          position: currentPosition + 1,
-                          prefix: row.key,
-                        },
-                      });
-                    }}
-                    key={`${index}-${row.key}`}
-                  >
-                    <Icon className={ICON_CLASS} variant="folder" /> {row.key}
-                  </Button>
-                );
-              }
-              case 'type': {
-                return <TableDataText>Folder</TableDataText>;
-              }
-              default:
-                return <TableDataText>{row[column.key]}</TableDataText>;
-            }
-          }
-        }
-      };
-
-      return (
-        <TableRow key={index}>
-          {LOCATION_DETAIL_VIEW_COLUMNS.map((column) => {
-            return (
-              <TableData key={`${index}-${column.header}`} variant={column.key}>
-                {renderTableData(row, column)}
-              </TableData>
-            );
-          })}
-        </TableRow>
-      );
-    },
-    [handleUpdateState, currentPosition, path]
-  );
-
-  return hasItems && !isLoading ? (
-    <TableControl
-      columns={LOCATION_DETAIL_VIEW_COLUMNS}
-      data={tableData}
-      renderHeaderItem={renderHeaderItem}
-      renderRowItem={renderRowItem}
-    />
-  ) : null;
-};
