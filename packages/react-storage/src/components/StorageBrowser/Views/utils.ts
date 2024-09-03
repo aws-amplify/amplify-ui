720ccf8af681140b1d2f3ccb190821aced08e12a
Heather <hbuchel@gmail.com>
2024-09-03 12:34:46 -0400

    use math.ceil

--- packages/react-storage/src/components/StorageBrowser/Views/utils.ts
+++ packages/react-storage/src/components/StorageBrowser/Views/utils.ts
@@ -16,11 +16,11 @@ export const listViewHelpers = ({
   disableRefresh: boolean;
   range: [start: number, end: number];
 } => {
-  const isLastPage =
-    // Checks if we have more results than pageSize and we're on the last page.
-    Math.round(resultCount / pageSize) === currentPage ||
-    // Checks if we have less results than pageSize (only one page)
-    Math.round(resultCount / pageSize) === 0;
+  // Use Math.ceil so we can round up. For example, if you have
+  // 4 results, and your page size is 3, the last page (Math.ceil(4/3)) will be 2.
+  // If you have 3 results and your page size is 4, the last page will be 1.
+  const isLastPage = Math.ceil(resultCount / pageSize) === currentPage;
+
   const start = currentPage === 1 ? 0 : (currentPage - 1) * pageSize;
   const end = currentPage === 1 ? pageSize : currentPage * pageSize;
 
