2ab42b638705c721cf6aeefbd1f9c32d709b6b68
Thadd Trinh <thadtrin@amazon.com>
2024-09-09 13:41:17 -0700

    chore: update package name in exports test

--- packages/react-ai/__tests__/exports.ts
+++ packages/react-ai/__tests__/exports.ts
@@ -1,6 +1,6 @@
 import * as exported from '../src';
 
-describe('@aws-amplify/ui-react', () => {
+describe('@aws-amplify/ui-react-ai', () => {
   describe('exports', () => {
     it('should match snapshot', () => {
       const sortedExports = Object.keys(exported).sort();
