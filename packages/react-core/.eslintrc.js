9601199936dc757d8b2a7e9083d299b5e716f8c2
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-09-11 15:56:15 -0700

    Cleanup

--- packages/react-core/.eslintrc.js
+++ packages/react-core/.eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   extends: '@aws-amplify/amplify-ui/react',
-  ignorePatterns: ['.eslintrc.js', 'dist'],
+  ignorePatterns: ['.eslintrc.js', 'dist', 'rollup.config.ts'],
   overrides: [
     {
       extends: '@aws-amplify/amplify-ui/jest',
       files: ['**/__mocks__/**', '**/__tests__/**'],
     },
     {
-      files: ['**/__tests__/**', '**/jest.*', 'rollup.config.ts'],
+      files: ['**/__tests__/**', '**/jest.*'],
       rules: {
         'import/no-extraneous-dependencies': 'off',
       },
