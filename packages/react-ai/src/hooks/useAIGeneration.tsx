dbd398d5ad499e5bc22e57c8229e63a59794ada9
Thadd Trinh <thadtrin@amazon.com>
2024-09-19 16:21:15 -0700

    fix(ai): update useAIGeneration to manage its own date state

--- packages/react-ai/src/hooks/useAIGeneration.tsx
+++ packages/react-ai/src/hooks/useAIGeneration.tsx
@@ -1,6 +1,7 @@
-import { DataState, useDataState } from '@aws-amplify/ui-react-core';
+import { DataState } from '@aws-amplify/ui-react-core';
 import { V6Client } from '@aws-amplify/api-graphql';
 import { getSchema } from '../types';
+import React from 'react';
 
 export interface UseAIGenerationHookWrapper<
   Key extends keyof AIGenerationClient<Schema>['generations'],
@@ -9,7 +10,7 @@ export interface UseAIGenerationHookWrapper<
   useAIGeneration: <U extends Key>(
     routeName: U
   ) => [
-    Awaited<DataState<Schema[U]['returnType']>>,
+    Awaited<GenerateState<Schema[U]['returnType']>>,
     (input: Schema[U]['args']) => void,
   ];
 }
@@ -20,7 +21,7 @@ export type UseAIGenerationHook<
 > = (
   routeName: Key
 ) => [
-  Awaited<DataState<Schema[Key]['returnType']>>,
+  Awaited<GenerateState<Schema[Key]['returnType']>>,
   (input: Schema[Key]['args']) => void,
 ];
 
@@ -28,6 +29,10 @@ type AIGenerationClient<T extends Record<any, any>> = Pick<
   V6Client<T>,
   'generations'
 >;
+
+const INITIAL_STATE = { hasError: false, isLoading: false, message: undefined };
+const LOADING_STATE = { hasError: false, isLoading: true, message: undefined };
+const ERROR_STATE = { hasError: true, isLoading: false };
 
 interface GraphQLFormattedError {
   readonly message: string;
@@ -37,6 +42,15 @@ interface GraphQLFormattedError {
   };
 }
 
+type SingularReturnValue<T> = Promise<{
+  data: T | null;
+  errors?: GraphQLFormattedError[];
+}>;
+
+type GenerateState<T> = DataState<T> & {
+  graphqlErrors?: GraphQLFormattedError[];
+};
+
 export function createUseAIGeneration<
   Client extends Record<'generations' | 'conversations', Record<string, any>>,
   Schema extends getSchema<Client>,
@@ -45,31 +59,45 @@ export function createUseAIGeneration<
     Key extends keyof AIGenerationClient<Schema>['generations'],
   >(
     routeName: Key
-  ) => {
+  ): [
+    state: GenerateState<Schema[Key]['returnType']>,
+    handleAction: (input: Schema[Key]['args']) => void,
+  ] => {
+    const [dataState, setDataState] = React.useState<
+      GenerateState<Schema[Key]['returnType']>
+    >(() => ({
+      ...INITIAL_STATE,
+      data: undefined,
+    }));
+
     const handleGenerate = (
       client.generations as AIGenerationClient<Schema>['generations']
     )[routeName];
 
-    const updateAIGenerationStateAction = async (
-      _prev: Schema[Key]['returnType'],
-      input: Schema[Key]['args']
-    ): Promise<
-      Schema[Key]['returnType'] & { graphqlErrors?: GraphQLFormattedError[] }
-    > => {
-      const result = await handleGenerate(input);
+    const generateHandler: (input: Schema[Key]['args']) => void =
+      React.useCallback(
+        (input) => {
+          setDataState(({ data }) => ({ ...LOADING_STATE, data }));
 
-      // handleGenerate returns a Promised wrapper around Schema[Key]['returnType'] which includes data, errors, and clientExtensions
-      // The type of data is Schema[Key]['returnType'] which useDataState also wraps in a data return
-      // TODO: follow up with how to type handleGenerate to properly return the promise wrapper shape
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      const data = (result as any).data as Schema[Key]['returnType'];
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-      const graphqlErrors = (result as any).errors;
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-assignment
-      return { ...data, ...(graphqlErrors ? { graphqlErrors } : {}) };
-    };
+          const promiseResult = handleGenerate(input);
 
-    return useDataState(updateAIGenerationStateAction, {});
+          (promiseResult as SingularReturnValue<Schema[Key]['returnType']>)
+            .then(({ data, errors }) => {
+              setDataState({ ...INITIAL_STATE, data, graphqlErrors: errors });
+            })
+            .catch(({ message }: Error) => {
+              setDataState(({ data, graphqlErrors }) => ({
+                ...ERROR_STATE,
+                data,
+                message,
+                graphqlErrors,
+              }));
+            });
+        },
+        [handleGenerate]
+      );
+
+    return [dataState, generateHandler];
   };
 
   return useAIGeneration;
