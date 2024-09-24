7f71b26e001433806bda3915126cc7e068fb4a53
Danny Banks <djb@amazon.com>
2024-09-23 20:59:07 -0700

    Update Style.tsx

--- packages/react/src/components/ThemeProvider/Style.tsx
+++ packages/react/src/components/ThemeProvider/Style.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-interface StyleProps extends React.StyleHTMLAttributes<{}> {
+interface StyleProps extends React.StyleHTMLAttributes<'style'> {
   cssText?: string;
 }
 
