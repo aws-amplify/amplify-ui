984fa990abae25040a763cd65126d7f8afb5bc12
thaddmt <68032955+thaddmt@users.noreply.github.com>
2024-09-26 11:10:45 -0700

    chore(liveness): allow selecting all cameras, allow camera selection … (#5833)
    * chore(liveness): allow selecting all cameras, allow camera selection on mobile
    
    * only show on mobile with facemovement challenge only
    
    * relax e2e tests on alpha
    
    * make more reaadable
    
    * fix
    
    * update names
    
    * refactor, add tests
    
    * Update packages/react-liveness/src/components/FaceLivenessDetector/LivenessCheck/LivenessCameraModule.tsx
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>
    
    * refactor
    
    ---------
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>

---
+++ packages/react-liveness/src/components/FaceLivenessDetector/LivenessCheck/__tests__/CameraSelector.test.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import { CameraSelector } from '../CameraSelector';
+import React from 'react';
+
+const mockMediaDevice: MediaDeviceInfo = {
+  deviceId: 'foobar',
+  groupId: 'foobar',
+  kind: 'videoinput',
+  label: 'foobar',
+  toJSON: jest.fn(),
+};
+
+describe('CameraSelector', () => {
+  it('should render', () => {
+    const result = render(
+      <CameraSelector onSelect={() => {}} devices={[mockMediaDevice]} />
+    );
+
+    expect(result.container).toBeDefined();
+  });
+});
