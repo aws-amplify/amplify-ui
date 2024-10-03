c573370b97a38332f0fbe7315550954cf2a995d0
Thadd Trinh <thadtrin@amazon.com>
2024-10-03 14:16:29 -0700

    fix?

--- packages/e2e/cypress/integration/common/ai.ts
+++ packages/e2e/cypress/integration/common/ai.ts
@@ -2,7 +2,6 @@ import { Then } from '@badeball/cypress-cucumber-preprocessor';
 import { escapeRegExp } from 'lodash';
 
 Then('I type {string}', (message: string) => {
-  cy.wait(1000); // todo: remove this once we figure out what's causing re-render
   cy.findByTestId('text-input').type(message);
 });
 
@@ -11,11 +10,13 @@ Then('I click the send message button', () => {
 });
 
 Then('I see {string} in the text input', (message: string) => {
-  cy.findByTestId('text-input').should('include', message);
+  cy.get('[data-testid="text-input"]')
+    .find('textarea')
+    .should('have.value', message);
 });
 
 Then('I click the {string} prompt', (name: string) => {
-  cy.wait(1000); // todo: remove this once we figure out what's causing re-render
+  cy.wait(2500); // todo: remove this once we figure out what's causing re-render
   cy.findByRole('button', {
     name: new RegExp(`^${escapeRegExp(name)}$`, 'i'),
   }).click();
