7ecc4adb0d86c67e7e4a9f69dae722f3e39bdb79
Emma Sauerborn <emma.m.sauerborn@gmail.com>
2024-10-04 17:14:08 -0400

    Add comments

--- packages/ui/src/theme/createTheme/resolveObject.ts
+++ packages/ui/src/theme/createTheme/resolveObject.ts
@@ -28,6 +28,10 @@ export function resolveObject<T>(object: Record<string, any>): T {
   }
 }
 
+/**
+ * Recursively traverses an object (slice) to resolve and uses
+ * compileValue to replace any string references found within it
+ */
 export function traverseObject<T>({
   slice,
   fullObj,
@@ -66,6 +70,13 @@ export function traverseObject<T>({
   return fullObj as T;
 }
 
+/**
+ * Resolves references in a value, performing recursive lookups when references are nested.
+ * value: The string that may contain references (e.g., {color.border.light}) that need to be replaced
+ * stack: keeps track of the current chain of references to detect circular references
+ * foundCirc: stores any detected circular references
+ * fullObj: The full object where references are looked up, essentially the source of all values
+ */
 export function compileValue({ value, stack, foundCirc, fullObj }) {
   let toRet = value,
     ref;
