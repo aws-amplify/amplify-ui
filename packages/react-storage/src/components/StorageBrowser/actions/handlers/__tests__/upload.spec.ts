8a9ee03cc57667c7e05ad0eb5070efe7ce295f05
Caleb Pollman <cpollman@amazon.com>
2024-10-08 10:39:43 -0700

    chore(storage-browser): add uploadHandler implementation

--- packages/react-storage/src/components/StorageBrowser/actions/handlers/__tests__/upload.spec.ts
+++ packages/react-storage/src/components/StorageBrowser/actions/handlers/__tests__/upload.spec.ts
@@ -1,7 +1,229 @@
-import { uploadHandler } from '../upload';
+import * as StorageModule from 'aws-amplify/storage';
+
+import {
+  MULTIPART_UPLOAD_THRESHOLD_BYTES,
+  uploadHandler,
+  UploadHandlerInput,
+} from '../upload';
+
+const isCancelErrorSpy = jest.spyOn(StorageModule, 'isCancelError');
+const uploadDataSpy = jest.spyOn(StorageModule, 'uploadData');
+
+const credentials = jest.fn();
+
+const config: UploadHandlerInput['config'] = {
+  accountId: 'accountId',
+  bucket: 'bucket',
+  credentials,
+  region: 'region',
+};
+
+const data = new File([], 'test-o');
+const prefix = 'prefix';
+
+const onCancel = jest.fn();
+const onComplete = jest.fn();
+const onError = jest.fn();
+const onProgress = jest.fn();
+
+const baseInput: UploadHandlerInput = {
+  config,
+  data,
+  prefix,
+  key: data.name,
+};
+
+const cancel = jest.fn();
+const pause = jest.fn();
+const resume = jest.fn();
+
+const error = new Error('Failed!');
 
 describe('uploadHandler', () => {
-  it('is null', () => {
-    expect(uploadHandler).toBeNull();
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('behaves as expected in the happy path', async () => {
+    uploadDataSpy.mockReturnValueOnce({
+      cancel,
+      pause,
+      resume,
+      result: Promise.resolve({ key: data.name }),
+      state: 'SUCCESS',
+    });
+
+    const { key, result } = uploadHandler({
+      ...baseInput,
+      options: { onComplete },
+    });
+
+    expect(await result).toBe('COMPLETE');
+
+    expect(key).toBe(baseInput.key);
+    expect(onComplete).toHaveBeenCalledTimes(1);
+    expect(onComplete).toHaveBeenCalledWith(baseInput.key);
+  });
+
+  it('calls upload with the expected values', () => {
+    uploadHandler({ ...baseInput, options: { preventOverwrite: true } });
+
+    const expected: StorageModule.UploadDataWithPathInput = {
+      data,
+      options: {
+        bucket: {
+          bucketName: config.bucket,
+          region: config.region,
+        },
+        locationCredentialsProvider: credentials,
+        onProgress: expect.any(Function),
+        preventOverwrite: true,
+      },
+      path: `${prefix}${data.name}`,
+    };
+
+    expect(uploadDataSpy).toHaveBeenCalledWith(expected);
+  });
+
+  it('calls provided onProgress callback as expected in the happy path', async () => {
+    uploadDataSpy.mockImplementation(({ options }) => {
+      // @ts-expect-error
+      options.onProgress({ totalBytes: 23, transferredBytes: 23 });
+
+      return {
+        cancel,
+        pause,
+        resume,
+        result: Promise.resolve({ key: data.name }),
+        state: 'SUCCESS',
+      };
+    });
+
+    const { key, result } = uploadHandler({
+      ...baseInput,
+      options: { onProgress },
+    });
+
+    expect(await result).toBe('COMPLETE');
+
+    expect(key).toBe(baseInput.key);
+    expect(onProgress).toHaveBeenCalledTimes(1);
+    expect(onProgress).toHaveBeenCalledWith(baseInput.key, 1);
+  });
+
+  it('calls provided onProgress callback as expected when `totalBytes` is `undefined`', async () => {
+    uploadDataSpy.mockImplementation(({ options }) => {
+      // @ts-expect-error
+      options.onProgress({ transferredBytes: 23 });
+
+      return {
+        cancel,
+        pause,
+        resume,
+        result: Promise.resolve({ key: data.name }),
+        state: 'SUCCESS',
+      };
+    });
+
+    const { key, result } = uploadHandler({
+      ...baseInput,
+      options: { onProgress },
+    });
+
+    expect(await result).toBe('COMPLETE');
+
+    expect(key).toBe(baseInput.key);
+    expect(onProgress).toHaveBeenCalledTimes(1);
+    expect(onProgress).toHaveBeenCalledWith(baseInput.key, undefined);
+  });
+
+  it('returns the expected callback values for a file size greater than 5 mb', async () => {
+    const bigData = new File(
+      [new ArrayBuffer(MULTIPART_UPLOAD_THRESHOLD_BYTES * 2)],
+      '😅'
+    );
+
+    uploadDataSpy.mockReturnValueOnce({
+      cancel,
+      pause,
+      resume,
+      result: Promise.resolve({ key: data.name }),
+      state: 'SUCCESS',
+    });
+
+    const { key, result, ...callbacks } = uploadHandler({
+      ...baseInput,
+      key: bigData.name,
+      data: bigData,
+    });
+
+    expect(await result).toBe('COMPLETE');
+
+    expect(key).toBe(bigData.name);
+    expect(callbacks).toStrictEqual({ cancel, pause, resume });
+  });
+
+  it('returns undefined callback values for a file size less than 5 mb', async () => {
+    const smallData = new File([], '😅');
+
+    uploadDataSpy.mockReturnValueOnce({
+      cancel,
+      pause,
+      resume,
+      result: Promise.resolve({ key: data.name }),
+      state: 'SUCCESS',
+    });
+
+    const { key, result, ...callbacks } = uploadHandler({
+      ...baseInput,
+      key: smallData.name,
+      data: smallData,
+    });
+
+    expect(await result).toBe('COMPLETE');
+
+    expect(key).toBe(smallData.name);
+    expect(callbacks).toStrictEqual({
+      cancel: undefined,
+      pause: undefined,
+      resume: undefined,
+    });
+  });
+
+  it('handles a failure as expected', async () => {
+    uploadDataSpy.mockReturnValueOnce({
+      cancel,
+      pause,
+      resume,
+      result: Promise.reject(error),
+      state: 'ERROR',
+    });
+
+    const { result } = uploadHandler({ ...baseInput, options: { onError } });
+
+    expect(await result).toBe('FAILED');
+
+    expect(onError).toHaveBeenCalledTimes(1);
+    expect(onError).toHaveBeenCalledWith(baseInput.key, error.message);
+  });
+
+  it('handles a cancel failure as expected', async () => {
+    // turn off console.warn in test output
+    jest.spyOn(console, 'warn').mockReturnValueOnce();
+    isCancelErrorSpy.mockReturnValue(true);
+    uploadDataSpy.mockReturnValueOnce({
+      cancel,
+      pause,
+      resume,
+      result: Promise.reject(error),
+      state: 'ERROR',
+    });
+
+    const { result } = uploadHandler({ ...baseInput, options: { onCancel } });
+
+    expect(await result).toBe('CANCELED');
+
+    expect(onCancel).toHaveBeenCalledTimes(1);
+    expect(onCancel).toHaveBeenCalledWith(baseInput.key);
   });
 });
--- packages/react-storage/src/components/StorageBrowser/actions/handlers/upload.ts
+++ packages/react-storage/src/components/StorageBrowser/actions/handlers/upload.ts
@@ -1,4 +1,11 @@
 import {
+  isCancelError,
+  uploadData,
+  UploadDataWithPathInput,
+} from 'aws-amplify/storage';
+import { isFunction } from '@aws-amplify/ui';
+
+import {
   CancelableTaskHandlerOutput,
   TaskHandler,
   TaskHandlerInput,
@@ -18,4 +25,65 @@ export interface UploadHandlerOutput extends CancelableTaskHandlerOutput {}
 export interface UploadHandler
   extends TaskHandler<UploadHandlerInput, UploadHandlerOutput> {}
 
-export const uploadHandler = null as unknown as UploadHandler;
+// 5MB for multipart upload
+// https://github.com/aws-amplify/amplify-js/blob/1a5366d113c9af4ce994168653df3aadb142c581/packages/storage/src/providers/s3/utils/constants.ts#L16
+export const MULTIPART_UPLOAD_THRESHOLD_BYTES = 5 * 1024 * 1024;
+
+const UNDEFINED_CALLBACKS = {
+  cancel: undefined,
+  pause: undefined,
+  resume: undefined,
+};
+
+export const uploadHandler: UploadHandler = ({
+  config,
+  key,
+  data,
+  options,
+  prefix,
+}) => {
+  const { bucket: bucketName, credentials, region } = config;
+  const { onCancel, onComplete, onError, onProgress } = options ?? {};
+
+  const isMultipart = data.size > MULTIPART_UPLOAD_THRESHOLD_BYTES;
+
+  const input: UploadDataWithPathInput = {
+    path: `${prefix}${key}`,
+    data,
+    options: {
+      ...options,
+      bucket: { bucketName, region },
+      locationCredentialsProvider: credentials,
+      onProgress: ({ totalBytes, transferredBytes }) => {
+        if (isFunction(onProgress))
+          onProgress(
+            key,
+            totalBytes ? transferredBytes / totalBytes : undefined
+          );
+      },
+    },
+  };
+
+  const { cancel, pause, resume, result: _result } = uploadData(input);
+
+  const result = _result
+    .then(() => {
+      if (isFunction(onComplete)) onComplete(key);
+      return 'COMPLETE' as const;
+    })
+    .catch((error: Error) => {
+      if (isCancelError(error)) {
+        if (isFunction(onCancel)) onCancel(key);
+        return 'CANCELED' as const;
+      }
+
+      if (isFunction(onError)) onError(key, error.message);
+      return 'FAILED' as const;
+    });
+
+  const callbacks = !isMultipart
+    ? UNDEFINED_CALLBACKS
+    : { cancel, pause, resume };
+
+  return { ...callbacks, key, result };
+};
