8f3d3820c421ca2b894d55f4fc33d8c9471d77e9
Jordan Van Ness <jordvn@amazon.com>
2024-10-11 15:18:20 -0700

    feat: add drag and drop to Storage Browser (#5885)
    * chore: update SET_ACTION to include files
    
    * feat: add drag-and-drop area to Upload Action and Location Detail views
    
    * test: add in-progress unit testing for drag-and-drop
    
    * test: add e2e testing for storage browser drag and drop
    
    * fix(test): fix issues with LocationDetailView unit test
    
    * chore: restoring unintentionally removed 'Navigate' to LocationDetailView
    
    * chore(tests): adding additional testing for folders and outlining upon drag over
    
    * chore: undoing out of scope change
    
    * chore: moving DropZoneElement functionality directly to Table
    
    * chore: removing DropZoneElement component
    
    * chore: removing unnecessary dropHandlers
    
    * chore: fix tests and update payload to actionType
    
    * chore: remove usage of useHandleUpload from Location Detail View
    
    * fix(tests): fixing bad formatting introduced when resolving merge conflict
    
    * chore: remove no longer used constant value in test
    
    * fix(tests): fix issue with drop action introduced to e2e testing by move to Table
    
    * Update packages/react-storage/src/components/StorageBrowser/views/Controls/Table.tsx
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>
    
    * Update packages/react-storage/src/components/StorageBrowser/views/LocationActionView/UploadControls.tsx
    
    Co-authored-by: Scott Rees <6165315+reesscot@users.noreply.github.com>
    
    * chore: addressing feedback on props and readability
    
    * fix(tests): fix name change typo
    
    * chore: move isFile helper to utils file
    
    * chore(tests): add unit test for isFile
    
    ---------
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>
    Co-authored-by: Scott Rees <6165315+reesscot@users.noreply.github.com>

--- packages/react-storage/src/components/StorageBrowser/views/Controls/Table.tsx
+++ packages/react-storage/src/components/StorageBrowser/views/Controls/Table.tsx
@@ -16,6 +16,7 @@ import { CLASS_BASE } from '../constants';
 import { compareDates, compareNumbers, compareStrings } from '../utils';
 
 import { DownloadControl } from './Download';
+import { useDropZone } from '@aws-amplify/ui-react-core';
 import { CheckboxControl } from '../../components/Checkbox';
 
 export type SortDirection = 'ascending' | 'descending' | 'none';
@@ -127,6 +128,7 @@ export type RenderRowItem<T> = (row: T, index: number) => JSX.Element;
 interface TableControlProps<T> {
   data: T[];
   columns: Column<T>[];
+  handleDroppedFiles?: (files: File[]) => void;
   renderHeaderItem: RenderHeaderItem<T>;
   renderRowItem: RenderRowItem<T>;
 }
@@ -134,13 +136,26 @@ interface TableControlProps<T> {
 export function TableControl<U>({
   data,
   columns,
+  handleDroppedFiles,
   renderHeaderItem,
   renderRowItem,
 }: TableControlProps<U>): React.JSX.Element {
   const ariaLabel = 'Table';
 
+  const { dragState, ...dropHandlers } = useDropZone({
+    onDropComplete: ({ acceptedFiles }) =>
+      handleDroppedFiles && handleDroppedFiles(acceptedFiles),
+  });
+
   return (
-    <Table aria-label={ariaLabel} className={BLOCK_NAME}>
+    <Table
+      {...dropHandlers}
+      aria-label={ariaLabel}
+      data-testid="storage-browser-table"
+      className={`${BLOCK_NAME} ${
+        dragState !== 'inactive' ? `${BLOCK_NAME}__dropzone` : ''
+      }`}
+    >
       <TableHead className={`${BLOCK_NAME}__head`}>
         <TableRow className={`${BLOCK_NAME}__row`}>
           {columns.map((column) => renderHeaderItem(column))}
@@ -168,8 +183,10 @@ const LocationDetailViewColumnSortMap = {
 const LocationDetailViewColumnEmptyHeaderMap = ['download'];
 
 export const LocationDetailViewTable = ({
+  handleDroppedFiles,
   range,
 }: {
+  handleDroppedFiles: (files: File[]) => void;
   range: [start: number, end: number];
 }): JSX.Element | null => {
   const [start, end] = range;
@@ -419,6 +436,7 @@ export const LocationDetailViewTable = ({
     <TableControl
       columns={LOCATION_DETAIL_VIEW_COLUMNS}
       data={tableData}
+      handleDroppedFiles={handleDroppedFiles}
       renderHeaderItem={renderHeaderItem}
       renderRowItem={renderRowItem}
     />
