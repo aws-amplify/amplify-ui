88351f241703f1eb233f05d336f1f194243b4e9f
Caleb Pollman <cpollman@amazon.com>
2024-10-23 13:05:20 -0700

    feat(storage-browser): add provider contexts (#5930)

---
+++ packages/react-storage/src/components/StorageBrowser/providers/configuration/credentials/types.ts
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import {
+  GetLocationCredentials,
+  LocationCredentialsStore,
+  CreateLocationCredentialsStoreInput,
+  LocationCredentialsProvider,
+  Permission,
+} from '../../../storage-internal';
+
+export type RegisterAuthListener = (onStateChange: () => void) => void;
+
+export type GetCredentials = (input: {
+  bucket: string;
+  permission: Permission;
+  prefix: string;
+}) => LocationCredentialsProvider;
+
+export interface CredentialsStore
+  extends Omit<LocationCredentialsStore, 'getProvider'> {
+  getCredentials: GetCredentials;
+}
+
+export interface CreateCredentialsStoreInput
+  extends CreateLocationCredentialsStoreInput {}
+
+export interface CredentialsProviderProps {
+  children?: React.ReactNode;
+  getLocationCredentials: GetLocationCredentials;
+  onDestroy?: () => void;
+  registerAuthListener: RegisterAuthListener;
+}
---
+++ packages/react-storage/src/components/StorageBrowser/providers/index.ts
@@ -0,0 +1,3 @@
+export { createConfigurationProvider } from './configuration';
+export { StoreProviderProps, StoreProvider } from './store/StoreProvider';
+export { CredentialsProviderProps } from './configuration/credentials';
