c812a87be1aff9d4ef010dff75de20b97d360bc6
ManojNB <manojnb95@gmail.com>
2024-10-24 13:43:58 -0700

    chore: add tests and remove props nested type

--- packages/react-storage/src/components/StorageBrowser/controls/hooks/useActionStart.tsx
+++ packages/react-storage/src/components/StorageBrowser/controls/hooks/useActionStart.tsx
@@ -1,20 +1,14 @@
 import { ActionStartProps } from '../../composables/ActionStart';
 import { useControlsContext } from '../../controls/context';
 
-export type UseActionStart = () => {
-  props?: ActionStartProps;
-};
-
-export const useActionStart: UseActionStart = () => {
+export const useActionStart = (): ActionStartProps => {
   const {
     data: { actionStartLabel, isActionStartDisabled },
     onActionStart,
   } = useControlsContext();
   return {
-    props: {
-      label: actionStartLabel,
-      isDisabled: isActionStartDisabled,
-      onStart: onActionStart,
-    },
+    label: actionStartLabel,
+    isDisabled: isActionStartDisabled,
+    onStart: onActionStart,
   };
 };
