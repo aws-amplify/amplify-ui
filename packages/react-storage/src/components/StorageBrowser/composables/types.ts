0bd6d9e171d5c32f6054046ce62b6571b643cff4
Chris Fang <cshfang@gmail.com>
2024-10-25 10:46:01 -0700

    Fix typing issue resulting from merge

--- packages/react-storage/src/components/StorageBrowser/composables/types.ts
+++ packages/react-storage/src/components/StorageBrowser/composables/types.ts
@@ -1,8 +1,10 @@
 import { DataTableProps } from './DataTable';
+import { DropZoneProps } from './DropZone';
 import { StatusDisplayProps } from './StatusDisplay';
 
 export interface Composables {
   DataTable: React.ComponentType<DataTableProps>;
+  DropZone: React.ComponentType<DropZoneProps>;
   StatusDisplay: React.ComponentType<StatusDisplayProps>;
 }
 
