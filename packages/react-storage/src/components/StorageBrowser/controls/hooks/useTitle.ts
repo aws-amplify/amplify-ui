d8d57da16add755b30a50bdeebb2677afe34febb
JoonWon Choi <requiemdeciel@gmail.com>
2024-10-30 10:28:05 -0700

    Address comments

--- packages/react-storage/src/components/StorageBrowser/controls/hooks/useTitle.ts
+++ packages/react-storage/src/components/StorageBrowser/controls/hooks/useTitle.ts
@@ -1,16 +1,12 @@
 import { TitleProps } from '../../composables/Title';
 import { useControlsContext } from '../../controls/context';
 
-export type UseTitle = () => {
-  props?: TitleProps;
-};
+type UseTitle = () => TitleProps;
 
 export const useTitle: UseTitle = () => {
   const { data } = useControlsContext();
 
   return {
-    props: {
-      title: data?.title,
-    },
+    title: data ? data.title : undefined,
   };
 };
