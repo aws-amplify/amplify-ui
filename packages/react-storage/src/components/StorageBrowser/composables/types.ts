ada46d443ecd4d97d6180f2c70ded755ef6b18fb
Chris F <5827964+cshfang@users.noreply.github.com>
2024-10-30 19:54:44 -0700

    chore(storage-browser): add composable DropZone (#5956)
    * chore(storage-browser): add composable DropZone
    
    * Fix typing issue resulting from merge

--- packages/react-storage/src/components/StorageBrowser/composables/types.ts
+++ packages/react-storage/src/components/StorageBrowser/composables/types.ts
@@ -1,4 +1,5 @@
 import { DataTableProps } from './DataTable';
+import { DropZoneProps } from './DropZone';
 import { StatusDisplayProps } from './StatusDisplay';
 import { DataRefreshProps } from './DataRefresh';
 import { ActionStartProps } from './ActionStart';
@@ -6,6 +7,7 @@ import { ActionStartProps } from './ActionStart';
 export interface Composables {
   DataRefresh: React.ComponentType<DataRefreshProps>;
   DataTable: React.ComponentType<DataTableProps>;
+  DropZone: React.ComponentType<DropZoneProps>;
   StatusDisplay: React.ComponentType<StatusDisplayProps>;
   ActionStart: React.ComponentType<ActionStartProps>;
 }
