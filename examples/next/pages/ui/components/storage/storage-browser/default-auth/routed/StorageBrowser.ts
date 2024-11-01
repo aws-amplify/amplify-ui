3395534d2f8f0e7e9f612e0564df7b56d4c7affa
ashika112 <akasivis@amazon.com>
2024-11-01 16:40:24 -0700

    missed cleanup

--- examples/next/pages/ui/components/storage/storage-browser/default-auth/routed/StorageBrowser.ts
+++ examples/next/pages/ui/components/storage/storage-browser/default-auth/routed/StorageBrowser.ts
@@ -9,13 +9,10 @@ import '@aws-amplify/ui-react-storage/styles.css';
 import '@aws-amplify/ui-react-storage/storage-browser-styles.css';
 
 import config from './aws-exports';
-import { routedAuth } from '../../managed-auth/routed/StorageBrowser';
 
 Amplify.configure(config);
 
 export const { StorageBrowser } = createStorageBrowser({
   elements: elementsDefault,
-  config: createAmplifyAuthAdapter({
-    registerAuthListener: routedAuth.registerAuthListener,
-  }),
+  config: createAmplifyAuthAdapter(),
 });
