e7e7a2e8b3832d29943f72e6a90c0c3848d49f8e
ashika112 <155593080+ashika112@users.noreply.github.com>
2024-11-04 15:31:37 -0800

    [Feat]: Default Auth list locations (#5977)
    * add pagination logic
    * update unit tests
    * update cache logic
    * fix integ tests for default Auth
    * update unit test to improve coverage

---
+++ packages/react-storage/src/components/StorageBrowser/adapters/createAmplifyAuthAdapter/createAmplifyListLocationsHandler.ts
@@ -0,0 +1,49 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { LocationAccess } from '../../actions/handlers';
+import {
+  ListLocations,
+  ListLocationsOutput,
+  listPaths,
+  ListPathsOutput,
+} from '../../storage-internal';
+import { getPaginatedLocations } from './getPaginatedLocations';
+import { mapAmplifyPermissions } from './mapAmplifyPermissions';
+
+export const createAmplifyListLocationsHandler = (): ListLocations => {
+  let cachedLocations: ListLocationsOutput['locations'] = [];
+
+  return async function listLocations(input = {}) {
+    const { pageSize, nextToken } = input;
+
+    if (cachedLocations.length > 0) {
+      return getPaginatedLocations({
+        locations: cachedLocations,
+        pageSize,
+        nextToken,
+      });
+    }
+
+    const { locations }: { locations: ListPathsOutput['locations'] } =
+      await listPaths();
+
+    const sanitizedLocations: LocationAccess[] = locations.map(
+      ({ bucket, permission, prefix, type }) => {
+        return {
+          type,
+          permission: mapAmplifyPermissions(permission),
+          scope: `s3://${bucket}/${prefix}`,
+        };
+      }
+    );
+
+    cachedLocations = sanitizedLocations;
+
+    return getPaginatedLocations({
+      locations: cachedLocations,
+      pageSize,
+      nextToken,
+    });
+  };
+};
