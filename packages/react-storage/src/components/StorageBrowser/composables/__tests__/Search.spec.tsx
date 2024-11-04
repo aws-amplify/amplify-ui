5ddb88590ebea6463fa576ab35c0cb373a5c0f0c
Jordan Van Ness <jordvn@amazon.com>
2024-11-04 14:55:01 -0800

    chore: refactor search to add composability support (#5993)
    * chore: first draft of refactoring search to composable
    
    * fix(tests): fix tests by updating search control test and removing addition of ref to input element props
    
    * chore: move onSearch outside of data in controls context
    
    * chore(styles): clean up style selectors and rules
    
    * chore: remove unintended changes
    
    * chore: restoring unintentionally removed styles
    
    * chore: bump up createStorageBrowser size limit
    
    * chore: additional bump to createStorageBrowser size limit
    
    ---------
    
    Co-authored-by: Pranav Malewadkar <pranavml@amazon.com>

---
+++ packages/react-storage/src/components/StorageBrowser/composables/__tests__/Search.spec.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Search } from '../Search';
+import { act } from 'react-test-renderer';
+import userEvent from '@testing-library/user-event';
+
+const searchTerm = 'search term';
+
+describe('Search', () => {
+  it('renders the Search composable', () => {
+    render(<Search onSearch={jest.fn()} />);
+
+    const field = screen.getByPlaceholderText('Search current folder');
+    const checkbox = screen.getByRole('checkbox');
+    const button = screen.getByRole('button', { name: 'Submit' });
+
+    expect(button).toBeInTheDocument();
+    expect(field).toBeInTheDocument();
+    expect(checkbox).toBeInTheDocument();
+  });
+
+  it('uses a checkbox to determine if subfolders should be included in search', () => {
+    const onSearch = jest.fn();
+
+    render(<Search onSearch={onSearch} />);
+
+    const submitButton = screen.getByRole('button', { name: 'Submit' });
+    act(() => {
+      submitButton.click();
+    });
+
+    expect(onSearch).toHaveBeenCalledWith('', false);
+
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).not.toBeChecked();
+
+    act(() => {
+      checkbox.click();
+    });
+    expect(checkbox).toBeChecked();
+
+    act(() => {
+      submitButton.click();
+    });
+    expect(onSearch).toHaveBeenCalledWith('', true);
+  });
+
+  it('searches for the term typed into the field', async () => {
+    const onSearch = jest.fn();
+
+    render(<Search onSearch={onSearch} />);
+
+    const inputField = screen.getByPlaceholderText('Search current folder');
+    await act(async () => {
+      await userEvent.type(inputField, searchTerm);
+    });
+
+    const submitButton = screen.getByRole('button', { name: 'Submit' });
+    act(() => {
+      submitButton.click();
+    });
+
+    expect(onSearch).toHaveBeenCalledWith(searchTerm, false);
+  });
+
+  it('calls onSearch when submit button is clicked', () => {
+    const onSearch = jest.fn();
+
+    render(<Search onSearch={onSearch} />);
+
+    const submitButton = screen.getByRole('button', { name: 'Submit' });
+    act(() => {
+      submitButton.click();
+    });
+    expect(onSearch).toHaveBeenCalledWith('', false);
+  });
+});
