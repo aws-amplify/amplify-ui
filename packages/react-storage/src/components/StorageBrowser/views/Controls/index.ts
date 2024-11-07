7e31f0455181fd0d4ac346eb608f56ece0c0b52a
JoonWon Choi <requiemdeciel@gmail.com>
2024-11-06 16:02:38 -0800

    Resolve conflict I

--- packages/react-storage/src/components/StorageBrowser/views/Controls/index.ts
+++ packages/react-storage/src/components/StorageBrowser/views/Controls/index.ts
@@ -1,9 +1,7 @@
 export { EmptyMessageControl } from './EmptyMessage';
 export { ExitControl } from './Exit';
 export { LoadingControl } from './Loading';
-export { MessageControl } from './Message';
-export { OverwriteControl } from './Overwrite';
 export { PaginateControl } from './Paginate';
 export { SearchControl } from './Search';
-export { TableControl } from './Table';
+export { TableControl, LocationDetailViewTable } from './Table';
 export { Controls } from './Controls';
