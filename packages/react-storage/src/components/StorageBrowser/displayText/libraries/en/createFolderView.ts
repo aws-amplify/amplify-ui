5289361ad9e605c94fd1cb07b6ada43fd21cbad0
dindjarinjs <187552781+dindjarinjs@users.noreply.github.com>
2024-11-08 16:19:22 -0700

    chore: fix create folder title

--- packages/react-storage/src/components/StorageBrowser/displayText/libraries/en/createFolderView.ts
+++ packages/react-storage/src/components/StorageBrowser/displayText/libraries/en/createFolderView.ts
@@ -4,8 +4,8 @@ import { DefaultCreateFolderViewDisplayText } from '../../types';
 export const DEFAULT_CREATE_FOLDER_VIEW_DISPLAY_TEXT: DefaultCreateFolderViewDisplayText =
   {
     ...DEFAULT_ACTION_VIEW_DISPLAY_TEXT,
-    title: 'Copy',
-    actionStartLabel: 'Copy',
+    title: 'Create folder',
+    actionStartLabel: 'Create folder',
     getValidationMessage: (_error) => {
       // @TODO: maybe add logic to check which character caused the failure
       return `Folder name must be at least one character and cannot contain a "/", ".", or ".."`;
