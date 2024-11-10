0fc6bdb9823ac40a5c61495c8af1f41e155404aa
Danny Banks <djb@amazon.com>
2024-11-09 21:24:00 -0800

    chore: starting to migrate some amplify components

--- packages/react-storage/src/components/StorageBrowser/StorageBrowserAmplify.tsx
+++ packages/react-storage/src/components/StorageBrowser/StorageBrowserAmplify.tsx
@@ -6,6 +6,12 @@ import {
   StorageBrowserProps as StorageBrowserPropsBase,
 } from './createStorageBrowser';
 import { createAmplifyAuthAdapter } from './adapters';
+import {
+  Breadcrumbs,
+  Flex,
+  SearchField,
+  SwitchField,
+} from '@aws-amplify/ui-react';
 
 export interface StorageBrowserProps extends StorageBrowserPropsBase {}
 
@@ -16,6 +22,55 @@ export const StorageBrowser = ({
     createStorageBrowser({
       elements: elementsDefault,
       config: createAmplifyAuthAdapter(),
+      components: {
+        Search: ({ onSearch, searchPlaceholder }) => {
+          const [includeSubfolders, setIncludeSubfolders] =
+            React.useState(false);
+          return (
+            <Flex direction="row">
+              <SearchField
+                label="Search"
+                size="small"
+                placeholder={searchPlaceholder}
+                onSubmit={(e) => {
+                  onSearch?.(e, includeSubfolders);
+                }}
+                onClear={() => {
+                  onSearch?.('', includeSubfolders);
+                }}
+              />
+              <SwitchField
+                size="small"
+                label="include subfolders"
+                labelPosition="end"
+                isChecked={includeSubfolders}
+                onChange={(e) => {
+                  setIncludeSubfolders(e.target.checked);
+                }}
+              />
+            </Flex>
+          );
+        },
+        Navigation: ({ items }) => {
+          return (
+            <Breadcrumbs.Container>
+              {items.map((item, i) => {
+                return (
+                  <Breadcrumbs.Item key={i}>
+                    <Breadcrumbs.Link
+                      isCurrent={item.isCurrent}
+                      onClick={item.onNavigate}
+                    >
+                      {item.name}
+                    </Breadcrumbs.Link>
+                    {item.isCurrent ? null : <Breadcrumbs.Separator />}
+                  </Breadcrumbs.Item>
+                );
+              })}
+            </Breadcrumbs.Container>
+          );
+        },
+      },
     })
   ).current;
 
