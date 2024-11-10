7786fb66b75aaa1d9d1efb73dca652d56e63f038
Caleb Pollman <cpollman@amazon.com>
2024-11-09 16:47:16 -0800

    feat(sto-bro): expose components API for existing composables

--- packages/react-storage/src/components/StorageBrowser/ComponentsProvider.tsx
+++ packages/react-storage/src/components/StorageBrowser/ComponentsProvider.tsx
@@ -2,26 +2,27 @@ import React from 'react';
 
 import { ElementsProvider } from '@aws-amplify/ui-react-core/elements';
 
-import { ComposablesProvider } from './composables/context';
-import { Composables } from './composables/types';
+import { ComposablesProvider, Composables } from './composables';
 import { StorageBrowserElements } from './context/elements';
+
+export interface Components extends Partial<Composables> {}
 
 export interface ComponentsProviderProps {
   children?: React.ReactNode;
-  // temp disablng of linting rule to note where `components`/"slots" context is provided
-  // eslint-disable-next-line react/no-unused-prop-types
-  components?: Composables;
+  composables?: Composables;
   elements?: Partial<StorageBrowserElements>;
 }
 
 export function ComponentsProvider(
   props: ComponentsProviderProps
 ): React.JSX.Element {
-  const { children, elements } = props;
+  const { children, composables, elements } = props;
 
   return (
     <ElementsProvider elements={elements}>
-      <ComposablesProvider>{children}</ComposablesProvider>
+      <ComposablesProvider composables={composables}>
+        {children}
+      </ComposablesProvider>
     </ElementsProvider>
   );
 }
---
+++ packages/react-storage/src/components/StorageBrowser/composables/defaults.ts
@@ -0,0 +1,21 @@
+import { ActionCancel } from './ActionCancel';
+import { ActionStart } from './ActionStart';
+import { DataRefresh } from './DataRefresh';
+import { DataTable } from './DataTable';
+import { DropZone } from './DropZone';
+import { Navigation } from './Navigation';
+import { StatusDisplay } from './StatusDisplay';
+import { Search } from './Search';
+
+import { Composables } from './types';
+
+export const DEFAULT_COMPOSABLES: Composables = {
+  ActionCancel,
+  ActionStart,
+  DataRefresh,
+  DataTable,
+  DropZone,
+  Navigation,
+  StatusDisplay,
+  Search,
+};
