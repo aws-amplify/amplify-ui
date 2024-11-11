d23b158dfb0c1cd66becc59d0fc210fabb312fb9
Jordan Van Ness <jordvn@amazon.com>
2024-11-11 08:24:16 -0800

    chore: add Pagination to default composables

--- packages/react-storage/src/components/StorageBrowser/composables/defaults.ts
+++ packages/react-storage/src/components/StorageBrowser/composables/defaults.ts
@@ -7,6 +7,7 @@ import { FolderNameField } from './FolderNameField';
 import { LoadingIndicator } from './LoadingIndicator';
 import { Message } from './Message';
 import { Navigation } from './Navigation';
+import { Pagination } from './Pagination';
 import { StatusDisplay } from './StatusDisplay';
 import { Search } from './Search';
 
@@ -22,6 +23,7 @@ export const DEFAULT_COMPOSABLES: Composables = {
   LoadingIndicator,
   Message,
   Navigation,
+  Pagination,
   StatusDisplay,
   Search,
 };
