915abf30df12d1075c5ddf6d988e46998d209e44
Jordan Van Ness <jordvn@amazon.com>
2024-11-11 13:36:22 -0800

    chore(tests): update dropzone test to make onDrop async

--- packages/react/src/primitives/DropZone/__tests__/DropZone.test.tsx
+++ packages/react/src/primitives/DropZone/__tests__/DropZone.test.tsx
@@ -6,12 +6,12 @@ import { View } from '../../View';
 
 describe('DropZone', () => {
   it('should match snapshot', () => {
-    const { container } = render(<DropZone onDrop={() => {}} />);
+    const { container } = render(<DropZone onDrop={async () => {}} />);
     expect(container).toMatchSnapshot();
   });
 
   it('should have the right class', () => {
-    const { container } = render(<DropZone onDrop={() => {}} />);
+    const { container } = render(<DropZone onDrop={async () => {}} />);
     expect(container.firstChild).toHaveClass(ComponentClassName.DropZone);
   });
 
