6ac12dcd4b01c6ccbd88d73569f1cf8d41573561
ManojNB <manojnb95@gmail.com>
2024-11-14 00:46:07 +0530

    chore: remove isEmpty and use || for predicate

--- packages/react-storage/src/components/StorageBrowser/displayText/libraries/en/locationDetailView.ts
+++ packages/react-storage/src/components/StorageBrowser/displayText/libraries/en/locationDetailView.ts
@@ -1,6 +1,5 @@
 import { DEFAULT_LIST_VIEW_DISPLAY_TEXT } from './shared';
 import { DefaultLocationDetailViewDisplayText } from '../../types';
-import { isEmpty } from '@aws-amplify/ui';
 
 export const DEFAULT_LOCATION_DETAIL_VIEW_DISPLAY_TEXT: DefaultLocationDetailViewDisplayText =
   {
@@ -16,6 +15,6 @@ export const DEFAULT_LOCATION_DETAIL_VIEW_DISPLAY_TEXT: DefaultLocationDetailVie
     title: (location) => {
       const { current, key } = location;
       const { bucket = '' } = current ?? {};
-      return isEmpty(key) ? bucket : key;
+      return key || bucket;
     },
   };
