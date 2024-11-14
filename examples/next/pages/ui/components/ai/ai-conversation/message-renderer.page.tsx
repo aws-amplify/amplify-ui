2e6fd0c6b3fa093557f731a25b5dc51a1e13e71a
Danny Banks <djb@amazon.com>
2024-11-14 11:05:54 -0800

    chore: ai examples cleanup

--- examples/next/pages/ui/components/ai/ai-conversation/message-renderer.page.tsx
+++ examples/next/pages/ui/components/ai/ai-conversation/message-renderer.page.tsx
@@ -1,37 +1,12 @@
-import { Amplify } from 'aws-amplify';
 import { AIConversation } from '@aws-amplify/ui-react-ai';
 import '@aws-amplify/ui-react/styles.css';
 
-import outputs from './amplify_outputs';
-import { Authenticator, Card, Text } from '@aws-amplify/ui-react';
+import { Card, Text } from '@aws-amplify/ui-react';
 import Image from 'next/image';
 import { INITIAL_MESSAGES } from '../constants';
+import { convertBufferToBase64 } from '../utils';
 
-Amplify.configure(outputs);
-
-function arrayBufferToBase64(buffer: ArrayBuffer) {
-  let binary = '';
-  const bytes = new Uint8Array(buffer);
-  const len = bytes.byteLength;
-  for (let i = 0; i < len; i++) {
-    binary += String.fromCharCode(bytes[i]);
-  }
-  return window.btoa(binary);
-}
-
-function convertBufferToBase64(buffer: ArrayBuffer, format: string): string {
-  let base64string = '';
-  // Use node-based buffer if available
-  // fall back on browser if not
-  if (typeof Buffer !== 'undefined') {
-    base64string = Buffer.from(new Uint8Array(buffer)).toString('base64');
-  } else {
-    base64string = arrayBufferToBase64(buffer);
-  }
-  return `data:image/${format};base64,${base64string}`;
-}
-
-function Chat() {
+export default function Example() {
   return (
     <Card variation="outlined" width="50%" height="300px" margin="0 auto">
       <AIConversation
@@ -63,13 +38,5 @@ function Chat() {
         variant="bubble"
       />
     </Card>
-  );
-}
-
-export default function Example() {
-  return (
-    <Authenticator>
-      <Chat />
-    </Authenticator>
   );
 }
