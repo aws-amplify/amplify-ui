b1663fc1372a0785aea126cbb7fda74484960297
Danny Banks <djb@amazon.com>
2024-11-14 12:21:30 -0800

    chore: ai examples cleanup (#6088)

--- examples/next/pages/ui/components/ai/constants.tsx
+++ examples/next/pages/ui/components/ai/constants.tsx
@@ -1,12 +1,5 @@
 import { Icon } from '@aws-amplify/ui-react';
-
-function convertBufferToBase64(
-  buffer: ArrayBuffer,
-  format: 'png' | 'jpeg' | 'gif' | 'webp'
-): string {
-  const base64string = Buffer.from(new Uint8Array(buffer)).toString('base64');
-  return `data:image/${format};base64,${base64string}`;
-}
+import { convertBufferToBase64 } from './utils';
 
 export const PROMPTS = [
   {
