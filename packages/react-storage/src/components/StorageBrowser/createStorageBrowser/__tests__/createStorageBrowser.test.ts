0448aab2ce289f47c2d4dbb24e01be467eee1821
Caleb Pollman <cpollman@amazon.com>
2024-11-17 13:10:15 -0800

    chore(sto-bro): remove unused createStorageBrowser dir (#6117)

--- packages/react-storage/src/components/StorageBrowser/createStorageBrowser/__tests__/createStorageBrowser.test.ts
+++
@@ -1,6 +0,0 @@
-import { expectTypeTestsToPassAsync } from 'jest-tsd';
-
-// evaluates type defs in corresponding test-d.ts file
-it('should not produce static type errors', async () => {
-  await expectTypeTestsToPassAsync(__filename);
-});
--- packages/react-storage/src/components/StorageBrowser/createStorageBrowser/createStorageBrowser.ts
+++
@@ -1,7 +0,0 @@
-import { CreateStorageBrowser } from './types';
-
-export const createStorageBrowser: CreateStorageBrowser = (_) => {
-  // TODO: implement this function
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return {} as any;
-};
