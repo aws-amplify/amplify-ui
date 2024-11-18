95396d597ed6fdc560bd9bab64b012bfc93b4040
Allan Zheng <zheallan@amazon.com>
2024-11-17 22:26:00 -0800

    test: add file validation message display text test

--- packages/react-storage/src/components/StorageBrowser/displayText/libraries/en/__tests__/uploadView.spec.ts
+++ packages/react-storage/src/components/StorageBrowser/displayText/libraries/en/__tests__/uploadView.spec.ts
@@ -1,17 +1,30 @@
-import { ACTION_SCENARIOS } from './scenarios';
+import {
+  ACTION_SCENARIOS,
+  UPLOAD_ACTION_SCENARIOS,
+  UPLOAD_FILES_VALIDATION_SCENARIOS,
+} from './scenarios';
 import { DEFAULT_UPLOAD_VIEW_DISPLAY_TEXT } from '../uploadView';
 
-describe('CopyView display text values', () => {
+describe('UploadView display text values', () => {
   it('should match snapshot values', () => {
     expect(DEFAULT_UPLOAD_VIEW_DISPLAY_TEXT).toMatchSnapshot();
   });
 
-  it.each(ACTION_SCENARIOS)(
+  it.each(UPLOAD_ACTION_SCENARIOS)(
     '`getActionCompleteMessage` returns the expected values in the %s scenario',
     (_, counts) => {
       const { getActionCompleteMessage } = DEFAULT_UPLOAD_VIEW_DISPLAY_TEXT;
 
       expect(getActionCompleteMessage({ counts })).toMatchSnapshot();
+    }
+  );
+
+  it.each(UPLOAD_FILES_VALIDATION_SCENARIOS)(
+    '`getFilesValidationMessage` returns expected values in the %s scenario',
+    (_, invalidFiles) => {
+      const { getFilesValidationMessage } = DEFAULT_UPLOAD_VIEW_DISPLAY_TEXT;
+
+      expect(getFilesValidationMessage({ invalidFiles })).toMatchSnapshot();
     }
   );
 });
