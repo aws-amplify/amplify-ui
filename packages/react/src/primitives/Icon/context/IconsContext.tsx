0e71043383ddefd8f13b92115e73ae2b9cb9e17f
Caleb Pollman <cpollman@amazon.com>
2024-11-18 00:03:43 -0800

    feat(storage-browser): add Amplify IconsProvider (#6136)

--- packages/react/src/primitives/Icon/context/IconsContext.tsx
+++ packages/react/src/primitives/Icon/context/IconsContext.tsx
@@ -1,5 +1,38 @@
 import * as React from 'react';
 
+type StorageBrowserIconType =
+  | 'action-canceled'
+  | 'action-error'
+  | 'action-initial'
+  | 'action-progress'
+  | 'action-queued'
+  | 'action-success'
+  | 'cancel'
+  | 'create-folder'
+  | 'copy-file'
+  | 'delete-file'
+  | 'dismiss'
+  | 'download'
+  | 'error'
+  | 'exit'
+  | 'file'
+  | 'folder'
+  | 'info'
+  | 'loading'
+  | 'menu'
+  | 'paginate-next'
+  | 'paginate-previous'
+  | 'refresh'
+  | 'search'
+  | 'sort-ascending'
+  | 'sort-descending'
+  | 'sort-indeterminate'
+  | 'success'
+  | 'upload-file'
+  | 'upload-folder'
+  | 'vertical-kebab'
+  | 'warning';
+
 type ComponentIcons<Keys extends string> = {
   [Key in Keys]?: React.ReactNode;
 };
@@ -21,6 +54,7 @@ export type IconsContextInterface = {
   searchField?: ComponentIcons<'search'>;
   select?: ComponentIcons<'expand'>;
   stepperField?: ComponentIcons<'add' | 'remove'>;
+  storageBrowser?: ComponentIcons<StorageBrowserIconType>;
   storageManager?: ComponentIcons<
     'upload' | 'remove' | 'error' | 'success' | 'file'
   >;
