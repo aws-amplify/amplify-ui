13c966afd54f1907b006195bbbe3ccc63e18603c
Jordan Van Ness <jordvn@amazon.com>
2024-11-18 12:00:55 -0800

    chore: move public export to react/src/internal.ts

--- packages/react/src/internal.ts
+++ packages/react/src/internal.ts
@@ -8,6 +8,10 @@ export { useDropZone } from '@aws-amplify/ui-react-core';
 export * from './components/FilterChildren';
 export { AlertIcon } from './primitives/Alert/AlertIcon';
 export * from './primitives/Icon/internal';
+export {
+  STORAGE_BROWSER_ICON_PATHS,
+  StorageBrowserIconType,
+} from './primitives/Icon/context/StorageBrowserIcons';
 
 export {
   FileSelect,
--- packages/react/src/primitives/Icon/internal.ts
+++ packages/react/src/primitives/Icon/internal.ts
@@ -1,6 +1,2 @@
 export * from './icons';
 export { useIcons } from './context/useIcons';
-export {
-  STORAGE_BROWSER_ICON_PATHS,
-  StorageBrowserIconType,
-} from './context/StorageBrowserIcons';
