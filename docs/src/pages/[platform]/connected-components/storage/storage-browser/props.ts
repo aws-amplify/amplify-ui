bfd202b34a3a182be4418adf21e3d6d00a10979d
Danny Banks <djb@amazon.com>
2024-11-22 10:17:43 -0800

    fixing stuff

---
+++ docs/src/pages/[platform]/connected-components/storage/storage-browser/props.ts
@@ -0,0 +1,9 @@
+import { DEFAULT_STORAGE_BROWSER_DISPLAY_TEXT } from '@aws-amplify/ui-react-storage/dist/types/components/StorageBrowser/displayText';
+console.log(Object.keys(DEFAULT_STORAGE_BROWSER_DISPLAY_TEXT.CopyView));
+export const DISPLAY_TEXT = [
+  {
+    name: 'getFilesUploadedText?',
+    description: '',
+    type: `(count: number) => string`,
+  },
+];
