4bfb543e147b1649311212bddcc6108635fcac1a
dindjarinjs <187552781+dindjarinjs@users.noreply.github.com>
2024-11-22 14:27:17 -0700

    Update build-system-tests/scripts/build-android.sh

--- build-system-tests/scripts/build-android.sh
+++ build-system-tests/scripts/build-android.sh
@@ -35,7 +35,7 @@ if [ $BUILD_TOOL == 'expo' ]; then
 
   # Run npm run android in the background
   log "command" "npm run android -- --port 19000 >$LOG_FILE &"
-  npm run android -- --port 19000 >$LOG_FILE &
+  npm run android -- -p 19000 >$LOG_FILE &
   npx wait-on -t 20000 tcp:19000
 else
   log "command" "cd android >$LOG_FILE "
