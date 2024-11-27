e9b219fa1c0828aef6c00def2c2b063eca701555
Chris F <5827964+cshfang@users.noreply.github.com>
2024-11-26 16:45:05 -0800

    chore(storage-browser): Add control hooks for consistency (#6209)

--- packages/react-storage/src/components/StorageBrowser/controls/__tests__/PaginationControl.spec.tsx
+++ packages/react-storage/src/components/StorageBrowser/controls/__tests__/PaginationControl.spec.tsx
@@ -1,60 +1,34 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { PaginationControl } from '../PaginationControl';
-import { useControlsContext } from '../context';
+import { usePagination } from '../hooks/usePagination';
 import { useResolvedComposable } from '../hooks/useResolvedComposable';
 
-jest.mock('../context');
+jest.mock('../hooks/usePagination');
 jest.mock('../hooks/useResolvedComposable');
+jest.mock('../../composables/Pagination', () => ({
+  Pagination: () => <div data-testid="pagination" />,
+}));
 
 describe('PaginationControl', () => {
-  // assert mocks
-  const mockUseControlsContext = useControlsContext as jest.Mock;
-  const mockUseResolvedComposable = useResolvedComposable as jest.Mock;
+  const mockUsePagination = jest.mocked(usePagination);
+  const mockUseResolvedComposable = jest.mocked(useResolvedComposable);
 
   beforeAll(() => {
     mockUseResolvedComposable.mockImplementation(
-      (component: React.JSX.Element) => component
+      (component) => component as () => React.JSX.Element
     );
   });
 
   afterEach(() => {
-    mockUseControlsContext.mockReset();
-    mockUseResolvedComposable.mockReset();
+    mockUsePagination.mockClear();
   });
 
-  it('renders the PaginationControl', async () => {
-    mockUseControlsContext.mockReturnValue({
-      data: {
-        paginationData: {
-          hasNextPage: true,
-          highestPageVisited: 1,
-          onPaginate: jest.fn(),
-          page: 1,
-        },
-      },
-    });
-
+  it('renders', () => {
     render(<PaginationControl />);
 
-    const nav = screen.getByRole('navigation');
-    const list = screen.getByRole('list');
-    const listItems = await screen.findAllByRole('listitem');
-    const nextButton = screen.getByRole('button', { name: 'Go to next page' });
-    const prevButton = screen.getByRole('button', {
-      name: 'Go to previous page',
-    });
-    const nextIcon = nextButton.querySelector('svg');
-    const prevIcon = nextButton.querySelector('svg');
+    const pagination = screen.getByTestId('pagination');
 
-    expect(nextButton).toBeInTheDocument();
-    expect(prevButton).toBeInTheDocument();
-    expect(nextIcon).toBeInTheDocument();
-    expect(prevIcon).toBeInTheDocument();
-    expect(nextIcon).toHaveAttribute('aria-hidden', 'true');
-    expect(prevIcon).toHaveAttribute('aria-hidden', 'true');
-    expect(nav).toBeInTheDocument();
-    expect(list).toBeInTheDocument();
-    expect(listItems).toHaveLength(3);
+    expect(pagination).toBeInTheDocument();
   });
 });
