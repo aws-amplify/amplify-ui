85fda63661b4b9ea87f0c73b43168621e969a81e
Jordan Van Ness <jordvn@amazon.com>
2024-12-13 10:28:00 -0800

    fix(react-storage): default nullish progress to 100% if status is "complete" (#6186)
    * fix(react-storage): set upload to 100% if status 'complete' but progress nullish
    
    * chore: add comment explaining purpose of change
    
    * chore: add further detail to comment
    
    * chore: update comment to remove specifics which may change
    
    ---------
    
    Co-authored-by: dindjarinjs <187552781+dindjarinjs@users.noreply.github.com>

--- packages/react-storage/src/components/StorageBrowser/views/LocationActionView/getActionViewTableData.ts
+++ packages/react-storage/src/components/StorageBrowser/views/LocationActionView/getActionViewTableData.ts
@@ -169,7 +169,10 @@ export const getActionViewTableData = <T extends TaskData = TaskData>({
               type: 'number',
               content: {
                 value: progress,
-                displayValue: `${getPercentValue(progress ?? 0)}%`,
+                displayValue: `${getPercentValue(
+                  // Default progress to 100% if progress value is unavailable but status is recognized as complete
+                  progress ?? status === 'COMPLETE' ? 1 : 0
+                )}%`,
               },
             };
           }
