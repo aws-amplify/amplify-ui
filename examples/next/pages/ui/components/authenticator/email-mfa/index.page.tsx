ead1794f5e6be404a8d7f9fd59593456c5c8535b
James Jarvis <jjarvisp@amazon.com>
2025-01-30 16:21:28 -0800

    chore: initial state is sign in

--- examples/next/pages/ui/components/authenticator/email-mfa/index.page.tsx
+++ examples/next/pages/ui/components/authenticator/email-mfa/index.page.tsx
@@ -8,7 +8,7 @@ Amplify.configure(awsExports);
 
 export default function App() {
   return (
-    <Authenticator initialState="signIn">
+    <Authenticator>
       {({ signOut, user }) => (
         <main>
           <h1>Hello {user.username}</h1>
