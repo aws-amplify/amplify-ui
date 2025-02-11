4439bd666fabcc3491c495648500d097cfb93db5
James Jarvis <jjarvisp@amazon.com>
2025-02-10 17:43:43 -0800

    fix: e2e test selector

--- packages/e2e/detox/integration/common/authenticator.ts
+++ packages/e2e/detox/integration/common/authenticator.ts
@@ -197,8 +197,10 @@ When('I click the {string} button', async (name: string) => {
   }
 });
 
-When('I click the {string} radio button', async (label: string) => {
-  await element(by.id('amplify__radio-button__container')).tap();
+When('I click the {string} radio button', async (name: string) => {
+  await element(
+    by.text(name).withAncestor(by.id('amplify__radio-group__container'))
+  ).tap();
 });
 
 Then(
