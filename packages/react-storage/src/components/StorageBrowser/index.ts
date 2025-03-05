eefa255372dba7f8dc36b07c55210deef792aa68
Allan Zheng <zheallan@amazon.com>
2025-03-05 10:35:57 -0800

    chore: remove handler type export and use just I/O types

--- packages/react-storage/src/components/StorageBrowser/index.ts
+++ packages/react-storage/src/components/StorageBrowser/index.ts
@@ -1,25 +1,18 @@
 export { componentsDefault } from './componentsDefault';
 export { createStorageBrowser } from './createStorageBrowser';
 export {
-  CopyHandler,
   CopyHandlerInput,
   CopyHandlerOutput,
-  CreateFolderHandler,
   CreateFolderHandlerInput,
   CreateFolderHandlerOutput,
-  DeleteHandler,
   DeleteHandlerInput,
   DeleteHandlerOutput,
-  DownloadHandler,
   DownloadHandlerInput,
   DownloadHandlerOutput,
-  ListLocationItemsHandler,
   ListLocationItemsHandlerInput,
   ListLocationItemsHandlerOutput,
-  UploadHandler,
   UploadHandlerInput,
   UploadHandlerOutput,
-  ListLocationsHandler,
   ListLocationsHandlerInput,
   ListLocationsHandlerOutput,
   ActionViewConfig,
@@ -35,7 +28,6 @@ export {
   StorageBrowserAuthAdapter,
 } from './adapters';
 export {
-  GetLocationCredentials,
   GetLocationCredentialsInput,
   GetLocationCredentialsOutput,
 } from './credentials/types';
