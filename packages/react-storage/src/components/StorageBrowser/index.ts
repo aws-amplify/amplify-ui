6c05fd71ded74a1d342c1d68c16f245aa54f55d5
Allan Zheng <zheallan@amazon.com>
2025-03-05 09:51:48 -0800

    feat(ui-react-storage): export default handler configs and types

--- packages/react-storage/src/components/StorageBrowser/index.ts
+++ packages/react-storage/src/components/StorageBrowser/index.ts
@@ -1,10 +1,32 @@
 export { componentsDefault } from './componentsDefault';
 export { createStorageBrowser } from './createStorageBrowser';
 export {
+  CopyHandler,
+  CopyHandlerInput,
+  CopyHandlerOutput,
+  CreateFolderHandler,
+  CreateFolderHandlerInput,
+  CreateFolderHandlerOutput,
+  DeleteHandler,
+  DeleteHandlerInput,
+  DeleteHandlerOutput,
+  DownloadHandler,
+  DownloadHandlerInput,
+  DownloadHandlerOutput,
+  ListLocationItemsHandler,
+  ListLocationItemsHandlerInput,
+  ListLocationItemsHandlerOutput,
+  UploadHandler,
+  UploadHandlerInput,
+  UploadHandlerOutput,
+  ListLocationsHandler,
+  ListLocationsHandlerInput,
+  ListLocationsHandlerOutput,
   ActionViewConfig,
   ActionHandler,
   FileDataItem,
   ExtendedActionConfigs,
+  defaultActionConfigs,
 } from './actions';
 export {
   createAmplifyAuthAdapter,
@@ -12,6 +34,11 @@ export {
   CreateManagedAuthAdapterInput,
   StorageBrowserAuthAdapter,
 } from './adapters';
+export {
+  GetLocationCredentials,
+  GetLocationCredentialsInput,
+  GetLocationCredentialsOutput,
+} from './credentials/types';
 export { DefaultStorageBrowserDisplayText } from './displayText';
 export {
   CreateStorageBrowserInput,
