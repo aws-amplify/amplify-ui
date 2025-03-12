12128886af1f764d3389ac28971277fba743ea69
Caleb Pollman <cpollman@amazon.com>
2025-03-11 19:39:08 -0700

    remove plurality

--- packages/react-storage/src/components/StorageBrowser/views/hooks/useResolveTableData/useResolveTableData.ts
+++ packages/react-storage/src/components/StorageBrowser/views/hooks/useResolveTableData/useResolveTableData.ts
@@ -14,14 +14,14 @@ export default function useResolveDataTable<
   { items, props }: { items?: TItem[]; props: TProps }
 ): DataTableProps {
   return React.useMemo(() => {
-    const getRows = (item: TItem) => ({
+    const getRow = (item: TItem) => ({
       key: getRowKey({ item, props }),
       content: keys.map((key) => getCell({ key, item, props })),
     });
 
     const headers = keys.map((key) => getHeader({ key, props }));
-    const rows = (items ?? []).map(getRows);
+    const rows = (items ?? []).map(getRow);
 
     return { headers, rows };
-  }, [keys, getCell, getHeader, getRowKey, items, props]);
+  }, [getCell, getHeader, getRowKey, keys, items, props]);
 }
