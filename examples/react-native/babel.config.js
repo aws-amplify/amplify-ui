05fb647b8a1fcabd9064a8fd73efbab78a7d6180
James Jarvis <jjarvisp@amazon.com>
2025-03-20 09:11:35 -0700

    chore: remove babel optional require

--- examples/react-native/babel.config.js
+++ examples/react-native/babel.config.js
@@ -2,6 +2,5 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ['module:react-native-dotenv', { moduleName: '@env', path: '.env' }],
-    'optional-require',
   ],
 };
