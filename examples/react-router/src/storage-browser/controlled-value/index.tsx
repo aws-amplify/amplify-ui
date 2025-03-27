72bdcd02559f7ba4a56c5347a863199157cbc6f6
Caleb Pollman <cpollman@amazon.com>
2025-03-26 20:43:01 -0700

    chore(storage-browser): add react-router app, add StorageBrowser examples, add @aws-amplify/ui-test-utils

---
+++ examples/react-router/src/storage-browser/controlled-value/index.tsx
@@ -0,0 +1,31 @@
+import { useSearchParams } from 'react-router-dom';
+import { useCallback } from 'react';
+
+import { StorageBrowser } from '../storage-browser';
+
+export default function App() {
+  const [params, setParams] = useSearchParams({
+    value: JSON.stringify({}),
+  });
+
+  const value = params.get('value');
+
+  const handleValueChange = useCallback(
+    (nextValue: unknown) => {
+      setParams((prev) => {
+        prev.set('value', JSON.stringify(nextValue));
+
+        return prev;
+      });
+    },
+    [setParams]
+  );
+
+  return (
+    <StorageBrowser
+      // @ts-expect-error to be updated
+      onValueChange={handleValueChange}
+      value={value ? JSON.parse(value) : {}}
+    />
+  );
+}
---
+++ packages/test-utils/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: '@aws-amplify/amplify-ui',
+  ignorePatterns: ['.eslintrc.js', 'coverage', 'dist', 'node_modules'],
+  // point to local tsconfig
+  parserOptions: { project: ['tsconfig.json'], tsconfigRootDir: __dirname },
+  overrides: [
+    {
+      extends: ['@aws-amplify/amplify-ui/jest'],
+      files: ['**/__mocks__/**', '**/__tests__/**'],
+    },
+    { files: '*.mjs' },
+  ],
+};
