3bdcb11e6725d2b2c35ed757055d65f57459a1c6
Caleb Pollman <cpollman@amazon.com>
2025-05-20 09:49:45 -0700

    Update packages/react-storage/src/components/FileUploader/hooks/useUploadFiles/__tests__/useUploadFiles.spec.ts

--- packages/react-storage/src/components/FileUploader/hooks/useUploadFiles/__tests__/useUploadFiles.spec.ts
+++ packages/react-storage/src/components/FileUploader/hooks/useUploadFiles/__tests__/useUploadFiles.spec.ts
@@ -1,5 +1,4 @@
-import { renderHook } from '@testing-library/react';
-import { waitFor } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
 
 import * as Storage from 'aws-amplify/storage';
 
