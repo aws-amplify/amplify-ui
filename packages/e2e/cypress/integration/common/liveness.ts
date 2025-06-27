cd120e2fab9d5257ba00d00450346f8efae705ab
osama-rizk <osos2026@gmail.com>
2025-06-27 10:32:54 +0200

    fix(liveness): failed work flow

--- packages/e2e/cypress/integration/common/liveness.ts
+++ packages/e2e/cypress/integration/common/liveness.ts
@@ -3,7 +3,7 @@ import { escapeRegExp } from 'lodash';
 
 // the current value returned by the service is 15000,
 // the below value can be increased as needed
-const LIVENESS_TIMEOUT = 60000;
+const LIVENESS_TIMEOUT = 90000;
 
 Then('I see the {string} timeout error', (message: string) => {
   cy.findByRole('document')
