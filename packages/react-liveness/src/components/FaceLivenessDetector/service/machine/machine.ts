b92d62dff456f9df1e08e99534b5fbad6339a206
riasatali42 <riasat.ali@enosisbd.com>
2025-08-07 22:27:45 +0600

    fix: resolved PR feedback, added console error, removed device info from the onUserCancel callback

--- packages/react-liveness/src/components/FaceLivenessDetector/service/machine/machine.ts
+++ packages/react-liveness/src/components/FaceLivenessDetector/service/machine/machine.ts
@@ -585,8 +585,8 @@ export const livenessMachine = createMachine<LivenessContext, LivenessEvent>(
             onCameraChange(deviceInfo);
           }
         } catch (callbackError) {
-          // console.error('Error in onCameraChange callback:', callbackError);
-          // Don't rethrow to prevent state machine from getting stuck
+          // eslint-disable-next-line no-console
+          console.error('Error in onCameraChange callback:', callbackError);
         }
       },
       callCameraNotFoundCallback: (context, event) => {
@@ -606,8 +606,8 @@ export const livenessMachine = createMachine<LivenessContext, LivenessEvent>(
             onCameraNotFound(requestedCamera, fallbackDevice);
           }
         } catch (callbackError) {
-          // console.error('Error in onCameraNotFound callback:', callbackError);
-          // Don't rethrow to prevent state machine from getting stuck
+          // eslint-disable-next-line no-console
+          console.error('Error in onCameraChange callback:', callbackError);
         }
       },
 
@@ -830,11 +830,10 @@ export const livenessMachine = createMachine<LivenessContext, LivenessEvent>(
         }
 
         try {
-          const deviceInfo = getSelectedDeviceInfo(context);
-          onUserCancel(deviceInfo);
+          onUserCancel();
         } catch (callbackError) {
-          // console.error('Error in onUserCancel callback:', callbackError);
-          // Don't rethrow to prevent state machine from getting stuck
+          // eslint-disable-next-line no-console
+          console.error('Error in onUserCancel callback:', callbackError);
         }
       },
       callUserTimeoutCallback: (context) => {
