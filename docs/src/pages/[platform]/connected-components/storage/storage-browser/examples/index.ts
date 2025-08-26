c2f134a85aa9a0822a5e64262f69fd5627afcdd8
osama-rizk <osos2026@gmail.com>
2025-08-26 13:41:31 +0200

    Add file preview to CustomUI section

--- docs/src/pages/[platform]/connected-components/storage/storage-browser/examples/index.ts
+++ docs/src/pages/[platform]/connected-components/storage/storage-browser/examples/index.ts
@@ -9,3 +9,4 @@ export { default as Icons } from './Icons';
 export { default as Default } from './Default';
 export { default as GenerateUrlViewComposed } from './GenerateUrlViewComposed';
 export { default as CustomFilePreview } from './CustomFilePreview';
+export { default as CustomUIFilePreview } from './CustomUIFilePreview';
