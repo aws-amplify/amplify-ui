414b67a7a7c5a747a36970e0db961934b71e6265
Koichi Ishida <wapa5pow@gmail.com>
2025-09-23 21:47:20 +0900

    fix: translation key mismatch (#6482)
    * fix: correct translation key mismatch
    
    * fix: remain existing translation
    
    * fix: remain existing translation
    
    * fix: french translation key mismatch
    
    * chore: add changeset
    
    ---------
    
    Co-authored-by: Caleb Pollman <cpollman@amazon.com>
    Co-authored-by: Michael Sober <msober@amazon.com>

--- packages/ui/src/i18n/dictionaries/authenticator/ja.ts
+++ packages/ui/src/i18n/dictionaries/authenticator/ja.ts
@@ -79,6 +79,7 @@ export const jaDict: AuthenticatorDictionary = {
   'Forgot Password': 'パスワードを忘れた ',
   'Have an account? ': 'アカウントを持っていますか？',
   'Incorrect username or password': 'ユーザー名かパスワードが異なります ',
+  'Incorrect username or password.': 'ユーザー名かパスワードが異なります ',
   'Invalid password format': 'パスワードの形式が無効です ',
   'Invalid phone number format':
     '不正な電話番号の形式です。\n+12345678900 の形式で入力してください',
