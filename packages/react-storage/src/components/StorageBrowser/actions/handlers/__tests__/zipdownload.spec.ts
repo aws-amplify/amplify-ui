5863b737010bd6d3c31ac0511d2172219c901d09
Philipp Andreas Paul <phandpau@amazon.de>
2025-09-26 14:28:34 +0200

    feat: add multi-file zip-download

---
+++ packages/react-storage/src/components/StorageBrowser/actions/handlers/__tests__/zipdownload.spec.ts
@@ -0,0 +1,100 @@
+import { getUrl, GetUrlInput } from '../../../storage-internal';
+
+import { zipDownloadHandler } from '../zipdownload';
+import type { DownloadHandlerInput } from '../download';
+
+jest.mock('../../../storage-internal');
+
+const baseInput: DownloadHandlerInput = {
+  config: {
+    accountId: 'accountId',
+    bucket: 'bucket',
+    credentials: jest.fn(),
+    customEndpoint: 'mock-endpoint',
+    region: 'region',
+  },
+  data: {
+    id: 'id',
+    key: 'prefix/file-name',
+    fileKey: 'file-name',
+  },
+};
+
+describe('zipDownloadHandler', () => {
+  const url = new URL('mock://fake.url');
+  const mockGetUrl = jest.mocked(getUrl);
+
+  beforeAll(() => {
+    if (!globalThis.fetch) {
+      globalThis.fetch = jest.fn(() => {
+        return Promise.resolve({
+          headers: {
+            get: (header: string) => {
+              if (header === 'content-length') {
+                return 100;
+              }
+            },
+          },
+          body: {
+            getReader: jest.fn(() => {
+              return {
+                read: () => ({
+                  value: 100,
+                  done: true,
+                }),
+              };
+            }),
+          },
+        }) as unknown as Promise<Response>;
+      });
+    }
+  });
+  beforeEach(() => {
+    const expiresAt = new Date();
+    expiresAt.setDate(expiresAt.getDate() + 1);
+    mockGetUrl.mockResolvedValue({ expiresAt, url });
+  });
+
+  afterEach(() => {
+    mockGetUrl.mockReset();
+  });
+
+  it('calls `getUrl` with the expected values', () => {
+    zipDownloadHandler(baseInput);
+
+    const expected: GetUrlInput = {
+      path: baseInput.data.key,
+      options: {
+        bucket: {
+          bucketName: baseInput.config.bucket,
+          region: baseInput.config.region,
+        },
+        customEndpoint: baseInput.config.customEndpoint,
+        locationCredentialsProvider: baseInput.config.credentials,
+        validateObjectExistence: true,
+        contentDisposition: 'attachment',
+        expectedBucketOwner: baseInput.config.accountId,
+      },
+    };
+
+    expect(mockGetUrl).toHaveBeenCalledWith(expected);
+  });
+
+  it('returns a complete status', async () => {
+    const { result } = zipDownloadHandler(baseInput);
+
+    expect(await result).toEqual({ status: 'COMPLETE' });
+  });
+
+  it('returns failed status', async () => {
+    const error = new Error('No download!');
+    mockGetUrl.mockRejectedValue(error);
+    const { result } = zipDownloadHandler(baseInput);
+
+    expect(await result).toEqual({
+      error,
+      message: error.message,
+      status: 'FAILED',
+    });
+  });
+});
