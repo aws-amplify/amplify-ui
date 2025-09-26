9901af26b3aea67c2ded7c08c7ff012f3c3e4769
Philipp Andreas Paul <phandpau@amazon.de>
2025-09-26 14:28:34 +0200

    add POC

--- packages/react-storage/src/components/StorageBrowser/actions/handlers/download.ts
+++ packages/react-storage/src/components/StorageBrowser/actions/handlers/download.ts
@@ -1,68 +1,105 @@
-import { getUrl } from '../../storage-internal';
-import type {
-  OptionalFileData,
-  TaskData,
-  TaskHandler,
-  TaskHandlerInput,
-  TaskHandlerOptions,
-  TaskHandlerOutput,
-} from './types';
+import {
+  DownloadHandlerInput,
+  DownloadHandlerOutput,
+} from "@aws-amplify/ui-react-storage/browser";
+import { getUrl } from "@aws-amplify/storage/internals";
+import {
+  ZipWriterAddDataOptions,
+  ZipWriter,
+  BlobWriter,
+  BlobReader,
+} from "@zip.js/zip.js";
 
-import { constructBucket } from './utils';
+const model = (() => {
+  let zipWriter: ZipWriter<Blob> | null;
+  return {
+    addFile(file: Blob, name: string, options: ZipWriterAddDataOptions) {
+      if (!zipWriter) {
+        zipWriter = new ZipWriter(new BlobWriter("application/zip"), {
+          bufferedWrite: true,
+        });
+      }
+      return zipWriter.add(name, new BlobReader(file), options);
+    },
+    async getBlobURL() {
+      if (zipWriter) {
+        const blobURL = URL.createObjectURL(await zipWriter.close());
+        zipWriter = null;
+        return blobURL;
+      } else {
+        throw new Error("Zip file closed");
+      }
+    },
+  };
+})();
 
-export interface DownloadHandlerData extends OptionalFileData, TaskData {
-  fileKey: string;
-}
+const constructBucket = ({
+  bucket: bucketName,
+  region,
+}: DownloadHandlerInput["config"]) => ({ bucketName, region });
 
-export interface DownloadHandlerOptions extends TaskHandlerOptions {}
-
-export interface DownloadHandlerInput
-  extends TaskHandlerInput<DownloadHandlerData, DownloadHandlerOptions> {}
-
-export interface DownloadHandlerOutput
-  extends TaskHandlerOutput<{ url: URL }> {}
-
-export interface DownloadHandler
-  extends TaskHandler<DownloadHandlerInput, DownloadHandlerOutput> {}
-
-function downloadFromUrl(fileName: string, url: string) {
-  const a = document.createElement('a');
-
-  a.href = url;
-  a.download = fileName;
-  a.target = '_blank';
-  document.body.appendChild(a);
-
-  a.click();
-
-  document.body.removeChild(a);
-}
-
-export const downloadHandler: DownloadHandler = ({
-  config,
-  data: { key },
-}): DownloadHandlerOutput => {
-  const { accountId, credentials, customEndpoint } = config;
-
-  const result = getUrl({
+const download = async ({ config, data: { key } }: DownloadHandlerInput) => {
+  const { customEndpoint, credentials, accountId } = config;
+  const { url } = await getUrl({
     path: key,
     options: {
       bucket: constructBucket(config),
       customEndpoint,
       locationCredentialsProvider: credentials,
       validateObjectExistence: true,
-      contentDisposition: 'attachment',
+      contentDisposition: "attachment",
       expectedBucketOwner: accountId,
     },
-  })
-    .then(({ url }) => {
-      downloadFromUrl(key, url.toString());
-      return { status: 'COMPLETE' as const, value: { url } };
-    })
-    .catch((error: Error) => {
-      const { message } = error;
-      return { error, message, status: 'FAILED' as const };
-    });
-
-  return { result };
+  });
+  const response = await fetch(url, { mode: "cors" });
+  const blob = await response.blob();
+  const [filename] = key.split("/").reverse();
+  await model.addFile(blob, filename, {});
 };
+
+const customDownloadHandler = (() => {
+  const q = new Set<string>();
+  let timer: ReturnType<typeof setTimeout>;
+  return (input: DownloadHandlerInput): DownloadHandlerOutput => {
+    const {
+      data: { key },
+    } = input;
+    const [, folder] = key.split("/").reverse();
+    q.add(key);
+    const result = download(input)
+      .then(() => {
+        q.delete(key);
+        return {
+          status: "COMPLETE",
+        };
+      })
+      .catch((e) => {
+        const error = e as Error;
+        q.delete(key);
+        return {
+          status: "FAILED",
+          message: error.message,
+          error,
+        };
+      })
+      .finally(() => {
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(() => {
+          if (q.size === 0) {
+            model.getBlobURL().then((blobURL) => {
+              if (blobURL) {
+                const anchor = document.createElement("a");
+                const clickEvent = new MouseEvent("click");
+                anchor.href = blobURL;
+                anchor.download = `${folder || "archive"}.zip`;
+                anchor.dispatchEvent(clickEvent);
+              }
+            });
+          }
+        }, 250);
+      });
+    return { result: result as DownloadHandlerOutput["result"] };
+  };
+})();
+
+export { customDownloadHandler };
