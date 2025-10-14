c9f6882fb16f5620321e34567acd56ec57ff11e8
riasatali42 <riasat.ali@enosisbd.com>
2025-10-14 19:47:23 +0600

    chore: added deviceInfo while calling the onError callback in liveness testing

--- packages/react-liveness/src/components/FaceLivenessDetector/service/machine/__tests__/machine.test.ts
+++ packages/react-liveness/src/components/FaceLivenessDetector/service/machine/__tests__/machine.test.ts
@@ -1400,10 +1400,17 @@ describe('Liveness Machine', () => {
         LivenessErrorState.RUNTIME_ERROR
       );
       expect(mockComponentProps.onError).toHaveBeenCalledTimes(1);
-      expect(mockComponentProps.onError).toHaveBeenCalledWith({
-        state: LivenessErrorState.RUNTIME_ERROR,
-        error,
-      });
+      expect(mockComponentProps.onError).toHaveBeenCalledWith(
+        {
+          state: LivenessErrorState.RUNTIME_ERROR,
+          error,
+        },
+        {
+          deviceId: mockCameraDevice.deviceId,
+          groupId: mockCameraDevice.groupId,
+          label: mockCameraDevice.label,
+        }
+      );
     });
   });
 });
