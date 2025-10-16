65a874d6d6eaa2679d60b74c3fb11b8c59d4ce25
Philipp Andreas Paul <phandpau@amazon.de>
2025-10-16 15:00:36 +0200

    fix: unit-tests

--- packages/svelte/vite.config.ts
+++ packages/svelte/vite.config.ts
@@ -7,36 +7,36 @@ import { defineConfig } from 'vite';
 const resolvePath = (str: string) => path.resolve(__dirname, str);
 
 export default defineConfig({
-	plugins: [sveltekit(), svelteTesting()],
-	test: {
-		environment: 'happy-dom'
-	},
-	resolve: {
-		alias: [
-			{
-				find: './runtimeConfig',
-				replacement: './runtimeConfig.browser'
-			}
-		]
-	},
-	build: {
-		lib: {
-			entry: resolvePath('./src/lib/index.ts'),
-			formats: ['es', 'cjs'],
-			name: 'ui-svelte',
-			fileName: (format: string) => (format === 'es' ? 'index.js' : `index.${format}`)
-		},
-		rollupOptions: {
-			plugins: [dynamicImportVars],
-			external: [
-				'@aws-amplify/auth',
-				'@aws-amplify/core',
-				'@aws-amplify/core/internals/utils',
-				'aws-amplify',
-				'aws-amplify/auth',
-				'aws-amplify/core',
-				'aws-amplify/utils'
-			]
-		}
-	}
+  plugins: [sveltekit(), svelteTesting()],
+  test: {
+    environment: 'jsdom'
+  },
+  resolve: {
+    alias: [
+      {
+        find: './runtimeConfig',
+        replacement: './runtimeConfig.browser'
+      }
+    ]
+  },
+  build: {
+    lib: {
+      entry: resolvePath('./src/lib/index.ts'),
+      formats: ['es', 'cjs'],
+      name: 'ui-svelte',
+      fileName: (format: string) => (format === 'es' ? 'index.js' : `index.${format}`)
+    },
+    rollupOptions: {
+      plugins: [dynamicImportVars],
+      external: [
+        '@aws-amplify/auth',
+        '@aws-amplify/core',
+        '@aws-amplify/core/internals/utils',
+        'aws-amplify',
+        'aws-amplify/auth',
+        'aws-amplify/core',
+        'aws-amplify/utils'
+      ]
+    }
+  }
 });
