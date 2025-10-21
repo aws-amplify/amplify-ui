1e584a1ffeae3bdd5906025f59a683de2699c8bc
Philipp Andreas Paul <phandpau@amazon.de>
2025-10-21 10:13:10 +0200

    fix: adjust versioning related files for svelte (#6725)
    * fix: add changelog for svelte
    
    * fix: move prettier dependency
    
    * fix: adjust changeset files
    
    * chore: linting
    
    * fix: adjust generateVersion for svelte

--- scripts/generateVersion.js
+++ scripts/generateVersion.js
@@ -2,7 +2,7 @@
  * This is a script that generates a constant, `VERSION` with the version
  * of the package. The version of a package will be used in logging and error messages.
  */
-const path = require('path');
+const { resolve, join } = require('path');
 const fs = require('fs-extra');
 
 const packages = [
@@ -14,14 +14,13 @@ const packages = [
   'packages/react-notifications',
   'packages/react-liveness',
   'packages/react-storage',
+  'packages/svelte',
   'packages/vue',
 ];
 
 packages.forEach((package) => {
   const { version } = require(`../${package}/package.json`);
-
-  fs.writeFileSync(
-    path.resolve(`${package}/src/version.ts`),
-    `export const VERSION = '${version}';\n`
-  );
+  const srcDir = package.includes('svelte') ? join('src', 'lib') : 'src';
+  const targetFile = resolve(join(package, srcDir, 'version.ts'));
+  fs.writeFileSync(targetFile, `export const VERSION = '${version}';\n`);
 });
