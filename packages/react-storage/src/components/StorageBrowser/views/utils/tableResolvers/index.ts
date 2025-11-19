14e446e81b1743e6232b4f6db767d5d024668843
Philipp Andreas Paul <phandpau@amazon.de>
2025-11-19 10:53:24 +0100

    feat(storage): add progress for download (#6747)

--- packages/react-storage/src/components/StorageBrowser/views/utils/tableResolvers/index.ts
+++ packages/react-storage/src/components/StorageBrowser/views/utils/tableResolvers/index.ts
@@ -1,5 +1,8 @@
 export { COPY_TABLE_RESOLVERS } from './copyResolvers';
 export { DELETE_TABLE_RESOLVERS } from './deleteResolvers';
-export { DOWNLOAD_TABLE_RESOLVERS } from './downloadResolvers';
+export {
+  DOWNLOAD_TABLE_KEYS,
+  DOWNLOAD_TABLE_RESOLVERS,
+} from './downloadResolvers';
 export { UPLOAD_TABLE_KEYS, UPLOAD_TABLE_RESOLVERS } from './uploadResolvers';
 export { FILE_DATA_ITEM_TABLE_KEYS } from './constants';
