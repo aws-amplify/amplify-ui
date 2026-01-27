fb1c4e132e2a3c31eaa0365aa325060effd19f8f
Thalles Passos <contato@thalles.me>
2026-01-27 14:14:29 +0000

    fix: ignore startScreen and use the client's background color

--- packages/react-liveness/src/components/FaceLivenessDetector/service/utils/liveness.ts
+++ packages/react-liveness/src/components/FaceLivenessDetector/service/utils/liveness.ts
@@ -189,7 +189,6 @@ export function drawLivenessOvalInCanvas({
   oval,
   scaleFactor,
   videoEl,
-  isStartScreen,
 }: {
   canvas: HTMLCanvasElement;
   oval: LivenessOvalDetails;
@@ -208,11 +207,10 @@ export function drawLivenessOvalInCanvas({
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
     // fill the canvas with a transparent rectangle if no color is defined
-    ctx.fillStyle = isStartScreen
-      ? getComputedStyle(canvas).getPropertyValue(
-          '--amplify-colors-background-primary'
-        )
-      || '#fff';
+    ctx.fillStyle =
+      getComputedStyle(canvas).getPropertyValue(
+        '--amplify-colors-background-primary'
+      ) || '#fff';
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
     // On mobile our canvas is the width/height of the full screen.
