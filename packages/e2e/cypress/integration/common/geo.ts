5c6ccad2cba9ef0a794ce22b3d59503f9e5d2ddf
osama-rizk <osos2026@gmail.com>
2026-01-28 15:12:32 +0100

    update te common step

--- packages/e2e/cypress/integration/common/geo.ts
+++ packages/e2e/cypress/integration/common/geo.ts
@@ -4,39 +4,46 @@ When('I search for {string}', (searchTerm: string) => {
   cy.intercept(/.*places.*/).as('searchResults');
   cy.findByRole('textbox', {
     name: /search/i,
+    timeout: 30000,
   }).type(searchTerm);
   cy.wait('@searchResults');
 });
 
 When('I select the first search result', () => {
-  cy.findAllByRole('listitem').first().click();
+  cy.findAllByRole('listitem', { timeout: 30000 }).first().click();
 });
 
 When('I clear the search results', () => {
   cy.findByRole('textbox', {
     name: /search/i,
+    timeout: 30000,
   }).trigger('mouseenter');
   /**
    * Adding 'force' as the clear button is hidden until we hover on textbox,
    * and the click action seems to happen before the hover thus failing the check for the clear button element.
    */
-  cy.findByRole('button', { name: 'Clear' }).click({ force: true });
+  cy.findByRole('button', { name: 'Clear', timeout: 30000 }).click({
+    force: true,
+  });
 });
 
 When('I click on a map marker', () => {
-  cy.get('.maplibregl-marker').first().click({ force: true });
+  cy.get('.maplibregl-marker', { timeout: 30000 })
+    .first()
+    .click({ force: true });
 });
 
 Then('I see results for my search term', () => {
-  cy.get('.suggestions').should('be.visible');
+  cy.get('.suggestions', { timeout: 30000 }).should('be.visible');
 });
 
 Then('I see no search results', () => {
-  cy.get('.suggestions').should('not.be.visible');
+  cy.get('.suggestions', { timeout: 30000 }).should('not.be.visible');
 });
 
 Then('the search input is empty', () => {
   cy.findByRole('textbox', {
     name: /search/i,
+    timeout: 30000,
   }).should('have.value', '');
 });
