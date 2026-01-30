28b2b9115a5fec17f2922cbad3916778c9c9492c
Pranav Malewadkar <7400617+pranavosu@users.noreply.github.com>
2026-01-30 10:10:51 -0800

    ci: webgl

--- packages/e2e/cypress.config.ts
+++ packages/e2e/cypress.config.ts
@@ -21,11 +21,18 @@ export default defineConfig({
 
       Object.assign(config.env, process.env);
 
-      // This is a chrome launch option which enables fake videos
-      on('before:browser:launch', (_browser, launchOptions) => {
+      // This is a chrome launch option which enables fake videos and WebGL for CI
+      on('before:browser:launch', (browser, launchOptions) => {
         launchOptions.args.push(
           '--use-file-for-fake-video-capture=cypress/fixtures/faceRecording.y4m'
         );
+
+        // Enable software WebGL rendering for maplibre-gl in headless CI environments
+        if (browser.name === 'chrome' && browser.isHeadless) {
+          launchOptions.args.push('--disable-gpu');
+          launchOptions.args.push('--use-gl=swiftshader');
+          launchOptions.args.push('--enable-webgl');
+        }
 
         return launchOptions;
       });
