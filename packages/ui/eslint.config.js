755af1fb14c40b043d8755b1dac45ff71643b4c9
Philipp Andreas Paul <phandpau@amazon.de>
2026-03-11 15:40:05 +0100

    fix: remove unused flat config compat code

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -9,14 +9,6 @@ const { fixupPluginRules } = require('@eslint/compat');
 const tsParser = require('@typescript-eslint/parser');
 const js = require('@eslint/js');
 
-const { FlatCompat } = require('@eslint/eslintrc');
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
 module.exports = defineConfig([
   {
     files: ['src/**/*.ts'],
